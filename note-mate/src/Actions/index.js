37a1cc673ed3691713b37fb641c13c61d7304f02
Clara Andrew <candrewwani@gmail.com>
2018-02-06 16:13:57 -0500

    Update/Delete Functionality Added
    Actions/index.js: Added actions to handle add, delete, add in both server and serverless.
    
    Reducers/index.js: Added cases to the reducer and intial state to handle add, delete, and update.
    
    App.js: Added methods for add, delete, and update functionalities.
    
    index.js: Added redux middleware, thunk, and logger to handle axios in the future.

---
+++ note-mate/src/Actions/index.js
@@ -0,0 +1,80 @@
+// import axios from 'axios';
+
+export const NOTEADDED = 'NOTEADDED';
+export const NOTEUPDATED = 'NOTEUPDATED';
+export const NOTEDELETED = 'NOTEDELETED';
+
+
+export const addNote = note => {
+    return {
+        type: NOTEADDED,
+        payload: note
+    }
+}
+
+export const updateNote = note => {
+    return {
+        type: NOTEUPDATED,
+        payload: note
+    }
+}
+
+export const deleteNote = id => {
+    return {
+        type: NOTEDELETED,
+        payload: id
+    }
+}
+
+// export const GETTINGNOTES = 'GETTINGNOTES';
+// export const NOTESRECEIVED = 'NOTESRECEIVED';
+
+// export const ADDINGNOTE = 'ADDINGNOTE';
+
+// export const UPDATINGNOTE = 'UPDATINGNOTE';
+
+// export const DELETINGNOTE = 'DELETINGNOTE';
+
+// export const ERROR = 'ERROR';
+
+
+// actions for when the server is set up
+
+// const url = 'url goes here'
+
+// export const getNotes = () => {
+//     return dispatch => {
+//     dispatch({type: GETTINGNOTES})
+//     axios.get(url)
+//     .then(response => {dispatch({type: NOTESRECEIVED, payload: response.data})})
+//     .catch(error => {dispatch({type: ERROR, payload: error})});
+//     }
+// }
+
+// export const addNote = note => {
+//     return dispatch => {
+//         dispatch({type: ADDINGNOTE})
+//         axios.post(url, note)
+//         .then(response => {dispatch({type: NOTEADDED, payload: response.data})})
+//         .catch(error => {dispatch({type: ERROR, payload: error})});
+//     }
+// }
+
+// export const updateNote = (note, id) => {
+//     return dispatch => {
+//         dispatch({type: UPDATINGNOTE})
+//         axios.put(`${url}/${id}`)
+//         .then(response => {dispatch({type: NOTEUPDATED, payload: response.data})})
+//         .catch(error => {dispatch({type: ERROR, payload: error})});
+        
+//     }
+// }
+
+// export const deleteNote = (note, id) => {
+//     return dispatch => {
+//         dispatch({type: DELETINGNOTE})
+//         axios.delete(`${url}/${id}`)
+//         .then(response => {dispatch({type: NOTEDELETED, payload: response.data})})
+//         .catch(error => {dispatch({type: ERROR, payload: error})});
+//     }
+// }
--- note-mate/src/index.js
+++ note-mate/src/index.js
@@ -2,15 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import rootReducer from './Reducers'
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from './Reducers';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 
 ReactDOM.render(
 <Provider store={store}>
     <App />
 </Provider>, document.getElementById('root'));
-registerServiceWorker();
