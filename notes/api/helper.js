80570afaaaab1ca1d7c8abb0c2aa2da75eb6797f
Samuel Cha <sunchipssam@gmail.com>
2018-03-19 10:40:08 -0700

    Changed helper to reflect model change

--- notes/api/helper.js
+++ notes/api/helper.js
@@ -1,10 +1,13 @@
 const send = (res, code, msg, err = undefined) => {
-  res.status(code).json({ msg, err });
+  err === undefined
+    ? res.status(code).json(msg)
+    : res.status(code).json({ msg, err });
 };
 
 const message = {
-  noTitleNoTex: 'Please provide a title and/or text',
-  created: 'Note created.',
+  noTitleNoTex: 'Please provide a title and/or content.',
+  created: 'Note created successfully.',
+  createdError: 'Server error creating note.',
   serverError: 'Server error.',
 };
 
