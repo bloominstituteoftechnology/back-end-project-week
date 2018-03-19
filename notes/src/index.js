ea5600fee1962e3f4c2f447cf705091fafc44641
unknown <brcampbell001@outlook.com>
2018-03-19 16:57:28 -0500

    Added FrontEnd from Last Project

--- notes/src/index.js
+++ notes/src/index.js
@@ -2,7 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux'; 
+import rootReducer from './reducers'
+//import thunk from 'redux-thunk';
+//import logger from 'redux-logger';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const store = createStore(rootReducer);
+
+ReactDOM.render(
+<Provider store={store}>
+    <App />
+</Provider>, document.getElementById('root'));
