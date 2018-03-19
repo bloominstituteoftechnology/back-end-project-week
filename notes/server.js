79b56258e157f0fa255d6a3b97fa1d02c667e63b
Samuel Cha <sunchipssam@gmail.com>
2018-03-19 14:07:52 -0700

    Added cors to backend

--- notes/server.js
+++ notes/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 
 const router = require('./router');
 
@@ -9,6 +10,7 @@ const debug = false;
 debug ? server.use(morgan('combined')) : null;
 
 server.use(express.json());
+server.use(cors({ origin: 'http://localhost:3000', credentials: true }));
 server.use('/api', router);
 
 server.get('/', (req, res) => {
