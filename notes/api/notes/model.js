46798957bbde2c75f5cfc1b165feaacc1c1db3e5
Samuel Cha <sunchipssam@gmail.com>
2018-03-19 09:52:34 -0700

    Added notes model

---
+++ notes/api/notes/model.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+const mongodAuth = require('../../config').mongodAuth;
+
+mongoose.connect('mongodb://localhost/notes_db', mongodAuth);
+// mongoose.connect('mongodb://localhost/notes_db');
+
+const NoteSchema = new Schema({
+  title: {
+    required: true,
+    type: String,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  createdOn: {
+    required: true,
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// NoteSchema.methods.getTitle = function() {
+//   return this.question;
+// };
+
+// NoteSchema.methods.getAnswer = function() {
+//   return this.answer || 'Question not answered yet.';
+// };
+
+// NoteSchema.statics.getAllAmas = cb => {
+//   Ama.find({}, (err, amas) => {
+//     if (err) console.log(err);
+
+//     cb(amas);
+//   });
+// };
+
+const Note = mongoose.model('Note', NoteSchema);
+
+module.exports = Ama;
