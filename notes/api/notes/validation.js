8d57175ce31cefc425d6cb3ffb19319948e18ca3
Samuel Cha <sunchipssam@gmail.com>
2018-03-19 11:59:40 -0700

    Added validation for view existing note

--- notes/api/notes/validation.js
+++ notes/api/notes/validation.js
@@ -1,6 +1,8 @@
 const { error } = require('../../config').status;
 const { send, message } = require('../helper');
 
+const controller = require('./controller');
+
 module.exports = {
   note: (req, res, next) => {
     const { title, content } = req.body;
@@ -12,5 +14,25 @@ module.exports = {
     }
 
     next();
+  },
+  id: (req, res, next) => {
+    const { id } = req.params;
+
+    controller
+      .requestBy(req.params.id)
+      .then(note => {
+        if (!note) {
+          send(res, error.miss, {
+            message: message.requestIdError,
+            note: note,
+          });
+
+          return;
+        }
+
+        req.note = note;
+        next();
+      })
+      .catch(err => send(res, error.server, message.requestIdServerError, err));
   },
 };
