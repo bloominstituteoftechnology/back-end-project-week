f57884791a5b7bba1b591274bd33f879bf3c76ef
Samuel Cha <sunchipssam@gmail.com>
2018-03-19 12:29:57 -0700

    Added users endpoint

---
+++ notes/api/users/endpoint.js
@@ -0,0 +1,51 @@
+const router = require('express').Router();
+
+const { error, success } = require('../../config').status;
+const { send, message } = require('../helper');
+
+// const validate = require('./validation');
+// const controller = require('./controller');
+
+router.route('/').get((req, res) => {
+  send(res, success.ok, { users: 'running' });
+  // controller.request(notes => {
+  //   if (notes.err) {
+  //     send(res, error.server, message.requestError, notes.err);
+  //     return;
+  //   }
+
+  //   send(res, success.ok, notes);
+  // });
+});
+//   .post(validate.note, (req, res) => {
+//     controller
+//       .create(req.body)
+//       .then(savedNote => send(res, success.created, savedNote))
+//       .catch(err => send(res, error.server, message.createdError, err));
+//   });
+
+// router
+//   .route('/:id')
+//   .get(validate.id, (req, res) => {
+//     res.status(success.ok).json(req.note);
+//   })
+//   .put(validate.update, validate.id, (req, res) => {
+//     const { title, content } = req.body;
+//     const updatedNote = {
+//       title: title || req.note.title,
+//       content: content || req.note.content,
+//     };
+
+//     controller
+//       .update(req.params.id, updatedNote)
+//       .then(updatedNote => send(res, success.ok, updatedNote))
+//       .catch(err => send(res, error.server, message.updateError, err));
+//   })
+//   .delete(validate.id, (req, res) => {
+//     controller
+//       .del(req.params.id)
+//       .then(deletedNote => send(res, success.ok, deletedNote))
+//       .catch(err => send(res, error.server, message.deleteError, err));
+//   });
+
+module.exports = router;
--- notes/router.js
+++ notes/router.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 
 const notesEndpoint = require('./api/notes/endpoint');
+const usersEndpoint = require('./api/users/endpoint');
 
 router.use('/notes', notesEndpoint);
+router.use('/users', usersEndpoint);
 
 router.route('/').get((req, res) => {
   res.send({ router: 'running' });
