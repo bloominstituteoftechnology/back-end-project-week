12b69819277313cb503a2beb66bd634f118a7739
Your Name <haywood.d.johnson@gmail.com>
2018-03-20 14:54:24 -0500

    It's a mess...

--- lambdanotes/src/App.js
+++ lambdanotes/src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
-import Button from './Misc/button/button.js';
+import Button from './Misc/button/button';
+import NavDrawer from './NavDrawer/NavDrawer';
+import MenuItem from './NavDrawer/MenuItem/MenuItem';
 
+const handleToggle = () => this.setState({ open: !this.state.open });
+const handleClose = () => this.setState({ open: false });
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Button className="al-right" />
+      <div>
+        <NavDrawer />
+        <div /*className="App"*/>
+          <Button className="button--aliRight" />
+        </div>
       </div>
     );
   }
---
+++ lambdanotes/src/NavDrawer/MenuItem/MenuItem.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class MenuItem extends Component {
+  render() {
+    return <div>Placeholder</div>;
+  }
+}
+
+export default MenuItem;
