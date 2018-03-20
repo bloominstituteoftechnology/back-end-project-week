6a62fbb86ad7751e9b7a0f0b5e90640c0345ccea
Troy Williams <troyDonwilliams@gmail.com>
2018-03-20 18:55:11 -0500

    server sending file but route is getting html

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const helmet = require('helmet');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const morgan = require('morgan');
+const path = require('path');
 
 const app = express();
 const userRouter = require('./users/userRoutes');
@@ -14,9 +15,12 @@ app.use(express.json());
 app.use(helmet());
 app.use(cors());
 // app.use(morgan('combined'));
+app.use(express.static(path.resolve(__dirname, 'note-mate/frontend/build')));
 
-app.get('/', (req, res) => {
-  res.send('Working');
+app.get('*', (req, res) => {
+  res.sendFile(
+    path.resolve(__dirname, 'note-mate/frontend/build', 'index.html')
+  );
 });
 
 app.use('/user', userRouter);
@@ -25,8 +29,8 @@ app.use('/notes', authenticate, noteRouter);
 mongoose
   .connect(`mongodb://${dbuser}:${dbpass}@ds119449.mlab.com:19449/lambdanotes`)
   .then(() => {
-    const port = process.env.PORT || 8080;
-    app.listen(port, () => console.log(`Running on port: ${port}`));
+    const PORT = process.env.PORT || 8080;
+    app.listen(PORT, () => console.log(`Running on port: ${PORT}`));
   })
   .catch(err => {
     console.log('error connecting to database');
