8ba5971c80e3e3d53fd1a037724960cf2bb9dc9f
LordOrbnauticus <haywood.d.johnson@gmail.com>
2018-03-19 19:06:59 -0500

    button test... If this works...

--- lambdanotes/src/App.js
+++ lambdanotes/src/App.js
@@ -1,19 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from './src/Misc/button.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="wrapper">
-          <input id="customBox" className="customBox" type="checkbox" />
-          <label for="customBox" />
-
-          <div className="one fas fa-pen-square" />
-          <div className="two fas fa-star" />
-          <div className="three fas fa-share" />
-        </div>
+        <Button />
       </div>
     );
   }
