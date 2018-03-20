e5eced9d815d8e4313a62976a42e2cd1550005ea
Your Name <haywood.d.johnson@gmail.com>
2018-03-19 20:14:29 -0500

    Button may be on point...

--- lambdanotes/src/App.js
+++ lambdanotes/src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Button from './Misc/button/button.js'
+import Button from './Misc/button/button.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Button />
+        <Button className="al-right" />
       </div>
     );
   }
--- lambdanotes/src/Misc/button/button.js
+++ lambdanotes/src/Misc/button/button.js
@@ -1,21 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import './button.css';
 
-
 class Button extends React.Component {
-    render() {
-        return (
-            <div className="wrapper">
-                <input id="customBox" className="customBox" type="checkbox" />
-                <label for="customBox" />
+  render() {
+    return (
+      <div className="wrapper">
+        <input id="customBox" className="customBox" type="checkbox" />
+        <label for="customBox" />
 
-                <div className="one fas fa-pen-square" />
-                <div className="two fas fa-star" />
-                <div className="three fas fa-share" />
-            </div>
-        )
-    }
+        <div className="one fas fa-pen-square" />
+        <div className="two fas fa-star" />
+        <div className="three fas fa-share" />
+      </div>
+    );
+  }
 }
 
 export default Button;
