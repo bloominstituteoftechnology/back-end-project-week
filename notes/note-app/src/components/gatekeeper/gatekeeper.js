648d1a2c60f2c429c8d1aaca5d7ccfb5a83e02f8
Samuel Cha <sunchipssam@gmail.com>
2018-03-20 13:52:30 -0700

    Removed auth state
    Replaced this with jwt

--- notes/note-app/src/components/gatekeeper/gatekeeper.js
+++ notes/note-app/src/components/gatekeeper/gatekeeper.js
@@ -5,7 +5,7 @@ import { appK } from '../../config';
 export default ComposedComponent => {
   class CheckAuthentication extends Component {
     componentWillMount() {
-      if (!this.props.authenticated && !localStorage.getItem(appK)) {
+      if (!localStorage.getItem(appK)) {
         window.alert('Please log in first');
         this.props.history.push('/login');
       }
@@ -14,7 +14,7 @@ export default ComposedComponent => {
     render() {
       return (
         <div className="CheckAuthentication">
-          {this.props.authenticated || localStorage.getItem(appK) ? (
+          {localStorage.getItem(appK) ? (
             <ComposedComponent history={this.props.history} />
           ) : null}
         </div>
@@ -24,7 +24,7 @@ export default ComposedComponent => {
 
   const mapStateToProps = state => {
     return {
-      authenticated: state.auth.authenticated,
+      // authenticated: state.auth.authenticated,
     };
   };
 
