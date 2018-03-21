381a2d6798161612ce48197d29da30e5f9ec93b7
Your Name <haywood.d.johnson@gmail.com>
2018-03-21 03:37:13 -0500

    Navbar looks cool... -ish I need to finish then connect the API

--- lambdanotes/src/components/Misc/AniButton/AniButton.js
+++ lambdanotes/src/components/Misc/AniButton/AniButton.js
@@ -2,18 +2,10 @@ import React from 'react';
 
 import './AniButton.css';
 
-handleAx = () => {
-  ax.classList.toggle('change');
-};
-
 class AniButton extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state.
-  }
   render() {
     return (
-      <div className="container" onclick="handleAx">
+      <div className="container">
         <div className="ln1" />
         <div className="ln2" />
         <div className="ln3" />
