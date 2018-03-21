1bba051df5890cd9262550327522e84059b7d8c1
Your Name <haywood.d.johnson@gmail.com>
2018-03-21 13:04:39 -0500

    something weird is going on...

--- lambdanotes/src/App.js
+++ lambdanotes/src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import NavButton from './components/Misc/NavButton/NavButton';
-import NavDrawer from './components/NavDrawer/NavDrawer';
-import MenuItem from './components/NavDrawer/MenuItem/MenuItem';
+import Menu from './components/Menu/Menu';
+import MenuItem from './components/Menu/MenuItem/MenuItem';
 import NavBar from './components/Nav/NavBar';
 
-const handleToggle = () => this.setState({ open: !this.state.open });
-const handleClose = () => this.setState({ open: false });
 class App extends Component {
   render() {
     return (
       <div>
         <NavBar className="App-Header" />
-        <NavDrawer />
+        <Menu />
         <div /*className="App"*/>
           <NavButton className="button--aliRight" />
         </div>
---
+++ lambdanotes/src/components/Menu/Menu.js
@@ -0,0 +1,27 @@
+// import React, { Component } from 'react';
+// import { PropTypes } from 'react';
+
+// import './Menu.css';
+
+// showLeft = () => {
+//   this.refs.left.show();
+// },
+// class Menu extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { open: false };
+//   }
+
+//   handleToggle = () => this.setState({ open: !this.state.open });
+//   handleClose = () => this.setState({ open: false });
+
+//   render() {
+//     return (
+//       <div>
+//         <div className={this.state.open ? 'open' : ''}>
+//           {this.props.children}
+//         </div>
+//       </div>
+//     );
+//   }
+// }
