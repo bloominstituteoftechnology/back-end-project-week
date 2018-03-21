0308d7d782a030afe8fa7cfe14357be2c063a17f
unknown <brcampbell001@outlook.com>
2018-03-21 14:50:40 -0500

    fE: Updated root index with Routes etc

--- FrontEnd/src/index.js
+++ FrontEnd/src/index.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux'; 
-import rootReducer from './reducers'
-//import thunk from 'redux-thunk';
-//import logger from 'redux-logger';
+import { createStore, applyMiddleware } from 'redux';
+import { Router, Route, browserHistory } from 'react-router';
+import reduxThunk from 'redux-thunk';
 
-const store = createStore(rootReducer);
+
+
+import rootReducer from './reducers'
+import App from './App';
+import Login from './components/auth/login';
+import RequireAuthorization from './components/auth/require_auth';
+import { AUTH_USER } from './actions/types';
+
+const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+const store = createStoreWithMiddleware(rootReducer);
+
+const token = localStorage.getItem('token');
+if (token) {
+    store.dispatch({ type: AUTH_USER });
+}
 
 ReactDOM.render(
-<Provider store={store}>
-    <App />
-</Provider>, document.getElementById('root'));
+    <Provider store={store}>
+        <Router history={browserHistory}>
+            <Route path='/' component={App}>
+                <Route path='login' component={Login} />
+            </Route>
+        </Router>
+    </Provider>, document.getElementById('root'));
