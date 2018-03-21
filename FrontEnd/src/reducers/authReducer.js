72e9eb255cfc5bb54a14e021305d9683de67b9ec
unknown <brcampbell001@outlook.com>
2018-03-21 14:38:34 -0500

    fE: Split off reducers and combined in index

--- FrontEnd/src/reducers/authReducer.js
+++ FrontEnd/src/reducers/authReducer.js
@@ -5,16 +5,24 @@ import {
     FETCH_MESSAGE
 } from '../actions/types';
 
-export default function (state = {}, action) {
-    switch (action.type) {
-        case AUTH_USER:
-            return { ...state, error: '', authenticated: true };
-        case UNAUTH_USER:
-            return { ...state, authenticated: false };
-        case AUTH_ERROR:
-            return { ...state, error: action.payload };
-        case FETCH_NOTES:
-            return { ...state, message: action.payload };
+const initialState = {
+    authenticated: false
+};
+
+const authReducer = (state = initialState, action) => {
+    {
+        switch (action.type) {
+            case AUTH_USER:
+                return { ...state, error: '', authenticated: true };
+            case UNAUTH_USER:
+                return { ...state, authenticated: false };
+            case AUTH_ERROR:
+                return { ...state, error: action.payload };
+            case FETCH_MESSAGE:
+                return { ...state, message: action.payload };
+        }
+        return state;
     }
-    return state;
 }
+
+export default authReducer;
--- FrontEnd/src/reducers/index.js
+++ FrontEnd/src/reducers/index.js
@@ -1,64 +1,12 @@
-import { ADD_NOTE, EDIT_NOTE, DELETE_NOTE } from '../actions'
+import { combineReducers } from 'redux';
+import { reducer as form } from 'redux-form';
+import authReducer from './authreducer';
+import noteReducer from './noteReducer';
 
-let id = 0;
-const initialState = {
-    notes: [{
-        id: id,
-        title: '',
-        content: ''
-    }],
-}
-
-
-const rootReducer = (state = initialState, action) => {
-    switch(action.type) {
-        case ADD_NOTE:
-            ++id;
-            return {
-                ...state, 
-                notes: [...state.notes, 
-                    {...action.payload, id: id}
-                ]};
-        case EDIT_NOTE:
-            return {
-                ...state, 
-                notes: state.notes.map(note => {
-                if (note.id === action.payload.id) {
-                    return action.payload
-                } return note;
-            })}
-        case DELETE_NOTE:
-            return {
-                ...state, 
-                notes: state.notes.filter(note => 
-                    note.id !== action.payload)};
-
-        default:
-            return state;
-    }
-}
+const rootReducer = combineReducers({
+  form,
+  auth: authReducer,
+  note: noteReducer
+});
 
 export default rootReducer;
-
-/* export default (state = [], action) => {
-    switch (action.type) {
-        case actionTypes.ADD_NOTE:
-        ++id;
-        return [
-            ...state,
-            Object.assign({}, action.note)
-        ];
-
-        case actionTypes.DELETE_NOTE:
-        return state.filter((data, i) => i !== action.id);
-
-        case actionTypes.EDIT_NOTE:
-        return [
-            ...state,
-            note: state.note.map(note => {})
-        ]
-
-        default:
-            return state;
-    }
-} */
