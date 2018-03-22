1d242258bafa98fe9324849ff7ef2c8f2b700adb
unknown <brcampbell001@outlook.com>
2018-03-22 17:36:07 -0500

    fE: Messy - Note Form and Note List Containers Done

---
+++ FrontEnd/src/containers/noteForm/index.js
@@ -0,0 +1,63 @@
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+
+import * as actions from '../../actions/index';
+
+class NoteForm extends Component {
+
+    constructor() {
+        super();
+    }
+
+    edit_Note(prop, event) {
+        this.props.dispatch(
+            actions.edit_Note({
+                [prop]: event.target.value
+            })
+        );
+    }
+
+    add_Note() {
+        this.props.dispatch(actions.addNoteRequest(Object.assign({}, this.props.noteForm, this.props.userInfo)));
+    }
+
+    update_Note() {
+        this.props.dispatch(actions.updateNoteRequest(this.props.noteForm));
+    }
+
+    render() {
+        return (
+            <li className="notemate-add-note">
+                <input className="notemate-add-note-title" type="text" placeholder="Title" onChange={this.edit_note.bind(this, 'title')} value={this.props.noteForm.title} />
+                <textarea className="notemate-add-note-body" placeholder="Body" onChange={this.edit_note.bind(this, 'body')} value={this.props.noteForm.body} />
+                <button onClick={this.props.noteForm.updating &&
+                    this.update_note.bind(this) ||
+                    this.add_note.bind(this)}>
+                    {`${this.props.noteForm.updating ? 'Update' : 'Add'} note`}
+                </button>
+            </li>
+        );
+    }
+}
+
+mapStateToProps = store => {
+    return {
+        noteForm: store.noteForm,
+        userInfo: {
+            email: store.user && store.user.email,
+        }
+    };
+}
+
+noteForm.propTypes = {
+    noteForm: PropTypes.shape({
+        title: PropTypes.string,
+        body: PropTypes.string,
+        updating: PropTypes.bool,
+        edit_Index: PropTypes.number,
+        edit_Id: PropTypes.string
+    }),
+    dispatch: PropTypes.func.isRequired
+};
+
+export default connect(mapStateToProps)(NoteForm);
---
+++ FrontEnd/src/containers/noteList/index.js
@@ -0,0 +1,63 @@
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+
+import Notes from '../../components/notes/notes';
+
+import * as actions from '../../actions/index';
+
+class NoteList extends Component {
+
+    constructor() {
+        super();
+    }
+
+    edit_Note(note, index) {
+        this.props.dispatch(actions.edit_note({
+            updating: true,
+            title: note.title,
+            body: note.body,
+            edit_Index: index,
+            edit_Id: note._id
+        }));
+    }
+
+    delete_note(_id) {
+        this.props.dispatch(actions.deletenoteRequest(_id));
+    }
+
+    render() {
+        return (
+            <ul className="notemate-note-list">
+                {
+                    this.props.notes.map((note, index) =>
+                        <ListNote note={note}
+                            index={index}
+                            key={note._id}
+                            delete={this.delete_note.bind(this)}
+                            edit={this.edit_note.bind(this)}
+                            email={this.props.email} />)
+                }
+            </ul>
+        );
+    }
+}
+
+mapStateToProps = store => {
+    return {
+        notes: store.notes,
+        email: store.user && store.user.email
+    };
+}
+
+noteList.propTypes = {
+    notes: PropTypes.arrayOf(PropTypes.shape({
+        title: PropTypes.string.isRequired,
+        body: PropTypes.string.isRequired,
+        createdDate: PropTypes.string.isRequired,
+        email: PropTypes.string,
+    })).isRequired,
+    email: PropTypes.string,
+    dispatch: PropTypes.func.isRequired
+};
+
+export default connect(mapStateToProps)(NoteList);
