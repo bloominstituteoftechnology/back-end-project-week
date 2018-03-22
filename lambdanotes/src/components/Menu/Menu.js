0824dc28a03606a3dcade8faab4001097158d16f
Your Name <haywood.d.johnson@gmail.com>
2018-03-22 14:33:31 -0500

    My gosh...

--- lambdanotes/src/components/Menu/Menu.js
+++ lambdanotes/src/components/Menu/Menu.js
@@ -1,27 +1,26 @@
-// import React, { Component } from 'react';
-// import { PropTypes } from 'react';
+import React, { Component } from 'react';
+import { PropTypes } from 'react';
 
-// import './Menu.css';
+import './Menu.css';
 
-// showLeft = () => {
-//   this.refs.left.show();
-// },
-// class Menu extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = { open: false };
-//   }
+class Menu extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { open: false };
+  }
 
-//   handleToggle = () => this.setState({ open: !this.state.open });
-//   handleClose = () => this.setState({ open: false });
+  handleToggle = () => this.setState({ open: !this.state.open });
+  handleClose = () => this.setState({ open: false });
 
-//   render() {
-//     return (
-//       <div>
-//         <div className={this.state.open ? 'open' : ''}>
-//           {this.props.children}
-//         </div>
-//       </div>
-//     );
-//   }
-// }
+  render() {
+    return (
+      <div>
+        <div className={this.state.open ? 'open' : ''}>
+          {this.props.children}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Menu;
