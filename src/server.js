61de1e26e649a032fb06fdc9b03b2f60f9cbcd00
Guelmis Cortina <cguelmis@yahoo.com>
2018-03-22 17:58:29 -0400

    set up mongo connection through mlab

--- src/server.js
+++ src/server.js
@@ -3,8 +3,9 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const routes = require('./routes');
+const {dbuser, dbpass} = require('./config');
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/note-mate');
+mongoose.connect('mongodb://<dbuser>:<dbpass>@ds153752.mlab.com:53752/note-mate');
 
 const corsOptions = {
 	origin: 'http://localhost:3000',
