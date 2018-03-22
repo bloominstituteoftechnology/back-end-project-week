b630dc728322735c552015ee1abb4744e97ee0c0
Guelmis Cortina <cguelmis@yahoo.com>
2018-03-22 18:00:36 -0400

    add config file

--- src/config.js
+++ src/config.js
@@ -1,3 +1,5 @@
 module.exports = {
-	secret: 'e5SPiqsEtjexkTj3Xqovsjzq8ovjfgVDFMfUzSmJO21dtXs4re'
+	secret: 'e5SPiqsEtjexkTj3Xqovsjzq8ovjfgVDFMfUzSmJO21dtXs4re',
+	dbuser: 'admin',
+	dbpass: 'password'
 }
