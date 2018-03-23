56f5e448bfde054de0b6c844000abb537738d231
Troy Williams <troyDonwilliams@gmail.com>
2018-03-22 19:54:31 -0500

    moving the frontend out of the note-mate folder

--- server.js
+++ server.js
@@ -15,12 +15,10 @@ app.use(express.json());
 app.use(helmet());
 app.use(cors());
 // app.use(morgan('combined'));
-app.use(express.static(path.resolve(__dirname, 'note-mate/frontend/build')));
+app.use(express.static(path.resolve(__dirname, 'frontend/build')));
 
 app.get('/', (req, res) => {
-  res.sendFile(
-    path.resolve(__dirname, 'note-mate/frontend/build', 'index.html')
-  );
+  res.sendFile(path.resolve(__dirname, 'frontend/build', 'index.html'));
 });
 
 app.use('/user', userRouter);
