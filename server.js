cb8ef1ea5759202088a9f5222b201643fbc13ae8
Dani Tacheny <dani.tacheny@gmail.com>
2018-04-02 15:02:47 -0500

    add GET, POST, PUT and DELETE for /notes endpoint

--- server.js
+++ server.js
@@ -24,11 +24,56 @@ server.use(cors(corsOptions));
 server.use(express.json());
 
 server.get('/notes', (req, res) => {
-  Note.find({}).then(notes => {
-    res.status(200).json(notes);
-  }).catch(err => {
-    res.status(500).json()
-  })
-})
+  Note.find({})
+    .then(notes => {
+      res.status(200).json(notes);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ msg: 'There was an error retrieving the notes.', error: err });
+    });
+});
+
+server.post('/notes', (req, res) => {
+  const noteInfo = req.body;
+  const note = new Note(noteInfo);
+  note
+    .save()
+    .then(savedNote => {
+      res.status(200).json(savedNote);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ msg: 'There was an error saving the note.', error: err });
+    });
+});
+
+server.put('/notes', (req, res) => {
+  const note = req.body;
+  Note.findOneAndUpdate({ id: note.id }, note, { new: true })
+    .then(updatedNote => {
+      res.status(200).json(updatedNote);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ msg: 'There was an error updating the note.', error: err });
+    });
+});
+
+server.delete('/notes', (req, res) => {
+  const { id } = req.body;
+  Note.findOneAndRemove({ id })
+    .then(deletedNote => {
+      res.status(200).json(deletedNote);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ msg: 'There was an error deleting the note.', error: err });
+    });
+});
 
 server.listen(port, () => console.log(`Server is listening on port ${port}`));
