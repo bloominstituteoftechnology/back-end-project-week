7f0e917998fe2c2ee15e0c4bfaa792bbbd45947f
Dixie Korley <dixiekorley@gmail.com>
2018-04-02 17:30:05 -0400

    Imported sample .json files into MongoDB

--- lambda-notes/server.js
+++ lambda-notes/server.js
@@ -15,7 +15,7 @@ const corsOptions = {
 };
 
 db
-  .connectTo('lambda-notes-users')
+  .connectTo('lambda-notes')
   .then(() => console.log('\n... API Connected to Database ...\n'))
   .catch(err => console.log('\n*** ERROR Connecting to Database ***\n', err));
 
