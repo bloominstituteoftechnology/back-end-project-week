b4542148a0ef7a7c79fbf79cee179dc4737f18db
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2018-04-02 17:50:06 -0400

    Got basic GET and POST requests working

---
+++ backend/models/UserModel.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const Schema = mongoose.Schema;
+const BCRYPT_COST = 11;
+
+const UserSchema = new Schema({
+  username: {
+    required: true,
+    type: String,
+    unique: true,
+    lowercase: true
+  },
+  password: {
+    required: true,
+    type: String
+  }
+});
+
+UserSchema.pre('save', function(next) {
+  bcrypt.hash(this.password, BCRYPT_COST, (err, hash) => {
+    if (err) return next(err);
+    this.password = hash;
+    next();
+  });
+});
+
+module.exports = mongoose.model('User', UserSchema);
--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,34 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const Note = require('./models/NoteModel.js');
 const server = express();
 
+server.use(express.json());
 const PORT = process.env.PORT || 5000;
 
 mongoose
   .connect('mongodb://localhost/lambdanotes')
   .then(() => console.log('Successfully connected to MongoDB'))
   .catch(err => console.error('Failed to connect to MongoDB', err));
+
+server.get('/notes', (req, res) => {
+  Note.find({}, (err, notes) => {
+    if (err) res.status(500).json('Failed to get notes: ', err);
+    res.status(200).json(notes);
+  });
+});
+
+server.post('/notes', (req, res) => {
+  const { title, content } = req.body;
+  console.log(req.body);
+  if (!title || !content)
+    res.status(422).json('You need to enter a title and content');
+  const newNote = new Note({ title, content });
+  newNote
+    .save()
+    .then(savedNote => res.status(200).json(savedNote))
+    .catch(err => res.status(500).json('Error saving note: ', err));
+});
 
 server.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
