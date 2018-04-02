c61c5cebb2adfe7d2bbb1bc9c9162989b7226a00
Aaron Burk <aarondburk@gmail.com>
2018-04-02 13:13:09 -0500

    connected server to DB

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const helmet = require('helmet');
 const mongoose = require('mongoose');
@@ -14,11 +15,18 @@ server.use(helmet());
 
 server.use('/api/notes', noteRouter);
 
-
-
 server.get('/', function(req, res) {
   res.status(200).json({ api: 'running...' })
 });
+
+mongoose
+  .connect(`mongodb://${process.env.DB_HOST}`)
+  .then(conn => {
+    console.log('\n\nConnected to Mongo');
+  })
+  .catch(err => {
+    if (err) console.error(err);
+  })
 
 
 module.exports = { server };
