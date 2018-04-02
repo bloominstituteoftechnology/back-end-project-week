6556b2f133dabbfc0e0376030bf4932db8ff9a11
Smithface <kylewibblesmith@gmail.com>
2018-04-02 18:51:51 -0400

    added deleteNote controller

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -3,12 +3,14 @@ const addNote = require('../controllers/addNote');
 const login = require('../controllers/login');
 const getNotes = require('../controllers/getNotes');
 const modifyNote = require('../controllers/modifyNote');
+const deleteNote = require('../controllers/deleteNote');
 
 
 module.exports = server => {
   server.route('/api/users').post(createUser);
   server.route('/api/notes').post(addNote);
   server.route('/api/notes').put(modifyNote);
+  server.route('/api/notes').delete(deleteNote);
   server.route('/api/login').post(login);
   server.route('/api/getNotes').post(getNotes);
 };
