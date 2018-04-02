9460bdbde401e7349b0be185b86bb5939d279549
Aaron Burk <aarondburk@gmail.com>
2018-04-02 16:14:15 -0500

    update user model
    - add pre save hook and checkPassword method to
      handle hashing of and comparing hashed passwords

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const UserSchema = new mongoose.Schema({
   username: {
     type: String,
+    unique: true,
+    lowercase: true,
     required: true,
   },
   password:{
@@ -10,6 +13,21 @@ const UserSchema = new mongoose.Schema({
     required: true,
   },
 });
+
+UserSchema.pre('save', function(next) {
+  bcrypt.hash(this.password, 11, (err, hash) => {
+    if (err) return next(err);
+    this.password = hash;
+    next();
+  });
+});
+
+UserSchema.methods.checkPassword = function(potentialPassword, cb) {
+  bcrypt.compare(potentialPassword, this.password, (err, isMatch) => {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
 
 const UserModel = mongoose.model('User', UserSchema);
 
