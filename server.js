85a3bbf4777c00d6eecf3503510adc2bcdab2e9c
Mephestys <mephestys@gmail.com>
2018-04-02 12:04:12 -0600

    Server.js basics added: - Connects to MongoDB and starts listen server for API calls

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const port = process.env.PORT || 5000;
+
+const server = express();
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  credentials: true,
+};
+
+server.use(express.json());
+server.use(cors(corsOptions));
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/lambda-notes', {}, err => {
+  if (err) throw new Error(err);
+  console.log('Connected to database');
+});
+
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
