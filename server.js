dc31cb341004d3940a2274d3692e0117ce319b55
Smithface <kylewibblesmith@gmail.com>
2018-04-02 13:16:56 -0400

    connected server to routes.js

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const helmet = require('helmet');
+const routes = require('./api/routes');
 
 const server = express();
+server.use(helmet());
 server.use(express.json());
 
+routes(server);
 
 module.exports = server;
