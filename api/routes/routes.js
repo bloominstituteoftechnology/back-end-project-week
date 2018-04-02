f05fdf213468b30d363b09fbb1cc213f317bf9f8
Smithface <kylewibblesmith@gmail.com>
2018-04-02 16:25:36 -0400

    added login controller
    login works successfully when given correct name/password combo

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -1,8 +1,10 @@
 const createUser = require('../controllers/createUser');
 const addNote = require('../controllers/addNote');
+const login = require('../controllers/login');
 
 
 module.exports = server => {
   server.route('/api/users').post(createUser);
   server.route('/api/notes').post(addNote);
+  server.route('/api/login').post(login);
 };
