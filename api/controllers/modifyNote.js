a21235c7ff5c19813b14fd664e207460c6496c53
Smithface <kylewibblesmith@gmail.com>
2018-04-02 18:39:45 -0400

    modifyNote now handles errors properly

--- api/controllers/modifyNote.js
+++ api/controllers/modifyNote.js
@@ -4,6 +4,10 @@ const modifyNote = (req, res) => {
   const { author, id, title, content } = req.body;
   Note.findByIdAndUpdate(id, { title, content }, { new: true } )
     .then(note => {
+      if (note === null) {
+        res.status(422).json({ message: 'Note by that Id was not found.'});
+        return;
+      }
       res.status(200).json(note);
     })
     .catch(err => {
