032282c126a85bf3a73f8e357e4d489a6dcbce5f
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2018-04-02 13:13:38 -0400

    Added models folder; wrote basic NoteSchema

---
+++ backend/models/NoteModel.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const NoteSchema = {
+  title: {
+    required: true,
+    type: String
+  },
+  content: {
+    required: true,
+    type: String
+  },
+  createdOn: {
+    type: Date,
+    default: Date.now
+  }
+};
+
+module.exports = mongoose.model('Note', NoteSchema);
