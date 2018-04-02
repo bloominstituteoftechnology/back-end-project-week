b1858943594273bc2ff7192105ecd9db21fda8a0
Aaron Burk <aarondburk@gmail.com>
2018-04-02 17:23:21 -0500

    add authentication components to app
    - components are empty at this point.

--- lambda-notes/src/containers/leftRail.js
+++ lambda-notes/src/containers/leftRail.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Authenticator from '../components/authenticator';
 // import ViewNotesBtn from '../components/viewNotesBtn';
 // import CreateNewNote from '../components/createNoteBtn';
 
@@ -6,6 +7,7 @@ class LeftRail extends Component {
     render() {
         return (
             <div className="left-rail">
+                <Authenticator />
                 <h1 className="left-rail__title">
                     Lambda Notes
                 </h1>
--- lambda-notes/src/index.js
+++ lambda-notes/src/index.js
@@ -2,6 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
 import App from './App';
+import notes from './components/noteList';
+import SignIn from './components/signin';
+import SignUp from './components/signup';
+import SignOut from './components/signout';
+import RequireAuth from './components/HOC/requireAuth';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk';
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom';
+import reducers from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider
+    store={createStoreWithMiddleware(
+    reducers,
+    window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION__()
+    )}
+  >
+    <Router>
+      <Route path="/" component={App} />
+      <Route path="/signin" component={SignIn} />
+      <Route path="/users" component={RequireAuth(notes)} />
+      <Route path="/signout" component={SignOut} />
+      <Route path="/signup" component={SignUp} />
+    </Router>
+  </Provider>,
+  document.getElementById('root')
+);
+
 
