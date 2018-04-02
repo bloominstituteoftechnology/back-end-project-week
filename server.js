4671246e77253fbeb03f29a4f27a4c0dc9b098bb
shobanavv <shobanavv@gmail.com>
2018-04-02 13:45:44 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors"); // https://www.npmjs.com/package/cors
+const mongoose = require("mongoose");
+
+const server = express();
