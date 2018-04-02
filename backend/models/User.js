5319934b39eaacab4c5d61e53bede4e669e1c7c0
aceredesign <steven.magadan@hotmail.com>
2018-04-02 09:42:09 -0700

    added User Model

---
+++ backend/models/User.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const Schema = new mongoose.Schema();
+
+const UserSchema = Schema({
+  firstName: {
+    type: String,
+    required: true,
+    index: true
+  },
+  lastName: {
+    type: String,
+    required: true,
+    index: true
+  },
+  email: {
+    type: String,
+    require: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+UserSchema.pre("save", next => {
+  bcrypt.hash(this.password, 11, (err, hash) => {
+    if (err) return next(err);
+    this.password = hash;
+    next();
+  });
+});
+
+UserSchema.methods.checkPassword = function(hashPass, callback) {
+  bcrypt.compare(hashPass, this.password, (err, isMatch) => {
+    if (err) return callback(err);
+    callback(isMatch);
+  });
+};
+
+module.exports = mongoose.model("User", UserSchema);
