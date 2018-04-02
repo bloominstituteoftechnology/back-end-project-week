47af12b4246d18dd2cadb89991c83db3ea46cb45
Christopher Beards <beardscm@gmail.com>
2018-04-02 13:39:32 -0400

    DeleteNote is Functional

--- api/controllers/index.js
+++ api/controllers/index.js
@@ -2,10 +2,13 @@ const { createUser } = require('./User');
 const { login } = require('./Login');
 const { getUsers } = require('./GetUsers');
 const { newNote } = require('./NewNote');
+const { deleteNote } = require('./DeleteNote');
+
 
 module.exports = {
   createUser,
   login,
   getUsers,
   newNote,
+  deleteNote,
 };
--- api/routes/Routes.js
+++ api/routes/Routes.js
@@ -1,9 +1,10 @@
 const { validateToken } = require('../services/auth');
-const { createUser, login, getUsers, newNote } = require('../controllers');
+const { createUser, login, getUsers, newNote, deleteNote } = require('../controllers');
 
 module.exports = server => {
   server.route('/api/users').post(createUser);
   server.route('/api/login').post(login);
   server.route('/api/get').get(validateToken, getUsers);
   server.route('/api/new-note').post(validateToken, newNote);
+  server.route('/api/note/delete/:id').delete(validateToken, deleteNote);
 };
