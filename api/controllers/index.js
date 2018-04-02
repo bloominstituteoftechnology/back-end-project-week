b98e09f57ba38442d5dc01cf15ef5b956734da81
Christopher Beards <beardscm@gmail.com>
2018-04-02 13:32:53 -0400

    NewNote Functional

--- api/controllers/index.js
+++ api/controllers/index.js
@@ -1,9 +1,11 @@
 const { createUser } = require('./User');
 const { login } = require('./Login');
 const { getUsers } = require('./GetUsers');
+const { newNote } = require('./NewNote');
 
 module.exports = {
   createUser,
   login,
-  getUsers
+  getUsers,
+  newNote,
 };
--- api/routes/Routes.js
+++ api/routes/Routes.js
@@ -1,8 +1,9 @@
 const { validateToken } = require('../services/auth');
-const { createUser, login, getUsers } = require('../controllers');
+const { createUser, login, getUsers, newNote } = require('../controllers');
 
 module.exports = server => {
   server.route('/api/users').post(createUser);
   server.route('/api/login').post(login);
   server.route('/api/get').get(validateToken, getUsers);
+  server.route('/api/new-note').post(validateToken, newNote);
 };
