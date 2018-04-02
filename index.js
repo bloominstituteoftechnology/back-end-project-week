09857659da9ffd8b107cf7cfa5315948fc594401
JohnJohnX4 <JohnJohnX4@gmail.com>
2018-04-02 12:08:29 -0400

    changed server file, moved into index.js

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-const { server } = require('.src/server.js');
+const bodyParser = require('body-parser');
+const express = require('express');
+const bcrypt = require('bcrypt');
+const cors = require('cors');
+const server = express();
+
 const PORT = 5000;
+
 server.listen(PORT);
+server.use(express.json());
+
 console.log("Server listening on: ", PORT)
+console.log("Hello World!");
+
+// eventually will store all routes in another file, once they are all built.
+// routes(server);
