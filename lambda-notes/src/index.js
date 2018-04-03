795a15233a6f1bd9ea86a8f602468158fadb62c0
Aaron Burk <aarondburk@gmail.com>
2018-04-03 12:49:55 -0500

    update index.js
    - add parent div to route paths
    - defined createStoreWithMiddleware variable

--- lambda-notes/src/index.js
+++ lambda-notes/src/index.js
@@ -16,6 +16,8 @@ import {
 } from 'react-router-dom';
 import reducers from './reducers';
 
+const createStoreWithMiddleware = applyMiddleware(ReduxThunk)(createStore);
+
 ReactDOM.render(
   <Provider
     store={createStoreWithMiddleware(
@@ -25,11 +27,13 @@ ReactDOM.render(
     )}
   >
     <Router>
-      <Route path="/" component={App} />
-      <Route path="/signin" component={SignIn} />
-      <Route path="/users" component={RequireAuth(notes)} />
-      <Route path="/signout" component={SignOut} />
-      <Route path="/signup" component={SignUp} />
+      <div>
+        <Route path="/" component={App} />
+        <Route path="/signin" component={SignIn} />
+        <Route path="/users" component={RequireAuth(notes)} />
+        <Route path="/signout" component={SignOut} />
+        <Route path="/signup" component={SignUp} />
+      </div>
     </Router>
   </Provider>,
   document.getElementById('root')
