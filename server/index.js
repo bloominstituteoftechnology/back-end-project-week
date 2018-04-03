efb46203a9c5bf324451730a5265318dae8e7b0f
Richard Reis <richardreeze1@gmail.com>
2018-04-02 20:01:16 -0700

    testing heroku

---
+++ server/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('This will be a notes app');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
