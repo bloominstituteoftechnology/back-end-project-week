df2afa16b887f27a99c5ce2b0ccdbb2f335db526
Aaron Wilder <element1028@gmail.com>
2018-04-03 12:56:36 -0700

    now schema points to schema.js file

--- Backend/server.js
+++ Backend/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
-const Schema = mongoose.Schema;
+const Schema = require('./schema');
 const PORT = 5000;
 const server = express();
 
