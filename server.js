a78c700a1e241c499454e7f5921699d8630ced11
mixelpixel <mixelpix@mac.com>
2018-04-03 15:48:56 -0400

    First stab at the server
    I added all the required packages
    list them
    I added a listen ever
    there is a get  for the root to return everything in the datavase

---
+++ server.js
@@ -0,0 +1,32 @@
+// require express body-parser and mongoose
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+const PORT = 5000;
+const server = express();
+
+server.use(bodyParser.json());
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/demoSchema');
+
+server.get('/', (request, response) => {
+  Schema.find({}, (err, database) => {
+    if(err) {
+      response.status(500);
+      response.json(err);
+    }else {
+      response.json(database);
+    }
+  });
+});
+
+server.listen(PORT, err => {
+  if(err) {
+    console.log('Something is not right in the world.', err);
+  } else {
+    console.log(`The server is listening on port number: ${PORT}.`);
+  }
+});
