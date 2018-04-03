7c55b75f70061c8fd97a05bab89d8dd52c30a4b2
Smithface <kylewibblesmith@gmail.com>
2018-04-03 14:03:24 -0400

    added axios to WelcomePage
    front end is ready to go in terms of registering a user, just need to connect the ends with cors

--- lambda_notes/src/components/mainpage/welcome/WelcomePage.js
+++ lambda_notes/src/components/mainpage/welcome/WelcomePage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import './welcomepage.css';
 
 class WelcomePage extends React.Component {
@@ -6,7 +7,8 @@ class WelcomePage extends React.Component {
     name: '',
     password: '',
     confirmpassword: '',
-    welcomeSwitchValue: 'buttons'
+    welcomeSwitchValue: 'buttons',
+    message: ''
   }
 
   render() {
@@ -14,6 +16,7 @@ class WelcomePage extends React.Component {
       <div>
         <div className="welcomePage__welcome">Welcome to Lambda Notes</div>
         <div> {this.welcomeSwitch(this.state.welcomeSwitchValue)} </div>
+        <div> {this.state.message} </div>
       </div>
     );
   }
@@ -22,21 +25,21 @@ class WelcomePage extends React.Component {
     switch(param) {
       case 'login':
         return <div>
-          <form>
+          <form onSubmit={this.loginUser}>
             <input name="name" type="text" placeholder="Name" onChange={this.handleChange} value={this.state.name}/>
             <input name="password" type="password" placeholder="Password" onChange={this.handleChange} value={this.state.password}/>
-            <button>Submit</button>
-            <button onClick={() => this.changeWelcomeSwitch('buttons')}>Cancel</button>
+            <button type="submit">Submit</button>
+            <button onClick={this.cancel}>Cancel</button>
           </form>
         </div>;
       case 'register':
         return <div>
-          <form>
+          <form onSubmit={this.registerUser}>
             <input name="name" type="text" placeholder="Name" onChange={this.handleChange} value={this.state.name}/>
             <input name="password" type="password" placeholder="Password" onChange={this.handleChange} value={this.state.password}/>
             <input name="confirmpassword" type="password" placeholder="Confirm Password" onChange={this.handleChange} value={this.state.confirmpassword}/>
-            <button>Submit</button>
-            <button onClick={() => this.changeWelcomeSwitch('buttons')}>Cancel</button>
+            <button type="submit">Submit</button>
+            <button onClick={this.cancel}>Cancel</button>
           </form>
         </div>;
       case 'buttons':
@@ -55,6 +58,46 @@ class WelcomePage extends React.Component {
 
   changeWelcomeSwitch = (value) => {
     this.setState({ ...this.state, welcomeSwitchValue: value });
+  };
+
+  loginUser = (event) => {
+    event.preventDefault();
+
+  };
+
+  registerUser = (event) => {
+    const name = this.state.name;
+    const password = this.state.password
+    event.preventDefault();
+    if (this.state.confirmpassword !== password) {
+      this.changeMessage('Passwords do not match');
+      return;
+    }
+    axios
+      .post('http://localhost:3030/api/users', { name, password })
+      .then(res => {
+        this.changeMessage(res.message);
+      })
+      .catch(err => {
+        this.changeMessage(err.message);
+        console.error(err.err);
+      });
+  };
+
+  changeMessage = (message) => {
+    this.setState({ message: message });
+  };
+
+  cancel = (event) => {
+    if (event) {
+      event.preventDefault();
+    }
+    this.setState({
+      name: '',
+      password: '',
+      confirmpassword: '',
+      welcomeSwitchValue: 'buttons'
+    });
   };
 }
 
