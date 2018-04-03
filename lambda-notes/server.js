d22850530aa6c8011955ab026a783ca5c982555f
Kevin <16568166+sirkevinc@users.noreply.github.com>
2018-04-03 13:15:16 -0400

    Removed and deleted old unused files

--- lambda-notes/server.js
+++ lambda-notes/server.js
@@ -1,32 +1,3 @@
-// const express = require('express');
-// const mongoose = require('mongoose');
-// const cors = require('cors');
-// const port = process.env.PORT || 5000;
-// const noteRouter = require('./api/routes/NoteRoutes');
-
-// const corsOptions = {
-//   origin: 'http://localhost:3000',
-//   credentials: true
-// };
-
-// const server = express();
-// server.use(express.json());
-// server.use(cors(corsOptions));
-
-// server.use('/api/notes', noteRouter);
-
-// mongoose.connect('mongodb://localhost/lambda_notes')
-//   .then(res => {
-//     console.log('MongoDB Connected Successfully');
-//   })
-//   .catch(err => {
-//     console.log('DB Connection failed');
-//   });
-
-// server.listen(port, (req, res) => {
-//   console.log(`Server listening on port ${port}`);
-// });
-
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
