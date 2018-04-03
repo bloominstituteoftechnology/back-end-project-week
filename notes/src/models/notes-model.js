7cb7e46de0881da276ffbcadf2c4307a5ff31584
Tyson Williams <tyson.j.williams@gmail.com>
2018-04-03 17:09:36 -0600

    Add Note Schema
    Add 'title', 'content', 'created', 'updated'

---
+++ notes/src/models/notes-model.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const noteSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  created: {
+    type: Date,
+    default: Date.now,
+  },
+  updated: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('Note', noteSchema);
