2f437e81d7f27c8d23d2e66e5fce0bf49c835c62
Jonathan Matthew Brunt <jbrunt.mobile@gmail.com>
2018-04-03 10:37:49 -0500

    Add Hook And Method In User Schema
    - in user.js:
    - add pre-save hook to hash password
    - add method to compare hash and raw paswword
    - disable ESLint no unnamed function rule locally to rid warning
    - arrow function cannot be used without bind, bc of 'this' context needed

--- API/Schemas/user.js
+++ API/Schemas/user.js
@@ -1,3 +1,5 @@
+/* eslint func-names: [0] */
+
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
@@ -15,5 +17,17 @@ const User = new mongoose.Schema({
     required: true,
   },
 });
+
+User.pre('save', function (next) {
+  bcrypt.hash(this.password, SALT_ROUNDS, (err, hash) => {
+    if (err) return next(err);
+    this.password = hash;
+    next();
+  });
+});
+
+User.methods.checkPassword = function (rawPassword, cb) {
+  bcrypt.compare(rawPassword, this.password, cb);
+};
 
 module.exports = mongoose.model('User', User);
