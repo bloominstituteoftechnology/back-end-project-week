53987b9af35894fc9feb43b0fabc5cdced40f39a
unknown <davsoudry@gmail.com>
2018-04-03 18:18:54 -0500

    added more middleware

--- middlewares.js
+++ middlewares.js
@@ -31,3 +31,40 @@ const hashedPassword = (req, res, next) => {
             throw new Error(err);
         });
 };
+
+const loggedIn = (req, res, next) => {
+    const { username } = req.session;
+    console.log(req.session);
+    if (!username) {
+      sendUserError('User is not logged in', res);
+      return;
+    }
+    User.findOne({ username }, (err, user) => {
+      if (err) {
+        sendUserError(err, res);
+      } else if (!user) {
+        sendUserError('User does exist', res);
+      } else {
+        req.user = user;
+        next();
+      }
+    });
+  };
+
+  const restrictedPermissions = (req, res, next) => {
+    const path = req.path;
+    if (/restricted/.test(path)) {
+      if (!req.session.username) {
+        sendUserError('user not autorized', res);
+        return;
+      }
+    }
+    next();
+  };
+  
+  module.exports = {
+    sendUserError,
+    hashedPassword,
+    loggedIn,
+    restrictedPermissions,
+  };
