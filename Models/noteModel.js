c478d637c26ff872f1b118c5df66db5665e95cf1
Nate152 <35231160+Nate152@users.noreply.github.com>
2018-04-03 16:36:12 -0400

    Added Models, Added bcrypt, Adjusted file structure
    -Added userModel.js
    -Added noteModel.js
    -Added bcrypt as dependency, wrote hash and compare functions in userModel
    -Added Models Folder
    -Added Controllers Folder

---
+++ Models/noteModel.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
+const NoteSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('Note', NoteSchema);
--- server.js
+++ server.js
@@ -1,1 +1,23 @@
 const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const server = express();
+const PORT = process.env.PORT || 5000;
+
+server.use(cors());
+
+mongoose
+  .connect('mongodb://localhost/LambdaNotes')
+  .then(connect => {
+    console.log('Connected to MongoDB');
+  })
+  .catch(error => {
+    console.error('Database connection failed');
+  });
+
+server.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
+
+module.exports = { server };
