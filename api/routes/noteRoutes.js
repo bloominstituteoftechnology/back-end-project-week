f6a8711f14a352d14ed9f863ca3824d37f37efb7
german gonzalez <gcomlnk@gmail.com>
2018-04-03 16:37:21 -0400

    Updating dependencies in json package

--- api/routes/noteRoutes.js
+++ api/routes/noteRoutes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const noteController = require('../controllers/noteController');
 let appRouter = express.Router();
 
+// EndPoint: /api/note/
+
 appRouter.route('/').get(noteController.getNotes);
 
 module.exports = appRouter;
