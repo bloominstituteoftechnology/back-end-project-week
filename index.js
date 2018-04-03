d0fea9c4a16d73f857c879a68d8093be2a16ab65
Richard Reis <richardreeze1@gmail.com>
2018-04-02 20:02:53 -0700

    heroku works?

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.get('/', (req, res) => {
-  res.send('This will be a notes app');
-});
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT);
