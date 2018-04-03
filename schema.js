f63752c695fb92ac89f9ff03f20d970d53f4e69b
mixelpixel <mixelpix@mac.com>
2018-04-03 15:26:42 -0400

    basic schema setup
    will it work?

---
+++ schema.js
@@ -0,0 +1,19 @@
+// require mongoose
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const demoSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+})
+
+mongoose.connect('mongodb://localhost/demoSchema');
+
+module.exports = mongoose.model('demoSchema', demoSchema);
