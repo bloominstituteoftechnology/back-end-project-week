5f7b7d03af82d5ee99dfc5deff87f96cb0e7d8c9
Dani Tacheny <dani.tacheny@gmail.com>
2018-04-03 16:24:00 -0500

    add logout button to sidebar
    - convert Sidebar component to a class component
    - lines 10-16: create logout method to activate when logout button is
    clicked
    - line 28: if the user is logged in, show the logout button

--- notes/src/comps/Sidebar.js
+++ notes/src/comps/Sidebar.js
@@ -1,21 +1,41 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 import '../styles/Sidebar.css';
 
-const Sidebar = props => {
-  return (
-    <div className="sidebar">
-      <span className="sidebar__title">Lambda Notes</span>
-      <div className="sidebar__buttons">
-        <Link to="/" className="sidebar__button">
-          <div>View Your Notes</div>
-        </Link>
-        <Link to="/new" className="sidebar__button">
-          <div>+ Create New Note</div>
-        </Link>
-      </div>
-    </div>
-  );
-};
+import { logout } from '../actions';
 
-export default Sidebar;
+class Sidebar extends React.Component {  
+  logout = () => {
+    axios
+      .post('http://localhost:3030/logout')
+      .then()
+      .catch(err => console.log(err));
+    this.props.logout();
+  };
+  render() {
+    return (
+      <div className="sidebar">
+        <span className="sidebar__title">Lambda Notes</span>
+        <div className="sidebar__buttons">
+          <Link to="/" className="sidebar__button">
+            <div>View Your Notes</div>
+          </Link>
+          <Link to="/new" className="sidebar__button">
+            <div>+ Create New Note</div>
+          </Link>
+          {this.props.authed && <div onClick={this.logout} className="sidebar__button">Logout</div>}
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    authed: state.authed,
+  }
+}
+
+export default connect(mapStateToProps, { logout })(Sidebar);
