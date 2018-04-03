5d8a71daa7c6abcc1f91c2bcf23cc676559ad066
Aaron Burk <aarondburk@gmail.com>
2018-04-03 12:52:41 -0500

    add signout component

---
+++ lambda-notes/src/components/signout.js
@@ -0,0 +1,16 @@
+
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { logout } from '../actions';
+
+class SignOut extends Component {
+  componentWillMount() {
+    this.props.logout();
+  }
+
+  render() {
+    return <div className="logout">You have signed out</div>;
+  }
+}
+
+export default connect(null, { logout })(SignOut);
