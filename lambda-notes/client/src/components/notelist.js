12797936fd2ffc18eb9ffec8c1888ba0dc9836b1
Dixie Korley <dixiekorley@gmail.com>
2018-04-04 18:32:02 -0400

    Deleted Note.js, moved it to NoteList

--- lambda-notes/client/src/components/notelist.js
+++ lambda-notes/client/src/components/notelist.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import Note from './icons/note';
+import { Link } from 'react-router-dom';
 
-const NoteList = () => {
-    return (
-     <Note/>
-    );
+class NoteList extends Component {
+    constructor() {
+        super();
+        this.state = {
+            content: ''
+        }
+    }
+
+    contentInput(event) {
+        this.setState({ content: event.target.value });
+    }
+    
+    render() {
+        return (
+            <Link className='link__second' to='/view'><div className="note__icon">
+            <p className="note__icon__title">Test for title</p>
+            <hr className="style__one"/>
+            <p>Test for snippet of note</p>
+        </div></Link>
+        );
+    }
+
+
 };
 
 export default NoteList;
