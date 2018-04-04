9c967f5eb4139b16e249bd9f7276585a8db0d679
Amanda Phillips <amanda@phillipsdev.org>
2018-04-03 20:30:06 -0500

    set base case for routes then commented out

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -4,10 +4,10 @@ const { createUser, login } = require('../controllers');
 module.exports = server => {
   server.route('/api/users').post(createUser);
   server.route('/api/login').post(login);
-  server
-    .route('/api/notes')
-    .get()
-    .post()
-    .put()
-    .delete();
+  // server
+  //   .route('/api/notes')
+  //   .get()
+  //   .post()
+  //   .put()
+  //   .delete();
 };
