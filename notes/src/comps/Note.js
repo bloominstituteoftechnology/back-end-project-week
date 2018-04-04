67fbfe76f2f1a95fb3a77a88e84d8cfd28ad31ea
Dani Tacheny <dani.tacheny@gmail.com>
2018-04-04 16:47:22 -0500

    change id to _id in Note component

--- notes/src/comps/Note.js
+++ notes/src/comps/Note.js
@@ -7,13 +7,13 @@ import '../styles/Note.css';
 const Note = props => {
   // this is the note object with the id that matches the url
   const note = props.notes.filter(
-    note => String(note.id) === props.match.params.id
+    note => String(note._id) === props.match.params.id
   )[0];
 
   return (
     <div className="note__container">
       <div className="note__options">
-        <Link to={`/edit/${note.id}`}>
+        <Link to={`/edit/${note._id}`}>
           <div className="note__option-item">edit</div>
         </Link>
         <div className="note__option-item" onClick={props.toggleModal}>delete</div>
