44a075ed0066d0cc34f8b5468c9e550dba24b300
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2018-04-04 02:16:12 -0400

    Styling tweaks to signup component

--- lambdanotes/src/components/signup.js
+++ lambdanotes/src/components/signup.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'; // eslint-disable-line
 import styled from 'styled-components';
 
+// Styles
 const SignupStyled = styled.div`
   display: flex;
   flex-direction: column;
@@ -10,6 +11,11 @@ const SignupStyled = styled.div`
   background-color: rgb(243, 243, 243);
   border-left: 1px solid rgb(151, 151, 151);
   border-right: 1px solid rgb(151, 151, 151);
+  align-items: center;
+
+  h2 {
+    padding: 20px;
+  }
 
   button {
     width: 100px;
@@ -26,8 +32,31 @@ const SignupStyled = styled.div`
       border: 2px solid white;
     }
   }
+
+  input,
+  textarea {
+    outline: 1px solid rgba(0 0 0 0);
+    border-style: solid;
+    border: 1px solid grey;
+    padding: 20px;
+    margin: 10px;
+    width: 50%;
+
+    &:hover {
+      border-style: solid;
+      outline: 1px solid rgba(0 0 0 0);
+      border: 1px solid black;
+    }
+
+    &:focus {
+      outline: 1px solid rgb(94, 190, 195);
+      border: 1px solid rgb(94, 190, 195);
+      border-style: solid;
+    }
+  }
 `;
 
+// Signup Component
 class Signup extends Component {
   render() {
     return (
