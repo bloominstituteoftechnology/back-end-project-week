b0a4c3986ff11f0ab298bd882723a980675b277a
Smithface <kylewibblesmith@gmail.com>
2018-04-04 11:03:19 -0400

    added authentication middleware to get note route

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -4,6 +4,7 @@ const login = require('../controllers/login');
 const getNotes = require('../controllers/getNotes');
 const modifyNote = require('../controllers/modifyNote');
 const deleteNote = require('../controllers/deleteNote');
+const authenticate = require('../controllers/authenticate');
 
 
 module.exports = server => {
@@ -12,5 +13,5 @@ module.exports = server => {
   server.route('/api/notes').put(modifyNote);
   server.route('/api/notes').delete(deleteNote);
   server.route('/api/login').post(login);
-  server.route('/api/getNotes').post(getNotes);
+  server.route('/api/getNotes').post(authenticate, getNotes);
 };
