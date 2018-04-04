5bddf0ba1ebefb1c9b74168924e2955d655ccf98
Dixie Korley <dixiekorley@gmail.com>
2018-04-04 18:45:29 -0400

    Working on Notelist

--- lambda-notes/client/src/components/notelist.js
+++ lambda-notes/client/src/components/notelist.js
@@ -1,17 +1,40 @@
 import React from 'react';
 import Note from './icons/note';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
 
 class NoteList extends Component {
     constructor() {
         super();
         this.state = {
+            oldContent: [],
             content: ''
         }
     }
 
     contentInput(event) {
         this.setState({ content: event.target.value });
+    }
+
+    addContent(event) {
+        event.preventDefault();
+        const newContent = this.state.oldContent;
+        axios.post('http://localhost:5000/create-post', newContent)
+        .then((data) => {
+            // newContent.push(this.state.content);
+            // this.setState({
+            //     content: '',
+            //     content: newContent
+            // });
+            //
+            localStorage.setItem(newContent, data.data._id);
+            setTimeout(() => {
+                window.location = '/list'
+            }, 200)
+        })
+        .catch(err => {
+            console.log({ 'error': err.response.error });
+        });
     }
     
     render() {
