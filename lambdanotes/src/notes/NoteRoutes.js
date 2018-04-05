5b2f1dcc6f07ee50dfd549dbdb4f86f91062937f
Nathan Daniel <natdf@icloud.com>
2018-04-05 17:36:05 -0500

    Commit 6 * NoteRoutes.js: - built display single note - built delete note - built edit note - all functioning properly (finally!)

--- lambdanotes/src/notes/NoteRoutes.js
+++ lambdanotes/src/notes/NoteRoutes.js
@@ -4,6 +4,8 @@ const Note = require('./NoteSchema');
 
 const router = express.Router();
 
+// CREATE NOTE
+
 router.post('/createNote', (req, res) => {
   const { title, content } = req.body;
   const note = new Note({ title, content });
@@ -17,6 +19,8 @@ router.post('/createNote', (req, res) => {
     });
 });
 
+// DISPLAY NOTES
+
 router.get('/displayNotes', (req, res) => {
   Note.find({})
     .then(notes => {
@@ -24,6 +28,67 @@ router.get('/displayNotes', (req, res) => {
     })
     .catch(err => {
       res.status(500).json({ msg: 'Error getting notes', error: err });
+    });
+});
+
+// DISPLAY SINGLE NOTE
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+
+  Note.findById(id)
+    .then(note => {
+      if (note) {
+        res.status(200).json(note);
+      } else {
+        res.status(404).json({ msg: 'Note not found', error: err });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ msg: 'Error getting note', error: err });
+    });
+});
+
+// DELETE NOTE
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+
+  Note.findByIdAndRemove(id)
+    .then(note => {
+      if (note) {
+        res.status(200).json(note);
+      } else {
+        res.status(404).json({ msg: 'Note not found', error: err });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ msg: 'Error deleting note', error: err });
+    });
+});
+
+// EDIT NOTE
+
+router.put('/:id', (req, res) => {
+  const { id } = req.params;
+  const noteData = req.body;
+
+  Note.findByIdAndUpdate(id, noteData)
+    .then(note => {
+      if (note) {
+        res.status(200).json(note);
+      } else {
+        res.status(404).json({ msg: 'Note not found' });
+      }
+    })
+    .catch(err => {
+      if (err.name === 'Error!') {
+        res
+          .status(400)
+          .json({ msg: `The id: ${err.value} is not valid`, error: err });
+      } else {
+        res.status(500).json({ msg: 'Error updating note', error: err });
+      }
     });
 });
 
