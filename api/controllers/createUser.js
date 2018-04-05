640604031a72a68049afb48708cf02cb82d2509b
Smithface <kylewibblesmith@gmail.com>
2018-04-05 12:18:26 -0400

    added tests for user login

--- api/controllers/createUser.js
+++ api/controllers/createUser.js
@@ -2,15 +2,19 @@ const User = require('../models/userModel');
 
 const createUser = (req, res) => {
   const { name, password } = req.body;
-  const newUser = new User({ name: name.toLowerCase(), password });
-  newUser
-    .save()
-    .then(user => {
-      res.status(200).json({ message: 'User successfully created.'});
-    })
-    .catch(err => {
-      res.status(422).json({ message: 'User could not be created because of an error', err});
-    });
+  if(name && password) {
+    const newUser = new User({ name: name.toLowerCase(), password });
+    newUser
+      .save()
+      .then(user => {
+        res.status(200).json({ message: 'User successfully created.'});
+      })
+      .catch(err => {
+        res.status(422).json({ message: 'User could not be created because of an error', err});
+      });
+  } else {
+    res.status(422).json({ message: 'Name & Password required.'});
+  }
 };
 
 module.exports = createUser;
--- server.test.js
+++ server.test.js
@@ -29,6 +29,19 @@ describe('Users', () => {
     });
   });
 
+  beforeEach(async () => {
+    const user3 = new User({
+      name: 'monkey7',
+      password: 'bananas'
+    });
+    const user4 = new User({
+      name: 'Torque',
+      password: 'blorp'
+    });
+    await user3.save();
+    await user4.save();
+  });
+
   afterEach(done => {
     // simply remove the collections from your DB.
     User.remove({}, (err) => {
@@ -45,6 +58,11 @@ describe('Users', () => {
       password: 'bananas'
     };
 
+    const user2 = {
+      nombre: 'Charles',
+      password: 'beans'
+    };
+
     it('should return a success message when user is created', (done) => {
       chai.request(server)
       .post('/api/users')
@@ -56,13 +74,62 @@ describe('Users', () => {
       });
     });
 
+    it('should return a message when no name provided', (done) => {
+      chai.request(server)
+      .post('/api/users')
+      .send(user2)
+      .end((err, res) => {
+        expect(res.status).to.equal(422);
+        expect(res.body.message).to.equal('Name & Password required.');
+        done();
+      });
+    });
+
   });
 
-//   describe('[POST] /api/login', () => {
-//     it('should do a thing', (done) => {
-      
-//     });
-//   });
+  describe('[POST] /api/login', () => {
+    const test = {
+      name: 'monkey7',
+      password: 'bananas'
+    };
+
+    it('should return a logged in message + token when logging in', (done) => {
+      chai.request(server)
+      .post('/api/login')
+      .send(test)
+      .end((err, res) => {
+        expect(res.status).to.equal(200);
+        expect(res.body.message).to.equal('Successfully logged in as monkey7.');
+        expect(res.body.token).to.not.be.undefined;
+        done();
+      });
+    });
+
+    it('should return an error when given a name not in db', (done) => {
+      chai.request(server)
+      .post('/api/login')
+      .send(Object.assign({}, { name: 'taco', password:'bell' }))
+      .end((err, res) => {
+        expect(res.status).to.equal(422);
+        expect(res.body.message).to.equal('Invalid Username and/or Password');
+        expect(res.body.token).to.be.undefined;
+        done();
+      });
+    });
+
+    it('should return an error when password does not match', (done) => {
+      test.password = 'apples';
+      chai.request(server)
+      .post('/api/login')
+      .send(test)
+      .end((err, res) => {
+        expect(res.status).to.equal(422);
+        expect(res.body.message).to.equal('Invalid Username and/or Password');
+        expect(res.body.token).to.be.undefined;
+        done();
+      });
+    });
+  });
 // });
 
 // describe('Notes', () => {
