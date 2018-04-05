be1b8128ce85abd63453604d54d6969592dab157
Dixie Korley <dixiekorley@gmail.com>
2018-04-05 14:05:09 -0400

    axios get displaying error on backend, using dummy data

--- lambda-notes/client/src/components/notelist.js
+++ lambda-notes/client/src/components/notelist.js
@@ -1,19 +1,42 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 class NoteList extends Component {
+    constructor() {
+        super();
+        this.state = {
+            posts: []
+        }
+    }
+
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+        // axios.get('https://localhost/5000/posts')
+            .then((res) => {
+                console.log(res.data);
+                this.setState({ posts: res.data });
+            })
+            .catch((err) => {
+                console.log('You still need your posts. Try again.')
+            })
+    }
 
     render() {
         return (
-            <Link className='link__second' to='/view'><div className="note__icon">
-                <p className="note__icon__title">{this.props.title}</p>
-                <hr className="style__one" />
-                <p>{this.props.content}</p>
-            </div></Link>
-        );
+            <div>
+                {this.state.posts.map((post) => {
+                    return (
+                        <div key={post._id} className="note__icon">
+                            <p className="note__icon__title">{post.title}</p>
+                            <hr className="style__one" />
+                            <p>{post.body}</p>
+                        </div>
+                    )
+                })}
+            </div>
+        )
     }
-
-
 };
 
 export default NoteList;
