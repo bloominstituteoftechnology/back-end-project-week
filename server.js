b7e0e3086c9cbc1940879ab6c57a7a20e2fd0034
Eric <erichech@gmail.com>
2018-04-05 16:02:20 -0400

    comment out cors

--- server.js
+++ server.js
@@ -7,13 +7,13 @@ const SECRET = process.env.SECRET;
 const routes = require('./api/routes/routes');
 
 const server = express();
-const corsOptions = {
-  origin: 'http://localhost:3000',
-  credentials: true,
-};
+// const corsOptions = {
+//   origin: 'http://localhost:3000',
+//   credentials: true,
+// };
 
 server.use(express.json());
-server.use(cors(corsOptions));
+// server.use(cors(corsOptions));
 server.use(
   session({
     secret: SECRET,
