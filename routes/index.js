fecc7a8beb6cfbe43db9c1319fb166f436813d47
Res Yeg <shaqk702@gmail.com>
2018-04-04 17:35:30 -0700

    minor fix to routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,9 @@ const { createUser, login, createNote, getNotes, updateNote, deleteNote } = requ
 const { validateToken } = require('../services/auth');
 
 module.exports = server => {
-    server
-        .route('/api/users')
-        .post(createUser)
-        // .get(validateToken, getUsers);
+    server.route('/api/users').post(createUser)
     server.post('/api/login', login);
     server.route('/api/notes').post(createNote);
-        // .get(validateToken, getNotesByUserId);
     server.route('/api/users/:id').get(validateToken, getNotes);
     server.route('/api/notes/:authorId/:noteId').put(validateToken, updateNote);
     server.delete('/api/notes/:noteId', validateToken, deleteNote);
