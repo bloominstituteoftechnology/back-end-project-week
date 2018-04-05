839b5273d5511287c0bd03f3ab7dca7a2f0b18ff
Eric <erichech@gmail.com>
2018-04-05 16:34:26 -0400

    cors

--- server.js
+++ server.js
@@ -1,28 +1,32 @@
 const express = require('express');
 const session = require('express-session');
-//const cors = require('cors');
+const cors = require('cors');
 
 const SECRET = process.env.SECRET;
 
 const routes = require('./api/routes/routes');
 
 const server = express();
-// const corsOptions = {
-//   origin: 'https://boring-pare-625725.netlify.com',
-//   credentials: true,
-// };
+const corsOptions = {
+  origin: 'https://boring-pare-625725.netlify.com',
+  credentials: true,
+};
 
-server.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'null');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,contenttype');
-    res.setHeader('Access-Control-Allow-Credentials', true);
+// server.use(function (req, res, next) {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,contenttype');
+//     res.setHeader('Access-Control-Allow-Credentials', true);
 
-    next();
-});
+//     next();
+// });
+
 
 server.use(express.json());
-//server.use(cors(corsOptions));
+server.use(cors(corsOptions));
+
+server.options('*', cors())
+
 server.use(
   session({
     secret: SECRET,
