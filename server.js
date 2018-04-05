0036bfc90937a38012428351afe9666b57b271ac
Eric <erichech@gmail.com>
2018-04-05 16:27:31 -0400

    cors

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const server = express();
 // };
 
 server.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'null');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,contenttype');
     res.setHeader('Access-Control-Allow-Credentials', true);
