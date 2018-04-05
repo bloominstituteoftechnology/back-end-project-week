620a98f8e315c8e6e1eece0730ba1ceacef2dd26
Tyson Williams <tyson.j.williams@gmail.com>
2018-04-05 16:09:29 -0600

    Add password functionality to User Schema
    pre-hook 'save' to hash password
    check password method on User Schema

--- notes/src/models/user-model.js
+++ notes/src/models/user-model.js
@@ -3,6 +3,8 @@ const bcrypt = require('bcrypt');
 
 const Schema = mongoose.Schema;
 
+const BCRYPT_COST = 11;
+
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/users');
 
@@ -10,5 +12,22 @@ const UserSchema = new Schema({
   username: { type: String, required: true, unique: true, lowercase: true },
   password: { type: String, required: true }
 });
+
+UserSchema.pre('save', function (next) {
+  const user = this;
+  if(!user.isModified('password')) return next();
+  bcrypt.hash(user.password, BCRYPT_COST, (err, hash) => {
+    if (err) return next(err);
+    user.password = hash;
+    next();
+  });
+});
+
+UserSchema.methods.checkPassword = function (potentialPassword, cb) {
+  bcrypt.compare(potentialPassword, this.password, (err, isMatch) => {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
 
 module.exports = mongoose.model('User', UserSchema);
