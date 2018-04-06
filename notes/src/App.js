37bc9a8b87050ccfcbe199296511892bd4ff2d0e
Tyson Williams <tyson.j.williams@gmail.com>
2018-04-06 12:33:51 -0600

    Add paths to Frontend App
    signup
    login

--- notes/src/App.js
+++ notes/src/App.js
@@ -10,6 +10,8 @@ class App extends Component {
     return (
       <Router>
         <div>
+          <Route path="/signup" component={Signup} />
+          <Route path="/login" component={Login} />
           <Route path="/" component={AllNotes} exact />
           <Route path="/new-note" component={NewNote} />
           <Route path="/notes/:id" component={SingleNote} />
