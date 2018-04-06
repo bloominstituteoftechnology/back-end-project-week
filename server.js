325ea958c81cb7c4192f1c8d53ea1753805adf9d
Cody Windeknecht <cmwindeknecht@gmail.com>
2018-04-06 13:30:39 -0400

    Trying Random Things to Get Heroku Working...

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3000;
 const corsOptions = {
   methods: 'GET, PUT, POST, DELETE',
   preflightContinue: false,
-  origin: 'http://localhost:3001',
+  // origin: 'http://localhost:3001',
   credentials: true,
 };
 
