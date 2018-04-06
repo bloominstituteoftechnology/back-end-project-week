4671e315b9f8e65f74fe43c1550ad674a8b082c6
Dani Tacheny <dani.tacheny@gmail.com>
2018-04-06 11:02:24 -0500

    change NoteList back
    it fixed the original problem but causes the notes to be fetched way too
    many times

--- notes/src/comps/NoteList.js
+++ notes/src/comps/NoteList.js
@@ -7,55 +7,49 @@ import { selectNote, sortNotes, fetchNotes } from '../actions';
 import '../styles/NoteList.css';
 
 // props include the list of notes from App.js
-class NoteList extends React.Component {
-  componentDidMount = () => {
-    this.props.fetchNotes();
-  }
-  
-  render() {
-    return (
-      <div className="noteList__container">
-        <h1 className="notesList__header">Your Notes:</h1>
-        <CSVLink
-          data={this.props.notes}
-          filename={'my-notes.csv'}
-          className="noteList__download"
+const NoteList = props => {
+  return (
+    <div className="noteList__container">
+      <h1 className="notesList__header">Your Notes:</h1>
+      <CSVLink
+        data={props.notes}
+        filename={'my-notes.csv'}
+        className="noteList__download"
+      >
+        download
+      </CSVLink>
+      <div className="noteList__sort-options">
+        <div className="noteList__sort-title">sort by:</div>
+        <div
+          onClick={() => props.sortNotes('date')}
+          className="noteList__sort-option"
         >
-          download
-        </CSVLink>
-        <div className="noteList__sort-options">
-          <div className="noteList__sort-title">sort by:</div>
-          <div
-            onClick={() => this.props.sortNotes('date')}
-            className="noteList__sort-option"
-          >
-            date
-          </div>
-          <div
-            onClick={() => this.props.sortNotes('title')}
-            className="noteList__sort-option"
-          >
-            name
-          </div>
+          date
         </div>
-        <div className="noteList">
-          {this.props.notes.map(note => {
-            return (
-              <div key={note._id} className="notelist__item">
-                <Link
-                  to={`/view/${note._id}`}
-                  onClick={() => this.props.selectNote(note._id)}
-                >
-                  <NoteThumb note={note} />
-                </Link>
-              </div>
-            );
-          })}
+        <div
+          onClick={() => props.sortNotes('title')}
+          className="noteList__sort-option"
+        >
+          name
         </div>
       </div>
-    );
-  }
-}
+      <div className="noteList">
+        {props.notes.map(note => {
+          return (
+            <div key={note._id} className="notelist__item">
+              <Link
+                to={`/view/${note._id}`}
+                onClick={() => props.selectNote(note._id)}
+              >
+                <NoteThumb note={note} />
+              </Link>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
 const mapStateToProps = state => {
   return {
@@ -63,4 +57,6 @@ const mapStateToProps = state => {
   };
 };
 
-export default connect(mapStateToProps, { selectNote, sortNotes, fetchNotes })(NoteList);
+export default connect(mapStateToProps, { selectNote, sortNotes, fetchNotes })(
+  NoteList
+);
