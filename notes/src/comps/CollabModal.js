3da73859dfe6cb3c9ef108ca0f80d9d1c0240263
Dani Tacheny <dani.tacheny@gmail.com>
2018-04-06 12:59:34 -0500

    remove Link from CollabModal.js

--- notes/src/comps/CollabModal.js
+++ notes/src/comps/CollabModal.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
 import { toggleCollabModal, addNote } from '../actions';
 import '../styles/CollabModal.css';
 
