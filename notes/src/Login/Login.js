4e104fdfa9c26790295d59c6b514015270fee502
Tyson Williams <tyson.j.williams@gmail.com>
2018-04-06 14:18:47 -0600

    Add login page
    User can successfully login

---
+++ notes/src/Login/Login.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+//import { connect } from 'react-redux';
+import axios from 'axios';
+import './Login.css';
+
+class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      username: '',
+      password: ''
+    }
+  }
+
+  handleUserInput = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  LoginUser = () => {
+    const { username, password } = this.state;
+    const LoginUser = { username, password };
+    console.log(LoginUser);
+    axios
+      .post('http://localhost:5000/login', LoginUser)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.error('There was an error logging in', err);
+      });
+  };
+
+  render() {
+    return (
+      <div className="Login">
+        <header className="Login-header">
+          <h1 className="Login-title">Login to Lambda Notes!</h1>
+        </header>
+        <form>
+          <input
+            onChange={this.handleUserInput}
+            name="username"
+            type="text"
+            placeholder="username"
+          />
+          <input
+            onChange={this.handleUserInput}
+            name="password"
+            type="text"
+            placeholder="password"
+          />
+          <button type="button" onClick={ () => this.LoginUser() }>
+            Login
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Login;
