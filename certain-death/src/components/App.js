26e4a6d8a19f4cadf38b4bd9182e0de4e6b4317a
Jonathan Matthew Brunt <jbrunt.mobile@gmail.com>
2018-04-06 00:00:14 -0500

    404 Handling
    - in App.js:
    - import and use Switch to wrap routes
    - provide NotFound route as fallback
    - in notfound.js:
    - update inline styling of component

--- certain-death/src/components/App.js
+++ certain-death/src/components/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import styled from 'styled-components';
 import UserPage from './Pages/userpage';
 import List from './Pages/list';
 import NewNote from './Pages/newnote';
 import FullNote from './Pages/fullnote';
 import UpdateNote from './Pages/updatenote';
+import NotFound from './Modals/notfound';
 
 const StyledApp = styled.div`
   width: 830px;
@@ -19,11 +20,14 @@ class App extends Component {
     return (
       <Router basename={process.env.PUBLIC_URL}>
         <StyledApp>
-          <Route path='/' component={UserPage} exact />
-          <Route path='/list' component={List} />
-          <Route path='/fullnote/:id' component={FullNote} />
-          <Route path='/newnote' component={NewNote} />
-          <Route path='/updatenote/:id' component={UpdateNote} />
+          <Switch>
+            <Route path='/' component={UserPage} exact />
+            <Route path='/list' component={List} />
+            <Route path='/fullnote/:id' component={FullNote} />
+            <Route path='/newnote' component={NewNote} />
+            <Route path='/updatenote/:id' component={UpdateNote} />
+            <Route component = {NotFound} />
+          </Switch>
         </StyledApp>
       </Router>
     );
--- certain-death/src/components/Modals/notfound.js
+++ certain-death/src/components/Modals/notfound.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 const NotFound = () => (
-  <div style={{ padding: '10px' }}>
+  <div style={{ padding: '10px', backgroundColor: 'white', textAlign: 'center' }}>
     <h3 style={{ fontSize: '4rem' }}>404 page not found</h3>
-    <p style={{ fontSize: '2rem' }}>We are sorry but the page you are looking for does not exist.</p>
+    <p style={{ fontSize: '2rem', marginTop: '20px' }}>We are sorry but the page you are looking for does not exist.</p>
   </div>
 );
 
