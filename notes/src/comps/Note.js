438a7e9fb6afe4ce2e2fccbe7e172dc0dc77d633
Dani Tacheny <dani.tacheny@gmail.com>
2018-04-06 11:12:59 -0500

    add note cloning

--- notes/src/comps/Note.js
+++ notes/src/comps/Note.js
@@ -1,25 +1,37 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { toggleModal } from '../actions';
+import { toggleModal, addNote } from '../actions';
 import '../styles/Note.css';
 
-const Note = props => {
-  return (
-    <div className="note__container">
-      <div className="note__options">
-        <Link to={`/edit/${props.selectedNote._id}`}>
-          <div className="note__option-item">edit</div>
-        </Link>
-        <div className="note__option-item" onClick={props.toggleModal}>delete</div>
+class Note extends React.Component {
+  cloneNote = (event) => {
+    const { title, content } = this.props.selectedNote;
+    this.props.addNote({ title, content });
+  }
+  
+  render() {
+    return (
+      <div className="note__container">
+        <div className="note__options">
+          <Link to={`/edit/${this.props.selectedNote._id}`}>
+            <div className="note__option-item">edit</div>
+          </Link>
+          <Link to={'/'} onClick={this.cloneNote} >
+            <div className="note__option-item">copy</div>
+          </Link>
+          <div className="note__option-item" onClick={this.props.toggleModal}>
+            delete
+          </div>
+        </div>
+        <div className="note">
+          <div className="note__title">{this.props.selectedNote.title}</div>
+          <div className="note__content">{this.props.selectedNote.content}</div>
+        </div>
       </div>
-      <div className="note">
-        <div className="note__title">{props.selectedNote.title}</div>
-        <div className="note__content">{props.selectedNote.content}</div>
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 const mapStateToProps = state => {
   return {
@@ -27,4 +39,4 @@ const mapStateToProps = state => {
   };
 };
 
-export default connect(mapStateToProps, { toggleModal })(Note);
+export default connect(mapStateToProps, { toggleModal, addNote })(Note);
