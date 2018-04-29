585b803382770c403b73adc630e6a6dd0c11db85
AJ Genung <DragonRydrz@me.com>
2018-04-29 13:39:07 -0400

    Server running locally, Router files set up.

---
+++ server/Note/newNoteRouter.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const router = express.Router();
+const Note = require('./Note');
+
+module.exports = router;
---
+++ server/User/registerRouter.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const router = express.Router();
+const User = require('./User');
+
+module.exports = router;
