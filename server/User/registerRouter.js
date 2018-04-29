1e15afb6e92458227d00c7d403b6a57c7cc48938
AJ Genung <DragonRydrz@me.com>
2018-04-29 14:04:02 -0400

    create new user working!  =D

--- server/User/registerRouter.js
+++ server/User/registerRouter.js
@@ -2,6 +2,17 @@ const express = require('express');
 const router = express.Router();
 const User = require('./User');
 
-router.post('/', (res, req) => {});
+router.post('/', (req, res) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    res.statusCode(422).json({ message: 'Username and Password are required' });
+  } else {
+    const newUser = new User({ username, password });
+    newUser.save().then(response => {
+      console.log(response);
+      res.json({ message: 'User Created!  Please login!' });
+    });
+  }
+});
 
 module.exports = router;
