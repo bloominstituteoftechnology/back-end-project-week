fb3464164ed43c5b291c7ffd043ba1f48f2dabb7
AJ Genung <DragonRydrz@me.com>
2018-04-29 14:24:54 -0400

    login verifying.  need to create JWT and send correct data.

--- server/User/loginRouter.js
+++ server/User/loginRouter.js
@@ -2,4 +2,24 @@ const express = require('express');
 const router = express.Router();
 const User = require('./User');
 
+router.post('/', (req, res) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    res.status(422).json({ message: 'Username and password is required' });
+  } else {
+    User.findOne({ username }, (err, user) => {
+      console.log(user, 'user');
+      if (err || user === null)
+        res.status(422).json({ message: 'first Invalid credentials' });
+      user.checkPassword(password, (err, valid) => {
+        if (valid) {
+          res.json({ message: 'Loggin Success' });
+        } else {
+          res.status(422).json({ message: 'Invalid credentials' });
+        }
+      });
+    });
+  }
+});
+
 module.exports = router;
