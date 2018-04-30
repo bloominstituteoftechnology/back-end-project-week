86cf5bfe0eae98acf89b58b90348469e02adfd41
Kaitlyn Schorr <kaitlynschorr@gmail.com>
2018-04-30 11:23:30 -0500

    Added express, helmet, mongoose, and morgan.

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const helmet = require("helmet");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+
+mongoose
+  .connect("mongodb://localhost/budgetdb")
+  .then(() => {
+    console.log("Connected to Mongo.");
+  })
+  .catch(err => {
+    console.log("Error connecting to the database");
+  });
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+
+const port = process.env.PORT || 5000;
+server.listen(port, () => {
+  console.log(`Server up and running on ${port}`);
+});
