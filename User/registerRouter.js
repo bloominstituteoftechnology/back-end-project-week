e3b2bf9c3e250bceff59c68c2c07a9022671f974
AJ Genung <DragonRydrz@me.com>
2018-04-30 13:45:21 -0400

    added catch for register errors

--- User/registerRouter.js
+++ User/registerRouter.js
@@ -8,10 +8,15 @@ router.post('/', (req, res) => {
     res.statusCode(422).json({ message: 'Username and Password are required' });
   } else {
     const newUser = new User({ username, password });
-    newUser.save().then(response => {
-      console.log(response);
-      res.json({ message: 'User Created!  Please login!' });
-    });
+    newUser
+      .save()
+      .then(response => {
+        console.log(response);
+        res.json({ message: 'User Created!  Please login!' });
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      });
   }
 });
 
