34f8af4ef2d7c684883b4a5722988b7967be9db7
zackhitch <zackhitch@users.noreply.github.com>
2018-04-30 16:19:04 -0700

    updated to more descriptive variable names - ZH

--- api/Controllers/noteEdit.js
+++ api/Controllers/noteEdit.js
@@ -3,9 +3,9 @@ const Note = require('../Models/Note');
 const noteEdit = (req, res) => {
   const { title, content } = req.body;
   const author = req.decoded.username;
-  Note.findByIdAndUpdate(req.params.id, { title, content })
-    .then(note => {
-      res.status(200).json(note);
+  Note.findByIdAndUpdate(req.params.id, { title, content }, { new: true })
+    .then(updatedNote => {
+      res.status(200).json(updatedNote);
     })
     .catch(err => {
       res.status(500).json({ Error: `Unable to edit note: ${err}` });
