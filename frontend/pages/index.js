52d36485f3c183bd1afac10dc03f66326bc89918
Brett Sullivan <opticalfalicy@gmail.com>
2018-04-30 16:27:10 -0500

    Frontend deployed

--- frontend/pages/index.js
+++
@@ -1,9 +0,0 @@
-import EditNote from './edit';
-import Front from './front-page';
-import NewNote from './new';
-import SingleNote from './singular';
-
-export { EditNote };
-export { Front };
-export { NewNote };
-export { SingleNote };
