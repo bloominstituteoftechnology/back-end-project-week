52b2d22a70c0a639423a213a4a339c4241f91ad6
AJ Genung <DragonRydrz@me.com>
2018-04-30 12:28:20 -0400

    changed file structure

--- server/secrets/config.js
+++
@@ -1,8 +0,0 @@
-const config = {
-  database:
-    'mongodb://lambdanotes1:lambdanotes1@ds261429.mlab.com:61429/lambda-notes',
-  secret: 'Bend the knee!',
-  salt: "Dragons are a real man's best friend",
-};
-
-module.exports = config;
