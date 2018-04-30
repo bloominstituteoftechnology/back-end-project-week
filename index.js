6d1d975427a83481802cb114f226cd036d64225f
Justin Kaseman <justinkaseman@live.com>
2018-04-30 09:59:44 -0700

    mLab setup

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const mongoose = require('mongoose');
+const helmet = require('helmet');
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const server = express();
+
+const path = process.env.MONGOLAB_URI || 'mongodb://localhost/notes';
+mongoose.connect(path);
+
+server.use(helmet());
+server.use(express.json());
+
+server.get('/', (req, res) => res.send('API Running...'));
+
+const PORT = process.env.PORT || 5000;
+express
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
