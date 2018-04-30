b92156f89c287d17bc8bad9a132f9f44ae47a45a
Omoniyi Alimi <omoniyialimi@Omoniyis-MacBook-Pro.local>
2018-04-30 19:06:09 -0400

    my server

---
+++ User/server.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const cors = require('cors');
+const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+const server = express();
+
+mongoose
+.connect('mongodb://localhost/back-end')
+.then(cnn => {
+    console.log('\n=== connected to mongo ===\n');
+})
+.catch(err => {
+    console.log('\n=== not connected to mongo\n');
+})
+
+server.get('/', function (req,res){
+    res.send({api: 'up and running'});
+});
+
+server.listen(5000, () => console.log('\n=== API on port 5k ===\n'));
