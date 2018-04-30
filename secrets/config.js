0a05c7f8a9762178afbca21d9c27cdec58bed482
AJ Genung <DragonRydrz@me.com>
2018-04-30 12:40:32 -0400

    revelaed secrets

---
+++ secrets/config.js
@@ -0,0 +1,8 @@
+const config = {
+  database:
+    'mongodb://lambdanotes1:lambdanotes1@ds261429.mlab.com:61429/lambda-notes',
+  secret: 'Bend the knee!',
+  salt: "Dragons are a real man's best friend",
+};
+
+module.exports = config;
