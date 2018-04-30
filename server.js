c6b6e9a8d8e50f49584d3553a3edef4ccac508d1
mjd02 <mostdarryl@gmail.com>
2018-04-30 13:18:50 -0400

    created index page

--- server.js
+++ server.js
@@ -8,5 +8,9 @@ mongoose.connect('mongodb://localhost/backend')
 .catch(err=>{console.log('not connected')}
 );
 
+server.get('/', (req,res) => {
+  res.status(200).json({api:'running'});
+});
+
 const port = process.env.PORT || 5000;
 server.listen(port, ()=>console.log(`\n=== API up on port: ${port} ===\n`));
