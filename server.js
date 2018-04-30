59bf82ed2322142974b001ed615d38a60cb02024
Nick Coultas <nncoultas@yahoo.com>
2018-04-30 10:28:46 -0700

    server running locally

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+
+const server = express();
+
+
+server.use(morgan('dev'));
+server.use(express.json());
+
+module.exports = server;
