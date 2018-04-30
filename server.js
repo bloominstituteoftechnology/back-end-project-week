d4c7d4c6db7eae5cd0bcae640a7410a6c033d842
Ajmal Jalal <ajmaljalal58@gmail.com>
2018-04-30 15:00:23 -0700

    bug resolved

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const helmet = require('helmet');
-const cors = require('cors');
+//const helmet = require('helmet');
+//const cors = require('cors');
 //const mongoose = require('mongoose');
 
 /*mongoose
@@ -17,8 +17,8 @@ const friendsController = require('./friends/friendsController');
 
 const server = express();
 
-server.use(helmet());
-server.use(cors());
+//server.use(helmet());
+//server.use(cors());
 server.use(express.json());
 
 server.get('/', (req, res) => {
