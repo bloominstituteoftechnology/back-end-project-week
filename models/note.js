4d917aba827a3f67c43a8502be08dac99fb1a13e
Megan Williamson <meganawilliamson@gmail.com>
2018-04-30 15:48:58 -0700

    adding endpoints to test app deployment

---
+++ models/note.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const noteSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+  },
+});
+
+const Note = mongoose.model('Note', noteSchema);
+
+module.exports = Note;
--- server.js
+++ server.js
@@ -1,11 +1,54 @@
 const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+require('dotenv').config();
+
+const Note = require('./models/note');
+
 const server = express();
 
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('\n==== Connected to Mongo ====\n');
+  })
+  .catch(() => {
+    console.log('\n++++ ERROR connecting to Mongo ++++\n');
+  });
+
+server.use(morgan('combined'));
+server.use(cors());
 server.use(express.json());
 
-server.get('/', (req, res) => {
-  res.status(200).json({ api: 'Running...' });
+// Controllers
+// sanity check
+server.get('/api/notes', (req, res) => {
+  Note.find({})
+    .then(notes => {
+      res.json(notes);
+    })
+    .catch(err => {
+      res.json({ err: err.message });
+    });
 });
+
+// POST
+server.post('/api/notes', (req, res) => {
+  const note = new Note(req.body);
+
+  note
+    .save()
+    .then(savedNote => {
+      res.json(savedNote);
+    })
+    .catch(err => {
+      console.log(err.message);
+    });
+});
+// GET
+// DELETE
+// PUT
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
