e2679021a30e1ad8cc980a599e3cae80e6a4d0b2
Cristina Laryea <crispad06@gmail.com>
2018-04-30 17:34:00 -0400

    added server code and server running

---
+++ app.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const server = require('./server');
+const port = process.env.Port || 5000;
+
+mongoose.connect('mongodb://localhost/backEnd', {}, err => {
+    if(err) return console.log(err);
+    console.log('Connected to Mongo');
+});
+
+server.listen(port, err => {
+    if(err) console.log(err);
+    console.log(`Server running on ${port}`)
+})
