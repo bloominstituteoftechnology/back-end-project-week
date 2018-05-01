8a4d5e2a6716defd25eab1f726749781a9b611f6
astro11 <aquila.strother@asu.edu>
2018-05-01 15:39:49 -0400

    deleted model conditions

--- users/userModel.js
+++ users/userModel.js
@@ -4,13 +4,9 @@ const ObjectId = mongoose.Schema.Types.ObjectId;
 const User = new mongoose.Schema({
     username: {
         type: String,
-        required: true,
-        unique: true,
-        lowercase: true,
     },
     password: {
         type: String,
-        required: true,
     },
 });
 
