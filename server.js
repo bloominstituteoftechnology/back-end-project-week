12d4f508239b3a3fc6a197a572da57fee278c237
Kaitlyn Schorr <kaitlynschorr@gmail.com>
2018-05-01 11:35:38 -0500

    Linked user controller to server

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const helmet = require("helmet");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
 const noteController = require("./notes/notesController");
+const userController = require("./user/userController");
 const cors = require("cors");
 
 mongoose
@@ -21,6 +22,7 @@ server.use(cors({ origin: "https://kaits-lambda-notes.netlify.com" }));
 server.use(morgan("combined"));
 server.use(express.json());
 server.use("/api/notes", noteController);
+server.use("/api/user", userController);
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
