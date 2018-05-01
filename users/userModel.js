1a55b3842981b1d3ab3c39ee53cf48e4a54ea883
astro11 <aquila.strother@asu.edu>
2018-05-01 16:53:29 -0400

    testing model condition

--- users/userModel.js
+++ users/userModel.js
@@ -4,6 +4,7 @@ const ObjectId = mongoose.Schema.Types.ObjectId;
 const User = new mongoose.Schema({
     username: {
         type: String,
+        required: true,
     },
     password: {
         type: String,
