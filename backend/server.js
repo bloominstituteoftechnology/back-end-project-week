f17d8559905e0beace5195945fd41200438713ed
Brett Sullivan <opticalfalicy@gmail.com>
2018-05-01 11:33:34 -0500

    Route test

--- backend/server.js
+++ backend/server.js
@@ -13,8 +13,15 @@ mongoose.connect('mongodb://heroku_20g8ndz2:5er5o5d19qcdnj3v92fegvvh6m@ds111050.
     console.log('Mango Up Bruh');
 })
 
+server.get('/', (req, res) => {
+    res.status(200).json({api: 'harambe'})
+})
+
+
 server.listen(port, err => {
     if (err) console.log(err);
     console.log(`Mangos Grown at ${port}`)
 })
+
+
 
