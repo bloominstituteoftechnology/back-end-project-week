04db512fc05c830959bbb8ecd395c2f9b7ce4f61
arcadia-ego <arcadia-ego@github.com>
2018-05-01 11:17:26 -0700

    user.js error

--- User.js
+++ User.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 
-const userSchema = new Mongoose.Schema({
+const userSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
