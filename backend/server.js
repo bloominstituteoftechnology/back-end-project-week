f73071e2634db23fa655292fad9ba3a583442dc2
Brett Sullivan <opticalfalicy@gmail.com>
2018-05-01 18:44:37 -0500

    Working on implementing put and delete routes

--- backend/server.js
+++ backend/server.js
@@ -55,6 +55,37 @@ server.post('/api/notes/new', (req, res) => {
     });
 })
 
+server.put('/api/notes/:_id', (req, res) => {
+    // const { _id, title, content } = req.body;
+    // const id = { _id };
+    // if(!id) {
+    //     return res.status(422).json({error: 'Must provide a valid ID.'});
+    // }
+    // Note.findById(id, (err, note) => {
+    //     if(err || note === null) {
+    //         res.status(422).json({error: 'Cannot find a note with that ID.'});
+    //         return;
+    //     }
+    //     note.title = title
+    //     note.content = content
+    //     .save((saveErr, savedNote) => {
+    //         if(err || note === null) {
+    //             res.status(500);
+    //             res.json({ error:'Something really bad happened.'})
+    //             return;
+    //         }
+    //         res.json(note);
+    //     });
+    // });
+
+    Note.findByIdAndUpdate(req.params._id, req.body)
+    .then
+});
+
+
+server.delete('/api/note/:_id', (req, res) => {
+    
+})
 
 // New User Routes //
 
