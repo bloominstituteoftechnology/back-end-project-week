2d57670438206c82e48c9517372a5ff184a3ff64
Kevin Jolley <serrowpls@gmail.com>
2018-05-01 14:53:09 -0500

    Heroku Deploy

--- server.js
+++ server.js
@@ -1,1 +1,31 @@
 const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+// Server
+const server = express();
+
+// Middelware
+server.use(express.json());
+server.use(morgan());
+server.use(helmet());
+server.use(cors());
+server.use(logger);
+
+// Logger
+const logger = (req, res, next) => {
+  console.log('d-(OvO")z looks correct to me', req.body);
+
+  next();
+};
+
+// Server Code
+server.get('/', (req, res) => {
+  // API Check
+  res.json({ api: 'Running..' });
+});
+
+// Port
+const port = server.listen(port, () => console.log('API Running on Heroku'));
