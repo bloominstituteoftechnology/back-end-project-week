82a81d66ccee2f39a4861b34cba684cede86da34
RiaClay <sherria.rydyn@gmail.com>
2018-05-01 11:34:29 -0500

    corrected folders for routes and models. Fixed server.js file based on these changes.

--- server.js
+++ server.js
@@ -1,27 +1,29 @@
 const express = require('express');
 const helmet = require('helmet');
 const mongoose = require('mongoose');
+const cors = require('cors');
+
+const routes = require('./routes/appRoutes');
 
 mongoose
     .connect('mongodb://localhost/lambdanotesdb')
     .then(() => console.log('\n=== Connected to the Lambda Notes database ===\n'))
     .catch(() => console.log('\n=== Error connecting to the database ===\n'))
 
-const noteRouter = require('./notes/noteRouter');
-const userRouter = require('./users/userRouter');
 
 const server = express();
+const connectFrontEnd = {};
 
 server.use(helmet());
 server.use(express.json());
+server.use(cors());
+
+routes(server);
 
 server
     .get('/', (req, res) => {
         res.status(200).json({ api: '\n=== Connected to Server...\n'});
     });
-
-server.use('/api/users', userRouter);
-server.use('/api/notes', noteRouter);
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
