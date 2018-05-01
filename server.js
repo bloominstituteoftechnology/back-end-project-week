11893721cc566cf3ee67d9c09112b949739c80dd
Edward Manda <lilkups@yahoo.com>
2018-05-01 09:25:19 -0500

    lets improve on

--- server.js
+++ server.js
@@ -1,9 +1,49 @@
 const express = require('express');
+const mongoose = require('mongoose'); // needs to go into model
 
 const server = express();
 
-server.get('/', (req, res) => {
-  res.json('API is running ok ok!');
+// will go into model, also add a note author
+const noteSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+      uppercase: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Note = mongoose.model('Note', noteSchema);
+server.get('/', (req, res, next) => {
+  Note.find({})
+    .then(notes => {
+      console.log(notes);
+      //res.status(200).json('API is running ok ok!', notes);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+server.get('/post', (req, res, next) => {
+  const newNote = new Note({ title: 'debugger', content: 'just work please!' });
+  newNote
+    .save()
+    .then(response => {
+      res.status(200).json(response);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
 });
 
 module.exports = server;
