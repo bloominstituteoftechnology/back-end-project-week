54db664838954571761b74444270e178b424fe4e
Kenny <khpham24@gmail.com>
2018-05-01 16:40:08 -0600

    added some dependencies, deployed to heroku and mlab

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const helmet = require('helmet');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://dbh61.mlab.com:27617/lambdanotes', {}, err => {
+  if (err) return console.log(err);
+  console.log('\n===Connected to MLAB database===\n');
+});
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+
+server.get('/', (req, res) => res.send('API Running...'));
+
+const port = process.env.PORT || 5000;
+server.listen(port, () =>
+  console.log(`\n===API running on http://localhost:${port}===\n`)
+);
