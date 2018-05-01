6a325dfed4221048c3970a61e0d6c22ccaf2743c
AJ Genung <DragonRydrz@me.com>
2018-05-01 10:42:40 -0400

    authentication/protected middleware built

--- User/loginRouter.js
+++ User/loginRouter.js
@@ -2,26 +2,32 @@ const express = require('express');
 const router = express.Router();
 const User = require('./User');
 const { makeToken } = require('./auth');
+const { authenticate } = require('./security');
 
-router.post('/', (req, res) => {
-  const { username, password } = req.body;
-  if (!username || !password) {
-    res.status(422).json({ message: 'Username and password is required' });
-  } else {
-    User.findOne({ username }, (err, user) => {
-      if (err || user === null)
-        res.status(422).json({ message: 'first Invalid credentials' });
-      user.checkPassword(password, (err, valid) => {
-        if (valid) {
-          const token = makeToken(user);
-          console.log(token);
-          res.json({ token, username: user.username, notes: user.notes });
-        } else {
-          res.status(422).json({ message: 'Invalid credentials' });
-        }
-      });
-    });
-  }
+router.post('/', authenticate, (req, res) => {
+  // const { username, password } = req.body;
+  // if (!username || !password) {
+  //   res.status(422).json({ message: 'Username and password is required' });
+  // } else {
+  //   User.findOne({ username }, (err, user) => {
+  //     if (err || user === null)
+  //       res.status(422).json({ message: 'first Invalid credentials' });
+  //     user.checkPassword(password, (err, valid) => {
+  //       if (valid) {
+  //         const token = makeToken(user);
+  //         console.log(token);
+  //         res.json({ token, username: user.username, notes: user.notes });
+  //       } else {
+  //         res.status(422).json({ message: 'Invalid credentials' });
+  //       }
+  //     });
+  //   });
+  // }
+  console.log(req.user);
+  const { _id, username, notes } = req.user;
+  const user = { id: _id, username };
+  const token = makeToken(user);
+  res.json({ token, user: req.user });
 });
 
 module.exports = router;
---
+++ User/security.js
@@ -0,0 +1,57 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./User');
+const { ExtractJwt } = require('passport-jwt');
+const JwtStrategy = require('passport-jwt').Strategy;
+const jwt = require('jsonwebtoken');
+const { secret } = require('../secrets/config');
+
+const localStrategy = new LocalStrategy(function(username, password, done) {
+  User.findOne({ username }, function(err, user) {
+    if (err) {
+      return done(err);
+    }
+    if (!user) {
+      return done(null, false);
+    }
+    user.checkPassword(password, (err, valid) => {
+      if (err) {
+        return done(err);
+      }
+
+      if (valid) {
+        const { _id, username, notes } = user;
+        return done(null, { _id, username, notes });
+      }
+      return done(null, false);
+    });
+  });
+});
+
+const jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: secret,
+};
+
+const jwtStrategy = new JwtStrategy(jwtOptions, function(payload, done) {
+  User.findById(payload.sub)
+    .select('-password')
+    .then(user => {
+      if (user) {
+        done(null, user);
+      } else {
+        done(null, false);
+      }
+    })
+    .catch(err => {
+      return done(err, false);
+    });
+});
+
+passport.use(localStrategy);
+passport.use(jwtStrategy);
+
+const authenticate = passport.authenticate('local', { session: false });
+const restricted = passport.authenticate('jwt', { session: false });
+
+module.exports = { authenticate, restricted };
