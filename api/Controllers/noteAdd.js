a696da78dd5f2b55026c14a48fcfc5b96956d127
zackhitch <zackhitch@users.noreply.github.com>
2018-05-01 14:01:34 -0700

    changes in attempt to get auth working

--- api/Controllers/noteAdd.js
+++ api/Controllers/noteAdd.js
@@ -3,30 +3,35 @@ const Note = require('../Models/Note');
 
 const noteAdd = (req, res) => {
   const { title, content } = req.body;
-  const username = req.username;
+  const author = req.username;
   let id;
 
-  User.findOne({ username })
-    .then(user => {
-      id = user._id;
-    })
-    .catch(err => {
-      res.json({ Error: `Unable to find user ${err}` });
-    });
-
-  if (author) {
-    const newNote = new Note({ id, title, content });
-    newNote
-      .save()
-      .then(savedNote => {
-        res.status(200).json({ Message: 'Note successfully saved!' });
+  const findUser = username => {
+    User.findOne({ username })
+      .then(user => {
+        id = user._id;
       })
       .catch(err => {
-        res.status(500).json({ Error: `Unable to save new note: ${err}` });
+        res.json({ Error: `Unable to find user ${err}` });
       });
-  } else {
-    res.status(422).json({ Error: `User error: ${err}` });
-  }
+  };
+
+  const saveNote = async () => {
+    await findUser(author);
+    if (author) {
+      const newNote = new Note({ id, title, content });
+      newNote
+        .save()
+        .then(savedNote => {
+          res.status(200).json({ Message: 'Note successfully saved!' });
+        })
+        .catch(err => {
+          res.status(500).json({ Error: `Unable to save new note: ${err}` });
+        });
+    } else {
+      res.status(422).json({ Error: `User error: ${err}` });
+    }
+  };
 };
 
 module.exports = noteAdd;
