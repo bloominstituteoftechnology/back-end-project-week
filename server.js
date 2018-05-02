d751e82ae7803859ceb196b4abb3ca7e0e64db1c
Walter Woodward <walter.sekope.woodward@gmail.com>
2018-05-02 18:01:13 -0400

    dependencies added and mLab cors morgan helmet mongoose attempting to connect to mLab whilest also not breaking Heroku dummyData from Front End inside [GET] /

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const mongoose = require('mongoose');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 5000
+
+mongoose
+  .connect('mongodb://pacManKana:LambdaN0t3s>@ds111050.mlab.com:11050/lambda-notes')
+  .then(cnn => {
+    console.log('\n=== connected to mongo ===\n');
+  })
+  .catch(err => {
+    console.log('\n=== ERROR connecting to mongo ===\n');
+  });
 
 const server = express();
 
