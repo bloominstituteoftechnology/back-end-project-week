3b652dd88e4d6127408cd6aa87c921a68823a907
AJ Genung <DragonRydrz@me.com>
2018-05-02 17:54:40 -0400

    removed cors origin option

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const server = express();
 server.use(morgan('dev'));
 server.use(express.json());
 server.use(helmet());
-server.use(cors({ origin: 'http://localhost:3000' }));
+server.use(cors());
 
 server.get('/', (req, res) => {
   res.json({ message: 'API running successfully! YAY!' });
