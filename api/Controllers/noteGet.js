d0297de55e99915bc133261745d4a93c751c742b
zackhitch <zackhitch@users.noreply.github.com>
2018-05-02 13:45:42 -0700

    testing populate

--- api/Controllers/noteGet.js
+++ api/Controllers/noteGet.js
@@ -7,6 +7,7 @@ const noteGet = (req, res) => {
     const id = req.params.id;
     console.log(req.body);
     User.findById(id)
+      .populate('note')
       .then(user => {
         console.log(user);
         res.status(200).json(user.notes);
