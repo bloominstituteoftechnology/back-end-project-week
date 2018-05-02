db02ae7fc0e37a73c3c235a40ab885636228f4fc
Walter Woodward <walter.sekope.woodward@gmail.com>
2018-05-02 11:00:57 -0400

    began to write server post in route.js

--- server/setup/routes.js
+++ server/setup/routes.js
@@ -1,5 +1,11 @@
 module.exports = function(server) {
+
   server.get('/', function(req, res) {
     res.send({ api: 'up and running' });
   });
+
+  server.post('/api/register', function(req, res) {
+      const credentials = req.body;
+      res.send({api: 'up and running'})
+  })
 };
