d58f77ef937a6ef859b27d81028b22d0fb69e90b
Walter Woodward <walter.sekope.woodward@gmail.com>
2018-05-02 18:11:23 -0400

    all middleware .use removed trying to get Heroku deploy working again

--- server.js
+++ server.js
@@ -6,8 +6,6 @@ const cors = require('cors');
 
 const PORT = process.env.PORT || 5000
 
-server.use(express.json());
-
 mongoose
   .connect('mongodb://pacManKana:LambdaN0t3s>@ds111050.mlab.com:11050/lambda-notes')
   .then(cnn => {
