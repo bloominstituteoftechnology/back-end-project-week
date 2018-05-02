568dc7bf5719da24b38a3bbcdc113bfbaa8f5c83
Johnathan Huggett <johnathanhuggett@Johnathans-MacBook-Pro.local>
2018-05-02 19:17:47 -0400

    Johnathan Huggett, user and note routes complete

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require("express");
-const mongoose = require("mongoose"); // needs to go into model
+const mongoose = require("mongoose");
+const cors = require("cors");
+const morgan = require("morgan");
+const helmet = require("helmet");
 
 const server = express();
-server.use(express.json());
 
-// morgan
-// cors
-// helmet
+server.use(express.json());
+server.use(cors());
+server.use(helmet());
+server.use(morgan("dev"));
 
 const routes = require("./api/routes");
 
