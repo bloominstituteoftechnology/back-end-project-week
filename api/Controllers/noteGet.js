8c5db5f3d1c8d9a6bf29dd729970cb016dd6eb02
zackhitch <zackhitch@users.noreply.github.com>
2018-05-02 10:32:23 -0700

    update noteGet to find user and return list of notes

--- api/Controllers/noteGet.js
+++ api/Controllers/noteGet.js
@@ -1,15 +1,12 @@
 const Note = require('../Models/Note');
+const User = require('../Models/User');
 
 const noteGet = (req, res) => {
   const author = req.body.username;
   const id = req.body.id;
-  Notes.find({ id })
-    .then(userNotes => {
-      if (userNotes === null) {
-        res.status(200).json({ Message: 'User has no notes!' });
-      } else {
-        res.status(200).json(userNotes);
-      }
+  User.findById({ id })
+    .then(user => {
+      res.status(200).json(user.notes);
     })
     .catch(err => {
       res.status(500).json({ Error: `Unable to get notes: ${err}` });
