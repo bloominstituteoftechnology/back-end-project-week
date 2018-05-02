33296626ad3d6998c8dae0db61a46769c09f8b4a
astro11 <aquila.strother@asu.edu>
2018-05-02 15:27:06 -0400

    changing routes

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(express.static(__dirname + '/public'));
 app.use(helmet());
 app.use(express.json());
 app.use(cors());
-app.use('/api/users', userController);
+app.use('/api/register', userController);
 
 
 // set the home page route
