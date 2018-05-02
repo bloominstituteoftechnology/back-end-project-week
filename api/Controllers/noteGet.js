589c9d70df7560c95e957208009ed2aae05a7697
zackhitch <zackhitch@users.noreply.github.com>
2018-05-02 11:06:42 -0700

    added req param support for get

--- api/Controllers/noteGet.js
+++ api/Controllers/noteGet.js
@@ -4,18 +4,19 @@ const User = require('../Models/User');
 const noteGet = (req, res) => {
   // const author = req.body.username;
   if (req.params.id) {
-  const id = req.params.id;
-  console.log(req.body);
-  User.findById({ id })
-    .then(user => {
-      console.log(user);
-      res.status(200).json(user.notes);
-    })
-    .catch(err => {
-      res.status(500).json({ Error: `Unable to get notes: ${err}` });
-    });
+    const id = req.params.id;
+    console.log(req.body);
+    User.findById({ id })
+      .then(user => {
+        console.log(user);
+        res.status(200).json(user.notes);
+      })
+      .catch(err => {
+        res.status(500).json({ Error: `Unable to get notes: ${err}` });
+      });
   } else {
     console.log('no id');
   }
+};
 
 module.exports = noteGet;
