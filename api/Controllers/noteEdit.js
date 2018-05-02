07c55924966106e1ef54cdf0aba16e9877521a2e
zackhitch <zackhitch@users.noreply.github.com>
2018-05-02 15:12:24 -0700

    removed unused variable

--- api/Controllers/noteEdit.js
+++ api/Controllers/noteEdit.js
@@ -2,7 +2,7 @@ const Note = require('../Models/Note');
 
 const noteEdit = (req, res) => {
   const { title, content } = req.body;
-  const author = req.decoded.username;
+  // const author = req.decoded.username;
   Note.findByIdAndUpdate(req.params.id, { title, content }, { new: true })
     .then(updatedNote => {
       res.status(200).json(updatedNote);
