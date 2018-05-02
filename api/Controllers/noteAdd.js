b2d90934e6d32b4df8ad6427dc22a604666de144
zackhitch <zackhitch@users.noreply.github.com>
2018-05-01 17:56:13 -0700

    noteAdd changes

--- api/Controllers/noteAdd.js
+++ api/Controllers/noteAdd.js
@@ -6,17 +6,6 @@ const noteAdd = (req, res) => {
   const author = req.body.username;
   let id;
 
-  // const findUser = username => {
-  //   User.findOne({ username: req.body.username })
-  //     .then(user => {
-  //       id = user._id;
-  //       console.log(`===USER ID: ===`, id);
-  //     })
-  //     .catch(err => {
-  //       res.json({ Error: `Unable to find user ${err}` });
-  //     });
-  // };
-
   const saveNote = () => {
     // await findUser(author);
     User.findOne({ username: req.body.username })
@@ -61,11 +50,6 @@ const noteAdd = (req, res) => {
   };
 
   saveNote();
-  // try {
-  //   saveNote();
-  // } catch (err) {
-  //   console.log(err);
-  // }
 };
 
 module.exports = noteAdd;
