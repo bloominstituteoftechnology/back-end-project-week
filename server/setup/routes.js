107417ca5444898ca5737392f4ebeffc8227e876
Walter Woodward <walter.sekope.woodward@gmail.com>
2018-05-02 11:51:37 -0400

    `register` complete
    added jsonwebtoken dependency to server
    token up and server running
    scrapped idea of separating makeToken and bycrypt into their own files

--- server/setup/routes.js
+++ server/setup/routes.js
@@ -1,16 +1,29 @@
-const User = require('./users/User')
+const User = require('./users/User');
+const jwt = require('jsonwebtoken');
+
+function makeToken(user) {
+  const timestamp = new Date().getTime();
+  const payload = {
+    sub: user._id,
+    email: user.email,
+    iat: timestamp
+  };
+  const secret = 'I love Sci Fi';
+  const options = { expiresIn: 300000 }; // 300,000 milliseconds or 5 minutes
+  return jwt.sign(payload, secret, options);
+}
 
 module.exports = function(server) {
-
   server.get('/', function(req, res) {
     res.send({ api: 'up and running' });
   });
 
   server.post('/api/register', function(req, res) {
-      const credentials = req.body;
-      const user = new User(credentials);
-      user.save().then(inserted => {
-          res.status(201).json(inserted);
-      })
-  })
+    const credentials = req.body;
+    const user = new User(credentials);
+    user.save().then(inserted => {
+      const token = makeToken(inserted);
+      res.status(201).json(token);
+    });
+  });
 };
--- server/setup/users/bycrypt.js
+++
@@ -1,8 +0,0 @@
-const bcrypt = require('bcrypt');
-
-userSchema.pre('save', function (next) {
-    bcrypt.hash(this.password, 10).then(hash => {
-        this.password = hash;
-        next();
-    })
-})
