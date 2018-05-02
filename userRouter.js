65f3e6f893082585774b5b65adea856a353a8904
unknown <bonifacemtk15@yahoo.com>
2018-05-02 10:19:04 -0400

    updated file

--- userRouter.js
+++ userRouter.js
@@ -1,10 +1,10 @@
-const router = require('express').Router();
+const express = require('express');
 const User = require('./userModel');
 const router = express.Router();
 
 router
   .route('/')
-  .get( (req, res) => {
+  .get('/api/user', (req, res) => {
     User.find({}).then(users => {
       res.status(200).json(users);
 
@@ -14,7 +14,7 @@ router
     });
   })
 
-  .post( (req, res) => {
+  .post('/api/user', (req, res) => {
     const user = new User(req.body);
 
     user
@@ -24,6 +24,66 @@ router
     .catch(err => res.status(500).json(err));
     
   });
+
+  router
+  .route('/:id')
+  .get('/api/user', (req, res) => {
+    user.findById(req.params.id)
+    .then(user => {
+res.status(200).json(user);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+  })
+
+  .delete('/api/user', (req, res) => {
+    const { id } = req.params;
+    User.findByIdAndRemove(id)
+    .then(response => {
+      if(response === null) {
+        res.status(404).json({message:'not found'});
+      }else{
+      res.status(200).json(response);
+      }
+    })
+    .catch(err => {
+      if(err.name === 'CastError'){
+        res
+        .status(400)
+        .json({message:'The id is invalid' });
+      }else{
+        res.status(500)
+        .json({errorMessage:'The friend could not be removed', err});
+      }
+  });
+  })
+
+  .put('/api/user', (req, res) => {
+    user.findByIdAndUpdate(req.params.id, req.body)
+    .then(response => {
+      if(response === null){
+        res.status(404).json({ message:'not found'});
+      }
+      else{
+        res.status(200).json(response);
+      }
+      
+    })
+    .catch(err =>{
+      if(err.name === 'CastError'){
+        res.status(400).json({
+          message:'The id provided is invalid, please check and try again'
+        })
+      }
+      else{
+        res.status(500).json({
+          errorMessage:'the message has been removed',err
+        })
+      }
+    })
+  });
+
 
 
   module.exports = router;
