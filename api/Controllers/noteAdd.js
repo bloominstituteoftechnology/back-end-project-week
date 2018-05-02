37e3711b4f45d01eac0e24021e8f04e3116dbb13
zackhitch <zackhitch@users.noreply.github.com>
2018-05-01 17:30:37 -0700

    modified server

--- api/Controllers/noteAdd.js
+++ api/Controllers/noteAdd.js
@@ -6,41 +6,56 @@ const noteAdd = (req, res) => {
   const author = req.body.username;
   let id;
 
-  const findUser = username => {
+  // const findUser = username => {
+  //   User.findOne({ username: req.body.username })
+  //     .then(user => {
+  //       id = user._id;
+  //       console.log(`===USER ID: ===`, id);
+  //     })
+  //     .catch(err => {
+  //       res.json({ Error: `Unable to find user ${err}` });
+  //     });
+  // };
+
+  const saveNote = () => {
+    // await findUser(author);
     User.findOne({ username: req.body.username })
       .then(user => {
         id = user._id;
         console.log(`===USER ID: ===`, id);
+        console.log(`===AUTHOR===:`, author);
+        if (author) {
+          const newNote = new Note({
+            author: id,
+            title: title,
+            content: content,
+          });
+          console.log(`===NEW NOTE===`, newNote);
+          newNote
+            .save()
+            .then(savedNote => {
+              res.status(200).json({ Message: 'Note successfully saved!' });
+            })
+            .catch(err => {
+              res
+                .status(500)
+                .json({ Error: `Unable to save new note: ${err}` });
+            });
+        } else {
+          res.status(422).json({ Error: `User error: ${err}` });
+        }
       })
       .catch(err => {
         res.json({ Error: `Unable to find user ${err}` });
       });
   };
 
-  const saveNote = async () => {
-    await findUser(author);
-    console.log(`===AUTHOR===:`, author);
-    if (author) {
-      const newNote = new Note({ author: id, title: title, content: content });
-      console.log(`===NEW NOTE===`, newNote);
-      newNote
-        .save()
-        .then(savedNote => {
-          res.status(200).json({ Message: 'Note successfully saved!' });
-        })
-        .catch(err => {
-          res.status(500).json({ Error: `Unable to save new note: ${err}` });
-        });
-    } else {
-      res.status(422).json({ Error: `User error: ${err}` });
-    }
-  };
-
-  try {
-    saveNote();
-  } catch (err) {
-    console.log(err);
-  }
+  saveNote();
+  // try {
+  //   saveNote();
+  // } catch (err) {
+  //   console.log(err);
+  // }
 };
 
 module.exports = noteAdd;
