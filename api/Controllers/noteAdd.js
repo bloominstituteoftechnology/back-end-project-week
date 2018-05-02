45e29a622bf5bd1cd50521ac8eff40dacf0e2f5b
zackhitch <zackhitch@users.noreply.github.com>
2018-05-01 17:39:21 -0700

    modified server

--- api/Controllers/noteAdd.js
+++ api/Controllers/noteAdd.js
@@ -34,6 +34,17 @@ const noteAdd = (req, res) => {
           newNote
             .save()
             .then(savedNote => {
+              user.notes.push(savedNote);
+              user
+                .save()
+                .then(res => {
+                  res.status(200).json(res);
+                })
+                .catch(err => {
+                  res
+                    .status(500)
+                    .json({ Error: `Error saving note to user: ${err}` });
+                });
               res.status(200).json({ Message: 'Note successfully saved!' });
             })
             .catch(err => {
