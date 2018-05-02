a5e7f9314a741e0ecf23a1136d525ea0398e090e
Cristina Laryea <crispad06@gmail.com>
2018-05-02 17:37:39 -0400

    fixed localhost issue

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const server = require('./server');
 const port = process.env.PORT || 5000;
 
-mongoose.connect('mongodb://localhost:5000/backEnd', {}, err => {
+mongoose.connect('mongodb://localhost/backEnd', {}, err => {
     if(err) return console.log(err);
     console.log('Connected to Mongo');
 });
