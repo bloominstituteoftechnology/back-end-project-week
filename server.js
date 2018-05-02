d56fde0d553aba5638a00d56ce79bedfaa067130
Shaun Kelly <Shaun@redskelly.local>
2018-05-02 00:49:22 -0400

    fixes, frontend deployed w/ netlify

--- server.js
+++ server.js
@@ -15,4 +15,4 @@ server.use(express.json());
 server.get('/', (req, res) => res.send('API Running...'));
 
 const port = process.env.PORT || 5000;
-server.listen(port, () => console.log(`\n===API running on http://localhost:${port}===\n`));
+server.listen(port, () => console.log(`\n===API running on ${port}===\n`));
