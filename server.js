a3849e23b5e5155c1e67727828da62de6c5c807b
Walter Woodward <walter.sekope.woodward@gmail.com>
2018-05-03 11:12:20 -0400

    helmet removed from server.js

--- server.js
+++ server.js
@@ -7,12 +7,10 @@ const PORT = process.env.PORT || 5000;
 // ========== MIDDLEWARE ============== //
 
 const express = require('express');
-const helmet = require('helmet');
 const cors = require('cors');
 
 const app = express();
 
-app.use(helmet());
 app.use(express.json());
 app.use(cors());
 
