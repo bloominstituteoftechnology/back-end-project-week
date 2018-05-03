f6fb894401d2f66dd53164513adae8320c4207d2
unknown <bonifacemtk15@yahoo.com>
2018-05-03 18:35:56 -0400

    updated files

--- userModel.js
+++ userModel.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
 email:{
@@ -15,5 +15,18 @@ password:{
 }
 
 })
+UserSchema.pre('save', function (next) {
+    bcrypt.hash(this.password, 12, (err, hash) => {
+      if (err) {
+        return next(err);
+      }
+  
+      this.password = hash;
+      return next();
+    });
+  });
+  UserSchema.methods.isPasswordValid = function (passwordGuess) {
+    return bcrypt.compare(passwordGuess, this.password);
+  };
 const userModel = mongoose.model('User', userSchema);
 module.exports = userModel;
