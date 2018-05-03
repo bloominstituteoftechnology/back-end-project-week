99ab87b24d165fd9206311c53dbb8005ffc0e6bc
William Pelton <williamrpelton@gmail.com>
2018-05-02 21:47:42 -0500

    added forgotten delete and put, and improved denial feedback for unauthorized CRUD attempts on notes

--- data/notes/NoteRouter.js
+++ data/notes/NoteRouter.js
@@ -7,38 +7,65 @@ router.use(validate);
 
 router
     .get('/', (req, res) => {
-        Note
-            .find({ username: req.user.username })
-            .then(notes => res.status(200).json(notes))
-            .catch(err => res.status(500).json(err));
+        if (!req.user) {
+            res.status(400).json({ message: 'You are not logged in.'})
+        } else {
+            Note
+                .find({ username: req.user.username })
+                .then(notes => res.status(200).json(notes))
+                .catch(err => res.status(500).json(err));
+        }
     })
-    .post('/', (req, res) => {
-        const { title, text } = req.body
-        console.log(req.user.username);
-        const note = new Note({ 
-            username: req.user.username, 
-            title, 
-            text 
-        });
-        note
-            .save()
-            .then(savedNote => res.status(200).json(savedNote))
-            .catch(err => res.status(500).json(err));
+    .get('/:noteId', (req, res) => {
+        if (!req.user) {
+            res.status(400).json({ message: 'You are not logged in.'})
+        } else {
+            const { noteId } = req.params
+            Note
+                .findById({ noteId })
+                .then(note => res.status(200).json(note))
+                .catch(err => res.status(500).json(err));
+        }
+    })
+    .post('/', (req, res) => {        
+        if (!req.user) {
+            res.status(400).json({ message: 'You are not logged in.'})
+        } else {
+            const { title, text } = req.body
+            console.log(req.user.username);
+            const note = new Note({ 
+                username: req.user.username, 
+                title, 
+                text 
+            });
+            note
+                .save()
+                .then(savedNote => res.status(200).json(savedNote))
+                .catch(err => res.status(500).json(err));
+        }
     })    
     .put('/:noteId', (req, res) => {
-        const { title, text } = req.body;
-        const { noteId } = req.params;
-        Note
-            .findByIdAndUpdate(noteId, { title, text })
-            .then(updatedNote => res.status(200).json(updatedNote))
-            .catch(err => res.status(500).json(err));
+        if (!req.user) {
+            res.status(400).json({ message: 'You are not logged in.'})
+        } else {
+            const { title, text } = req.body;
+            const { noteId } = req.params;
+            Note
+                .findByIdAndUpdate(noteId, { title, text })
+                .then(updatedNote => res.status(200).json(updatedNote))
+                .catch(err => res.status(500).json(err));
+        }
     })
     .delete('/:noteId', (req, res) => {
-        const { noteId } = req.params;
-        Note
-            .findByIdAndRemove(noteId)
-            .then(response => res.status(200).json(response))
-            .catch(err => res.status(500).json(err));
+        if (!req.user) {
+            res.status(400).json({ message: 'You are not logged in.'})
+        } else {
+            const { noteId } = req.params;
+            Note
+                .findByIdAndRemove(noteId)
+                .then(response => res.status(200).json(response))
+                .catch(err => res.status(500).json(err));
+        }
     });
 
 module.exports = router;
