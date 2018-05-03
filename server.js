ae5adbf371eae8b0f55dceec442b31b60944fc21
habib1234731 <habibrehman1234731@gmail.com>
2018-05-03 18:27:08 -0400

    removed client folder to see if server works

--- server.js
+++ server.js
@@ -5,14 +5,15 @@ const morgan = require('morgan');
 const routes = require('./routes/routes');
 const server = express();
 
-const corsOptions = {
-    origin: 'https://vigorous-goldstine-ca9e37.netlify.com',
-    credentials: true
-}
+// const corsOptions = {
+//     origin: 'https://vigorous-goldstine-ca9e37.netlify.com',
+//     credentials: true
+// }
 
 server.use(helmet());
 server.use(express.json());
 server.use(morgan('dev'));
-server.use(cors(corsOptions));
+//server.use(cors(corsOptions));
+server.user(cors());
 routes(server);
 module.exports = server;
