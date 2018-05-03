e2047890a6c45bdc9b22a9be6a97d8b4b0e279bf
Walter Woodward <walter.sekope.woodward@gmail.com>
2018-05-02 20:04:58 -0400

    removes dummyData layer from data

--- server.js
+++ server.js
@@ -23,47 +23,45 @@ mongoose
     console.log('\n=== ERROR connecting to mongo ===\n');
   });
 
-  server.get('/', (req, res) => {
-    res.send({
-      dummyData: [
-        {
-          title: 'Note Title 1',
-          text:
-            'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.',
-          id: 1
-        },
-        {
-          title: 'Note Title 2',
-          text:
-            'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.',
-          id: 2
-        },
-        {
-          title: 'Note Title 3',
-          text:
-            'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.',
-          id: 3
-        },
-        {
-          title: 'Note Title 4',
-          text:
-            'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.',
-          id: 4
-        },
-        {
-          title: 'Note Title 5',
-          text:
-            'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.',
-          id: 5
-        },
-        {
-          title: 'Note Title 6',
-          text:
-            'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.',
-          id: 6
-        }
-      ]
-    });
-  });
+server.get('/', (req, res) => {
+  res.send(
+    {
+      title: 'Note Title 1',
+      text:
+        'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.',
+      id: 1
+    },
+    {
+      title: 'Note Title 2',
+      text:
+        'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.',
+      id: 2
+    },
+    {
+      title: 'Note Title 3',
+      text:
+        'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.',
+      id: 3
+    },
+    {
+      title: 'Note Title 4',
+      text:
+        'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.',
+      id: 4
+    },
+    {
+      title: 'Note Title 5',
+      text:
+        'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.',
+      id: 5
+    },
+    {
+      title: 'Note Title 6',
+      text:
+        'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.',
+      id: 6
+    }
+  );
+});
 
 server.listen(PORT);
