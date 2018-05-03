662a004956d767a2ec38eccd879587dacdda5267
Walter Woodward <walter.sekope.woodward@gmail.com>
2018-05-03 19:35:26 -0400

    PORT to environment variable

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
-// const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
-const PORT = 5000;
+// const PORT = 5000;
 
 // ========== MIDDLEWARE ============== //
 
