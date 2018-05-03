9114fe5fdab5b710f347db72df205bcbba6b2544
Walter Woodward <walter.sekope.woodward@gmail.com>
2018-05-03 11:03:21 -0400

    server.use removed

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ const cors = require('cors');
 
 const server = express();
 
-server.use(helmet());
-server.use(express.json());
-server.use(cors());
+// server.use(helmet());
+// server.use(express.json());
+// server.use(cors());
 
 server.get('/', (req, res) => {
   res.send({
