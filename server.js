c2169b2c857e533655180c41bd4580a42f40b0ec
Walter Woodward <walter.sekope.woodward@gmail.com>
2018-05-03 09:36:17 -0400

    Heroku running, adding dummyData

--- server.js
+++ server.js
@@ -5,7 +5,47 @@ const PORT = process.env.PORT || 5000
 const server = express(); 
  
 server.get('/', (req, res) => { 
-    res.send({api: 'up and running'}); 
-}) 
+  res.send({ 
+    dummyData: [ 
+      { 
+        title: 'Note Title 1', 
+        text: 
+          'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.', 
+        id: 1 
+      }, 
+      { 
+        title: 'Note Title 2', 
+        text: 
+          'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.', 
+        id: 2 
+      }, 
+      { 
+        title: 'Note Title 3', 
+        text: 
+          'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.', 
+        id: 3 
+      }, 
+      { 
+        title: 'Note Title 4', 
+        text: 
+          'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.', 
+        id: 4 
+      }, 
+      { 
+        title: 'Note Title 5', 
+        text: 
+          'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.', 
+        id: 5 
+      }, 
+      { 
+        title: 'Note Title 6', 
+        text: 
+          'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.', 
+        id: 6 
+      } 
+    ] 
+  }); 
+}); 
+
  
 server.listen(PORT)
