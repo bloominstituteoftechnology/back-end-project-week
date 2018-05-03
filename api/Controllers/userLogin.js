686fe7ab301e459f8e771e8e374353456c6d2de5
zackhitch <zackhitch@users.noreply.github.com>
2018-05-03 09:19:31 -0700

    removed if else block for troubleshooting

--- api/Controllers/userLogin.js
+++ api/Controllers/userLogin.js
@@ -5,27 +5,27 @@ const mysecret = 'Why can’t banks keep secrets? There are too many tellers!';
 const userLogin = (req, res) => {
   const { username, password } = req.body;
 
-  if (username && password) {
-    console.log(`BACKEND: username: ${username} and password: ${password}`);
-    User.findOne({ username })
-      .then(user => {
-        user.checkPassword(password, (nonMatch, hashMatch) => {
-          if (nonMatch !== null) {
-            res.status(422).json({ Error: 'Password is incorrect!' });
-          }
-          if (hashMatch) {
-            const payload = { username: user.username, id: user._id };
-            const token = jwt.sign(payload, mysecret);
-            res.status(200).json({ token, id: user._id });
-          }
-        });
-      })
-      .catch(err => {
-        res.status(500).json({ Error: `Unable to login:`, err });
+  // if (username && password) {
+  console.log(`BACKEND: username: ${username} and password: ${password}`);
+  User.findOne({ username })
+    .then(user => {
+      user.checkPassword(password, (nonMatch, hashMatch) => {
+        if (nonMatch !== null) {
+          res.status(422).json({ Error: 'Password is incorrect!' });
+        }
+        if (hashMatch) {
+          const payload = { username: user.username, id: user._id };
+          const token = jwt.sign(payload, mysecret);
+          res.status(200).json({ token, id: user._id });
+        }
       });
-  } else {
-    res.status(422).json({ Error: 'Username and Password required.' });
-  }
+    })
+    .catch(err => {
+      res.status(500).json({ Error: `Unable to login:`, err });
+    });
+  // } else {
+  //   res.status(422).json({ Error: 'Username and Password required.' });
+  // }
 };
 
 module.exports = userLogin;
