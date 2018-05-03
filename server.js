0f9a1926c22697895afa20f2c95288fda205bca0
Walter Woodward <walter.sekope.woodward@gmail.com>
2018-05-03 11:25:44 -0400

    morgan added, checking Heroku

--- server.js
+++ server.js
@@ -9,11 +9,13 @@ const PORT = process.env.PORT || 5000;
 
 const cors = require('cors');
 const helmet = require('helmet');
+const morgan = require('morgan');
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use(helmet());
+app.use(morgan('dev'));
 
 mongoose
   .connect(
