00d6db2dd0a8737b522b9ec3378172ce5c9d4bd0
zackhitch <zackhitch@users.noreply.github.com>
2018-05-03 10:24:52 -0700

    troubleshooting login removed log of req

--- api/Controllers/userLogin.js
+++ api/Controllers/userLogin.js
@@ -4,36 +4,28 @@ const mysecret = 'Why can’t banks keep secrets? There are too many tellers!';
 
 const userLogin = (req, res) => {
   const { username, password } = req.body;
-  console.log(req);
-  // if (username && password) {
-  console.log(`BACKEND: username: ${username} and password: ${password}`);
-  User.findOne({ username })
-    .then(user => {
-      user.checkPassword(password, (nonMatch, hashMatch) => {
-        // if (hashMatch) {
-        //   const payload = { username, id: user._id };
-        //   const token = jwt.sign(payload, mysecret);
-        //   res.status(200).json({ token, id: user._id });
-        // } else {
-        //   res.status(422).json({ Error: 'Password is incorrect!' });
-        // }
-        if (nonMatch !== null) {
-          res.status(422).json({ Error: 'Password is incorrect!' });
-        } else if (nonMatch === null && hashMatch) {
-          const payload = { username, id: user._id };
-          const token = jwt.sign(payload, mysecret);
-          res.status(200).json({ token, id: user._id });
-        } else {
-          res.status(500).json(`SOMETHING IS REALLY WRONG`);
-        }
+  if (username && password) {
+    console.log(`BACKEND: username: ${username} and password: ${password}`);
+    User.findOne({ username })
+      .then(user => {
+        user.checkPassword(password, (nonMatch, hashMatch) => {
+          if (nonMatch !== null) {
+            res.status(422).json({ Error: 'Password is incorrect!' });
+          } else if (nonMatch === null && hashMatch) {
+            const payload = { username, id: user._id };
+            const token = jwt.sign(payload, mysecret);
+            res.status(200).json({ token, id: user._id });
+          } else {
+            res.status(500).json(`SOMETHING IS REALLY WRONG`);
+          }
+        });
+      })
+      .catch(err => {
+        res.status(500).json({ Error: `Unable to login:`, err });
       });
-    })
-    .catch(err => {
-      res.status(500).json({ Error: `Unable to login:`, err });
-    });
-  // } else {
-  //   res.status(422).json({ Error: 'Username and Password required.' });
-  // }
+  } else {
+    res.status(422).json({ Error: 'Username and Password required.' });
+  }
 };
 
 module.exports = userLogin;
