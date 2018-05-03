7172ed4635c2e4dcc3d65817f02e86c659f82895
Ajmal Jalal <ajmaljalal58@gmail.com>
2018-05-03 11:48:02 -0700

    bug fixed

--- server.js
+++ server.js
@@ -27,7 +27,9 @@ server.use(cors(
   }));
 
 server.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000/');
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
