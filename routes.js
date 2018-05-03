dd1746d2320f3bce7ff435bb6e1da2f6ecab8f6b
joshuacoyne <josh.j.coyne@gmail.com>
2018-05-03 11:53:16 -0400

    Base functionality working

--- routes.js
+++ routes.js
@@ -2,22 +2,26 @@ const User = require('./UserModel');
 const bcrypt = require('bcrypt');
 const session = require('express-session');
 
+function authenticate(req, res, next) {
+    if (req.session && req.session.userId)
+        next();
+    else
+        res.json({ msg:'You must be logged in to do this function.' })
+}
+
 module.exports = function(server) {
 
-    // server.get('/api/cookie', (req, res) => {
-    //     req.session.test = 'There should be a cookie';
-    //     console.log('have a cookie');
-    //     res.json({msg:'Cookie time'});
-    // })
 
     server.get('/api/logout', (req, res) => {
         if (req.session)
-            session.destroy((err) => {
+            req.session.destroy((err) => {
                 if (err)
                     res.ststus(500).json({msg: 'logout failed'});
                 else
                     res.status(200).json({msg: 'Successfully logged out'});
             });
+        else
+            res.send({msg: 'Not Logged In'});
     });
     
     server.post('/api/register', (req, res) => {
@@ -25,7 +29,6 @@ module.exports = function(server) {
         user
         .save()
         .then(newUser => {
-            console.log('here');
             req.session.username = newUser.username;
             req.session.userId = newUser._id;
             res.status(201).json(newUser);
@@ -34,10 +37,9 @@ module.exports = function(server) {
             res.status(500).json({error: 'Could not create new user.'});
         });
     });
-    //need to check to make sure that the user is logged in when this endpoint is hit, custom middleware
-    // check if (req.session && req.session.name) next()
-    server.put('/api/update', (req, res) => {
-        const { username, notes } = req.body;
+    
+    server.put('/api/update', authenticate, (req, res) => {
+        const { notes } = req.body;
         User
         .findByIdAndUpdate(req.session.userId, { notes })
         .then (response => {
@@ -54,7 +56,6 @@ module.exports = function(server) {
         .findOne({ username })
         .then(user => {
             if(user) {
-               consol.log('user');
                 bcrypt.compare(password, user.password, function(err, valid) {                 
                     if (valid) {
                         req.session.username = username;
@@ -67,11 +68,11 @@ module.exports = function(server) {
             }
         })
         .catch(err => {
-        
             res.status(500).json({error:'There was an error'});
         });
     });
 
+    //This end point checks to see if a username exists in the db. returns true if it does else false
     server.post('/api/username', (req, res) => {
         const { username } = req.body;
         User
