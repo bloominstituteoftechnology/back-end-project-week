d8cdb48cd314c8505af811c2fd3c9940342e7c22
Ajmal Jalal <ajmaljalal58@gmail.com>
2018-05-03 12:15:46 -0700

    bug fixed

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ server.use(cors(
   }));
 
 server.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'https://wonderful-snyder-42b641.netlify.com/');
+  res.header('Access-Control-Allow-Origin', 'https://wonderful-snyder-42b641.netlify.com');
   res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
