06de392e8ca11260f5a58b1f0bd3013e5b55c203
Walter Woodward <walter.sekope.woodward@gmail.com>
2018-05-03 09:38:30 -0400

    Heroku currently running
    middleware added inside server.js

--- server.js
+++ server.js
@@ -1,51 +1,62 @@
-const express = require('express'); 
- 
-const PORT = process.env.PORT || 5000 
- 
-const server = express(); 
- 
-server.get('/', (req, res) => { 
-  res.send({ 
-    dummyData: [ 
-      { 
-        title: 'Note Title 1', 
-        text: 
-          'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.', 
-        id: 1 
-      }, 
-      { 
-        title: 'Note Title 2', 
-        text: 
-          'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.', 
-        id: 2 
-      }, 
-      { 
-        title: 'Note Title 3', 
-        text: 
-          'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.', 
-        id: 3 
-      }, 
-      { 
-        title: 'Note Title 4', 
-        text: 
-          'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.', 
-        id: 4 
-      }, 
-      { 
-        title: 'Note Title 5', 
-        text: 
-          'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.', 
-        id: 5 
-      }, 
-      { 
-        title: 'Note Title 6', 
-        text: 
-          'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.', 
-        id: 6 
-      } 
-    ] 
-  }); 
-}); 
+const express = require('express');
 
- 
-server.listen(PORT)
+const PORT = process.env.PORT || 5000;
+
+const server = express();
+
+// ========== MIDDLEWARE ============== //
+
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+
+server.use(helmet());
+server.use(morgan('dev'));
+server.use(express.json());
+server.use(cors());
+
+server.get('/', (req, res) => {
+  res.send({
+    dummyData: [
+      {
+        title: 'Note Title 1',
+        text:
+          'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.',
+        id: 1
+      },
+      {
+        title: 'Note Title 2',
+        text:
+          'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.',
+        id: 2
+      },
+      {
+        title: 'Note Title 3',
+        text:
+          'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.',
+        id: 3
+      },
+      {
+        title: 'Note Title 4',
+        text:
+          'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.',
+        id: 4
+      },
+      {
+        title: 'Note Title 5',
+        text:
+          'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.',
+        id: 5
+      },
+      {
+        title: 'Note Title 6',
+        text:
+          'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.',
+        id: 6
+      }
+    ]
+  });
+});
+
+server.listen(PORT);
