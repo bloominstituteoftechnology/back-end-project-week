42a92981c6b649d79ea4c0e77406efefdab45573
Walter Woodward <walter.sekope.woodward@gmail.com>
2018-05-03 11:23:34 -0400

    helmet added, checking heroku

--- server.js
+++ server.js
@@ -8,11 +8,12 @@ const PORT = process.env.PORT || 5000;
 // ========== MIDDLEWARE ============== //
 
 const cors = require('cors');
-
+const helmet = require('helmet');
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(helmet());
 
 mongoose
   .connect(
