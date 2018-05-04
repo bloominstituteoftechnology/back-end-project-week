a400971d3580f7d62922b2044eb0b4c99bd61fcf
Robert Rak <robertrak@hotmail.com>
2018-05-04 13:32:18 -0400

    test

--- notes/noteController.js
+++ notes/noteController.js
@@ -12,7 +12,7 @@ router
             title: 'Your first note',
             body: 'Memories go here'
           }];
-          res.status(200).json({ notes: defaultNotes }, req.body);
+          res.status(200).json({ notes: defaultNotes, body: req.body});
         }
         res.status(200).json(notes);
       })
