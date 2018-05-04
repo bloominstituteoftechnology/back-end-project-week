4091abb2d6db40e387e8dbba5d99040cf3d75bf1
Shaun Kelly <Shaun@redskelly.local>
2018-05-04 01:38:19 -0400

    userRouter

--- user/userRouter.js
+++ user/userRouter.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const User = require('./userModel');
 const router = express.Router();
+const { secret } = require('../utils/auth');
 
 // user sign-up
 router.route('/').post((req, res) => {
@@ -23,11 +24,22 @@ router.route('/login').post((req, res) => {
     User.findOne({ username })
       .then(user => {
         if (user) {
-          user.checkPassword(password); // resume here, need JWT
+          user.checkPassword(password, (nonMatch, hashMatch) => {
+            if (nonMatch !== null) {
+              res.status(422).json({ Error: 'Enter the correct password' });
+            }
+            if (hashMatch) {
+              const payload = { username: user.username, id: user._id };
+              const token = jwt.sign(payload, secret, { expiresIn: '1h' });
+              return res.json({ token, user });
+            } else {
+              return res.status(422).json({ Error: 'Incorrect password' });
+            }
+          });
         }
       })
-      .catch();
-  }
+      .catch(err => res.status(500).json(err));
+  } else res.status(422).json({ Error: 'Enter valid username and password' });
 });
 
 module.exports = router;
