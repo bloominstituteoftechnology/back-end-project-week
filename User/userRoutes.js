27304e39027224b139ca38e066b85fd3f4e15f0a
William <wtpwinberg@gmail.com>
2018-05-04 16:59:29 -0700

    User routs added

--- User/userRoutes.js
+++ User/userRoutes.js
@@ -1,7 +1,52 @@
-// GET
+const express = require('express');
+const router = express.Router();
+const User = require('./userModel');
+const { authenticate, restricted, tokenize } = require('../authentication');
 
-// POST
+// POST new user
+router.post('/register', (req, res) => {
+  const { username, password } = req.body;
 
-// PUT
+  if (!username || !password) {
+    res.status(422).json({ message: 'Must enter username AND password' });
+  } else {
+    const newUser = new User({ username, password });
+    newUser
+      .save()
+      .then(response => {
+        const userToken = tokenize(newUser);
 
-// DELETE
+        res.json({ token, user: response.data });
+      })
+      .catch(err => res.status(500).json(err));
+  }
+});
+
+// LOGIN
+
+router.post('/login', authenticate, (req, res) => {
+  const { _id, username, notes } = req.user;
+  const userToken = tokenize({ _id, username });
+
+  User
+    .findById(_id)
+    .select('-password')
+    .populate('notes')
+    .then(user => res.json({ userToken, user }));
+});
+
+router.get('/login', restricted, (req, res) => {
+  const { _id } = req.user;
+
+  User
+    .findById(_id)
+    .select('-password')
+    .populate('notes')
+    .then(user => res.json({ user }))
+    .catch(err => res.status(500).json(err));
+});
+
+// LOGOUT
+router.get('/logout', (req, res) => {
+  res.json({ message: 'Logged out >:]' });
+});
