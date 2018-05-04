89b9548c9ffeef94947d7f8acdc4e69b1658468d
Juber Ahmed <a.juber@hotmail.com>
2018-05-03 21:04:53 -0400

    Added mlab database

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const port = process.env.PORT || 5000;
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/back-end-week', {}, (error, res) => {
+mongoose.connect('mongodb://admin1:12345@ds215370.mlab.com:15370/heroku_jbnbkkl6', {}, (error, res) => {
   if (error) console.log(error, 'Could not connect to the database');
   console.log('/n/n--Connected to the database--/n/n');
 });
