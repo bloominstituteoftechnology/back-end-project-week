2d428f586870747d3fb38377a05cac17f828e0c5
AJ Genung <DragonRydrz@me.com>
2018-05-19 23:45:15 -0400

    more testing

--- User/registerRouter.js
+++ User/registerRouter.js
@@ -6,24 +6,24 @@ const { makeToken } = require('../secrets/security');
 router.post('/', (req, res) => {
   const { username, password } = req.body;
   if (!username || !password) {
-    res.statusCode(422).json({ message: 'Username and Password are required' });
+    res.status(422).json({ message: 'Username and Password are required' });
   } else {
-    User.findOne({ username }).then(response => {
-      if (!response) {
-        const newUser = new User({ username, password });
-        newUser
-          .save()
-          .then(response => {
-            const token = makeToken(newUser);
-            res.json({ token, user: response });
-          })
-          .catch(err => {
-            res.status(500).json({ message: 'Server Error' });
-          });
-      } else {
-        res.status(444).json({ message: 'Username already exists.' });
-      }
-    });
+    // User.findOne({ username }).then(response => {
+    // if (!response) {
+    const newUser = new User({ username, password });
+    newUser
+      .save()
+      .then(response => {
+        const token = makeToken(newUser);
+        res.json({ token, user: response });
+      })
+      .catch(err => {
+        res.status(500).json({ message: 'Server Error' });
+      });
+    // } else {
+    // res.status(401).json({ message: 'Username already exists.' });
+    // }
+    // });
   }
 });
 
