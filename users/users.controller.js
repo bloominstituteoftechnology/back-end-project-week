b2e780286809dbc5d0afd5391013a61db450df7e
jeffrey <jeffreyneilflynn@gmail.com>
2018-05-27 12:54:55 -0400

    PUT and DELTE users implemented

--- users/users.controller.js
+++ users/users.controller.js
@@ -10,18 +10,39 @@ const GET = (req, res) => {
         res.status(204).json({ message: 'there are no users is our database' }) :
         res.status(200).json(users)
     })
-    .catch(err => res.status(500).json({ error: 'you broke the server. thanks for nothing.' }))
+    .catch(err => res.status(500).json({ error: 'you broke the server. thanks for nothing' }))
 }
 
 const POST = (req, res) => {
   User
     .create(req.body)
     .then(user => res.status(201).json(user))
-    .catch(err => res.status(500).json({ error: 'cannot create new users at this time.' }))
+    .catch(err => res.status(500).json({ error: 'cannot create new users at this time' }))
+}
+
+const PUT = (req, res) => {
+  const { id } = req.params;
+  const updates = req.body;
+  User
+    .findByIdAndUpdate(id, updates)
+    .then(updated => res.status(200).json(updated))
+    .catch(err => res.status(500).json({ error: 'cannot update user at this time' }))
+}
+
+const DELETE = (req, res) => {
+  const { id } = req.params;
+  User
+    .findOneAndRemove(id)
+    .then(deleted => res.status(200).json({ success: 'user successfully deleted' }))
+    .catch(err => res.status(500).json({ error: 'cannot delete this user' }))
 }
 
 router.route('/')
   .get(GET)
   .post(POST)
+
+router.route('/:id')
+  .put(PUT)
+  .delete(DELETE)
 
 module.exports = router;
