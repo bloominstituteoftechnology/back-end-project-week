baba5ff66cb5ee84f14f8216a958a71e81dec159
jeffrey <jeffreyneilflynn@gmail.com>
2018-05-28 18:01:09 -0400

    added configs for heroku deployment

--- app.js
+++ app.js
@@ -6,4 +6,4 @@ mongoose
   .then(() => console.log("\n~ ~ ~ connected to mongo via mLab ~ ~ ~\n"))
   .catch(() => console.log("\n~ ~ ~ error connecting to mLab databse ~ ~ ~\n"))
 
-server.listen(8080, () => console.log("\n~ ~ ~ server connected to port 8080 ~ ~ ~\n"))
+server.listen(process.env.PORT || 8080, () => console.log("\n~ ~ ~ server connected to port 8080 ~ ~ ~\n"))
