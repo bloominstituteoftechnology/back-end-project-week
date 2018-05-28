7af2bbc53670f722eb14bbb3be83f6d317c02e0f
unknown <bsa6.23.94@gmail.com>
2018-05-28 17:57:52 -0400

    working on the basic reqs for the server side code in controller finished the / route

--- notes/notesController.js
+++ notes/notesController.js
@@ -15,8 +15,26 @@ router
     .put(putid)
     .delete(deleteid)
 
+function get(req, res) {
+    Note.find().then(foo => {
+        res.status(200).json(foo);
+    })
+    .catch(err => {
+        res.status(500).json({ errorMessage: "Note List was not found"});
+    });
+}
 
-
+function post(req, res) {
+    const note = new Note(req.body);
+    note
+        .save()
+        .then(stuff => {
+            res.status(201).json(stuff);
+        })
+        .catch(err => {
+            res.status(500).json({ message: 'Make sure to fill out the title and body text areas with strings.'})
+        });
+}
 
 
 module.exports = router;
