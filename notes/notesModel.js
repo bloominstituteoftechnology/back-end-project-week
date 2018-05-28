dede36d6c9f754d79ff570baa122e5bf8d0be8ca
unknown <bsa6.23.94@gmail.com>
2018-05-28 17:51:55 -0400

    notes model should be done for now

--- notes/notesModel.js
+++ notes/notesModel.js
@@ -1,4 +1,29 @@
 const mongoose = require('mongoose');
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
+const definition = {
+    title: {
+        type: String,
+        required: true
+    },
+    body: {
+        type: String,
+        required: true
+    },
+    createdOn: {
+        type: Date,
+        required: true,
+        default: Date.now,
+    },
+};
+
+const options = {
+    timestamp: true
+}
+
+const noteSchema = new mongoose.Schema(definition, options);
+
+const notesModel = mongoose.model('Note', noteSchema, 'notes');
 
 
 
