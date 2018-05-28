b12d73d3544a8d1e192c380969ce9299d7e56b3b
unknown <bsa6.23.94@gmail.com>
2018-05-28 18:32:31 -0400

    controller basically done

--- notes/notesController.js
+++ notes/notesController.js
@@ -36,5 +36,44 @@ function post(req, res) {
         });
 }
 
+function getid(req, res) {
+    const id = req.params.id;
+    Note.findById(id).then(free => {
+        res.status(200).json(free);
+    })
+    .catch(err => {
+        res.status(500).json({ errorMessage: "Note was not found"});
+    });
+}
+
+function deleteid(req, res) {
+    const id = req.params.id;
+    if(!Note.findById(id)) {
+        res.status(404).json({ message: 'Note not found'});
+    }
+    Note.findByIdAndRemove(id)
+    .then(remove => {
+        res.status(201).json(remove);
+    })
+    .catch(err => {
+        res.status(500).json({ errorMessage: "The note could not be removed" })
+    });
+}
+
+function putid(req, res) {
+    const id = req.params.id;
+    if(!Note.findById(id)) {
+        res.status(404).json({ message: 'Note not found'});
+    }
+    Note.findByIdAndUpdate(id)
+    .then(add => {
+        res.status(201).json(add);
+    })
+    .catch(err => {
+        res.status(500).json({ errorMessage: "The note could not be removed" })
+    });
+}
+
+
 
 module.exports = router;
