c70b2c813361124c03532d8f69ebd4dd21febbf9
AJ Genung <DragonRydrz@me.com>
2018-05-28 16:05:44 -0400

    added local database connection

--- secrets/config.js
+++ secrets/config.js
@@ -1,6 +1,6 @@
 const config = {
-  database:
-    'mongodb://lambdanotes1:lambdanotes1@ds261429.mlab.com:61429/lambda-notes',
+  database: 'mongodb://localhost/lambdanotestesting',
+  // 'mongodb://lambdanotes1:lambdanotes1@ds261429.mlab.com:61429/lambda-notes',
   secret: 'Bend the knee!',
   salt: "Dragons are a real man's best friend",
 };
