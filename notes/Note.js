50056650a7ba0a88fc69758fb11bb8b2c41e4567
Velsu <riftmage50@gmail.com>
2018-05-28 17:34:50 +0200

    Added Schema for Notes, configured database and env

---
+++ notes/Note.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
+const Note = new mongoose.Schema({
+  user_id: {
+    type: ObjectId,
+    ref: "User",
+    required: true
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  content: {
+    type: String,
+    required: true
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now()
+  }
+});
+
+module.exports = mongoose.model("Note", Note);
--- server.js
+++ server.js
@@ -1,18 +1,52 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const cors = require("cors");
 const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+const cors = require("cors");
 const helmet = require("helmet");
 
 const server = express();
 
+const usersRouter = require("./users/usersRouter.js");
+const notesRouter = require("./notes/notesRouter.js");
+
+const corsOptions = {
+  origin: "https://dalambdanotes.herokuapp.com/",
+  credentials: true
+};
+
+server.use(bodyParser.json());
+server.use(cors(corsOptions));
+server.use(helmet());
+
 const path = process.env.MONGO_URI;
 
-mongoose.connect(path);
+server.use(
+  session({
+    secret: process.env.SECRET,
+    store: new MongoStore({
+      url: process.env.SESSIONS_DB,
+      ttl: 60 * 10
+    }),
+    resave: true,
+    saveUninitialized: false,
+    cookie: { maxAge: 1 * 24 * 60 * 60 * 1000 }, // 1 day in milliseconds
+    name: "DontPWNmeh"
+  })
+);
+
+mongoose
+  .connect(path)
+  .then(response => {
+    console.log("connected to DB");
+  })
+  .catch(error => {
+    console.log("Something went wrong with DB");
+  });
 server.get("/", (req, res) => res.send("API Running...!"));
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () =>
-  console.log(`Listening on ${PORT} ${process.env.dbpath}`)
+  console.log(`Listening on ${PORT} ${process.env.MONGO_URI}`)
 );
