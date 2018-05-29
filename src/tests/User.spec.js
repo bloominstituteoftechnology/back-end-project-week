5a7dd979bcc5db560f1aefa3e89cf32e9ae8a775
Jared Cuffe <jcuffe@gmail.com>
2018-05-29 14:51:44 -0700

    Authentication testing

--- src/tests/User.spec.js
+++ src/tests/User.spec.js
@@ -1,25 +1,45 @@
 const User = require('../models/User')
 const mongoose = require('mongoose')
+const { userName, password } = require('faker').internet
 const { mongoUri, mongoOptions } = require('../server/config')
 
 describe('User', () => {
+  let userData
+  
   beforeAll(() => {
     return mongoose.connect(mongoUri, mongoOptions)
   })
   
   afterAll(() => {
-    return mongoose.disconnect()
+    return User.remove()
+    .then(() => mongoose.disconnect())
+  })
+  
+  beforeEach(() => {
+    userData = {
+      username: userName(),
+      password: password()
+    }
   })
 
   it('Hashes the password before saving', async () => {
-    const userData = {
-      username: 'Jared',
-      password: '13111'
-    }
-    
+    user = await User.create(userData)
+    expect(user.password).not.toBe(userData.password)
+  })
+
+  it('Validates a provided password', async () => {
     return User.create(userData)
-      .then(user => {
-        expect(user.password).not.toBe(userData.password)
+      .then(async () => {
+        user = await User.login(userData)
+        expect(user.username).toBe(userData.username)
+      })
+  })
+
+  it('Rejects an incorrect password', async () => {
+    return User.create(userData)
+      .then(async () => {
+        user = await User.login({ username: userData.username, password: '' })
+        expect(user).toBeNull()
       })
   })
 })
