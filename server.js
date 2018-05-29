67cb3d4809252682f57d980dad835ed2e2ed6767
Eric Gomez <eric_gomez_1@hotmail.com>
2018-05-29 09:55:16 -0700

    fixing the heroku h10 error

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 const mongoose = require('mongoose');
-const port = process.env.PORT || 8000
+let port = process.env.PORT || 3000;
+
+
 mongoose.connect('mongodb://ChickenChaser:bulldog1@ds139950.mlab.com:39950/lambda-notes')
   .then(() => {
-    console.log('connected to databse!');
+    console.log('connected to database!');
   })
   .catch(err => {
     console.log(err);
