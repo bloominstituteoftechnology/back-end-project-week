1490a82d74c22aeb8021fb4e917702231ff1b2f0
totsuya <supertatortots@gmail.com>
2018-05-29 15:42:49 -0400

    added ntoeSchema

---
+++ modals/noteModal/noteSchema.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const noteSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  body: {
+    type: String,
+    required: true
+  }
+})
+
+module.exports = mongoose.model('Note', noteSchema);
