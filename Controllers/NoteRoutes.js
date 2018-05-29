d3b5c620b5df3d00ad8d7f00c89309f888511b4e
jaivonmassena <jaivonmassena@gmail.com>
2018-05-29 21:16:44 +0200

    finised post and get route

--- Controllers/NoteRoutes.js
+++ Controllers/NoteRoutes.js
@@ -1,5 +1,28 @@
 const router = require('express').Router()
-const Notes = require('../Models/NoteModels')
+const Note = require('../Models/NoteModels')
 
 router
-  .route
+  .route("/")
+  .get((req, res)=>{
+    Note
+    .find()
+    .then((note)=> {
+      res.status(200).json(note)
+    })
+    .catch((err) => {
+      res.status(500).json(err)
+    })
+  })
+  .post((req, res) => {
+    const note = new Note(req.body)
+
+    note
+      .save()
+      .then(() => {
+        res.status(201).json(Budget)
+      })
+      .catch((err) => {
+        res.status(500).json(err)
+    })
+  })
+module.exports = router;
--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const noteRouters = require("./Controllers/NoteRoutes")
 const app = express();
 
 app.use(express.json());
@@ -18,8 +18,8 @@ mongoose
   app.get("/",(req, res)=>{
     res.send('Api running');
   })
-  app.get("/notes",(req, res)=>{
-  })
+  app.use("/notes", noteRouters)
+
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`\n=== API up on port: ${port} ===\n`));
