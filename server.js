868ebc32887a8f9c94ee3d325d819775bd946fcb
Robert Hogan <roberthogan@Roberts-MacBook-Pro.local>
2018-05-29 18:47:26 -0400

    Robert Hogan. Initial commit. Setting up all deployment phases and getting server up and running.

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express'); // import express
+const server = express();
+const cors = require('cors');
+const Joi = require('joi'); // validator
+
+server.use(express.json()); // middleware to parse json objs. 
+server.use(cors({}));
+
+server.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+//no hard-coded port below
+const port = process.env.PORT || 5000;
+server.listen(port, err => console.log(`Server listening on ${port}...`)); // listen on port
