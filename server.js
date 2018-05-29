c6eee143824fdfc3752eeb5571bcdb35442684b4
nickbrenn <nbren@tutanota.com>
2018-05-29 14:25:22 -0400

    Fixed port bug

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const cors = require("cors");
 
+const port = process.env.PORT || 3333;
+
 const server = express();
-server.unsubscribe(cors({}));
+
+server.use(express.json());
+server.use(cors({}));
 
 server.get("/", (req, res) => {
   res.json({ Message: "Hello World" });
 });
 
-server.listen(3333, err => {
+server.listen(port, err => {
   if (err) console.log(err);
-  else console.log("Magic happening on 3333");
+  else console.log(`Magic happening on ${port}`);
 });
