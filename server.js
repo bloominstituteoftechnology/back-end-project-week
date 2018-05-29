6ddc5e4467db4d3b017bf3a1dfb2f881800af915
Jacob Leon Lyerla <umasour@yahoo.com>
2018-05-29 09:51:55 -0700

    Server is up
    i don't have routes set up but the basic server stuff is up

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const port = process.env.PORT || 5500;
+mongoose
+.connect('mongodb://JacobLeonLyerla:Otokonoko1990@ds237770.mlab.com:37770/jlldb')
+mongoose.connection.once('open',()=>{
+    console.log('connected to db')
+})
+.catch(err=>{
+    console.log('Could not connect to mongodb',err)
+})
+
+
+const server = express();
+server.use(cors());
+server.use(express.json());
+
+server.get('/', (req, res) => {
+  res.status(200).json({ api: `api running on port ${port}` });
+});
+server.listen(port, () => console.log(`\n=== API up on port: ${port} ===\n`));
