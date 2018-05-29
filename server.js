53b2dcb01b28698eeb3cd7669fbdcadadc5c520f
Justin Hong <jhong2009@gmail.com>
2018-05-29 16:46:33 -0700

    in server.js: set up basic server on port 3000

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+
+const server = express();
+
+// middleware
+server.use(cors());
+server.use(express.json());
+
+// root route
+server.get("/", (req, res) => {
+	res.json({ Message: "Hello there friend" });
+})
+
+server.listen(3000, (err) => {
+	if (err) console.log(err);
+	console.log("\n === Server listening on port 3000 === \n");
+});
