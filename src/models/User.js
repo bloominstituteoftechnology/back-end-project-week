5ab5f1dbe2229543b37cfc302cdfea77217d394a
Jared Cuffe <jcuffe@gmail.com>
2018-05-29 14:51:18 -0700

    Static login method; hash only new passwords

--- src/models/User.js
+++ src/models/User.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
-const _id = mongoose.SchemaTypes.ObjectId
 
 const userSchema = mongoose.Schema({
   username: {
@@ -12,16 +11,34 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true
   },
-  notes: [{ type: _id, ref: 'Note' }]
+  session: String
 })
 
-userSchema.pre('save', function (next) {
-  bcrypt.hash(this.password, 10)
-    .then(hash => {
-      this.password = hash
-      next()
-    })
-    .catch(next)
+userSchema.pre('save', async function (next) {
+  if (this.isNew) {
+    const hash = await bcrypt.hash(this.password, 10)
+    this.password = hash
+    this.session = Date.now()
+  }
+  next()
 })
+
+userSchema.statics.login = async function (userData) {
+  let user = await this.findOne({ username: userData.username })
+
+  if (!user) {
+    throw new Error('User not found')
+  }
+
+  const valid = await bcrypt.compare(userData.password, user.password)
+
+  if (valid) {
+    user.session = Date.now()
+    user = await user.save()
+    return user
+  } else {
+    return null
+  }
+}
 
 module.exports = mongoose.model('User', userSchema)
