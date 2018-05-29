7fc9f341db79289dddbb931130cbe01a01c94f6b
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2018-05-29 13:08:49 -0700

    Implemented Dynamic Port for Heroku

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+
+const port = process.env.PORT || 8000;
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
 server.use(cors());
+
 
 server.get('/', (req, res) => {
     res.json({ Message: 'Hello World' });
@@ -13,7 +16,7 @@ server.get('/', (req, res) => {
 
 server.listen(8000, err => {
     if(err) console.log(err);
-    console.log('API Running on 8000');
+    console.log(`API Running on ${port}`);
 });
 
 module.exports = server;
