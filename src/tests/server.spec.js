447d6862bb40f94779752c972d38633857a3282b
Jared Cuffe <jcuffe@gmail.com>
2018-05-29 14:51:53 -0700

    Auth route tests

--- src/tests/server.spec.js
+++ src/tests/server.spec.js
@@ -1,28 +1,42 @@
 const mongoose = require('mongoose')
 const server = require('../server/server')
-const request = require('supertest')
+const request = require('supertest')(server)
+const User = require('../models/User')
 const { userName, password } = require('faker').internet
+const { mongoUri, mongoOptions } = require('../server/config')
 
 describe('API server', () => {
+  beforeAll(() => {
+    return mongoose.connect(mongoUri, mongoOptions)
+  })
+
+  afterAll(() => {
+    return User.remove()
+      .then(() => mongoose.disconnect())
+  })
+
   it('says hello when greeted at the front door', async () => {
-    const response = await request(server).get('/')
+    const response = await request.get('/')
     expect(response.status).toBe(200)
     expect(response.body).toEqual({ message: 'hello' })
   })
 
-  describe('authentication routes', () => {
-    ['register', 'login'].forEach(route => {
-      it(`provides a token when valid user credentials are POSTed to ${route}`, async () => {
-        const url = `/api/user/${route}`
-        const userData = {
-          username: userName(), 
-          password: password()
-        }
+  describe('Auth routes', () => {
+    let userData = {
+      username: userName(),
+      password: password()
+    }
 
-        const response = await request(server).post(url).send(userData)
-        expect([200, 201]).toContain(response.status)
-        expect(response.body.token).toBeDefined()
-      })
+    it('Provides a token when a new user is created', async () => {
+      const response = await request.post('/api/user/register').send(userData)
+      expect(response.status).toBe(201)
+      expect(response.body.token).toBeDefined()
+    })
+
+    it('Provides a token when an existing user is logged in', async () => {
+      const response = await request.post('/api/user/login').send(userData)
+      expect(response.status).toBe(200)
+      expect(response.body.token).toBeDefined()
     })
   })
 })
