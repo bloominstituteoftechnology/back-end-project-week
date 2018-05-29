0e3475340ac733073b5acb8d3f0b25147262e1d8
Liz McKenzie <elizabethbraggisgreat@gmail.com>
2018-05-29 16:33:20 -0400

    added process.env.PORT || to server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const port = 2121;
+const port = process.env.PORT || 2121;
 
 const server = express();
 server.use(cors({}));
