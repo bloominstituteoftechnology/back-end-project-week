97ba5d0cfebc964213ebfd6160a31b601759b43d
jaivonmassena <jaivonmassena@gmail.com>
2018-05-29 17:45:44 +0200

    added endpoint

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express();
 app.use(express.json());
 
 
+
 mongoose
   .connect('mongodb://jaivon:massena890@ds237700.mlab.com:37700/lambdanotesdb')
   .then(mongo =>{ console.log('connected to database')}
@@ -13,3 +14,10 @@ mongoose
   .catch(err => {
     console.log('Error connecting to database', err)
   })
+
+  app.get("/",(req, res)=>{
+    res.send('Api running');
+  })
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`\n=== API up on port: ${port} ===\n`));
