82494aa293c5d99950008d1b4bd8c53bb97bfdf3
faulkenfett11 <36997363+faulkenfett11@users.noreply.github.com>
2018-05-29 13:32:04 -0700

    heroku

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 
 const server = express();
-const port = process.env.PORT || 8181;
+const port = process.env.PORT || 3333;
 
 server.use(express.json())
 server.use(cors({}));
 server.use(helmet());
 
 mongoose
-	.connect("mongodb://<dbuser>:<dbpassword>@ds139970.mlab.com:39970/lambda-notes-backend")
+	.connect("mongodb://faulkenfett11:fau11jus@ds139970.mlab.com:39970/lambda-notes-backend")
 	.then(() => console.log('==connected=='))
 	.catch(() => console.log('==error connecting=='))
 
@@ -21,7 +21,5 @@ server.get('/', (req, res) => {
 
 server.listen(port, err => {
 	if (err) console.log(err);
-	console.log(`Running on ${port}`);
+	console.log(`==Running on ${port}==`);
 });
-
-module.exports = server;
