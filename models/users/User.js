5df2c9b4b888a500af9d40867de948cc4eba481a
Devon Bacon <devon.bacon@gmail.com>
2018-05-29 13:52:52 -0700

    models - added user folder and schema

---
+++ models/users/User.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const User = mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = mongoose.model('User', User);
