f78e28b5beac6012b6f54621ecf70bf47830ce17
tanking <tanking@github.com>
2018-05-29 15:36:13 -0400

    Set up initial route

--- server.js
+++ server.js
@@ -7,5 +7,9 @@ const server = express();
 // middleware
 server.use(express.json());
 
+server.get('/', (req, res) => {
+    res.send({ Message: 'api running' })
+})
+
 const port = process.env.PORT || 3000;
 server.listen(port, () => console.log(`Server running on port: ${port}`))
