99a0ef33bc49615e56af195f4fb7951da35415c6
Devon Bacon <devon.bacon@gmail.com>
2018-05-29 13:57:12 -0700

    server.js - DELETE request for users route

--- server.js
+++ server.js
@@ -61,6 +61,15 @@ server.route('/notes')
         if(err) res.status(201).json(err);
         else res.status(500).json(user);
       });
+    })
+    .delete((req, res) => {
+      User.findByIdAndRemove(req.body.id)
+        .then(() => {
+          res.json('User successfully deleted');
+        })
+        .catch(err => {
+          res.status(500).json(err);
+        });
     });
 
 server.listen(port, err => {
