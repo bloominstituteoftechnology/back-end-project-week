57a92d38289e95f5287bbd83558b0ebca921f662
Natalie <natalieschennum@gmail.com>
2018-05-29 11:29:04 -0700

    Setup with Dependencies, Server.js, and Heroku

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express'); 
+const bodyParser = require('body-parser');
+const cors = require('cors'); 
+
+const port = process.env.PORT || 2886; 
+const server = express(); 
+server.use(cors({})); 
+server.use(bodyParser.json()); 
+
+server.get('/', (req, res) => {
+    res.json({Message: "Up and at'um!"});
+});
+
+server.listen(port, err => {
+    if (err) console.log(err); 
+    console.log(`Happening on ${port}!`)
+});
