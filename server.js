e14c2e90801068d8412224def082c413bb1e2c26
Chris <cjmarsh725@gmail.com>
2018-05-29 10:31:08 -0700

    Added helmet and connection to database

--- server.js
+++ server.js
@@ -1,15 +1,27 @@
 const express = require('express');
+const helmet = require('helmet');
 const cors = require('cors');
+const mongoose = require('mongoose');
+
+const notesRouter = require('./notes/notesRouter');
+
+mongoose
+  .connect('mongodb://cjmarsh:password@ds139920.mlab.com:39920/notes')
+  .then(mongo => console.log('Connected to DB'))
+  .catch(err => console.log('Error connecting to DB', err));
 
 const server = express();
 const port = process.env.PORT;
 
 server.use(express.json());
+server.use(helmet());
 server.use(cors({}));
 
 server.get('/', (req, res) => {
   res.json({Message: `Server listening on port ${port}`});
 })
+
+server.use('/notes', notesRouter);
 
 server.listen(port, err => {
   if (err) console.log(err);
