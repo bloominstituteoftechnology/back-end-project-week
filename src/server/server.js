9c5af5848d7ca5b38ba420dacc4115574a32378e
Jared Cuffe <jcuffe@gmail.com>
2018-05-29 14:51:59 -0700

    Auth route implementation

--- src/server/server.js
+++ src/server/server.js
@@ -1,13 +1,37 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const { getSessionToken } = require('./util')
 const { mongoUri, mongoOptions } = require('./config')
+const User = require('../models/User')
 
 const server = express()
 
 server.use(express.json())
 
+
+
 server.get('/', (req, res) => {
   res.send({ message: 'hello' })
+})
+
+server.post('/api/user/register', async (req, res, next) => {
+  const user = await User.create(req.body)
+  const token = getSessionToken(user)
+  res.status(201).send({ token })
+})
+
+server.post('/api/user/login', async (req, res, next) => {
+  const user = await User.login(req.body)
+  if (!user) {
+    res.status(400).send({ error: 'Invalid username or password' })
+  }
+  const token = getSessionToken(user)
+  res.status(200).send({ token })
+})
+
+server.use((err, req, res, next) => {
+  console.log(err)
+  res.status(500).send({ error: err })
 })
 
 module.exports = server
