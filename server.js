f9ab99c08765deb3675462ae1e3eea92458f096f
Devon Bacon <devon.bacon@gmail.com>
2018-05-29 12:04:37 -0700

    server.js - connected to mongo db

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
+const mongoose = require('mongoose');
 const express = require('express');
 const cors = require('cors');
 
 const port = process.env.PORT || 3333;
 const server = express();
+
+mongoose
+  .connect('mongodb://devon:dvnbcn44@ds239940.mlab.com:39940/lambda-notes')
+  .then(() => {
+    console.log('connected to production database');
+  })
+  .catch(err => {
+    console.log('error connecting to production database', err);
+  });
 
 server.unsubscribe(cors({}));
 server.unsubscribe(express.json());
@@ -10,6 +20,14 @@ server.unsubscribe(express.json());
 server.get('/', (req, res) => {
   res.json({ message: 'Hello World!' });
 });
+
+server.route('notes')
+  .get((req, res) => {
+    
+  })
+  .post((req, res) => {
+
+  })
 
 server.listen(port, err => {
   if(err) console.log(err);
