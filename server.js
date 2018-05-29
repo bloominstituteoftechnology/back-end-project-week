8611ec0be7b84e8b7d28162923e20fe43732d903
Cruise Brantley <cruise.brantley@gmail.com>
2018-05-29 14:17:38 -0500

    deployment test

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
-const express = require('express');
-const cors = require('cors');
-const morgan = require('morgan');
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
 
+const port = process.env.PORT || 3333;
 const server = express();
 
 server.use(cors({}));
 server.use(express.json());
-server.use(morgan('combined'));
+server.use(morgan("combined"));
+
+server.get("/", (req, res) => {
+  res.json({ Message: "Hello World" });
+});
+
+server.listen(port, err => {
+  if (err) console.log(err);
+  console.log(`Focus your attack on port ${port}`);
+});
