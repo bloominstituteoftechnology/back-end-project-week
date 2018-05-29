bd5769c40163c4a2c2d57c64ecc81984a70e3c0e
Eric Gomez <eric_gomez_1@hotmail.com>
2018-05-29 10:09:29 -0700

    fixing error 10

--- server.js
+++ server.js
@@ -3,22 +3,22 @@ const cors = require('cors');
 const helmet = require('helmet');
 const mongoose = require('mongoose');
 let port = process.env.PORT || 3000;
+const server = express();
 
-
-mongoose.connect('mongodb://ChickenChaser:bulldog1@ds139950.mlab.com:39950/lambda-notes')
-  .then(() => {
-    console.log('connected to database!');
-  })
-  .catch(err => {
-    console.log(err);
-  })
+// mongoose.connect('mongodb://ChickenChaser:bulldog1@ds139950.mlab.com:39950/lambda-notes')
+//   .then(() => {
+//     console.log('connected to database!');
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   })
 
 server.use(cors({}))
 server.use(helmet());
 server.use(express.json());
 
 server.get('/', (req, res) => {
-  res.json({status: 'connect'})
+  res.json({status: 'connected'})
 })
 
 server.listen(port , () => {
