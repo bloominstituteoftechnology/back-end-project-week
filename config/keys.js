d24c4972b6e85f409edeecc484a3279815489fb8
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2018-05-29 12:21:05 -0700

    added config folder and file for keeping db secret

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURL:
+    "mongodb://tylarpierson:Renee1995@ds239930.mlab.com:39930/lambdanotes",
+    secret: 't19y95l11a10r'
+};
--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 const port = process.env.PORT || 5000
 server = express();
+
+const db = require('./config/keys').mongoURL;
+mongoose
+  .connect(db)
+  .then(() => console.log('Connected to DB'))
+  .catch(err => console.log(err));
 
 server.use(express.json());
 server.use(cors({}));
 
 server.get('/', (req, res) => {
-  res.json({Message: 'Hello World'});
+  res.status(200).json({Message: 'Hello World'});
 });
 
 server.listen(port, () => {
