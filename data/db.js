f910720a899718a72da0866d5ae3d745ee46abc3
Yasin Shuman <email@yasin.io>
2018-05-29 16:23:25 -0700

    trying to update with env variables for db

--- data/db.js
+++ data/db.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
-const { dbLogin, dbPw } = require("../utils/dbConfig.js");
+// const { dbLogin, dbPw } = require("../utils/dbConfig.js");
+const dbLogin = process.env.MONGO_DBLOGIN;
+const dbPw = process.env.MONGO_DBPW;
 const uri = `mongodb://${dbLogin}:${dbPw}@ds139960.mlab.com:39960/lambda-notes`;
 module.exports = {
   connect: function() {
