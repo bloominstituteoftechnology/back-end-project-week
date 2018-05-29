3b17a30489a4a2a1d0465ddd48de6548fce5ac1c
Jared Cuffe <jcuffe@gmail.com>
2018-05-29 11:50:15 -0700

    User specification

---
+++ tests/User.spec.js
@@ -0,0 +1,25 @@
+const User = require('../models/User')
+const mongoose = require('mongoose')
+const { mongoUri, mongoOptions } = require('../config.js')
+
+describe('User', () => {
+  beforeAll(() => {
+    return mongoose.connect(mongoUri, mongoOptions)
+  })
+  
+  afterAll(() => {
+    return mongoose.disconnect()
+  })
+
+  it('Hashes the password before saving', async () => {
+    const userData = {
+      username: 'Jared',
+      password: '13111'
+    }
+    
+    return User.create(userData)
+      .then(user => {
+        expect(user.password).not.toBe(userData.password)
+      })
+  })
+})
