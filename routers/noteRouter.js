b8e3150a011b973a89d38cbace951ae8e4d8c829
Anthony <anthony@anthonys-mbp.lan>
2018-05-29 19:43:25 -0400

    Got Delete endpoint working

--- routers/noteRouter.js
+++ routers/noteRouter.js
@@ -2,30 +2,7 @@
 const router = require('express').Router();
 const Note = require('../models/noteModel');
 
-router
-    .route('/')
-    .get()
-    .post(post);
-
-router
-    .route('/:id')
-    .get((req, res) => {
-        res.status(200).json({ route: '/api/notes/' + req.params.id });
-    })
-    .delete((req, res) => {
-        res.status(200).json({ status: 'work on delete!' });
-    })
-    .put((req, res) => {
-        res.status(200).json({ status: 'work on put' })
-    })
-
-// function get(req, res) {
-//     const
-    
-//     res.status(200).json({ route: '/api/notes/' });
-// }
-
-function post(req, res) {
+router.post('/', function post(req, res) {
     const noteData = req.body;
 
     const note = new Note(noteData);
@@ -38,6 +15,32 @@ function post(req, res) {
         .catch(err => {
             res.status(500).json(err);
         });
-}
+});
+
+router.get('/', function get(req, res) {
+    Note.find().then(notes => {
+        res.status(200).json(notes);
+    });
+});
+
+router.get('/:id', (req, res) => {
+    res.status(200).json({ route: '/api/notes/' + req.params.id });
+});
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+
+    Note.findByIdAndRemove(id)
+    .then(note => { 
+        res.status(200).json(note);
+    })
+    .catch(err => {
+        res.status(404).json(err);
+    });
+});
+
+router.put('/:id', (req, res) => {
+    res.status(200).json({ status: 'work on put' })
+});
 
 module.exports = router;
