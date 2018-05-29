fa89f949bae06956be9ab4e464e1404f0e11886b
MINE <Mine@MINEs-MacBook-Pro-2.local>
2018-05-29 11:25:15 -0700

    Database Up & Running

---
+++ models/notesModel.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
+const noteSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  users: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = mongoose.model('Note', noteSchema);
