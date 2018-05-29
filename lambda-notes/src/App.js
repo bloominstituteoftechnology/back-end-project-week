c953a86fb5ce95aad2ec886f03cdf0d650bc6f33
unknown <bsa6.23.94@gmail.com>
2018-05-29 14:51:30 -0400

    display without any styling done

--- lambda-notes/src/App.js
+++ lambda-notes/src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 import './App.css';
+import Notes from './components/display';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+   <Route exact path="/" component={Notes} />
       </div>
     );
   }
