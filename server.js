d4d113b41446f6be9658c353f49a15a95a695f38
Mark Wong <fmarkwong@gmail.com>
2018-05-29 12:31:11 -0700

    get one note and error handling

--- server.js
+++ server.js
@@ -19,14 +19,26 @@ mongoose.connect(`mongodb://${DB_USER}:${DB_PASS}@${DB_HOST}/lambdanotes`)
   .then(mongo => console.log('connected to database'))
   .catch(err => console.log(err));
 
-server.get('/api/notes', async (req, res) => {
+const asyncHandler = fn => (req, res, next) => fn(req, res, next).catch(next);
+const errorLog = (err, req, res, next) => {
+  console.log(err);
+  res.status(500).json(err.message);
+}
+
+server.get('/api/notes', asyncHandler(async (req, res) => {
   const response = await Note.find()
   res.status(200).json(response);
-});
+}));
 
-server.post('/api/notes', async (req, res) => {
+server.get('/api/notes/:id', asyncHandler(async (req, res) => {
+  const response = await Note.findById(req.params.id);
+  res.status(200).json(response);
+}));
+
+server.post('/api/notes', asyncHandler(async (req, res) => {
   const response = await Note.create(req.body)
   res.status(200).json(response);
-});
+}));
 
-
+// needs to be last: https://expressjs.com/en/guide/error-handling.html
+server.use(errorLog);
