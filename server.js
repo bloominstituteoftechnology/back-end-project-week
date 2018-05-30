a49422887f90d23dd1fa196f39370120b0b2531a
cgoez <goez2001@gmail.com>
2018-05-29 17:55:22 -0700

    Port fix

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 const server = express();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 server.use(cors({}));
 
