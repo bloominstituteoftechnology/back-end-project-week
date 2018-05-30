28da460ed17ca9382e5768f0ac68405857d7d83e
Shawn Stewart <s.stewart0220@gmail.com>
2018-05-30 16:14:59 -0400

    Added ability to create a note to the NoteController

---
+++ Notes/NoteController.js
@@ -0,0 +1,22 @@
+import Note from './NoteModel';
+import { userInfo } from 'os';
+
+const NoteController = {
+  createNote: (req, res) => {
+    const noteInfo = req.body;
+
+    if('title' in noteInfo && 'contents' in userInfo) {
+      const newNote = new Note(noteInfo);
+      newNote
+        .save()
+        .then(response => {
+          res.status(201).json({ note: resposne });
+        })
+        .catch(err => {
+          res.status(500).json({ err: 'Error created new note', err });
+        });
+    } else {
+      res.status(500).json({ err: 'A note requires a title and contents' });
+    }
+  }
+}
