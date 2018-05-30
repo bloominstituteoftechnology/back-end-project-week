fbe2f92a67676a6d53840571fa43d7e883f10043
Michal Chmielniak <michal@Michals-MacBook-Pro.local>
2018-05-30 11:17:01 -0400

    trying to connect to mongodb

--- server.js
+++ server.js
@@ -1,48 +1,23 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const session = require('express-session');
-const helmet = require('helmet');
+
+const express = require("express");
+const helmet = require("helmet");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const NotesRouter = require("./Notes/NotesRouter.js");
+const UserRouter = require("./Users/UserRouter.js");
+const cors = require("cors");
 const server = express();
-const UserRouter = require('./Users/UserRouter.js');
-const NotesRouter = require('./Notes/NotesRouter.js');
 
-const corsOptions = {
-  origin: 'https://mgclambdanotes.netifly.com',
-  credentials: true
-};
-
-server.use(cors(corsOptions));
-server.use(express.json());
 server.use(helmet());
-
-const path = process.env.MONGOLAB_URI || 'mongodb://localhost/notes';
-
-mongoose.connect(path);
-
-const MS = require('express-mongoose-store')(session, mongoose);
-
-server.use(
-  session({
-    secret: 'each night I wonder',
-    store: new MS(),
-    resave: true,
-    saveUninitialized: false,
-  })
-);
-
-const isLoggedIn = function (req, res, next) {
-  if (!req.session.auth) res.status(422).json('Not Authorized');
-  else if (req.session.id) {
-    next();
-  }
-};
-
-server.use('/api/notes', isLoggedIn, NotesRouter);
-server.use('/api/users', UserRouter);
+server.use(cors());
+server.use(morgan("combined"));
+server.use(express.json());
+server.use("/api/notes", NotesRouter);
+server.use("/api/user", UserRouter);
 
 server.get('/', (req, res) => res.send('API Is Running...'));
 
 const PORT = process.env.PORT || 5000;
 server
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+module.exports = server;
