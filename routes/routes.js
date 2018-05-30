be0358b70039e63b21fb3aaccd39b5e90fcfbe15
Robert Hogan <roberthogan@Roberts-MacBook-Pro.local>
2018-05-29 21:06:00 -0400

    Robert Hogan. Added route for all notes.

--- routes/routes.js
+++ routes/routes.js
@@ -1,2 +1,19 @@
-const express = require('express');
+const express = require('express'); // bring in express
+const Note = require('../notes/notes'); // bring in our Note
+
 const router = express.Router();
+
+router 
+.get('/', (req, res) => {
+    Note
+    .find()
+    .then(notes => {
+        res.status(200).json(notes);
+    })
+    .catch(err => {
+        res.status(500).json(err);
+    });
+})
+
+
+
