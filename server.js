f7696cd8b6a3f8b167780a79f21b5340777f0bd8
Liz McKenzie <elizabethbraggisgreat@gmail.com>
2018-05-30 14:54:46 -0400

    got mongo db connected

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const mongoose = require('mongoose');
+const mongooseDB = require('./config/keys').mongoURL;
+
 const port = process.env.PORT || 2121;
 
 const server = express();
@@ -11,7 +14,11 @@ server.get('/', (req, res) => {
   res.json({ Message: 'Any requests?' });
 });
 
+mongoose.connect(mongooseDB, () => {
+  console.log('connected to mongodb');
+});
+
 server.listen(port, err => {
   if (err) console.log(err);
-  console.log('>o0o0o spinning vinyls on 2121');
+  console.log('>o0o0o spinning vinyls on ${port}');
 });
