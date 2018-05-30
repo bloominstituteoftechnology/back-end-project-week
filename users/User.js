f87e667e70b3971e6d49db2f1bd6d21ef8c5a1ab
tanking <tanking@github.com>
2018-05-30 18:48:31 -0400

    Added hash and compare methods from crypt to User schema

--- users/User.js
+++ users/User.js
@@ -1,14 +1,31 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true,
+        unique: true
     },
     password: {
         type: String,
         required: true,
     }
 })
+
+userSchema.pre('save', function(next){
+    return bcrypt.hash(this.password, 11, (err, hash) => {
+        if (err) {
+            return next(err)
+        } 
+        this.password = hash;
+        
+        return next()
+    })
+})
+
+userSchema.methods.validatePassword = function(guess, cb) {
+    return bcrypt.compare(guess, this.password)
+}
 
 module.exports = mongoose.model('User', userSchema, 'users');
