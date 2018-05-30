24f76ca09f53202bddb062cac532f9957c75d320
kyle-kauzlarich <kauzlarich.kyle@gmail.com>
2018-05-30 10:45:17 -0700

    code cleaned up

--- components/NoteRoutes.js
+++ components/NoteRoutes.js
@@ -33,32 +33,4 @@ module.exports = server => {
             res.status(200).send('deleted')
         ).catch(err => res.status(500).send(err));
     });
-    // server.post('/login', (req,res) => {
-    //     const { username, password } = req.body;
-    //     User.findOne({ username }).then(user => {
-    //         if (user) {
-    //             user.isPasswordValid(password).then(isValid => {
-    //                 if (isValid) {
-    //                     req.session.username = user.username;
-    //                     res.send("Access Granted");
-    //                 } else {
-    //                     res.status(400).send("Invalid Password");
-    //                 }
-    //             });
-    //         } else {
-    //             res.status(400).send("Invalid Username");
-    //         }
-    //     }).catch(err => res.send(err));
-    // });
-    // server.get('/logout', (req, res) => {
-    //     if (req.session) {
-    //         req.session.destroy(function(err) {
-    //             if(err) {
-    //                 res.send('error logging out, did you login');
-    //             } else {
-    //                 res.send('Goodbye');
-    //             }
-    //         });
-    //     }
-    // });
   };
