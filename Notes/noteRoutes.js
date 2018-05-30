2d6bb142b1de91b4366ee824c89f81002ca5960f
faulkenfett11 <36997363+faulkenfett11@users.noreply.github.com>
2018-05-30 13:12:19 -0700

    troubleshooting

--- Notes/noteRoutes.js
+++ Notes/noteRoutes.js
@@ -14,7 +14,7 @@ router.get('/', (req, res) => {
 })
 
 router.get('/:id', (req, res) => {
-	const id = req.params;
+	const { id } = req.params;
 	Note
 		.findById(id)
 		.then(note => {
@@ -38,7 +38,7 @@ router.post('/', (req, res) => {
 })
 
 router.delete('/:id', (req, res) => {
-	const id = req.params;
+	const { id } = req.params;
 	Note
 		.findByIdAndRemove(id)
 		.then(removedNote => {
@@ -50,7 +50,7 @@ router.delete('/:id', (req, res) => {
 })
 
 router.put('/:id', (req, res) => {
-	const id = req.params;
+	const { id } = req.params;
 	Note
 		.findByIdAndUpdate(id, req.body)
 		.then(updatedNote => {
