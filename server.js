f0662c577878d17c34c99cca60b466fb0b80c735
Justin Hong <jhong2009@gmail.com>
2018-05-30 14:49:35 -0700

    added start script to package.json

--- server.js
+++ server.js
@@ -10,9 +10,9 @@ server.use(express.json());
 // root route
 server.get("/", (req, res) => {
 	res.json({ Message: "Hello there friend" });
-})
+});
 
-server.listen(3000, (err) => {
+server.listen(3000, err => {
 	if (err) console.log(err);
 	console.log("\n === Server listening on port 3000 === \n");
 });
