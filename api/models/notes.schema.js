0deed8a30e5071e331ca063c8672cea3c44059e9
jeffrey <jeffreyneilflynn@gmail.com>
2018-05-30 10:39:05 -0400

    added date field to notes schema

--- api/models/notes.schema.js
+++ api/models/notes.schema.js
@@ -5,7 +5,8 @@ const notesSchema = new mongoose.Schema({
   title: { type: String, required: true },
   content: { type: String, required: true },
   tags: [String],
-  postedBy: { type: ObjectId, ref: 'User', required: true }
+  postedBy: { type: ObjectId, ref: 'User', required: true },
+  createdAt: { type: Date, default: Date.now() }
 })
 
 module.exports = mongoose.model('Note', notesSchema)
