a84fded48094cab86f8db64c05e6d945f1490215
Shawn Stewart <s.stewart0220@gmail.com>
2018-05-30 12:56:49 -0400

    Set up connection to mongoose

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 const port = process.env.PORT || 5000;
 
 const server = express();
@@ -8,10 +9,15 @@ server.use(express.json());
 server.use(cors());
 
 server.get('/', (req, res) => {
-  res.json({ message: `The API is running on port ${port}.`});
+  res.json({ message: `Are you supposed to be here?`});
 });
+
+mongoose
+  .connect('mongodb://localhost/Back-End-Project')
+  .then(() => console.log('\n=== Successfully connected to MongoDB ==='))
+  .catch(err => console.log('\n=== Error connecting to MongoDB ==='));
 
 server.listen(port, err => {
   if (err) console.log(err);
-  console.log(`\n=== API running on port ${port}==`);
+  console.log(`\n=== API running on port ${port} ===`);
 });
