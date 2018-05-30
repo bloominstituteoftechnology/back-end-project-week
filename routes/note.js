1bf15d72ed8f189aa34a1e5d6738e570891b2186
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2018-05-30 12:58:18 -0700

    got CRUD operations on notes routes working

--- routes/note.js
+++ routes/note.js
@@ -1,15 +1,14 @@
 const express = require('express');
-const jwt = require('jsonwebtoken');
 const router = express.Router();
-const passport = require('passport');
 
 const Note = require('../modules/notes');
 const User = require('../modules/users');
 
 router
   .route('/')
-  .get(passport.authenticate('jwt', {session: false}), (req, res) => {
-    Note.find({user: req.user.id})
+  .get((req, res) => {
+    Note.find()
+    .select('title body -_id')
     .then(notes => {
       if (notes.length === 0) {
         res.status(404).json('You have not made any notes')
@@ -20,18 +19,20 @@ router
       res.status(500).json(err);
     })
   })
-  .post(passport.authenticate('jwt', {session: false}), (req, res) => {
-    let noteContent = {};
-    noteContent.user = req.user.id;
-    noteContent.title = req.body.title;
-    noteContent.body = req.body.body;
-
-    new Note(noteContent).save().then(note => res.json(note));
+  .post((req, res) => {
+   Note
+    .create(req.body)
+    .then(note => {
+      res.status(200).json(note);
+    })
+    .catch(err => {
+      res.status(500).json('Error creating note');
+    })
   });
 
 router
   .route('/:id')
-  .get(passport.authenticate('jwt', {session: false}), (req, res) => {
+  .get((req, res) => {
     Note.findById(req.params.id)
       .then(note => {
         if (note.user === req.user.id) {
@@ -43,28 +44,27 @@ router
         res.status(500).json(err);
       })
   })
-  .delete(passport.authenticate('jwt', {session: false}), (req, res) => {
-    Note.findById(req.params.id)
-      .then(note => {
-        if (note.user === req.user.id) {
-          Note.findByIdAndRemove(req.params.id) 
-            .then(note => {
-              return res.status(204).json(note);
-            })
-        }
-        res.status(400).json('No note found');
+  .delete((req, res) => {
+    const id = req.params.id;
+    Note
+      .findByIdAndRemove(id)
+      .then(response => {
+        res.status(200).json('Note deleted successfully');
+      })
+      .catch(err => {
+        res.status(500).json('error deleting note');
       })
   })
-  .put(passport.authenticate('jwt', {session: false}), (req, res) => {
-    Note.findById(req.params.id)
-      .then(note => {
-        if (note.user === req.user.id) {
-          Note.findByIdAndUpdate(req.params.id, {$set: req.body}, {new: true})
-            .then(note => {
-              res.status(200).json(note)
-            })
-        }
-        res.status(404).json('No note found');
+  .put((req, res) => {
+    const id = req.params.id;
+    const note = req.body;
+    Note 
+      .findByIdAndUpdate(id, note)
+      .then(response => {
+        res.status(200).json({ note });
+      })
+      .catch(err => {
+        res.status(500).json('Error updating note');
       })
   });
 
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const notes = require('./routes/note');
+const users = require('./routes/user');
 
 server = express();
 
@@ -15,6 +16,7 @@ server.use(express.json());
 server.use(cors({}));
 
 server.use('/notes', notes);
+server.use('/users', users);
 
 server.get('/', (req, res) => {
   res.status(200).json({Message: 'Hello World'});
