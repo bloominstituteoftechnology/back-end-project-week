c04a0deb059b462df7ff68a360ac520f652036b3
Robert Hogan <roberthogan@Roberts-MacBook-Pro.local>
2018-05-29 22:25:37 -0400

    Robert Hogan. Error corrected. Completed get for list of notes, working on get for single note.

--- routes/routes.js
+++ routes/routes.js
@@ -4,11 +4,27 @@ const Note = require('../notes/notes'); // bring in our Note
 const router = express.Router();
 
 router 
-.get('/', (req, res) => {
+.get('/', (req, res) => { // get a list of notes in db
     Note
     .find()
     .then(notes => {
         res.status(200).json(notes);
+    })
+    .catch(err => {
+        res.status(500).json(err);
+    });
+})
+.get('/:id', (req, res) => { // get a single id from the database
+    const { id } = req.params;
+
+    notes 
+    .findById(id)
+    .then(note => {
+        if(!note) {
+            res.status(404).json({ message: 'The note with the specified ID was not found. Sorry!' });
+        } else {
+            res.status(200).json(note);
+        }
     })
     .catch(err => {
         res.status(500).json(err);
