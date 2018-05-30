90d16d5c628bacf09d0c94e14eac449f5256df6b
unknown <bsa6.23.94@gmail.com>
2018-05-30 12:09:50 -0400

    markdown autodeploy and users filled out

--- notes/userModel.js
+++ notes/userModel.js
@@ -1,7 +1,26 @@
 const mongoose = require('mongoose');
+const ObjectId = mongoose.Schema.Types.ObjectId;
 
+const definition = {
+    firstName: {
+        type: String,
+        required: true
+    },
+    LastName: {
+        type: String,
+        required: true
+    },
+    createdOn: {
+        type: Date,
+        required: true,
+        default: Date.now,
+    },
+};
 
+const options = {
+    timestamp: true
+}
 
-
-
-// module.exports = userModel;
+const userSchema = new mongoose.Schema(definition, options);
+const userModel = mongoose.model('User', userSchema, 'users');
+module.exports = userModel;
