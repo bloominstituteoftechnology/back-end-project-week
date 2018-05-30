3dd859620e2de9e8278c025f93ff5dd29ac2a43f
kyle-kauzlarich <kauzlarich.kyle@gmail.com>
2018-05-29 17:24:29 -0700

    get all notes

--- components/NoteRoutes.js
+++ components/NoteRoutes.js
@@ -8,6 +8,11 @@ module.exports = server => {
         .then(note => res.status(201).send(note))
         .catch(err => res.status(500).send(err));
     });
+    server.get('/viewnotes', (req, res) => {
+        Note.find().then(notes => {
+            res.status(200).json(notes);
+        }).catch(err => res.status(500).json(err));
+    });
     // server.post('/login', (req,res) => {
     //     const { username, password } = req.body;
     //     User.findOne({ username }).then(user => {
