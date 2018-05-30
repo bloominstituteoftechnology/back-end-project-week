0148b1e72687d2554f350054f609541db6f4c666
imsdhk <jappleleaf@gmail.com>
2018-05-30 18:13:15 -0400

    wip

--- server.js
+++ server.js
@@ -1,16 +1,32 @@
 const express = require('express'); 
 const cors = require('cors'); 
 const server = express(); 
+const mongoose = require('mongoose');
+const Routes = require('./src/notes/noteRouter'); 
+
+const mlab =  'mongodb://imsdhk:75W238st@ds121599.mlab.com:21599/backend_lambda'; 
+
 
 server.use(cors({})); 
 server.use(express.json()); 
-const port =  process.env.PORT || 33333 
+
+
+
+mongoose.connect(mlab, () => {
+    console.log('connected to mLab');
+});
+
+
 
 server.get('/', (req, res) => {
     res.json({message: 'hello'})
 })
 
 
+server.use('/', Routes)
+
+
+const port =  process.env.PORT || 33333 
 server.listen(port, (err) => {
     if(err) console.log(err);
 
---
+++ src/notes/noteModel.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const NoteModel = new mongoose.Schema({
+  noteTitle: {
+    type: String,
+    required: true
+  },
+  noteBody: {
+    type: String,
+    required: true
+  },
+  createdOn: {
+    date: Date
+  }
+});
+
+module.exports = mongoose.model('Note', noteModel);
