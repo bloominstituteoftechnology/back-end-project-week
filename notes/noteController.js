af9aef400c1229292126352d5e2e76c3783d62ee
ajf412 <aaronjfaulkner@yahoo.com>
2018-05-30 13:42:07 -0400

    MVP complete.  User authentication started, not completed.

--- notes/noteController.js
+++ notes/noteController.js
@@ -33,19 +33,47 @@ router
   });
 
 router
-.route('/:id')
-.get((req, res) => {
-  Note.find({ _id: req.params.id })
-  .then(note => {
-    if (note.length > 0) {
-      res.status(201).json({ note })
-    } else {
-      res.status(404).json({ errorMessage: "The note with the specified ID does not exist." });
-    }
+  .route('/:id')
+  .get((req, res) => {
+    Note.find({ _id: req.params.id })
+    .then(note => {
+      if (note.length > 0) {
+        res.status(201).json({ note })
+      } else {
+        res.status(404).json({ errorMessage: "The note with the specified ID does not exist." });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ errorMessage: "Note information could not be retrieved." });
+    });
   })
-  .catch(err => {
-    res.status(500).json({ errorMessage: "Note information could not be retrieved." });
+  .put((req, res) => {
+    const { id } = req.params;
+    const update = ({ $set: req.body });
+
+    Note.findById(id)
+      .then(note => {
+        note.update(update)
+        .then(() => {
+          res.status(201).json(req.body);
+        })
+        .catch(err => {
+          res.status(500).json({ errorMessage: "The note information could not be modified." });
+        });
+      });
+  })
+  .delete((req, res) => {
+    Note.remove({ _id: req.params.id })
+    .then((note) => {
+      if(note) {
+        res.status(200).json({ status: 'Deleted note with ID of ' + req.params.id });
+      } else {
+        res.status(404).json({ errorMessage: "The note with the specified ID does not exist." });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ errorMessage: "The note could not be removed." });
+    });
   });
-});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,17 @@
+// Frameworks
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors');
+const helmet = require('helmet');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const cors = require('cors');
 
+// Components
+const noteController = require('./notes/noteController');
+const User = require('./users/UserModel');
+
+// Connect to the database
 mongoose
   .connect('mongodb://boaz:wasruthless@ds139970.mlab.com:39970/my-bible-app')
   .then(mongo => {
@@ -12,19 +21,61 @@ mongoose
     console.log('Error connecting to the database', err);
   });
 
-const noteController = require('./notes/noteController');
-
+// Uses
 const server = express();
+server.use(helmet());
 server.use(cors({}));
 server.use(bodyParser.json());
-// server.use(express.json());
-
 server.use('/api/notes', noteController);
 
+// User Authentication
+function authenticate(req, res, next) {
+  if(req.session && req.session.username) {
+    next();
+  } else {
+    res.status(401).send('MENE MENE TEKEL UPHARSIN');
+  }
+}
+
+server.use(
+  session({
+    secret: 'The goodness of God leadeth thee to repentence.',
+    cookie: {
+      maxAge: 1 * 24 * 60 * 60 * 1000,
+    },
+    httpOnly: true,
+    secure: false,
+    resave: true,
+    saveUninitialized: false,
+    name: 'GodIsWatchingYou',
+    store: new MongoStore({
+      url: 'mongodb://localhost/sessions',
+      ttl: 60 * 10,
+    })
+  })
+);
+
+server.get('/api/users', authenticate, (req, res) => {
+  User.find().then(users => res.send(users));
+});
+
+server.post('/api/register', function(req, res) {
+  const user = new User(req.body);
+  user
+    .save()
+    .then(user => res.status(201).send(user))
+    .catch(err => res.status(500).send(err));
+})
+
+server.post('/api/login')
+
+
+// Basic get test
 server.get('/', (req, res) => {
   res.json({ Message: 'Hello World' });
 });
 
+// Open server to port
 const port = process.env.PORT || 3333;
 server.listen(port, err => {
   if(err) console.log(err);
