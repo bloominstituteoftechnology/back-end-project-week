5be343f0d4adf8a467b9dea8b4b7912ccc659829
Robert Hogan <roberthogan@Roberts-MacBook-Pro.local>
2018-05-29 20:46:19 -0400

    Robert Hogan. correction of deployment error. Beginning to create routes.

--- server.js
+++ server.js
@@ -2,14 +2,24 @@ const express = require('express'); // import express
 const server = express();
 const cors = require('cors');
 const Joi = require('joi'); // validator
+const mongoose = require('mongoose'); // mongoose
+const router = require('./routes/routes'); // routes
 
 server.use(express.json()); // middleware to parse json objs. 
-server.use(cors({}));
+server.use('/api/notes', router); // routes
+server.use(cors());
+
+//connect to mongo...
+mongoose.connect('mongodb://rhogan29:121994bob@ds041593.mlab.com:41593/lambda-notes', {}, err => {
+    if (err) return console.log(err);
+    console.log('=== Connected to database! ===');
+})
+
 
 server.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-//no hard-coded port below
+//thou shalt hath no hard coded ports, so says I...
 const port = process.env.PORT || 5000;
-server.listen(port, () => console.log(`Server listening on ${port}...`)); // listen on port
+server.listen(port, () => console.log(`=== Server listening on ${port}... ===`)); // listen on port
