894b188bc00a7edd21d404b31f184178039e9049
Justin Rigler <justinmrigler@gmail.com>
2018-05-30 12:42:33 -0400

    Magic?

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const Port = 3000;
+const Port = process.env.PORT || 3000;
 
 // create express app
 const server = express();
