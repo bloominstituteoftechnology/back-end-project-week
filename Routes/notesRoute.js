e594ae024dcf3c3ca50fa407bf66a0a9067bc5d5
Eric Gomez <eric_gomez_1@hotmail.com>
2018-05-31 09:29:34 -0700

    my notes routes are all set up and ready to go

--- Routes/notesRoute.js
+++ Routes/notesRoute.js
@@ -8,3 +8,35 @@ router.route('/')
         res.json(r);
       })
   })
+  .post((req, res) => {
+    const note = new Note(req.body);
+    note.save()
+    .then(r => {
+      res.status(201).json(r)
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    })
+  })
+router.route('/:id')
+  .put((req, res) => {
+    const {id} = req.params;
+    Note.findByIdAndUpdate(id, req.body, {new: true}, (err, note) => {
+      if (err) return res.status(500).send(err);
+      return res.status(200).json(note);
+    })
+  })
+  .delete((req, res) => {
+    const {id} = req.params;
+    Note.findByIdAndRemove(id, (err, note) => {
+      if(err) return res.status(500).json(err);
+      else {
+        Note.find()
+        .then(r => {
+          res.status(200).json(r);
+        })
+      }
+    })
+  })
+
+module.exports = router;
