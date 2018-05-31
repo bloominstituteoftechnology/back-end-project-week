a9e97b5a266c4a7bd8d538e970f727f51815e5ba
2940cristian <2940cristian@gmail.com>
2018-05-31 16:09:15 -0700

    Added extra code to github redirect

--- Server.js
+++ Server.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 const userRouter = require("./Routers/UserRouter")
 const notesRouter = require("./Routers/NotesRouter")
 const StripeRouter = require("./Routers/StripeRouter")
+const request = require("superagent")
 
 //Database
 let uri = 'mongodb://2940cristian:dance360@ds235840.mlab.com:35840/noteslambda';
@@ -40,13 +41,25 @@ server.get("user/signin/callback", (req, res, next) => {
   const {code} = query;
 
   if(!code) {
-    return res.send({
+     res.send({
       success: false
     })
-  }
+  };
 
+  request
+  .post("https://github.com/login/oauth/access_token")
+  .send({
+    client_id: "7e0997d4f79135cba0f9",
+    client_secret: "6ecf33a0078cbff980b00c79ed03908477fd1d56",
+    code: code
+  })
+  .set("Accept", "application/json")
+  .then(function(result) {
+    const data = req.body;
+    result.send(data);
+  })
 
-  console.log('code', code)
+  
 })
 
 
