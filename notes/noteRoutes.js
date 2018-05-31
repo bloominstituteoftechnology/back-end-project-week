24d86d54618c4ed789c8f11a70dec98b17b1b88e
tanking <tanking@github.com>
2018-05-31 13:14:00 -0400

    Refactored server to add note routes

---
+++ notes/noteRoutes.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const router = express.Router();
+const Note = require('./Note');
+
+// POST notes - Title + Body/Content
+router.post('/notes', (req, res) => {
+
+    Note
+    .create(req.body)
+    .then(note => {
+        res.status(201).json({ note })
+    })
+    .catch(err => {
+        res.status(500).json({ Err: 'error creating note' })
+    })
+})
+
+// GET notes - Display Notes
+router.get('/notes', (req, res) => {
+
+    Note
+    .find().select('title body id')
+    .then(notes => {
+        res.status(200).json({ notes })
+    })
+    .catch(err => {
+        res.status(500).json({ Error: 'Notes not found'})
+    })
+})
+
+// GET notes - Display a specific note
+router.get('/note/:id', (req, res) => {
+    const id = req.params.id;
+
+    Note
+    .findById(id).select('title body')
+    .then(note => {
+        res.status(200).json({ note })
+    })
+    .catch(err => {
+        res.status(404).json({ Error: 'Cannot fulfill request '})
+    })
+})
+
+// PUT /note - Edits a note
+router.put('/note/:id', (req, res) => {
+    const id = req.params.id;
+    const note = req.body;
+
+    Note
+    .findByIdAndUpdate(id, note)
+    .then(response => {
+        res.status(200).json({ note })
+    })
+    .catch(err => {
+        res.status(500).json({ err: "Cannot edit"})
+    })
+})
+
+// DELETE /note
+router.delete('/note/:id', (req, res) => {
+    const id = req.params.id;
+    
+    Note
+    .findByIdAndRemove(id)
+    .then(note => {
+        res.status(200).send('Note deleted successfully');
+    })
+    .catch(err => {
+        res.status(500).json({ Error: "Cannot delete "})
+    })
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,16 +3,16 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
-const token = jwt.sign({ payload: 'sfksalfdsmfdmalfakmsda'}, 'lmefafjsmfoiawejwfmejwksfdm')
+const secret = 'jagskdflasasaarandomstring';
 
-const Note = require('./notes/Note');
+const noteRouter = require('./notes/noteRoutes');
 
 const server = express();
 
 const mongoDB = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@ds139920.mlab.com:39920/lambdanotes`;
 const port = process.env.PORT || 3001;
 
-// Connect to mongo
+// Connect to mlab
 mongoose
     .connect(mongoDB)
     .then(connect => {
@@ -25,79 +25,11 @@ mongoose
 // middleware
 server.use(express.json());
 server.use(cors());
+server.use('/api', noteRouter);
 
 // Initial GET
 server.get('/', (req, res) => {
     res.send({ Message: 'api running' })
-})
-
-// POST notes - Title + Body/Content
-server.post('/api/notes', (req, res) => {
-
-    Note
-    .create(req.body)
-    .then(note => {
-        res.status(201).json({ note })
-    })
-    .catch(err => {
-        res.status(500).json({ Err: 'error creating note' })
-    })
-})
-
-// GET notes - Display Notes
-server.get('/api/notes', (req, res) => {
-
-    Note
-    .find().select('title body id')
-    .then(notes => {
-        res.status(200).json({ notes })
-    })
-    .catch(err => {
-        res.status(500).json({ Error: 'Notes not found'})
-    })
-})
-
-// GET notes - Display a specific note
-server.get('/api/note/:id', (req, res) => {
-    const id = req.params.id;
-
-    Note
-    .findById(id).select('title body')
-    .then(note => {
-        res.status(200).json({ note })
-    })
-    .catch(err => {
-        res.status(404).json({ Error: 'Cannot fulfill request '})
-    })
-})
-
-// PUT /api/note - Edits a note
-server.put('/api/note/:id', (req, res) => {
-    const id = req.params.id;
-    const note = req.body;
-
-    Note
-    .findByIdAndUpdate(id, note)
-    .then(response => {
-        res.status(200).json({ note })
-    })
-    .catch(err => {
-        res.status(500).json({ err: "Cannot edit"})
-    })
-})
-
-// DELETE /api/note
-server.delete('/api/note/:id', (req, res) => {
-    const id = req.params.id;
-    
-    Note
-    .findByIdAndRemove(id)
-    .then(note => {
-        res.status(200).send('Note deleted successfully');
-    })
-    .catch(err => {
-        res.status(500).json({ Error: "Cannot delete "})
-    })
 })
 
 server.listen(port, () => console.log(`Server running on port: ${port}`))
