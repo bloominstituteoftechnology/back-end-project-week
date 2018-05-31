061949ce2a52a548937515ad167b19aa2fbe5adc
tanking <tanking@github.com>
2018-05-31 13:46:51 -0400

    GET route for list of users

--- server.js
+++ server.js
@@ -4,16 +4,16 @@ const helmet = require('helmet');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
-const secret = 'jagskdflasasaarandomstring';
 
+// Routes
 const noteRouter = require('./notes/noteRoutes');
+const userRouter = require('./users/userRoutes');
 
+// Initialize Server
 const server = express();
 
-const mongoDB = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@ds139920.mlab.com:39920/lambdanotes`;
-const port = process.env.PORT || 3001;
-
 // Connect to mlab
+const mongoDB = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@ds139920.mlab.com:39920/lambdanotes`;
 mongoose
     .connect(mongoDB)
     .then(connect => {
@@ -23,14 +23,17 @@ mongoose
         console.log('Not connected');
 })
 
-// middleware
+// Middleware
 server.use(express.json());
 server.use(cors());
 server.use('/api', noteRouter);
+server.use('/api', userRouter);
 
 // Initial GET
 server.get('/', (req, res) => {
     res.send({ Message: 'api running' })
 })
 
+// Connect to port
+const port = process.env.PORT || 3001;
 server.listen(port, () => console.log(`Server running on port: ${port}`))
--- users/userRoutes.js
+++ users/userRoutes.js
@@ -4,7 +4,7 @@ const User = require('./User');
 
 // POST - create a new user
 router.post('/user', (req,res) => {
-    
+
     User
     .create(req.body)
     .then(user => {
@@ -12,6 +12,19 @@ router.post('/user', (req,res) => {
     })
     .catch(err => {
         res.status(500).json({ Error: err})
+    })
+})
+
+// GET - retrieve list of users created
+router.get('/users', (req, res) => {
+
+    User
+    .find()
+    .then(users => {
+        res.status(200).json({ users })
+    })
+    .catch(err => {
+        res.status(500).json({ Error: err })
     })
 })
 
