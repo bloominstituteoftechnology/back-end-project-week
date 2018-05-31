30c3a7a46957b7d72934fb1baf2c7bba60b6f4d8
Justin Hong <jhong2009@gmail.com>
2018-05-30 23:04:46 -0700

    in usersRouter.js: fixed error where user was not saved

---
+++ users/usersRouter.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const router = express.Router();
+
+const User = require("./User");
+
+router.route("/").post((req, res) => {
+	const newUser = new User(req.body);
+
+	if (!req.body.username) {
+		res.status(400).json({ errorMessage: "Please provide a username" });
+	}
+
+	newUser
+		.save()
+		.then(newUser => {
+			res.status(201).json(newUser);
+		})
+		.catch(err => {
+			res.status(500).json({
+				errorMessage: "There was an error saving the user to the database."
+			});
+		});
+});
+
+module.exports = router;
