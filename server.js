de53e74ae286c1b08c2663830719ac7e76c1c0bc
tanking <tanking@github.com>
2018-05-31 15:19:40 -0400

    Added helper functions for authentication

--- server.js
+++ server.js
@@ -29,6 +29,26 @@ server.use(cors());
 server.use('/api', noteRouter);
 server.use('/api', userRouter);
 
+// Helper Functions
+const getTokenForUser = userObject => {
+    return jwt.sign(userObject, process.env.TOKEN_SECRET, { expiresIn: '1h' })
+}
+
+const validateToken = (req, res, next) => {
+    const token = req.headers.authorization;
+    if (!token) {
+        res.status(422).json({ Error: 'No token found' })
+    } else {
+        jwt.verify(token, process.env.TOKEN_SECRET, (err, decoded) => {
+            if (err) {
+                res.status(401).json({ Error: "Token invalid", message: err });
+            } else {
+                next();
+            }
+        })
+    }
+}
+
 // Initial GET
 server.get('/', (req, res) => {
     res.send({ Message: 'api running' })
