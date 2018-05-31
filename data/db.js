ec5a05353847d0d23be2fed753b287b768af8100
Justin Hong <jhong2009@gmail.com>
2018-05-31 10:38:34 -0700

    removed useMongoClient

--- data/db.js
+++ data/db.js
@@ -5,8 +5,7 @@ mongodb: module.exports = {
 		return mongoose.connect(
 			encodeURI(
 				`mongodb://justin:password123@ds139970.mlab.com:39970/lambdanotes-justinh`
-			),
-			{ useMongoClient: true }
+			)
 		);
 	}
 };
