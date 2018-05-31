7e51d7348251319b0f8d9eabe2ddf66560598a2d
Justin M <jekm321@gmail.com>
2018-05-31 16:55:57 -0400

    fixing db

--- server.js
+++ server.js
@@ -1,29 +1,29 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-// const { user, password } = require('./config/user');
+const { user, password } = require('./config/user');
 
 const port = process.env.PORT || 5000;
 
-mongoose.connect('mongodb://localhost/backEndProject')
-.then(mongo => {
-    console.log('connected to mongo');
-})
-.catch(err => {
-    console.log('error conneccting to mongo');
-});
+// mongoose.connect('mongodb://localhost/backEndProject')
+// .then(mongo => {
+//     console.log('connected to mongo');
+// })
+// .catch(err => {
+//     console.log('error conneccting to mongo');
+// });
 
 const notesController = require('./notes/notesController');
 const {routes, authenticate} = require('./users/usersController');
 
-// mongoose
-//     .connect(`mongodb://${user}:${password}@ds123084.mlab.com:23084/lambda-notes`)
-//     .then(res => {
-//         console.log('\n connected to mongodb \n');
-//     })
-//     .catch(err => {
-//         console.log('error connecting to mongodb',user,password, err);
-//     });
+mongoose
+    .connect(`mongodb://${user}:${password}@ds123084.mlab.com:23084/lambda-notes`)
+    .then(res => {
+        console.log('\n connected to mongodb \n');
+    })
+    .catch(err => {
+        console.log('error connecting to mongodb',user,password, err);
+    });
 
 const server = express();
 
