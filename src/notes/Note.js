6f0290a70b8ae869751101ba66a7707f2463ca81
sagdish <forsi1@yahoo.com>
2018-05-31 11:16:40 -0700

    updating Note.js schema

--- src/notes/Note.js
+++ src/notes/Note.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
+const ObjectId = mongoose.Schema.Types.ObjectId;
 
-const NoteSchema = mongoose.Schema({
+const NoteSchema = new mongoose.Schema({
+  user: {
+    type: ObjectId,
+    ref: 'User',
+  },
   title: {
     type: String,
     required: true,
