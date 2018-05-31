183b6b66ef91e69b7c5f163db68ca6f57ce4dfc0
Justin Hong <jhong2009@gmail.com>
2018-05-30 23:59:05 -0700

    in usersRouter.js: modified post request

--- users/usersRouter.js
+++ users/usersRouter.js
@@ -4,23 +4,21 @@ const router = express.Router();
 const User = require("./User");
 
 router.route("/").post((req, res) => {
-	const newUser = new User(req.body);
-	console.log(newUser);
 	if (!req.body.username) {
 		res.status(400).json({ errorMessage: "Please provide a username" });
-	}
-
-	newUser
-		.save()
-		.then(newUser => {
-			console.log(newUser);
-			res.status(201).json(newUser);
-		})
-		.catch(err => {
-			res.status(500).json({
-				errorMessage: "There was an error saving the user to the database."
+	} else {
+		const newUser = req.body;
+		newUser
+			.save()
+			.then(newUser => {
+				res.status(201).json(newUser);
+			})
+			.catch(err => {
+				res.status(500).json({
+					errorMessage: "There was an error saving the user to the database."
+				});
 			});
-		});
+	}
 });
 
 module.exports = router;
