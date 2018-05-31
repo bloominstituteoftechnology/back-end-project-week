f4e7fe252b01b9af920bc0aaf3a07875962a31d4
Shawn Stewart <s.stewart0220@gmail.com>
2018-05-31 00:39:51 -0400

    Editted get by ID

--- Notes/NoteController.js
+++ Notes/NoteController.js
@@ -16,7 +16,11 @@ const NoteController = {
     Note
       .findById(id)
       .then(response => {
-        res.status(200).json({ note: response })
+        if (response === null) {
+          res.status(404).json({ error: 'Error fetching note', err });
+        } else {
+          res.status(200).json({ note: response })
+        }
       })
       .catch(err => {
         res.status(404).json({ error: 'Error fetching note', err });
