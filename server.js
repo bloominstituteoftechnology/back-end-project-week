6042df6c19ae47d87fab9aa9c8762f7ea5258f0b
cgoez <goez2001@gmail.com>
2018-05-31 13:45:40 -0700

    Add mongodb to server

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+
+const { mongoLAB } = require("./api/utils/config");
 
 const server = express();
 const port = process.env.PORT || 3000;
 
 server.use(cors({}));
+
+mongoose
+  .connect(mongoLAB)
+  .then(() => {
+    console.log("\n~~~ Connected to MongoDB! ~~~\n");
+  })
+  .catch(error =>
+    console.log("\n --- Error connecting to MongoDB, send help! --- \n")
+  );
 
 server.get("/", (req, res) => res.send("API is running!"));
 
