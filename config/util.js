95cb2889f6090248fd0b0071ffa0f795359297b8
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2018-05-31 13:14:02 -0700

    refactored

--- config/util.js
+++ config/util.js
@@ -2,18 +2,17 @@ const jwt = require('jsonwebtoken');
 const User = require('../modules/users');
 
 const getToken = user => {
-  const paylaod = {
+  const payload = {
     id: user._id,
-    session: user.session
   }
-  return jwt.sign(paylaod, "t19y95l11a10r");
+  return jwt.sign(payload, "t19y95l11a10r");
 };
 
 const getUserToken = async (token) => {
   let user;
   try {
     const paylaod = jwt.verify(token, 't19y95l11a10r')
-    user = await User.findOne({_id: paylaod.id, session: paylaod.session})
+    user = await User.findOne({_id: payload.id})
   } catch (err) {
     user = null;
   }
