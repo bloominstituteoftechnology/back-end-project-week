6af9b0ed300337a4ff8e017a6a8171f5943828e6
unknown <sycode@gmail.com>
2018-05-30 21:54:48 -0600

    server created

--- server.js
+++ server.js
@@ -1,10 +1,32 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 8899
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const noteController = require("./notes/notesController");
+const userController = require("./users/userController");
+
+const port = process.env.PORT || 5000;
+
+const server = express();
+
+if (process.env.NODE_ENV !== 'test') {
+  server.listen(port, () => {
+    console.log(`Server running on ${port}`);
+  });
+  mongoose.connect("mongodb://${loginInfo.username}:${loginInfo.password}@")
+		.then(() => console.log('Successfully connected to MongoDB'))
+		.catch(err => console.log('Error connecting to MongoDB'));
+}
+
+server.use(helmet());
+server.use(cors());
+server.use(morgan("combined"));
+server.use(express.json());
+server.use("/notes", noteController);
+server.use("/user", userController);
+
+
+
+module.exports = server;
