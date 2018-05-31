ab01e9664adce01641c74e651460a29a0f631216
cgoez <goez2001@gmail.com>
2018-05-31 11:34:17 -0700

    Create controller folder, add userRegister

---
+++ controllers/userRegister.js
@@ -0,0 +1,29 @@
+const bcrypt = require("bcrypt");
+
+const User = require("../models/User.js");
+
+const userRegister = (req, res) => {
+  const { username, password, email } = req.body;
+  const user = new User({ username, password, email });
+
+  if (username && password && email) {
+    user
+      .save()
+      .then(newUser => {
+        // Check to see if user is made
+        console.log(newUser);
+        res.status(201).json(newUser);
+      })
+      .catch(error => {
+        res.status(500).json({
+          error: "There was an error during registration."
+        });
+      });
+  } else {
+    res.status(422).json({
+      message: "Please provide a valid username, password, and email address."
+    });
+  }
+};
+
+module.exports = { userRegister };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,20 +4,26 @@ const User = require("../models/User.js");
 
 const router = express();
 
-router.post("/register", (req, res) => {
-  const { username, password } = req.body;
-  const user = new User({ username, password });
+router.route("/register").post((req, res) => {
+  const { username, password, email } = req.body;
+  const user = new User({ username, password, email });
 
   user
     .save()
     .then(newUser => {
-      res.status(200).json(newUser);
+      res.status(201).json(newUser);
     })
     .catch(error => {
       res.status(500).json({
         error: "There was an error registering the new user."
       });
     });
+});
+
+router.route("/login").post((req, res) => {
+  const { username, password, email } = req.body;
+
+  
 });
 
 module.exports = router;
