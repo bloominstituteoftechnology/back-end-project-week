ae662d98580350c51e68f96b0ce94a4e27f0eb2a
Cruise Brantley <cruise.brantley@gmail.com>
2018-05-30 21:01:18 -0500

    Getting more information from heroku

--- middlewares/middlewares.js
+++ middlewares/middlewares.js
@@ -13,7 +13,7 @@ const authenticate = (req, res, next) => {
       next();
     });
   } else {
-    console.log("Repelled Invader");
+    console.log("Repelled Invader", token);
     return res.status(403).json({
       error: "You're not allowed in here!"
     });
