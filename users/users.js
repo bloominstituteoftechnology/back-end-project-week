73c435eadfbe2b0df539105acf0f8828ee6d43a0
Paul Fang <paulfang@pauls-mbp.home>
2018-05-31 16:08:34 -0700

    Working on Users.js

---
+++ users/users.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const Schema = mongoose.Schema;
+
+const SALT_ROUNDS = 11;
+
+const UserSchema = Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+});
+
+UserSchema.pre("save", function(next) {
+    bcrypt.hash(this.password, SALT_ROUNDS, (err, hash) => {
+        if(err) return next(err);
+        this.password = hash;
+        return next();
+    });
+});
+
+UserSchema.methods.checkPassword = function(plainTextPW, callBack) {
+    console.log(plainTextPW, this.password);
+    return bcrypt.compare(plainTextPW, this.password, callBack);
+};
+
+module.exports = mongoose.model("User", UserSchema);
