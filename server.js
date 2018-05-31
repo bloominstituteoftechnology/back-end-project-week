dd2b240c90bb923c8db0a27531af45d922f80a92
Paul Fang <paulfang@pauls-mbp.home>
2018-05-31 16:25:07 -0700

    Completed Server.js

--- server.js
+++ server.js
@@ -7,20 +7,19 @@ const server = express();
 
 const router = require('./routes/routes');
 
-server.use(express.json());
-server.use('/api/notes', router);
-server.use(cors());
+server.use(express.json())
+server.use(cors({}));
+server.use('/notes', noteRoutes);
 
 
-mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds011840.mlab.com:11840/lambda-notes', {}, err => {
-    if (err) return console.log(err);
-    console.log('=== Connected to database! ===');
-})
-
+mongoose
+	.connect("mongodb://<dbuser>:<dbpassword>@ds011840.mlab.com:11840/lambda-notes")
+	.then(() => console.log('==connected=='))
+	.catch(() => console.log('==error connecting=='))
 
 server.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+	res.json('==Server is connected==')
+})
 
 server.listen(port, err => {
     if(err) console.log(err);
