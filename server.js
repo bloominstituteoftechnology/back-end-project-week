850918814f435af3156b96c12437375a3155f621
MINE <Mine@MINEs-MacBook-Pro-2.local>
2018-05-31 11:18:12 -0700

    Heroku Live Testing 4

--- server.js
+++ server.js
@@ -1,74 +1,73 @@
 // npm or yarn init -y
 
-// const server = require('./server');
-// const express = require('express');
-// // const session = require('express-session');
-// const helmet = require('helmet');
-// // const cors = require('cors');
-// const bodyParser = require('body-Parser');
-// const mongoose = require('mongoose');
-
-// const server = express();
-
-// // const routes = require('./routes/routes');
-
-// // const corsOptions = {
-// //   origin: 'https://amanda-lambdanotes.herokuapp.com/',
-// //   credentials: true
-// // };
-
-// server.use(express.json());
-// server.use(helmet());
-// // server.use(cors(corsOptions));
-// server.use(bodyParser.json());
-
-// mongoose
-//   .connect('mongodb://amanda:amanda@ds133550.mlab.com:33550/lambdanotes-backend')
-//   .then(() => console.log('mLab Connected to Mongo'))
-//   .catch((err) => console.log(err))
-
-// // routes(server);
-
-// server.get('/', (req, res) => {
-//   res.send({ API: 'Running' });
-// });
-
-// const PORT = process.env.PORT || 5000;
-
-// server.listen(PORT, () =>
-//   console.log(`API running on ${PORT}`)
-// );
-
-// // module.exports = { server };
-
 const express = require('express');
-const mongoose = require('mongoose');
+// const session = require('express-session');
 const helmet = require('helmet');
-const cors = require('cors');
+// const cors = require('cors');
+const bodyParser = require('body-Parser');
+const mongoose = require('mongoose');
 
 const server = express();
 
-const corsOptions = {
-  origin: 'https://amanda-lambdanotes.herokuapp.com/',
-  credentials: true,
-};
+// const routes = require('./routes/routes');
+
+// const corsOptions = {
+//   origin: 'https://amanda-lambdanotes.herokuapp.com/',
+//   credentials: true
+// };
+
+server.use(express.json());
+server.use(helmet());
+// server.use(cors(corsOptions));
+server.use(bodyParser.json());
 
 mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log('\n=== Connected to Mongo ===\n');
-  })
-  .catch(err => {
-    console.log(err);
-  });
+  .connect('mongodb://amanda:amanda@ds133550.mlab.com:33550/lambdanotes-backend')
+  .then(() => console.log('mLab Connected to Mongo'))
+  .catch((err) => console.log(err))
 
-server.use(cors(corsOptions));
-server.use(helmet());
-server.use(express.json());
+// routes(server);
 
-routes(server);
-
-const port = process.env.PORT || 5000;
-server.listen(port, () => {
-  console.log(`Server up and running on ${port}`);
+server.get('/', (req, res) => {
+  res.send({ API: 'Running' });
 });
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () =>
+  console.log(`API running on ${PORT}`)
+);
+
+// module.exports = { server };
+
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const helmet = require('helmet');
+// const cors = require('cors');
+
+// const server = express();
+
+// const corsOptions = {
+//   origin: 'https://amanda-lambdanotes.herokuapp.com/',
+//   credentials: true,
+// };
+
+// mongoose
+//   .connect(process.env.MONGO_URI)
+//   .then(() => {
+//     console.log('\n=== Connected to Mongo ===\n');
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
+
+// server.use(cors(corsOptions));
+// server.use(helmet());
+// server.use(express.json());
+
+// routes(server);
+
+// const port = process.env.PORT || 5000;
+// server.listen(port, () => {
+//   console.log(`Server up and running on ${port}`);
+// });
