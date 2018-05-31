0b82a412ab255539dfd217be5a4e8f9c93a60279
jeffrey <jeffreyneilflynn@gmail.com>
2018-05-31 13:42:47 -0400

    minor type

--- api/controllers/index.js
+++ api/controllers/index.js
@@ -1,11 +1,9 @@
 const userRouter = require("./users.controller");
 const loginRouter = require("./login.controller");
 const notesRouter = require("./notes.controller");
-const googleRouter = require("./google.controller");
 
 module.exports = {
   userRouter,
   loginRouter,
-  notesRouter,
-  googleRouter
+  notesRouter
 }
