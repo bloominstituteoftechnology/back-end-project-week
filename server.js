0d16ec3769f5a905fb935fc44a137f85020295e2
Ashiko124 <greeneselectronics@gmail.com>
2018-05-31 21:20:35 -0600

    Altered app.js and server.js

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
+const mongoose = require('mongoose');
 const server = express();
 
 server.use(cors({}));
 server.use(bodyParser.json());
+
+
 
 server.get('/', (req, res) => {
     res.json({Message: 'I am alive!'});
@@ -22,5 +24,17 @@ server.put('/:id', (req, res) => {
     }
 })
 
+
+const port = process.env.PORT || 3333;
+
+mongoose.connect('mongodb://localhost/').then(() => {
+    console.log('connected to production database');
+}).catch(err => {
+    console.log('Error connection to production database')
+})
+
+server.listen(port, () => {
+    console.log(`Magic is happening on port ${port}`)
+})
 
 
