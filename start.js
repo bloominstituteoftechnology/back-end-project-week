a98a881305159589d9b72711690474a0d7fb0584
Eric Andrade <eric.andrade@gmail.com>
2018-06-01 09:42:50 -0400

    mLab and db connected. form still not passing data

--- start.js
+++ start.js
@@ -1,7 +1,10 @@
 require('dotenv').config();
+require('./models/Registration');
+
+const app = require('./app');
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.DATABASE, { useMongoClient: true });
+mongoose.connect(process.env.DATABASE);
 mongoose.Promise = global.Promise;
 mongoose.connection
   .on('connected', () => {
@@ -10,9 +13,6 @@ mongoose.connection
   .on('error', (err) => {
     console.log(`Connection error: ${err.message}`);
   });
-
-require('./models/Registration');
-const app = require('./app');
 
 const server = app.listen(3000, () => {
   console.log(`Express is running on port ${server.address().port}`);
