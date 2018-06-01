67af05652ea59b21c4c20ae865679beadee450ca
Ashiko124 <greeneselectronics@gmail.com>
2018-05-31 21:28:46 -0600

    altered app.js

--- greenesnotes/src/App.js
+++ greenesnotes/src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
   }
 
   callApi = async () => {
-    const response = await fetch('/');
+    const response = await fetch('/api/hello');
     const body = await response.json();
 
     if (response.status !== 200) throw Error(body.message);
