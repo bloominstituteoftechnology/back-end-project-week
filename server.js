6549baeb8360dfd19afd300058a4d182f1dc5057
Mark Wong <fmarkwong@gmail.com>
2018-06-01 12:15:12 -0700

    disable protected routes temp

--- server.js
+++ server.js
@@ -117,23 +117,23 @@ server.get('/logout', asyncHandler((req, res) => {
   res.redirect('/');
 }));
 
-server.post('/api/notes', protected, asyncHandler(async (req, res) => {
+server.post('/api/notes',  asyncHandler(async (req, res) => {
   const response = await Note.create(req.body)
   res.status(201).json(response);
 }));
 
-server.get('/api/notes', protected, asyncHandler(async (req, res) => {
+server.get('/api/notes',  asyncHandler(async (req, res) => {
   const response = await Note.find()
   res.status(200).json(response);
 }));
 
-server.get('/api/notes/:id', protected, asyncHandler(async (req, res) => {
+server.get('/api/notes/:id',  asyncHandler(async (req, res) => {
   const response = await Note.findById(req.params.id)
     || `Note with id ${req.params.id} not found`;
   res.status(200).json(response);
 }));
 
-server.put('/api/notes/:id', protected, asyncHandler(async (req, res) => {
+server.put('/api/notes/:id',  asyncHandler(async (req, res) => {
   // const sentScore = sentiment.analyze(req.body.content);
   // req.body.sentiment = sentScore.score;
   // req.body.comparative = sentScore.comparative;
@@ -157,7 +157,7 @@ server.put('/api/notes/:id', protected, asyncHandler(async (req, res) => {
   res.status(200).json(response);
 }));
 
-server.delete('/api/notes/:id', protected, asyncHandler(async (req, res) => {
+server.delete('/api/notes/:id',  asyncHandler(async (req, res) => {
   const response = await Note.findByIdAndRemove(req.params.id)
     || `Note with id ${req.params.id} not found`;
   res.status(200).json({ message: `Note with id ${response._id} deleted.`});
