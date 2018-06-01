12cb49ae9d1f7e286582c6daf17102eb57a69640
Henry K <mykim1891@hotmail.com>
2018-06-01 12:30:16 -0700

    Worked on Navbar

--- 1_react-client/src/App.js
+++ 1_react-client/src/App.js
@@ -11,7 +11,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <p className="App-intro">NoteIt.</p>
         <Route
           exact
           path="/"
--- 1_react-client/src/components/NoteList.js
+++ 1_react-client/src/components/NoteList.js
@@ -4,6 +4,8 @@ import { Link } from "react-router-dom";
 
 import NoteCard from "./NoteCard";
 
+import "./NoteList.css";
+
 export default class NoteList extends Component {
   constructor() {
     super();
@@ -47,6 +49,7 @@ export default class NoteList extends Component {
   }
 
   render() {
+    // search function
     let filteredNotes = this.state.notes.filter(note => {
       return (
         note.title.toLowerCase().indexOf(this.state.search.toLowerCase()) !==
@@ -56,33 +59,60 @@ export default class NoteList extends Component {
       );
     });
 
+    // alert if not logged in
+    this.state.isUnauthorized
+      ? alert("Please log-in first to view your notes.")
+      : null;
+
     return (
       <div>
-        {this.state.isUnauthorized
-          ? alert("Please log-in first to view your notes.")
-          : null}
-
-        <button onClick={this.signOutHandler}>Sign out</button>
-        <br />
-        <br />
-        <input
-          className="SearchBar_Input form-control"
-          placeholder="Search note"
-          type="text"
-          value={this.state.search}
-          onChange={this.updateSearch.bind(this)}
-        />
-        <h1>Your Notes:</h1>
-        {filteredNotes.map(eachNote => (
-          <div key={eachNote._id}>
-            <Link
-              to={`/notes/${eachNote._id}`}
-              style={{ textDecoration: "none" }}
-            >
-              <NoteCard eachNote={eachNote} props={this.props} />
-            </Link>
+        <nav className="NavBar">
+          <div className="Nav-left">
+            <h3>
+              <a class="active" href="/notes">
+                Home
+              </a>
+            </h3>
+            <h3>
+              <a href="#contact">Contact</a>
+            </h3>
+            <h3>
+              <a href="#support">Support</a>
+            </h3>
           </div>
-        ))}
+
+          <div className="Nav-middle">
+            <input
+              className="SearchBar_Input"
+              placeholder="Search note"
+              type="text"
+              value={this.state.search}
+              onChange={this.updateSearch.bind(this)}
+            />
+          </div>
+
+          <div className="Nav-right">
+            <h3>
+              <a href="/login" onClick={this.signOutHandler}>
+                Sign out
+              </a>
+            </h3>
+          </div>
+        </nav>
+
+        <body className='NoteList-body'>
+          <h1>Your Notes:</h1>
+          {filteredNotes.map(eachNote => (
+            <div key={eachNote._id}>
+              <Link
+                to={`/notes/${eachNote._id}`}
+                style={{ textDecoration: "none" }}
+              >
+                <NoteCard eachNote={eachNote} props={this.props} />
+              </Link>
+            </div>
+          ))}
+        </body>
       </div>
     );
   }
