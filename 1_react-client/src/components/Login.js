c2860c1fe825b0397c3c446221af8aa3ce4f1672
Henry K <mykim1891@hotmail.com>
2018-06-01 14:52:28 -0700

    Login page fully styled

--- 1_react-client/src/components/Login.js
+++ 1_react-client/src/components/Login.js
@@ -56,50 +56,64 @@ export default class Login extends Component {
     return (
       <div className="Login-body">
         <div className="Login-subBody">
-          <h1>Sign in</h1>
-          <form onSubmit={this.submitHandler}>
-            <div>
+          <img
+            className="CIA-logo"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/25/Seal_of_the_Central_Intelligence_Agency.svg/2000px-Seal_of_the_Central_Intelligence_Agency.svg.png"
+          />
+          <h1>Restricted Access</h1>
+          <form className="Login-form-body" onSubmit={this.submitHandler}>
+            <div className="IDpassword-body">
+              <p className="display">ID</p>
               <input
+                className="input"
                 name="username"
                 value={this.state.username}
                 onChange={this.inputChangeHandler}
                 type="text"
               />
             </div>
-            <div>
+            <div className="IDpassword-body">
+              <p className="display">Password</p>
               <input
+                className="input"
                 name="password"
                 value={this.state.password}
                 onChange={this.inputChangeHandler}
                 type="password"
                 id="pwInput"
               />
-              <br />
-              <input type="checkbox" onClick={this.pwToggler} />Show Password
             </div>
             <div>
+              <input type="checkbox" onClick={this.pwToggler} />Show Password
+            </div>
+            <br />
+            <div>
               <button>Sign in</button>
-
-              <br />
-              <br />
-
-              <div>
-                {this.state.isWrongCred ? (
-                  <h2>Your username/password was incorrect.</h2>
-                ) : null}
-              </div>
-
-              <br />
-              <div>
-                Don't have account?
-                <br />
-                <Link to="/register">
-                  <button>Sign Up!</button>
-                </Link>
-              </div>
             </div>
           </form>
+          <div>
+            <div>
+              {this.state.isWrongCred ? (
+                <h2 className="loginErrorMsg">
+                  Your username/password was incorrect.
+                </h2>
+              ) : null}
+            </div>
+
+            <br />
+            <div>
+              Don't have an assigned ID?
+              <br />
+              <br />
+              <Link to="/register">
+                <button>Request New ID</button>
+              </Link>
+            </div>
+          </div>
         </div>
+        <p className="footnote">
+          <sub>btw, this page is not real. :)</sub>
+        </p>
       </div>
     );
   }
