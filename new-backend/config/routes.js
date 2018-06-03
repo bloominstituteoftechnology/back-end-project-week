7938a8ae090d77349fbda5a3f3f7e9bed3a3f3ce
Yasin Shuman <email@yasin.io>
2018-06-02 22:20:38 -0700

    beginning to refactor the whole damn thing. This backend auth will be my bitch

---
+++ new-backend/config/routes.js
@@ -0,0 +1,12 @@
+const userRoutes = require("../users/userRoutes");
+const noteRoutes = require("../notes/noteRoutes");
+const authRoutes = require("../auth/authRoutes");
+
+module.exports = function(server) {
+  server.get("/", (req, res) => {
+    res.json({ message: "all good homie" });
+  });
+};
+server.use("/api/users/", userRoutes);
+server.use("/api/notes", noteRoutes);
+server.use("api/auth", authRoutes);
