db516f935e5666c7483a39b61144a8b1f6a5334a
Yasin Shuman <email@yasin.io>
2018-06-03 02:40:04 -0700

    added jonwebtoken and recreated maketoken

--- new-backend/auth/authRoutes.js
+++ new-backend/auth/authRoutes.js
@@ -1,12 +1,29 @@
 const router = require("express").Router();
+const jwt = require("jsonwebtoken");
 const User = require("../users/User");
+
+const mySecret = "Flint still doesnt have clean water.";
 
 router.post("/register", function(req, res) {
   User.create(req.body)
     .then(user => {
-      res.status(201).json(user);
+      const token = makeToken(user);
+      res.status(201).json({ user, token });
     })
     .catch(err => res.status(500).json(err));
 });
+
+function makeToken(user) {
+  const timestamp = new Date().getTime();
+  const payload = {
+    sub: user._id,
+    iat: timestamp,
+    username: user.username
+  };
+  const options = {
+    expiresIn: "24h"
+  };
+  return jwt.sign(payload, mySecret, options);
+}
 
 module.exports = router;
--- new-backend/utils/dbConfig.js
+++ new-backend/utils/dbConfig.js
@@ -1,8 +1,7 @@
 const dbLogin = "yshuman";
 const dbPw = "notes";
-const mySecret = "Flint still doesnt have clean water.";
+
 module.exports = {
   dbLogin,
-  dbPw,
-  mySecret
+  dbPw
 };
