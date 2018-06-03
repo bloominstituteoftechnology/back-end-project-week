d2dd776930f1967acae161ae20cdfbbfc7c4204e
Yasin Shuman <email@yasin.io>
2018-06-03 03:50:33 -0700

    trying to keep up with luis and his spaghetti code whre he moves everything from authroutes to routes....wtf

--- new-backend/_config/routes.js
+++ new-backend/_config/routes.js
@@ -1,13 +1,62 @@
-const userRoutes = require("../users/userRoutes");
-const noteRoutes = require("../notes/noteRoutes");
-const authRoutes = require("../auth/authRoutes");
+const jwt = require("jsonwebtoken");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+
+const User = require("../users/User");
+const mySecret = "Flint still doesnt have clean water";
+
+const localStrategy = new LocalStrategy(function(username, password, done) {
+  User.findOne({ username })
+    .then(user => {
+      if (!user) {
+        done(null, false);
+      } else {
+        user
+          .validatePassword(password)
+          .then(isValid => {
+            const { _id, username } = user;
+            return done(null, { _id, username });
+          })
+          .catch(err => {
+            return done(err);
+          });
+      }
+    })
+    .catch(err => done(err));
+});
+
+passport.use(localStorage);
+
+const authenticate = passport.authenticate("local", { sessions: false });
+
+function makeToken(user) {
+  const timestamp = new Date().getTime();
+  const payload = {
+    sub: user._id,
+    iat: timestamp,
+    username: user.username
+  };
+  const options = {
+    expiresIn: "24h"
+  };
+  return jwt.sign(payload, mySecret, options);
+}
 
 module.exports = function(server) {
   server.get("/", (req, res) => {
     res.json({ message: "all good homie" });
   });
 
-  server.use("/api/users", userRoutes);
-  // server.use("/api/notes", noteRoutes);
-  server.use("/api/auth", authRoutes);
+  server.post("/register", function(req, res) {
+    User.create(req.body)
+      .then(user => {
+        const token = makeToken(user);
+        res.status(201).json({ user, token });
+      })
+      .catch(err => res.status(500).json(err));
+  });
+
+  server.post("/login", authenticate, (req, res) => {
+    res.status(200).json({ token: makeToken(req.user), user: req.user });
+  });
 };
--- new-backend/auth/authRoutes.js
+++ new-backend/auth/authRoutes.js
@@ -1,29 +1,12 @@
-const router = require("express").Router();
-const jwt = require("jsonwebtoken");
-const User = require("../users/User");
+// const router = require("express").Router();
+// const jwt = require("jsonwebtoken");
 
-const mySecret = "Flint still doesnt have clean water.";
+// const User = require("../users/User");
 
-router.post("/register", function(req, res) {
-  User.create(req.body)
-    .then(user => {
-      const token = makeToken(user);
-      res.status(201).json({ user, token });
-    })
-    .catch(err => res.status(500).json(err));
-});
+// const mySecret = "Flint still doesnt have clean water.";
 
-function makeToken(user) {
-  const timestamp = new Date().getTime();
-  const payload = {
-    sub: user._id,
-    iat: timestamp,
-    username: user.username
-  };
-  const options = {
-    expiresIn: "24h"
-  };
-  return jwt.sign(payload, mySecret, options);
-}
+// router.post("/login", authenticate, (req, res) => {
+//   res.status(200).json({ token: makeToken(req.user), user: req.user });
+// });
 
-module.exports = router;
+// module.exports = router;
