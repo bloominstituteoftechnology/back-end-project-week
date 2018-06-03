ee3f82d396bc4205598f0db5eabd90b956e85566
Jeffrey Flynn <jeffreyneilflynn@gmail.com>
2018-06-03 11:21:56 -0400

    cors config

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ const server = express();
 
 server.use(express.json())
 server.use(helmet())
-server.use(cors({ origin: "https://lambdanotes-jeffreyflynn.netlify.com", credentials: true }))
+server.use(cors({ 
+  origin: "https://lambdanotes-jeffreyflynn.netlify.com", 
+  credentials: true,
+  allowedHeaders: ['Content-Type', 'Authorization']
+}))
 
 APIroutes(server)
 
