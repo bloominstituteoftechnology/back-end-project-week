825654c27e35bd62756b77660a6dcc24bf03ca63
bbenefield89 <bsquared18@gmail.com>
2018-06-25 17:53:35 -0400

    refactor(task_controllers/task_delete): uses async/await

--- controllers/task_controllers/task_delete.js
+++ controllers/task_controllers/task_delete.js
@@ -1,15 +1,33 @@
 const Task = require('../../database/models/TaskModel');
+const {
+  clientError: { notFound },
+  serverError
+} = require('../../http_status_codes');
 
-const taskDelete = (req, res) => {
+const taskDelete = async (req, res) => {
   const { id } = req.params;
+  const deletedTask = await Task.findByIdAndDelete(id);
 
-  Task.findByIdAndDelete(id)
-    .then(task => {
-      res.json(task);
-    })
-    .catch(err => {
-      res.json(err);
+  if (deletedTask === null)
+    return res.status(notFound).json({
+      status: notFound,
+      statusMessage: 'There is no task by that ID'
     });
+
+  res.json(deletedTask);
+  
+    // .then(task => {
+    //   if (task === null)
+    //     return res.status(notFound).json({
+    //       status: notFound,
+    //       statusMessage: 'There is no task by that ID'
+    //     })
+      
+    //   res.json(task);
+    // })
+    // .catch(err => {
+    //   res.status(serverError).json(err);
+    // });
 };
 
 module.exports = taskDelete;
