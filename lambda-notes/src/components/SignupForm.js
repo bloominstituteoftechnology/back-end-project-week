7748f528729a29c4d382ac570a95f9a6d2c02040
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2018-06-25 14:45:21 -0700

    signupForm styled

--- lambda-notes/src/components/SignupForm.js
+++ lambda-notes/src/components/SignupForm.js
@@ -43,28 +43,30 @@ class SignupForm extends React.Component {
 
     render() {
         return (
-            <div className="App">
-                <h2>Sign Up </h2>
-                <div className={this.state.error ? "error" : "hidden"}>
-                    {this.state.errorMessage}
-                </div>
-                <div className='signup-form'>
-                    <div className="form-group">
-                        <input className="form-control" placeholder="Username" name='username' type="text" value={this.state.username} onChange={this.handleInputChange} />
+            <div className="col-sm-9">
+                <div className='signup-form-div'>
+                    <h2>Sign Up </h2>
+                    <div className={this.state.error ? "error" : "hidden"}>
+                        {this.state.errorMessage}
                     </div>
-                    <div className="form-group">
-                        {/* <img className='red-x' src="http://pluspng.com/img-png/red-x-png-image-35396-500.png" />f */}
-                        <input className="form-control" placeholder="Password" name='password' type="password" value={this.state.password} onChange={this.handleInputChange} />
-                    </div>
-                    <div className='signup-buts'>
-                        <button type="submit" className="signup-button" onClick={this.createUser}>
-                            Submit
-                        </button>
-                        <Link to="/">
-                            <button className="home-button">
-                                Home
+                    <div className='signup-form'>
+                        <div className="form-group">
+                            <input className="form-control" placeholder="Username" name='username' type="text" value={this.state.username} onChange={this.handleInputChange} />
+                        </div>
+                        <div className="form-group">
+                            {/* <img className='red-x' src="http://pluspng.com/img-png/red-x-png-image-35396-500.png" />f */}
+                            <input className="form-control" placeholder="Password" name='password' type="password" value={this.state.password} onChange={this.handleInputChange} />
+                        </div>
+                        <div className='signup-buts'>
+                            <button type="submit" className="signup-button" onClick={this.createUser}>
+                                Submit
                             </button>
-                        </Link>
+                            <Link to="/">
+                                <button className="home-button">
+                                    Home
+                                </button>
+                            </Link>
+                        </div>
                     </div>
                 </div>
             </div>
