f63e350a81310a8091f4c86ae48e786ca3c04b13
Cassandra Lamendola <cass.lamendola@gmail.com>
2018-06-25 09:38:39 -0700

    add authentication for user

--- user/User.js
+++ user/User.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 
 const passwordLength = 4
+const SALT_ROUNDS = 12
 
 const userSchema = new mongoose.Schema({
   username: {
@@ -23,5 +25,20 @@ const userSchema = new mongoose.Schema({
 })
 
 const checkPasswordLength = password => password.length >= passwordLength
+
+userSchema.pre('save', function(next) {
+  return bcrypt.hash(this.password, SALT_ROUNDS)
+    .then( hashedPassword => {
+      this.password = hashedPassword
+      return next()
+    })
+    .catch( err => {
+      return next(err)
+    })
+})
+
+userSchema.methods.validatePassword = function(passwordGuess) {
+  return bcrypt.compare(passwordGuess, this.password)
+}
 
 module.exports = mongoose.model('User', userSchema)
