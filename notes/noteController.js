bf828f636ec655275eec5d8177d6df576e48b0e9
Bradiowave <divebombwalker@gmail.com>
2018-06-25 14:21:10 -0700

    added put method to routes

--- notes/noteController.js
+++ notes/noteController.js
@@ -8,8 +8,7 @@ router.route('/create')
     .post((req, res) => {
         const newNote = (new Notes({ title, textBody, tags } = req.body));
         if (!title || !textBody){
-            res.status(400).json({ errorMessage: 'Please provide a title and textBody for the note'});
-            return;
+            res.status(400).json({ errorMessage: 'Please provide a title and textBody for the note' });
         } else {
             newNote.save()
                 .then(note => res.status(201).json(note))
@@ -31,7 +30,6 @@ router.route('/get/:id')
             .then(note => {
                 if (note === null) {
                     res.status(404).json({ errorMessage: 'Note with specified ID not found' });
-                    return;
                 } else {
                     res.json(note);
                 }
@@ -39,13 +37,35 @@ router.route('/get/:id')
             .catch(err => {
                 if (err.name === "CastError") {
                     res.status(404).json({ errorMessage: 'Note with the specified ID not found' });
-                    return;
                 } else {
                     res.status(500).json({ errorMessage: 'The note could not be retrieved' })
                 }
             })
     })
 
-
+router.route('/edit/:id')
+    .put((req, res) => {
+        const { id } = req.params;
+        const changedNote = ({ title, textBody, tags } = req.body);
+        if (!title || !textBody) {
+            res.status(400).json({ errorMessage: 'Please provide a title and textBody for the note' });
+            return;
+        }
+        Notes.findByIdAndUpdate(id, changedNote, {new: true})
+            .then(note => {
+                if (note === null) {
+                    res.status(404).json({ errorMessage: 'Note with the specified ID not found' });
+                } else {
+                    res.json(note)
+                }
+            })
+            .catch(err => {
+                if (err.name === 'CastError') {
+                    res.status(404).json({ errorMessage: 'Note with the specified ID not found' });
+                } else {
+                    res.status(500).json({ errorMessage: 'The note could not be modified' })
+                }
+            })
+    })
 
 module.exports = router;
