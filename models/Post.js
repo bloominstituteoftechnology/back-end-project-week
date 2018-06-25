03d336dd0c7a32cea65e08b5f36f3fd7a8236ee1
Xang Thao <xangthao24@yahoo.com>
2018-06-25 13:42:33 -0700

    Added User and Post Models

---
+++ models/Post.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Create Schema
+const PostSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'users'
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String
+  },
+  date: {
+    type: Date,
+    default: Date.now()
+  }
+});
+
+module.exports = Post = mongoose.model('post', PostSchema);
