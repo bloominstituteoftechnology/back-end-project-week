275b63c05c7a33f10035b287cedbbe364f337c9d
udykas <udykas@gmail.com>
2018-06-25 15:41:56 -0400

    Server set up

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const feport = 3000;
+const beport = 5000;
+const server = express()
+
+server.use(helmet());
+server.use(cors({ origin: `http://localhost:${feport}` }));
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.status(200).json({ api: 'running' });
+});
+
+//Mongoose
+mongoose.Promise = glbal.Promise;
+
+mongoose
+    .conect('mongodb://localhost/lambdanotesdb')
+    .then(mongo => {
+        console.log('\n... API Connected to LambdaNotes DB ...\n')
+    })
+    .catch(err => {
+        console.log('\n*** ERROR Connecting to Database ***\n', err);
+    })
+
+server.listen(beport, () => {
+    console.log(`\n=== API up on port: ${beport} ===\n`)
+})
