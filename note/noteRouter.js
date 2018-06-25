7fa0990a7c8a9ad5f0c668bbdbefd30c8a1bd50e
Cassandra Lamendola <cass.lamendola@gmail.com>
2018-06-25 10:44:48 -0700

    create endpoint to edit a note

--- note/noteRouter.js
+++ note/noteRouter.js
@@ -1,36 +1,52 @@
 const router = require('express').Router()
 const Note = require('./Note')
 
-router.get('/', (req, res) => {
-  Note.find()
-    .then( notes => {
-      res.status(200).json(notes)
-    })
-    .catch( err => {
-      res.status(500).json(err.message)
-    })
-})
+router
+  .route('/')
+  .get((req, res) => {
+    Note.find()
+      .then( notes => {
+        res.status(200).json(notes)
+      })
+      .catch( err => {
+        res.status(500).json(err.message)
+      })
+  })
 
-router.post('/', (req, res) => {
-  const newNote = { title, content, checklist } = req.body
-  Note.create(newNote)
-    .then( note => {
-      res.status(201).json(note)
-    })
-    .catch( err => {
-      res.status(500).json(err.message)
-    })
-})
+  .post((req, res) => {
+    const newNote = { title, content, checklist } = req.body
+    Note.create(newNote)
+      .then( note => {
+        res.status(201).json(note)
+      })
+      .catch( err => {
+        res.status(500).json(err.message)
+      })
+  })
 
-router.get('/:id', (req, res) => {
-  const { id } = req.params
-  Note.findById(id)
-    .then( note => {
-      res.status(200).json(note)
-    })
-    .catch( err => {
-      res.status(500).json(err.message)
-    })
-})
+router
+  .route('/:id')
+  .get((req, res) => {
+    const { id } = req.params
+    Note.findById(id)
+      .then( note => {
+        res.status(200).json(note)
+      })
+      .catch( err => {
+        res.status(500).json(err.message)
+      })
+  })
+
+  .put((req, res) => {
+    const { id } = req.params
+    const editedNote = { title, content, checklist } = req.body
+    Note.findByIdAndUpdate(id, editedNote, {new: true})
+      .then( note => {
+        res.status(200).json(note)
+      })
+      .catch( err => {
+        res.status(500).json(err.message)
+      })
+  })
 
 module.exports = router
