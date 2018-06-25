035cbca1d9f7280e2a356c396c37b01a3ee6eacf
maxwashington <mnwashington18@gmail.com>
2018-06-25 14:32:45 -0500

    Server setup routes setup Note model started

---
+++ server/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const config = require('./config.js');
+const { dbuser, dbpassword, dbname } = config.secret;
+const server = express();
+
+const port = 5000;
+
+// Connect to mlab
+mongoose
+    .connect(`mongodb://${dbuser}:${encodeURIComponent(dbpassword)}//${dbname}`)
+    .then(() => {
+        console.log('connected to production database');
+        if (process.env.NODE_ENV !== 'test') {
+            server.listen(port, () =>
+                console.log(`Backend is running at ${port}`)
+            )
+        } else {
+            server.listen(port, () =>
+                console.log(`Running in Test Environment at ${port}`)
+            )
+        }
+    })
+    .catch(err => {
+        console.log('error connecting to production database, is MongoDB running?');
+    });
