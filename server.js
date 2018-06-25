9b0a744a9a6f9b0389522c901894cfbdd83d3918
Danny Vail <danny_vail@outlook.com>
2018-06-25 15:00:16 -0400

    Minor change to test auto deployment to Heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const server = require('express')();
 const PORT = process.env.PORT || 5000;
 
 server.get('/', (req, res) => {
-  res.status(200).json({ 'api': 'running' });
+  res.status(200).send('Lambda Notes API');
 });
 
 server.listen(PORT, () => {
