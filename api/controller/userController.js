9fe243179d58f81b9577c22b4b1dba6991c3fa04
A99 <A99@99s-macbook-pro.home>
2018-06-25 22:41:02 +0200

    Add CRUD operations to:
    -  CRUD users at users/:id
    -  GET all users at users/

--- api/controller/userController.js
+++ api/controller/userController.js
@@ -1,5 +1,38 @@
 const router = require('express').Router();
+const User = require('../model/User.js');
 
-router.route('')
+router.route('/')
+    .get((req, res) => {
+        User.find()
+            .then(response => res.json(response))
+            .catch(err => res.status(500).json({ error: err.message }));
+    })
+
+router.route('/:id')
+    .get((req, res) => {
+        const { id } = req.params;
+        User.findById(id)
+            .then(response => res.json(response))
+            .catch(err => res.status(500).json({ error: err.message }));
+    })
+    .post((req, res) => {
+        const newUser = ({ email, password } = req.params);
+        User.create(newUser)
+            .then(response => res.status(201).json(response))
+            .catch(err => res.status(500).json({ error: err.message }));
+    })
+    .put((req, res) => {
+        const updateUser = ({ email, password, firstName, lastName } = req.body);
+        const { id } = req.params;
+        User.findByIdAndUpdate(id, updateUser)
+            .then(response => res.status(202).json(response))
+            .catch(err => res.status(500).json({ error: err.message }));
+    })
+    .delete((req, res) => {
+        const { id } = req.params;
+        User.findByIdAndRemove(id)
+            .then(response => res.json(response))
+            .catch(err => res.status(500).json({ error: err.message }));
+    });
 
 module.exports = router;
