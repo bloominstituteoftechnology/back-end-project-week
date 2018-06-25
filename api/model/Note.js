34baa438b652bb50f9800082655112c35570ab55
A99 <A99@99s-macbook-pro.home>
2018-06-25 22:10:50 +0200

    Add Note model

--- api/model/Note.js
+++ api/model/Note.js
@@ -2,7 +2,14 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const NoteSchema = new Schema({
-
+    title: {
+        type: String,
+        required: true
+    },
+    body: {
+        type: String,
+        required: true
+    }
 });
 
 module.exports = mongoose.model('User', NoteSchema);
