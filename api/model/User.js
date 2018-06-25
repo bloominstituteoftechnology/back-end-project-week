6dbf0586f30d6c8ea5fb3f152dc140e04d13d41e
A99 <A99@99s-macbook-pro.home>
2018-06-25 22:16:49 +0200

    Add User model

--- api/model/User.js
+++ api/model/User.js
@@ -1,9 +1,21 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const { Schema } = mongoose;
+const ObjectId = mongoose.Schema.Types.ObjectId;
 
 const UserSchema = new Schema({
-
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: 1
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 8
+    },
+    notes: [{ type: ObjectId, ref: 'Note'}]
 });
 
 // add bcrypt life cycle method and native method
