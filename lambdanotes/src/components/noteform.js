e4e489ed278b981203f57de62410cd1241c92a0a
Zach Campbell <beazlebubbles@gmail.com>
2018-06-25 12:17:36 -0600

    Attached frontend to backend

--- lambdanotes/src/components/noteform.js
+++ lambdanotes/src/components/noteform.js
@@ -1,25 +1,24 @@
-import React from 'react';
-import './noteform.css';
-import { Form, FormGroup, Input} from 'reactstrap';
-import LambdaButton from './lambdabutton.js';
+import React from "react";
+import "./noteform.css";
+import { Form, FormGroup, Input } from "reactstrap";
+import LambdaButton from "./lambdabutton.js";
 
 //This form is able to create or edit depending on its props, and could do other
 //things as well if new callback functions were made for it.
 class NoteForm extends React.Component {
-
   //Store the inputs on local component state for the moment.
   constructor(props) {
     super(props);
     this.state = {
       myTitle: props.myTitle,
       myContent: props.myContent
-    }
+    };
   }
 
   //Update component state as user types.
-  handleFormType = (event) => {
+  handleFormType = event => {
     this.setState({ [event.target.name]: event.target.value });
-  }
+  };
 
   //This component needs to update its state when it gets new props.
   componentWillReceiveProps(newprops) {
@@ -32,7 +31,7 @@ class NoteForm extends React.Component {
   //The HTML:
   render() {
     return (
-      <Form id="note-form" >
+      <Form id="note-form">
         <h5 className="form-heading">{this.props.topText}</h5>
         <FormGroup>
           <Input
@@ -47,19 +46,24 @@ class NoteForm extends React.Component {
           <Input
             type="textarea"
             onChange={this.handleFormType}
-            name="myContent" id="content-input"
+            name="myContent"
+            id="content-input"
             placeholder="Note Content"
             value={this.state.myContent}
           />
         </FormGroup>
         <LambdaButton
-          myFunc={() => this.props.myMethod(this.props.fetcher,
-            "https://killer-notes.herokuapp.com/note/",
-            {title: this.state.myTitle, textBody: this.state.myContent},
-            this.props.myId)}
+          myFunc={() =>
+            this.props.myMethod(
+              this.props.fetcher,
+              "http://localhost:5000/api/notes",
+              { title: this.state.myTitle, textBody: this.state.myContent },
+              this.props.myId
+            )
+          }
           text="Save"
           color="green"
-          />
+        />
       </Form>
     );
   }
