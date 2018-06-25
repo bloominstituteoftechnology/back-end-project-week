8d23159edc67e8ff78bc05ac0ed04873f21b101d
MacBook <LonnieDavis@MacBookPros-MacBook-Pro.local>
2018-06-25 12:50:11 -0500

    Server
    Minium code to set server up for deployment

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');// common js modules
+//import express from 'express'; es2015 modules
+
+const app = express();//creates a running express server; this app object will set up to listen for http requests and route them
+
+
+app.get('/', (req, res) => { // creates a route handler; get is a method that gets some information about some record; req js object that represents incoming request
+//and has data about who is making request and some associated data within it
+// res has the data that will be returned to user that made request
+    res.send({ hi: 'there' });
+});
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
