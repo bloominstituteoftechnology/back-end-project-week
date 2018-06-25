df7d548d385b6e65b19d4ad4e4d7e3932ab65ae1
samar vir <samarvir1996@gmail.com>
2018-06-26 04:36:50 +0530

    added cors

--- server.js
+++ server.js
@@ -14,6 +14,13 @@ const server = express();
 server.use(express.json());
 server.use(cors());
 
+server.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 
 // intializing mongoos unkill
 mongoose.Promise = global.Promise;
