246683995a3c2d43b03b7b5e3900829d7f1fedcd
Zach Campbell <beazlebubbles@gmail.com>
2018-06-25 11:34:44 -0600

    Added CRUD to noteRouter using middleware

--- notes/noteRouter.js
+++ notes/noteRouter.js
@@ -1,9 +1,32 @@
-// const middleware = require("../middleware.js");
+const middleware = require("../middleware.js");
 const express = require("express");
 const router = express.Router();
+const Note = require("./Note.js");
 
-router.route(["/", "/:id"]).get((req, res) => {
-  res.json({ foo: "bar" });
-});
+router
+  .route(["/", "/:id"])
+  .get(middleware.getMiddleware(Note), (req, res) => {
+    req.getResult.then(notes => {
+      if (notes) res.json({ notes });
+      else res.status(404).json({ errorMessage: "No documents found" });
+    });
+  })
+  .post(
+    middleware.sanitizeMiddleware("note"),
+    middleware.postMiddleware(Note),
+    (req, res) => {
+      res.json(req.postResult);
+    }
+  )
+  .put(
+    middleware.sanitizeMiddleware("note"),
+    middleware.putMiddleware(Note),
+    (req, res) => {
+      res.json(req.putResult);
+    }
+  )
+  .delete(middleware.deleteMiddleware(Note), (req, res) => {
+    res.json(req.deleteResult);
+  });
 
 module.exports = router;
