8f94867c402dc0c0c80303fc0c5b4f6a3290afe0
samar vir <samarvir1996@gmail.com>
2018-06-26 01:52:16 +0530

    backend api working in postman

--- notes/noteRouter.js
+++ notes/noteRouter.js
@@ -21,12 +21,51 @@ router
 
         .get((req,res) => {
             noteModel
-                .find({}, '-_id')
+                .find({})
                     .then(item => {
                         res.status(200).json(item)
                     })
                     .catch(err => {
                         res.status(500).json({err: "no notes found"})
+                    })
+        })
+
+router
+    .route('/:id')
+        .get((req,res) => {
+            const {id} = req.params
+            noteModel.findById(id)
+                .then(item => {
+                    res.status(200).json(item)
+                })
+        })
+
+        .delete((req,res) => {
+            const {id} = req.params
+            noteModel.findByIdAndRemove(id)
+                .then(item => {
+                    if(item === null){
+                        res.status(404).json({ errorMessage: "The item with the specified ID does not something." })
+                        return;
+                    }
+
+                    res.status(200).json(item)
+                })
+                .catch(err => {
+                    res.status(500).json({error: "the note couldn't be removed"})
+                })
+        })
+
+        .put((req,res) => {
+            const {id} = req.params;
+            const { title, note } = req.body;
+            noteModel
+                .findByIdAndUpdate(id, { title, note })
+                    .then(item => {
+                        res.status(200).json(item)
+                    })
+                    .catch(err => {
+                        res.status(500).json({ error: "the note couldn't be modefied"})
                     })
         })
 
