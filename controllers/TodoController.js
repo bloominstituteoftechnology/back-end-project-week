ef80605c377cf9158d58bd8b7c64f6e61f14c34d
Brandon Hopper <brandonhopper0@gmail.com>
2018-06-25 16:25:53 -0400

    GET /id route created

--- controllers/TodoController.js
+++ controllers/TodoController.js
@@ -18,8 +18,25 @@ router
             res.status(200).json({response})
         })
         .catch(e =>{
-            res.status(400).json({error: 'could not fetch todos'})
+            res.status(400).json({errorMessage: 'could not fetch todos'})
         })
+    })
+
+router
+    .route('/:id')
+    .get((req, res) => {
+        const {id} = req.params
+        Todo.findById(id).then(foundTodo => {
+            console.log(foundTodo)
+            if(foundTodo === null){
+                res.status(404).json({errorMessage: 'sorry not Todos for you..'})
+                return;
+            }
+            res.status(200).json(foundTodo)
+        })
+        .catch(err => {
+            res.status(500).json({errorMessage: 'no Todo in the db'})
+        });
     })
 
 module.exports = router;
