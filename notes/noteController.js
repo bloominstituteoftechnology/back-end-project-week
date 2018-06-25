98a1b603e045716b519d4b182c5a86a4e627397c
Bradiowave <divebombwalker@gmail.com>
2018-06-25 14:07:13 -0700

    added get all and get by ID routes

--- notes/noteController.js
+++ notes/noteController.js
@@ -4,12 +4,11 @@ const Notes = require('./noteModel');
 
 // base route = '/note'
 
-router
-    .route('/create')
+router.route('/create')
     .post((req, res) => {
         const newNote = (new Notes({ title, textBody, tags } = req.body));
         if (!title || !textBody){
-            res.status(400).json({ errorMessage: 'Please provide a title and textBody'});
+            res.status(400).json({ errorMessage: 'Please provide a title and textBody for the note'});
             return;
         } else {
             newNote.save()
@@ -17,5 +16,36 @@ router
                 .catch(err => res.status(500))
         }
     })
+
+router.route('/get/all')
+    .get((req, res) => {
+        Notes.find()
+            .then(notes => res.json(notes))
+            .catch(err => res.status(500).json({ errorMessage: 'The notes information could not be retrieved' }))
+    })
+
+router.route('/get/:id')
+    .get((req, res) => {
+        const { id } = req.params;
+        Notes.findById(id)
+            .then(note => {
+                if (note === null) {
+                    res.status(404).json({ errorMessage: 'Note with specified ID not found' });
+                    return;
+                } else {
+                    res.json(note);
+                }
+            })
+            .catch(err => {
+                if (err.name === "CastError") {
+                    res.status(404).json({ errorMessage: 'Note with the specified ID not found' });
+                    return;
+                } else {
+                    res.status(500).json({ errorMessage: 'The note could not be retrieved' })
+                }
+            })
+    })
+
+
 
 module.exports = router;
