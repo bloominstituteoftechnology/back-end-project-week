b52fbcdb927267be8a634723ebd4febd3909128d
Danny Vail <danny_vail@outlook.com>
2018-06-25 19:00:11 -0400

    Added validation to Task model

--- api/models/Task.js
+++ api/models/Task.js
@@ -1,33 +1,59 @@
 const mongoose = require('mongoose');
 const ObjectId = mongoose.Schema.Types.ObjectId;
 
+const objectIdValid = (model, value, cb) => {
+  mongoose.model(model).count({ _id: value }, (error, count) => {
+    cb(error || count === 0 ? false : true);
+  });
+};
+
 const taskSchema = new mongoose.Schema({
   project: {
     type: ObjectId,
     ref: 'Project',
-    required: true
+    required: [true, 'Project id required'],
+    validate: {
+      isAsync: true,
+      validator: (val, cb) => objectIdValid('Project', val, cb),
+      message: 'Must be an id for an existing project'
+    }
   },
   title: {
     type: String,
-    required: true
+    required: [true, 'Task title required']
   },
   description: {
     type: String
   },
   assignee: {
     type: ObjectId,
-    ref: 'User'
+    ref: 'User',
+    validate: {
+      isAsync: true,
+      validator: (val, cb) => objectIdValid('User', val, cb),
+      message: 'Must be an id for an existing user'
+    }
   },
   dueDate: {
     type: Date
   },
   subtasks: {
     type: [ObjectId],
-    ref: 'Subtask'
+    ref: 'Subtask',
+    validate: {
+      isAsync: true,
+      validator: (val, cb) => objectIdValid('Subtask', val, cb),
+      message: 'Must be and id for an existing subtask'
+    }
   },
   tags: {
     type: [ObjectId],
-    ref: 'Tag'
+    ref: 'Tag',
+    validate: {
+      isAsync: true,
+      validator: (val, cb) => objectIdValid('Tag', val, cb),
+      message: 'Must be an id for an existing tag'
+    }
   },
   completed: {
     type: Boolean,
@@ -35,11 +61,21 @@ const taskSchema = new mongoose.Schema({
   },
   comments: {
     type: [ObjectId],
-    ref: 'Comment'
+    ref: 'Comment',
+    validate: {
+      isAsync: true,
+      validator: (val, cb) => objectIdValid('Comment', val, cb),
+      message: 'Must be an id for an existing user'
+    }
   },
   attachments: {
     type: [ObjectId],
-    ref: 'Attachment'
+    ref: 'Attachment',
+    validate: {
+      isAsync: true,
+      validator: (val, cb) => objectIdValid('Attachment', val, cb),
+      message: 'Must be an id for an existing attachment'
+    }
   }
 });
 
