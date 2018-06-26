aa6773311ac1a2532c38d2f33455876955a0e688
Brandon Hopper <brandonhopper0@gmail.com>
2018-06-26 12:44:24 -0400

    -installed firbase and created a folder for it

--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {BroweserRouter as Router} from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
