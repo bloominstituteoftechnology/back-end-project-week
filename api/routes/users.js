832f15e6c47b0eb6dc9696bb6b17ade56f6e4f69
Danny Vail <danny_vail@outlook.com>
2018-06-25 23:49:04 -0400

    Built PUT endpoint at api/users/:id to update an existing user - requires that all fields be sent in body

--- api/routes/users.js
+++ api/routes/users.js
@@ -47,7 +47,34 @@ router
       .catch(err => {
         sendErrorMessage(err, res, `The user with id ${id} could not be retrieved.`);
       });
+  })
+  .put('/:id', (req, res) => {
+    const { id } = req.params;
+    const { firstName, lastName, email, password } = req.body;
+    const options = {
+      new: true,
+      runValidators: true
+    };
+
+    User.findByIdAndUpdate(id, { firstName, lastName, email, password }, options)
+      .select({
+        _id: 0,
+        firstName: 1,
+        lastName: 1,
+        email: 1
+      })
+      .then(updatedUser => {
+        if (updatedUser) {
+          res.status(200).json(updatedUser);
+        } else {
+          res.status(404).json({ error: `The user with id ${id} does not exist.` });
+        }
+      })
+      .catch(err => {
+        sendErrorMessage(err, res, `The user with id ${id} could not be modified.`);
+      });
   });
+//   .delete();
 
 module.exports = {
   usersRouter: router
