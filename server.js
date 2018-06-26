7af9b7a4f1c44cc6913828a4c9a5cb07af4ad60c
Danny Vail <danny_vail@outlook.com>
2018-06-25 20:24:59 -0400

    Testing Swagger for API documentation so it can be written as API is created

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const server = require('express')();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocs = require('./api/docs/swagger.json');
 
 server.get('/', (req, res) => {
   res.status(200).send('Lambda Notes API');
 });
+
+server.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 module.exports = {
   server
