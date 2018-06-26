f44297e84dace57e6c39f6c3a0fad927176a61f5
Kelly F <this@whatever.com>
2018-06-25 17:45:10 -0700

    trying to get this to work with heroku

--- server/_config/db.js
+++ server/_config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 module.exports = {
-	connectTo: function(database = 'sandbox', host = 'localhost') {
-		return mongoose.connect(`mongodb://${host}/${database}`);
+	connectTo: function(database = 'notes', host = 'localhost') {
+		return mongoose.connect(`mongodb://${process.env.MLABUSER}:${process.env.MLABPASS}@ds018538.mlab.com:$(process.env.MLABPORT}/notes`);
 	},
 };
--- server/server.js
+++ server/server.js
@@ -3,7 +3,6 @@ const cors = require('cors');
 const db = require('./_config/db');
 const setupMiddleware = require('./_config/middleware');
 const setupRoutes = require('./_config/routes');
-const mongoose = require('mongoose');
 
 const server = express();
 
@@ -17,16 +16,16 @@ server.use(cors(corsOptions));
 setupMiddleware(server);
 setupRoutes(server);
 
-mongoose.Promise = global.Promise;
-
-mongoose.connect(
-	`mongodb://${process.env.MLABUSER}:${process.env.MLABPASS}@ds018538.mlab.com:${process.env.MLABPORT}/notes`, {},
-	error => {
-		if(error) console.log('\n***ERROR connecting to MongoDB ***\n', err);
-		else console.log('\n===MongoDB connected ***\n');
-	}
-);
-
+db.connectTo('notes')
+	.then(() => {
+		console.log('\n=== API connected to mongodb ===\n');
+		server.listen(8000,() =>
+			console.log('\n=== mongodb connected port 8000 ===\n')
+		);
+	})
+	.catch(err => {
+		console.log('\n === error connecting to mongodb === \n')
+	});
 
 const port = process.env.PORT || 8000;
 server.listen(port, () => console.log(`\n=== API up on port: $(port) === \n`));
