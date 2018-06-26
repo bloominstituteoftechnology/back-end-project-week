95975410eeec57eff7db57706232bd511f700af5
Kelly F <this@whatever.com>
2018-06-25 18:06:12 -0700

    undoing some changes

--- server/_config/db.js
+++ server/_config/db.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose');
 
 module.exports = {
 	connectTo: function(database = 'notes', host = 'localhost') {
-		return mongoose.connect(`mongodb://${process.env.MLABUSER}:${process.env.MLABPASS}@ds018538.mlab.com:$(process.env.MLABPORT}/notes`);
+		return mongoose.connect(`mongodb://${host}/${database}`);
 	},
 };
