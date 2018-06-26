75d079bf33b78198b4338fd647134c58fc732960
jesuarva <jesuarva.developer@gmail.com>
2018-06-26 19:08:31 +0200

    DEFINE Starting files.

---
+++ db/db.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+module.exports = {
+  connectTo: function(database = 'sandbox', host = 'localhost') {
+    return mongoose.connect(`mongodb://${host}/${database}`);
+  },
+  disconnect: function() {
+    return mongoose.disconnect();
+  },
+};
