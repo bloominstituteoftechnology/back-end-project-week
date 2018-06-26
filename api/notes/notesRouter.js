e875afd1a30818585116e7073467a5627c38e42e
Fausto Wladimir Fraga <wladimir917@hotmail.com>
2018-06-25 21:47:56 -0400

    Notes routes endpoints

--- api/notes/notesRouter.js
+++ api/notes/notesRouter.js
@@ -7,28 +7,87 @@ const router = express.Router();
 //End Points
 router
   .route('/')
-  .get((req, res) => {
-    Note
-      .find()
-      .then(foundNotes => 
-        res.json(foundNotes)
-      )
-      .catch(err => 
-        res.status(500).json(err)
-      )
-  })
   .post((req, res) => {
-    const note = req.body;
-    const newNote = new Note(note);
-    newNote
-      .save()
-      .then(savedNote => 
-        res.status(201).json(savedNote)
-      )
-      .catch(err => 
-        res.status(422).json({ error: err })
-      )
+    Note.create(req.body)
+      .then(note => {
+        res.status(201).json(note);
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ message: 'Error saving data to the DB', error: err });
+      });
   })
+  .get((req, res) => {
+    Note.find()
+      .then(notes => {
+        res.status(200).json(notes);
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ message: 'Something really bad happened', error: err });
+      });
+  })
+  .put((req, res) => {
+    res.status(422).json({ message: 'Id not provided' });
+  })
+  .delete((req, res) => {
+    res.status(422).json({ message: 'Id not provided' });
+  })
+
+router
+  .route('/:id')
+  .get((req, res) => {
+    const { id } = req.params;
+    Note.findById(id)
+      .then(note => {
+        if (note) {
+          res.json(note);
+        } else {
+          res.status(404).json({ error: 'The note with the specified ID does not exist.' });
+        }
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ message: 'Something really bad happened', error: err });
+      });
+  })
+  .put((req, res) => {
+    const { id } = req.params;
+    const changes = req.body;
+
+    //Returns the updated register
+    const options = {
+      new: true
+    }
+    Note.findByIdAndUpdate(id, changes, options )
+      .then(note => {
+        if (note) {
+          res.json(note);
+        } else {
+          res.status(404).json({ message: 'Note not found' });
+        }
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ message: 'Something really bad happened', error: err });
+      });
+  })
+  .delete((req, res) => {
+    const { id } = req.params;
+    Note.findByIdAndRemove(id)
+      .then(note => {
+        if (note) {
+          res.json({ message: 'Note deleted' });
+        } else {
+          res.status(404).json({ message: 'Note not found' });
+         }
+       })
+       .catch(err => res.status(500).json(err));
+});
 
 
 module.exports = router;
