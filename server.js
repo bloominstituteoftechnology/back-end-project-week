7185aec0aa50c52b2a2f15c4e3ab72e2ed8cd416
samar vir <samarvir1996@gmail.com>
2018-06-26 20:35:00 +0530

    changed port

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const mongoose = require("mongoose");
 const config = require('./config')
 
 //port me
-const port = process.env.port || 5000;
+const port = process.env.port ;
 
 //initializing lib lib
 const server = express();
