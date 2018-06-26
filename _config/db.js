89271f2505ad5c5ce8ad9ccf47c03f934f42263e
Fausto Wladimir Fraga <wladimir917@hotmail.com>
2018-06-26 13:06:01 -0400

    .env with mongodb deploy from github

--- _config/db.js
+++ _config/db.js
@@ -1,17 +1,8 @@
 const mongoose = require('mongoose');
 
-
-// module.exports = {
-//   connectTo: function(database = 'sandbox', host = 'localhost') {
-//     return mongoose.connect(`mongodb://${host}/${database}`);
-//   },
-// };
-
 // mongodb://<dbuser>:<dbpassword>@ds217921.mlab.com:17921/lambda_notes
-const uri = 'mongodb://<dbuser>:<dbpassword>@ds217921.mlab.com:17921/lambda_notes';
-console.log(process.env.mongo)
 module.exports = {
-  connectTo: function(dbuser = 'test', dbpassword = 'test1234', database = 'lambda_notes', host = 'ds217921.mlab.com') {
+  connectTo: function(dbuser = '', dbpassword = '', database = 'lambda_notes', host = 'ds217921.mlab.com') {
     return mongoose.connect(process.env.mongo);
   },
 };
