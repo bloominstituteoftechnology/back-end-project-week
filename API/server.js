e78d727d4e7c24ac8a08c6bd7acad3a5f24a83c0
unknown <sam.park.email@gmail.com>
2018-06-26 15:36:56 -0400

    cors?

--- API/server.js
+++ API/server.js
@@ -7,6 +7,11 @@ const helmet = require('helmet');
 
 const server = express();
 
+server.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 var whitelist = ['http://localhost:3000/', 'https://tender-ptolemy-5e2918.netlify.com/']
 var corsOptions = {
   origin: function (origin, callback) {
