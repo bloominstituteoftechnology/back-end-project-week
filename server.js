80689c203d84def0947a7c98820cda331f779625
Danny Vail <danny_vail@outlook.com>
2018-06-26 17:21:27 -0400

    Removed Procfile and commented out code from server.js

--- server.js
+++ server.js
@@ -26,14 +26,6 @@ server.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
 });
 
-// UI Production Routing
-// if (process.env.NODE_ENV === 'production') {
-//   server.use(express.static(path.join(__dirname, 'client/build')));
-//   server.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-//   });
-// }
-
 module.exports = {
   server
 };
