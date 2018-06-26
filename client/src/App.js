00e6ccd40858772e9dd9951603d769cfb2d9825d
NandoTheessen <nando.theessen@gmail.com>
2018-06-27 00:02:59 +0200

    Added components for Button, Welcome Page, Input, NoteList and the Sidebar

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,52 @@
 import React, { Component } from 'react';
+import axios from 'axios'
+import FirstView from './components/Firstview';
+import { Route, withRouter } from 'react-router';
+import Input from './components/Input';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      notes: [],
+      userid: '',
+      loggedin: false
+    }
+  }
+  login = (credentials) => {
+    axios.post('https://notes-backend-nodejs.herokuapp.com/api/login', credentials)
+      .then(data => {
+        console.log(data);
+        this.setState((prevState, props) => {
+          return { notes: data.data.notes, loggedin: true, userid: data.data.userid }
+        })
+        localStorage.setItem('token', data.token)
+        this.props.history.push('/notelist')
+      })
+  }
+  register = (newUser) => {
+    axios.post('https://notes-backend-nodejs.herokuapp.com/api/register', newUser)
+      .then(data => {
+        this.setState((prevState, props) => {
+          return { notes: data.notes, loggedin: true, userid: data.userid }
+        })
+        localStorage.setItem('token', data.token)
+      })
+  }
+  logout = () => {
+    localStorage.clear()
+    this.setState((prevState) => {
+      return { loggedin: false, userid: '', notes: [] }
+    })
+  }
   render() {
     return (
       <div className="App">
+        <Route exact path='/' component={FirstView} />
+        <Route exact path='/login' render={(props) => <Input {...props} login={this.login} page='login' />} />
       </div>
-
     );
   }
 }
 
-export default App;
+export default withRouter(App);
