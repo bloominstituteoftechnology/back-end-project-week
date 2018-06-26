198eb6832c92427c289cb975a847f6fead2930b9
Fausto Wladimir Fraga <wladimir917@hotmail.com>
2018-06-26 11:02:21 -0400

    Testing local database

--- _config/db.js
+++ _config/db.js
@@ -1,15 +1,15 @@
 const mongoose = require('mongoose');
 
-module.exports = {
-  connectTo: function(database = 'sandbox', host = 'localhost') {
-    return mongoose.connect(`mongodb://${host}/${database}`);
-  },
-};
-
-// // mongodb://<dbuser>:<dbpassword>@ds217921.mlab.com:17921/lambda_notes
-
 // module.exports = {
-//   connectTo: function(dbuser = 'test', dbpassword = 'test1234', database = 'lambda_notes', host = 'ds217921.mlab.com') {
-//     return mongoose.connect(`mongodb://${dbuser}:${dbpassword}@${host}:17921/${database}`);
+//   connectTo: function(database = 'sandbox', host = 'localhost') {
+//     return mongoose.connect(`mongodb://${host}/${database}`);
 //   },
 // };
+
+// mongodb://<dbuser>:<dbpassword>@ds217921.mlab.com:17921/lambda_notes
+
+module.exports = {
+  connectTo: function(dbuser = 'test', dbpassword = 'test1234', database = 'lambda_notes', host = 'ds217921.mlab.com') {
+    return mongoose.connect(`mongodb://${dbuser}:${dbpassword}@${host}:17921/${database}`);
+  },
+};
