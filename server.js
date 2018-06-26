3ff433e42f5741bd0d731a5be739ca53181cfc6e
Danny Vail <danny_vail@outlook.com>
2018-06-26 13:33:25 -0400

    Setting up front end deployment on Heroku - added production route to serve static client files - added heroku post build script to generate client build

--- server.js
+++ server.js
@@ -1,20 +1,31 @@
-const server = require('express')();
+const express = require('express');
 const swaggerUi = require('swagger-ui-express');
 const { projectsRouter } = require('./api/routes/projects');
 const { tasksRouter } = require('./api/routes/tasks');
 const { usersRouter } = require('./api/routes/users');
 const swaggerDocs = require('./api/docs/swagger.json');
 
-server.use(require('express').json());
+const server = express();
 
+// Middleware
+server.use(express.json());
+
+// API Routes
 server.use('/api/projects', projectsRouter);
 server.use('/api/tasks', tasksRouter);
 server.use('/api/users', usersRouter);
 server.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
-
 server.get('/api', (req, res) => {
   res.status(200).send('Lambda Notes API');
 });
+
+// UI Production Routing
+if (process.env.NODE_ENV === 'production') {
+  server.use(express.static(path.join(__dirname, 'client/build')));
+  server.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
 
 module.exports = {
   server
