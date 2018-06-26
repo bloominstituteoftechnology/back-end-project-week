0747881c6ab552696cadfd03ced12372a07e597c
jesuarva <jesuarva.developer@gmail.com>
2018-06-26 20:07:37 +0200

    ROUTERS: Define Starting files.

---
+++ server/routers/Notes.router.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const Notes = require('../../models/Notes.model');
+
+const RF = require('../../myTools/RouterFactory');
+
+const router = express.Router();
+
+module.exports = router;
---
+++ server/routers/User.router.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const Users = require('../../models/Users.model');
+
+const RF = require('../../myTools/RouterFactory');
+
+const router = express.Router();
+
+module.exports = router;
