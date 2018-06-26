16273fb4feb47bfc1a75fe34d80764b076f51de3
maribelcuales <maribelcuales@gmail.com>
2018-06-27 04:55:24 +0800

    added GET, POST, PUT, and DELETE methods to Notes.

--- Notes/NotesRouter.js
+++ Notes/NotesRouter.js
@@ -15,7 +15,7 @@ router
         const { title, text } = req.body;
         const newNote = new Note ({ title, text }); 
         if (!title || !text) {
-            res.status(400).json({ error: 'Please enter title and text. '});
+            res.status(400).json({ error: 'Please enter title and text.' });
             return; 
         }
         newNote
@@ -28,4 +28,50 @@ router
             });
     });
 
-    module.exports = router; 
+router
+    .route('/:id')
+    .get((req, res) => {
+        const { id } = req.params;
+        Note
+            .findById(id)
+            .then(response => {
+                res.status(200).json(response);
+            })
+            .catch(error => {
+                res.status(404).json({ error: 'The note with that ID does not exist.'})
+            }); 
+    })
+    .delete((req, res) => {
+        const { id } = req.params; 
+        Note
+            .findByIdAndRemove(id)
+            .then(response => {
+                if (response == 0) {
+                    res.status(404).json({ error: 'The note with that ID does not exist.' })
+                    return; 
+                }
+                res.json('Note was successfully removed!');
+            })
+            .catch(error => {
+                res.status(500).json({ error: 'Error removing friend.' }); 
+            }); 
+    })
+    .put((req, res) => {
+        const { id } = req.params;
+        const { title, text } = req.body;
+        const updatedNote = { title, text };
+        if (!title || !text) {
+            res.status(400).json({ error: 'Please enter title and text.' });
+            return; 
+        }
+        Note
+            .findByIdAndUpdate(id, updatedNote)
+            .then(response => {
+                res.json(response); 
+            })
+            .catch(error => {
+                res.status(500).json({ error: 'Note cannot be modified.' })
+            });
+    });
+
+module.exports = router; 
