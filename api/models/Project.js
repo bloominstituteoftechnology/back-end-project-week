cd73d4c728edfd5db6bb676fc7cc26c32b4fb227
Danny Vail <danny_vail@outlook.com>
2018-06-26 03:15:26 -0400

    Built GET endpoint got /api/projects to get all projects - modified project model -- made title unique - fixed typo in project model for members objectid array

--- api/models/Project.js
+++ api/models/Project.js
@@ -5,21 +5,24 @@ const ObjectId = mongoose.Schema.Types.ObjectId;
 const projectSchema = new mongoose.Schema({
   title: {
     type: String,
-    required: [true, 'Project title required']
+    required: [true, 'Project title required'],
+    unique: true
   },
   description: {
     type: String
   },
-  members: {
-    type: [ObjectId],
-    ref: 'User',
-    required: [true, 'Project must contain at least one user'],
-    validate: {
-      isAsync: true,
-      validator: (val, cb) => objectIdValid('User', val, cb),
-      message: 'Must be an id for an existing user'
+  members: [
+    {
+      type: ObjectId,
+      ref: 'User',
+      required: [true, 'Project must contain at least one user'],
+      validate: {
+        isAsync: true,
+        validator: (val, cb) => objectIdValid('User', val, cb),
+        message: 'Must be an id for an existing user'
+      }
     }
-  }
+  ]
 });
 
 module.exports = mongoose.model('Project', projectSchema);
--- api/routes/projects.js
+++ api/routes/projects.js
@@ -3,18 +3,27 @@ const Project = require('../models/Project');
 const { sendErrorMessage } = require('../utils/sendErrorMessage');
 
 router
-.post('/', (req, res) => {
-  const { title, description, members, tags } = req.body;
+  .post('/', (req, res) => {
+    const { title, description, members, tags } = req.body;
 
-  Project.create({ title, description, members, tags })
-    .then(project => {
-      res.status(201).json(project);
-    })
-    .catch(err => {
-      sendErrorMessage(err, res, 'The project could not be created.');
-    });
-})
-// .get('/', (req, res) => {})
+    Project.create({ title, description, members, tags })
+      .then(project => {
+        res.status(201).json(project);
+      })
+      .catch(err => {
+        sendErrorMessage(err, res, 'The project could not be created.');
+      });
+  })
+  .get('/', (req, res) => {
+    Project.find({})
+      .populate('members', {firstName: 1, lastName: 1})
+      .then(projects => {
+        res.status(200).json(projects);
+      })
+      .catch(err => {
+        res.status(500).json({ error: 'The list of projects could not be retrieved.'})
+      });
+  })
 // .get('/:id', (req, res) => {})
 // .update('/:id', (req, res) => {})
 // .delete('/:id', (req, res) => {});
