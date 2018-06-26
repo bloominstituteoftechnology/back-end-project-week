439635614f5c837b0a3dc607b86855bff4f614a8
Danny Vail <danny_vail@outlook.com>
2018-06-25 22:46:55 -0400

    Minor edit

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const server = require('express')();
+const swaggerUi = require('swagger-ui-express');
 const { projectsRouter } = require('./api/routes/projects');
 const { tasksRouter } = require('./api/routes/tasks');
 const { usersRouter } = require('./api/routes/users');
-const swaggerUi = require('swagger-ui-express');
 const swaggerDocs = require('./api/docs/swagger.json');
 
 server.use('/api/projects', projectsRouter);
