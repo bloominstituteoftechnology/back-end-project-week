70cf338a37a01e0ae6097c54b6047292615558f9
Kelly F <this@whatever.com>
2018-06-25 17:05:03 -0700

    server.js

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const db = require('./_config/db');
 const setupMiddleware = require('./_config/middleware');
 const setupRoutes = require('./_config/routes');
+const mongoose = require('mongoose');
 
 const server = express();
 
@@ -16,15 +17,18 @@ server.use(cors(corsOptions));
 setupMiddleware(server);
 setupRoutes(server);
 
-db.connectTo('auth')
-	.then(() => {
-		console.log('\n...API Connected ...\n');
-		server.listen(8000, () =>
-			console.log('\n===API Running on port 8000 ===\n')
-		);
-	})
-	.catch(err => {
-		console.log('\n*** ERROR connecting to MongoDB ***\n', err);
-	});
+mongoose.Promise = global.Promise;
+
+mongoose.connect(
+	`mongodb://${process.env.MLABUSER}:$(process.env.MLABPASS)@ds018538.mlab.com:${process.env.MLABPORT)/notes`, {},
+	error => {
+		if(error) console.log('\n***ERROR connecting to MongoDB ***\n', err);
+		else console.log('\n===MongoDB connected ***\n');
+	}
+};
+
+
+const port = process.env.PORT || 8000;
+server.listen(port, () => console.log(`\n=== API up on port: $(port) === \n`));
 	
 
