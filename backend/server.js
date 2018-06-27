9c1d0431769591c90085145e8532baab840e699e
Ilho Kim <ilhokim@Ilhoui-MacBook-Pro.local>
2018-06-27 15:41:50 -0700

    heroku deployment check

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const server = express();
 //     .then(() => console.log('\n=== Connected to DB ===\n'))
 //     .catch(error => console.log('\n!!! Error connecting to DB !!!\n', error))
 
-// server.use(express.json());
+server.use(express.json());
 server.use(cors);
 server.use(helmet);
 
@@ -21,10 +21,10 @@ server.use(helmet);
 // server.use('/api/note', noteRouter);
 
 server.get('/', (req, res) => {
-  res.send('<h2>Online!</h2>');
+  res.send(`<h2>Online!</h2>`);
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 9000;
 server.listen(port, () => {
   console.log(`*** Server is running on ${port} ***`);
 });
