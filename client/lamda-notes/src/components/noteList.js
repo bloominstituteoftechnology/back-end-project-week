698a705b89dc88ab466b2d58521aa647456896d3
maxwashington <mnwashington18@gmail.com>
2018-06-27 15:39:08 -0500

    Basic feature for clientside app done. On to the style

---
+++ client/lamda-notes/src/components/noteList.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+//import "./index.css";
+
+const mapStateToProps = state => {
+  return {
+    notesArray: state
+  };
+};
+
+class NotesList extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  componentWillMount() {
+    let reversed = Array.from(this.props.notesArray).reverse();
+    console.log("reversed in CWM is:", reversed);
+    this.setState({ notesArray: reversed });
+  }
+
+  //   <Link to ={'/note/' + what._id}>
+
+  generateNotes = (what, where) => {
+    return (
+      <Link to={`/note/${what._id}`} className="unstyledLink" key={what._id}>
+        <div className="note">
+          <div>
+            <h4>{what.title}</h4>
+            <hr />
+            <p>{what.body}</p>
+          </div>
+        </div>
+      </Link>
+    );
+  };
+
+  render() {
+    console.log("Props inside Notes List", this.props);
+    return (
+      <div className="notesList_container">
+        <div>
+          <h3 className="content-header">Your Notes:</h3>
+        </div>
+        <div className="notes_list">
+          {this.state.notesArray.map(this.generateNotes)}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default connect(mapStateToProps)(NotesList);
