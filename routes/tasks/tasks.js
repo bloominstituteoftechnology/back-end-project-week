fe7a86ea2fd204352109ab622e07a5d11a794c61
bbenefield89 <bsquared18@gmail.com>
2018-06-27 18:25:44 -0400

    feat(tasks.js): index get request is now a post feat(task_find_all.js): verifies JWT and searches for tasks with that users ID attached to them and returns

--- routes/tasks/tasks.js
+++ routes/tasks/tasks.js
@@ -6,9 +6,9 @@ const taskFind = require('../../controllers/task_controllers/task_find');
 const taskUpdate = require('../../controllers/task_controllers/task_update');
 const taskDelete = require('../../controllers/task_controllers/task_delete');
 
-router
-  .get('/', taskFindAll)
-  .post('/', taskCreate);
+router.post('/', taskFindAll)
+
+router.post('/new', taskCreate);
 
 router
   .get('/:id', taskFind)
