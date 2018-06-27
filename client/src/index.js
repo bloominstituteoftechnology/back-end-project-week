035ea8864dd8abed14aa32cbc395c50cdb1e5cfc
Brandon Hopper <brandonhopper0@gmail.com>
2018-06-27 12:22:44 -0400

    - made edits to json to deploy

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,5 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {BrowserRouter as Router} from 'react-router-dom';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
-registerServiceWorker();
