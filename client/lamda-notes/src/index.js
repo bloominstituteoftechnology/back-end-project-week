fa5607dd629c21e006ce9f2f5b3ac7f81880f68e
maxwashington <mnwashington18@gmail.com>
2018-06-27 15:28:51 -0500

    Created LeftNav and Main Content Created Note actions delete,edit, and create Added files for viewing note ToDo: Style

--- client/lamda-notes/src/index.js
+++ client/lamda-notes/src/index.js
@@ -1,8 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import { notesReducer } from "./reducers/reducer";
+const store = createStore(notesReducer, applyMiddleware(thunk, logger));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <Route path="/" component={App} />
+    </Router>
+  </Provider>,
+  document.getElementById("root")
+);
