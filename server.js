61ea76fae6532026f0d02dd032c86eb436821e6f
Brandon Hopper <brandonhopper0@gmail.com>
2018-06-27 16:25:23 -0400

    -path to create static deploy

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express'); 
 const helmet = require('helmet');
 const mongoose = require('mongoose');
-
+const cors = require('cors');
+const path = require('path');
 
 const TodoController = require('./controllers/TodoController');
 const AuthController = require('./controllers/AuthController');
@@ -10,7 +11,7 @@ const server = express();
 
 server.use(helmet());
 server.use(express.json());
-const server = express();
+
 server.use(
   cors({
   origin: "localhost://3000",
@@ -21,11 +22,16 @@ server.use(
 server.use('/api/todo', TodoController);
 server.use('/api/user', AuthController);
 
+server.use(express.static(path.join(__dirname, 'client/build')));
+
 // root route
 server.get('/', (req, res) => {
   res.status(200).json({api: 'running'})
 });
 
+server.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
 
 // connecting mongoose to local host and heroku
 mongoose.Promise = global.Promise;
