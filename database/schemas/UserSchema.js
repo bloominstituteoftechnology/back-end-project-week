68114b36d94c681bce6094e20c084080bf2a059f
bbenefield89 <bsquared18@gmail.com>
2018-06-27 12:32:44 -0400

    feat(UserSchema): uses bcrypt to hash passwords before saving

--- database/schemas/UserSchema.js
+++ database/schemas/UserSchema.js
@@ -1,11 +1,22 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
-const Schema = mongoose.Schema();
+const Schema = mongoose.Schema;
 
 const User = new Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   // email: { type: String, required: true, unique: true }
+});
+
+User.pre('save', function(next) {
+  bcrypt.hash(this.password, 10, (err, hash) => {
+    if (err)
+      return next(err);
+    
+    this.password = hash;
+    next();
+  });
 });
 
 module.exports = User;
