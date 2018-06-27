a6f586a1a611b7df39a5e28442bae554bb8c7be2
Danny Vail <danny_vail@outlook.com>
2018-06-27 16:49:36 -0400

    Cleanup formatting

--- api/routes/projects.js
+++ api/routes/projects.js
@@ -4,11 +4,10 @@ const { sendErr } = require('../utils/apiResponses');
 
 router
   .post('/', (req, res) => {
-    const { title, description, members } = req.body;
+    const newProject = req.body;
 
-    Project.create({ title, description, members })
-      .then(project => {
-        const { _id, title, description } = project;
+    Project.create(newProject)
+      .then(({ _id, title, description }) => {
         res.status(201).json({ _id, title, description });
       })
       .catch(err => {
@@ -16,9 +15,9 @@ router
       });
   })
   .get('/', (req, res) => {
-    Project.find({})
+    Project.find()
       .populate('members', { firstName: 1, lastName: 1 })
-      .select({ _id: 1, title: 1, members: 1 })
+      .select({ title: 1, members: 1 })
       .then(projects => {
         res.status(200).json(projects);
       })
@@ -27,11 +26,11 @@ router
       });
   })
   .get('/:id', (req, res) => {
-    const { id } = req.params;
+    const { id } = req.para;
 
     Project.findById(id)
       .populate('members', { firstName: 1, lastName: 1 })
-      .select({ _id: 1, title: 1, description: 1, members: 1 })
+      .select({ title: 1, description: 1, members: 1 })
       .then(project => {
         if (project) {
           res.status(200).json(project);
@@ -47,15 +46,15 @@ router
   })
   .put('/:id', (req, res) => {
     const { id } = req.params;
-    const { title, description, members } = req.body;
+    const updatedProject = req.body;
     const options = {
       new: true,
       runValidators: true
     };
 
-    Project.findByIdAndUpdate(id, { title, description, members }, options)
+    Project.findByIdAndUpdate(id, updatedProject, options)
       .populate('members', { firstName: 1, lastName: 1 })
-      .select({ _id: 1, title: 1, description: 1, members: 1 })
+      .select({ title: 1, description: 1, members: 1 })
       .then(updatedProject => {
         if (updatedProject) {
           res.status(200).json(updatedProject);
@@ -71,12 +70,11 @@ router
   })
   .delete('/:id', (req, res) => {
     const { id } = req.params;
-
+    
     Project.findByIdAndRemove(id)
       .then(deletedProject => {
         if (deletedProject) {
-          const { _id } = deletedProject;
-          res.status(200).json({ _id });
+          res.status(200).json(deletedProject._id);
         } else {
           res
             .status(404)
