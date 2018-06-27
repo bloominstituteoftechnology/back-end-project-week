0d02917695179762826a9561620986d51c5d5dbc
Danny Vail <danny_vail@outlook.com>
2018-06-27 15:31:11 -0400

    Built GET endpoint for /api/taks to get all tasks and a task by id

--- api/routes/tasks.js
+++ api/routes/tasks.js
@@ -2,39 +2,70 @@ const router = require('express').Router();
 const Task = require('../models/Task');
 const { sendErrorMessage } = require('../utils/sendErrorMessage');
 
-router.post('/', (req, res) => {
-  const {
-    project,
-    title,
-    description,
-    assignee,
-    dueDate,
-    subtasks,
-    tags,
-    completed,
-    comments,
-    attachments
-  } = req.body;
+router
+  .post('/', (req, res) => {
+    const {
+      project,
+      title,
+      description,
+      assignee,
+      dueDate,
+      subtasks,
+      tags,
+      completed,
+      comments,
+      attachments
+    } = req.body;
 
-  Task.create({
-    project,
-    title,
-    description,
-    assignee,
-    dueDate,
-    subtasks,
-    tags,
-    completed,
-    comments,
-    attachments
-  })
-    .then(task => {
-      res.status(201).json(task);
+    Task.create({
+      project,
+      title,
+      description,
+      assignee,
+      dueDate,
+      subtasks,
+      tags,
+      completed,
+      comments,
+      attachments
     })
-    .catch(err => {
-      sendErrorMessage(err, res, 'The task could not be created.');
-    });
-});
+      .then(task => {
+        res.status(201).json(task);
+      })
+      .catch(err => {
+        sendErrorMessage(err, res, 'The task could not be created.');
+      });
+  })
+  .get('/', (req, res) => {
+    Task.find({})
+      .then(tasks => {
+        res.status(200).json(tasks);
+      })
+      .catch(err => {
+        sendErrorMessage(err, res, 'The list of tasks could not be retrieved.');
+      });
+  })
+  .get('/:id', (req, res) => {
+    const { id } = req.params;
+
+    Task.findById(id)
+      .then(task => {
+        if (task) {
+          res.status(200).json(task);
+        } else {
+          res
+            .status(404)
+            .json({ error: `The task with id ${id} does not exist.` });
+        }
+      })
+      .catch(err => {
+        sendErrorMessage(
+          err,
+          res,
+          `The task with id ${id} could not be retrieved.`
+        );
+      });
+  });
 
 // router
 //   .post()
