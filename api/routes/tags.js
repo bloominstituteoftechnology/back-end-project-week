abcfc64072c03affc133d1ceb5f78c52683c476e
Danny Vail <danny_vail@outlook.com>
2018-06-27 21:00:40 -0400

    Built POST endpoint for /api/tags to create a new tag

---
+++ api/routes/tags.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+const Tag = require('../models/Tag');
+const { sendErr, sendRes } = require('../utils/apiResponses');
+
+router.post('/', (req, res) => {
+  const newTag = req.body;
+
+  Tag.create(newTag)
+    .then(tag => {
+      sendRes(res, '201', tag);
+    })
+    .catch(err => {
+      sendErr(res, err, 'The tag could not be created.');
+    });
+});
