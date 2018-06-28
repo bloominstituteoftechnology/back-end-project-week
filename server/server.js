4877867c5827cd25f1d281f1a6f58b99b49dc802
Kelly F <this@whatever.com>
2018-06-28 14:48:32 -0700

    troubleshooting error

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 const express = require ('express');
 const cors = require('cors');
-//const db = require('./_config/db'); I don't ever actually use this file because I do my mongoose.connect below
 const setupMiddleware = require('./_config/middleware');
 const setupRoutes = require('./_config/routes');
 const mongoose = require('mongoose');
@@ -11,12 +10,10 @@ const corsOptions = {
 	origin: 'https://nostalgic-kilby-8372d8.netlify.com',
 	credentials: true,
 };
-//updated with front end info so that CORS doesn't block my info
 server.use(cors(corsOptions));
 
 setupMiddleware(server);
 setupRoutes(server);
-//this makes it so my server.js isn't too long and I can call in my middleware and routes from elsewhere
 
 mongoose.Promise = global.Promise;
 
@@ -27,8 +24,6 @@ mongoose.connect(`mongodb://${process.env.MLABUSER}:${process.env.MLABPASSWD}@ds
 		console.log('\n===connected to mongodb ===\n')
 		}
 	});
-
-//oof this was hard to set up correctly but I finally got mlab working and then set env variables in heroku so that I wouldn't have all my login credentials floating around in github
 
 const port = process.env.PORT || 8000;
 server.listen(port, () => console.log(`\n=== API up on port: $(port) === \n`));
