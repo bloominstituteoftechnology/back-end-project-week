e772016c4fdbaba2c16763d7d85b08968e452027
Danny Vail <danny_vail@outlook.com>
2018-06-28 02:06:30 -0400

    Built POST endpoint for /api/attachments to save location of saved file

--- api/routes/attachments.js
+++ api/routes/attachments.js
@@ -2,6 +2,18 @@ const router = require('express').Router();
 const Attachment = require('../models/Attachment');
 const { sendErr, sendRes } = require('../utils/apiResponses');
 
+router.post('/', (req, res) => {
+  const newAttachment = req.body;
+
+  Attachment.create(newAttachment)
+    .then(attachment => {
+      sendRes(res, '201', attachment);
+    })
+    .catch(err => {
+      sendErr(res, err, 'The attachment could not be created.');
+    });
+});
+
 module.exports = {
   attachmentsRouter: router
 };
