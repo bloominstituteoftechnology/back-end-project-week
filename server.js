8251fa237ca53c0457e7266e74f0ad2016705d3a
Thuy Pham <tpham045@hotmail.com>
2018-06-28 13:07:36 -0700

    fix: Refused to execute script because of MIME type

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ server.use('/api/tag', tagRouter);
 // console.log(process.env.NODE_ENV)
 if (process.env.NODE_ENV !== 'dev') {
     server.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname + '/client/build/index.html'));
+        res.sendFile(path.join(__dirname, '/client/build/index.html'));
     });
 }
 
