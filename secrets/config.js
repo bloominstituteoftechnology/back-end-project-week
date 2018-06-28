ebad5bbc2b6d6d8e75b6d1cfaf369afac6d8b7bd
AJ Genung <DragonRydrz@me.com>
2018-06-27 23:18:32 -0400

    updated db

--- secrets/config.js
+++ secrets/config.js
@@ -1,6 +1,7 @@
 const config = {
-  database: 'mongodb://localhost/lambdanotestesting',
-  // 'mongodb://lambdanotes1:lambdanotes1@ds261429.mlab.com:61429/lambda-notes',
+  // database: 'mongodb://localhost/lambdanotestesting',
+  database:
+    'mongodb://lambdanotes1:lambdanotes1@ds261429.mlab.com:61429/lambda-notes',
   secret: 'Bend the knee!',
   salt: "Dragons are a real man's best friend",
 };
