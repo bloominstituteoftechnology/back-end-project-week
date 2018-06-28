df8292223394585f0c9045d3e6702a2b34e33711
Danny Vail <danny_vail@outlook.com>
2018-06-28 02:09:05 -0400

    Built DELETE endpoint for /api/attachments to remove link to saved file

--- api/routes/attachments.js
+++ api/routes/attachments.js
@@ -2,17 +2,29 @@ const router = require('express').Router();
 const Attachment = require('../models/Attachment');
 const { sendErr, sendRes } = require('../utils/apiResponses');
 
-router.post('/', (req, res) => {
-  const newAttachment = req.body;
+router
+  .post('/', (req, res) => {
+    const newAttachment = req.body;
 
-  Attachment.create(newAttachment)
-    .then(attachment => {
-      sendRes(res, '201', attachment);
-    })
-    .catch(err => {
-      sendErr(res, err, 'The attachment could not be created.');
-    });
-});
+    Attachment.create(newAttachment)
+      .then(attachment => {
+        sendRes(res, '201', attachment);
+      })
+      .catch(err => {
+        sendErr(res, err, 'The attachment could not be created.');
+      });
+  })
+  .delete('/:id', (req, res) => {
+    const { id } = req.params;
+
+    Attachment.findByIdAndRemove(id)
+      .then(deletedAttachment => {
+        sendRes(res, '200', deletedAttachment ? { _id: deletedAttachment._id } : null);
+      })
+      .catch(err => {
+        sendErr(res, err, `The attachment with id ${id} could not be removed.`);
+      });
+  });
 
 module.exports = {
   attachmentsRouter: router
