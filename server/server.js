7e06f9102ac33feb960d385d80a91e571c66087f
jesuarva <jesuarva.developer@gmail.com>
2018-06-29 01:47:55 +0200

    Playing with Passport.js

--- server/server.js
+++ server/server.js
@@ -1,14 +1,20 @@
 const express = require('express'); // remember to install your npm packages
 const helmet = require('helmet');
-const morgan = require('morgan');
+const logger = require('morgan');
 const cors = require('cors');
 const utils = require('../auth/utils');
+
+/**
+ * PASSPORT: import setup
+ */
+const passportSetup = require('../auth/passport');
 
 /**
  * IMPORT ROUTERS: import any needed Router.
  */
 const registerUser = require('../auth/register.router');
 const loginUser = require('../auth/loign.router');
+const authRouter = require('../auth/auth.router');
 const userRouter = require('./routers/User.router');
 const notesRouter = require('./routers/Notes.router');
 
@@ -18,15 +24,16 @@ const notesRouter = require('./routers/Notes.router');
 const server = express();
 
 /**
- * DEFINE: global Pre-Middlewares is any.
+ * DEFINE: global Pre-Middlewares if any.
  */
 const corsOptions = {
   origin: 'http://localhost:3000', // dafult create-react-app URL
   methods: [], // authorized HTTP verbs -> if ommit allow all HTTP verbs
   credentials: true,
 };
+server.use(logger('dev'));
 server.use(helmet());
-server.use(cors());
+server.use(cors({}));
 server.use(express.json());
 
 /**
@@ -34,12 +41,12 @@ server.use(express.json());
  */
 server.get('/', (req, res) => res.send('API Running...'));
 
-server.use('/api/register/', registerUser);
+server.use('/api/register', registerUser);
 server.use('/api/login', loginUser);
-server.use('/api/users', userRouter);
-
+server.use('/auth/google', authRouter);
 // CONTROL ACCESS TO ONLY REGISTERED USERS
 // server.use(utils.userHasToken);
+server.use('/api/users', userRouter);
 server.use('/api/notes', notesRouter);
 
 /**
