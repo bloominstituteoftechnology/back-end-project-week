48bf6da7db028ffc8f9b67643badcb2acd9ce4af
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2018-06-28 15:07:52 -0700

    corsOption fixed

--- _config/middleware.js
+++ _config/middleware.js
@@ -2,12 +2,20 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 const corsOptions = {
-    origin: "http://localhost:3000",
+    origin: "*",
     credentials: true
 };
 
 module.exports = function (server) {
     server.use(helmet());
     server.use(express.json());
+    server.use((req, res, next) => {
+        res.setHeader('Access-Control-Allow-Origin', '*'); // Cross Site Allowance
+        res.setHeader(
+            'Access-Control-Allow-Headers',
+            'Origin, X-Requested-With, Content-Type, Accept'
+        );
+        next();
+    });
     server.use(cors(corsOptions));
 };
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 const express = require('express');
-
+const port = process.env.PORT || 5000;
 const db = require('./_config/db');
 const setupMiddleware = require('./_config/middleware');
 const setupRoutes = require('./_config/routes');
@@ -14,7 +14,7 @@ setupRoutes(server);
 db.connectTo('lambdaNotes')
     .then(() => {
         console.log('\n... API Connected to lambdaNotes Database ...\n');
-        server.listen(5000, () =>
+        server.listen(port, () =>
             console.log('\n=== API running on port 5000 ===\n')
         );
     })
