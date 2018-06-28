1f8a6613207b3eb1985d27c3ee675f16f2d289f7
Danny Vail <danny_vail@outlook.com>
2018-06-28 01:38:26 -0400

    Built PUT endpoint for /api/comments to update existing comment

--- api/routes/comments.js
+++ api/routes/comments.js
@@ -2,17 +2,34 @@ const router = require('express').Router();
 const Comment = require('../models/Comment');
 const { sendErr, sendRes } = require('../utils/apiResponses');
 
-router.post('/', (req, res) => {
-  const newComment = req.body;
+router
+  .post('/', (req, res) => {
+    const newComment = req.body;
 
-  Comment.create(newComment)
-    .then(comment => {
-      sendRes(res, '201', comment);
-    })
-    .catch(err => {
-      sendErr(res, err, 'The comment could not be created.');
-    });
-});
+    Comment.create(newComment)
+      .then(comment => {
+        sendRes(res, '201', comment);
+      })
+      .catch(err => {
+        sendErr(res, err, 'The comment could not be created.');
+      });
+  })
+  .put('/:id', (req, res) => {
+    const { id } = req.params;
+    const updatedComment = req.body;
+    const options = {
+      new: true,
+      runValidators: true
+    };
+
+    Comment.findByIdAndUpdate(id, updatedComment, options)
+      .then(updatedComment => {
+        sendRes(res, '200', updatedComment);
+      })
+      .catch(err => {
+        sendErr(res, err, `The comment with id ${id} could not be modified.`);
+      });
+  });
 
 module.exports = {
   commentsRouter: router
