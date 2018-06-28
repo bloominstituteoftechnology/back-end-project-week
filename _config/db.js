20a319563b5b834837c849016c681dddeeaea2e8
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2018-06-28 14:17:16 -0700

    mlab deployed

--- _config/db.js
+++ _config/db.js
@@ -1,10 +1,11 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 const config = require('../config');
 const mlabUrl = config.mlab
 
 module.exports = {
     connectTo: function (database = 'sandbox', host = 'localhost') {
-        return mongoose.connect(mlabUrl);
+        return mongoose.connect(process.env.mongo);
     },
 };
 
--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const secret = 'hasta siempre Comandante';
-const mlab = 'mongodb://erinc6:abcd35@ds115396.mlab.com:15396/lambdanote'
 
-module.exports = { secret, mlab };
+
+module.exports = { secret };
