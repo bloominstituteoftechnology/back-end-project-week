c82459dde75ea9d5fcd2def97d1086768e123a5b
Danny Vail <danny_vail@outlook.com>
2018-06-28 01:30:06 -0400

    Built POST endpoint for /api/comments to create a new comment

--- api/routes/comments.js
+++ api/routes/comments.js
@@ -2,6 +2,18 @@ const router = require('express').Router();
 const Comment = require('../models/Comment');
 const { sendErr, sendRes } = require('../utils/apiResponses');
 
+router.post('/', (req, res) => {
+  const newComment = req.body;
+
+  Comment.create(newComment)
+    .then(comment => {
+      sendRes(res, '201', comment);
+    })
+    .catch(err => {
+      sendErr(res, err, 'The comment could not be created.');
+    });
+});
+
 module.exports = {
   commentsRouter: router
 };
