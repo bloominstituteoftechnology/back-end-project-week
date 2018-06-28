271eac632e40acb71e589eacadcb18dd63792df3
Danny Vail <danny_vail@outlook.com>
2018-06-27 21:23:57 -0400

    Built PUT endpoint for /api/tags to update a tag

--- api/routes/tags.js
+++ api/routes/tags.js
@@ -7,11 +7,27 @@ router
     const newTag = req.body;
 
     Tag.create(newTag)
-      .then((tag) => {
+      .then(tag => {
         sendRes(res, '201', tag);
       })
       .catch(err => {
         sendErr(res, err, 'The tag could not be created.');
+      });
+  })
+  .put('/:id', (req, res) => {
+    const { id } = req.params;
+    const updatedTag = req.body;
+    const options = {
+      new: true,
+      runValidators: true
+    };
+
+    Tag.findByIdAndUpdate(id, updatedTag, options)
+      .then(updatedTag => {
+        sendRes(res, '200', updatedTag);
+      })
+      .catch(err => {
+        sendErr(res, err, `The tag with id ${id} could not be modified.`);
       });
   });
 
