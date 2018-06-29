2dd550b6310359ba0ea62f1150a6f8fb06cbd629
Cassandra Lamendola <cass.lamendola@gmail.com>
2018-06-29 12:17:34 -0700

    enable connection to localhost for debugging front-end

--- _config/middleware.js
+++ _config/middleware.js
@@ -9,5 +9,6 @@ module.exports = function(server) {
   server.use(helmet())
   server.use(morgan('dev'))
   server.use(express.json())
-  server.use(cors({ origin: `https://${frontEndPort}`}))
+  // server.use(cors({ origin: `https://${frontEndPort}`}))
+  server.use(cors())
 }
