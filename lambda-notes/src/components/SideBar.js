13f62c78357b4c6939e45aaac9cbfa593c8369b1
Matthias Bakken <matthias.bakken@gmail.com>
2018-06-29 00:04:21 -0700

    fixed issue with navigating home from create new note or edit note views

--- lambda-notes/src/components/SideBar.js
+++ lambda-notes/src/components/SideBar.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import { CSVLink } from 'react-csv';
 
 function homeLink() {
-    return this.props.history.push('/');
+    window.location.href = "/";;
 }
 
 class SideBar extends Component {
