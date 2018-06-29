d4a887b890ae53d3ad8f9d19faf45f50bf7e8e7f
AmyShackles <amyshackles@gmail.com>
2018-06-29 10:29:49 -0700

    .

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ server.use(morgan('combined'))
 
 
 const corsOptions = {
-    origin: process.env.CORSORIGIN || 'localhost:3000',
+    origin: process.env.CORSORIGIN,
     credentials: true
 }
 server.use(cors(corsOptions))
