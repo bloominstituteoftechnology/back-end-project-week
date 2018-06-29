a9b64a49fddf479b97694347b9fa714707afdb81
Kelly F <this@whatever.com>
2018-06-29 09:00:03 -0700

    CORS experimentation is over for now. I'm reverting back to the original until I can learn more.

--- server/server.js
+++ server/server.js
@@ -6,21 +6,22 @@ const mongoose = require('mongoose');
 
 const server = express();
 
-const whitelist = ['localhost:3000', 'http://nostalgic-kilby-8372d8.netlify.com', 'https://nostalgic-kilby-8372d8.netlify.com']
-const corsOptions = {
-	origin: function (origin, cb) {
-		if(whitelist.indexOf(origin) !== -1) {
-			cb(null, true)
-		} else {
-			cb(new Error('Not allowed by CORS'))
-		}
-	}
-}
-
+//I tried setting up multiple origins with CORS but was never able to get it working correctly. Eventually I gave up and just stuck with the one origin due to time constraints. I would like to eventually figure this out, however.
+//const whitelist = ['localhost:3000', 'http://nostalgic-kilby-8372d8.netlify.com', 'https://nostalgic-kilby-8372d8.netlify.com']
 //const corsOptions = {
-//	origin: 'http://nostalgic-kilby-8372d8.netlify.com',
-//	credentials: true,
-//};
+//	origin: function (origin, cb) {
+//		if(whitelist.indexOf(origin) !== -1) {
+//			cb(null, true)
+//		} else {
+//			cb(new Error('Not allowed by CORS'))
+//		}
+//	}
+//}
+
+const corsOptions = {
+	origin: 'http://nostalgic-kilby-8372d8.netlify.com',
+	credentials: true,
+};
 //My husband tried to create and edit on my site and it turns out I had httpsEverywhere activated and then just cut and pasted the link here. CORS accepted all of my edits because it was coming from https://nostalgic.kilby but not his because his was coming from http://nostalgic.kilby. Important lesson learned.
 
 server.use(cors(corsOptions));
