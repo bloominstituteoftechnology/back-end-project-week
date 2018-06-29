64115f0879e4da3673cd4b24ec9d6c42d53cb66c
AmyShackles <amyshackles@gmail.com>
2018-06-29 09:17:14 -0700

    .

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -21,7 +21,7 @@ function restricted (req, res, next) {
 }
 
 router.get('/', restricted, (req, res) => {
-    Note.findOne({ author: req.session.username })   
+    Note.find()  
         .select('-__v -id')
         .then(notes => {
             res.status(200).json({ notes })
