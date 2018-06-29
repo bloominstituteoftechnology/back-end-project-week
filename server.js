1ca6acd64e5b3498f13ecc51213ebe636107ba2e
Bradiowave <divebombwalker@gmail.com>
2018-06-29 08:46:46 -0700

    changed Access-Control to netlify project

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ server.use(express.json());
 // http://localhost:3000 
 
 let allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Origin', "https://cocky-ride-fcf8cb.netlify.com");
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
