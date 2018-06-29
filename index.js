3ec3efeb0db5f100f8697a0391e3f43c04e50eb5
MacBook <LonnieDavis@MacBookPros-MacBook-Pro.local>
2018-06-29 15:04:16 -0500

    Cors config change

--- index.js
+++ index.js
@@ -1,25 +1,41 @@
 const express = require('express');// common js modules
 //import express from 'express'; es2015 modules
+const bodyParser = require('body-parser');
 require('dotenv').config();
 const mongoose = require('mongoose');
-const cors = require('cors');
+const CORS = require('cors');
 //const keys = require('./config/keys');
-
+const server = express();//creates a running express server; this app object will set up to listen for http requests and route them
+const corsOptions = {
+  origin: '*',
+  methods:'GET, HEAD, PUT, PATCH, POST, DELETE',
+  preflightContinue: false,
+  optionSuccessStatus: 204
+};
+server.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, x-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
+server.use(bodyParser.json());
+server.use(CORS(corsOptions));
 
 //mongoose.connect(keys.mongoURI)
-const server = express();//creates a running express server; this app object will set up to listen for http requests and route them
+
 const noteController = require('./noteTracker/noteController');
 
 server.use(express.json());
-
+//server.use(cors(corsOptions));
 //server.use(cors({
-  const corsOptions = {
-  origin: 'https://frosty-brahmagupta-26a4e9.netlify.com/',
-  //origin:'http://localhost:3000/',
+ /* const corsOptions = {
+  //origin: 'https://frosty-brahmagupta-26a4e9.netlify.com/',
+  origin:'http://localhost:3000/',
   credentials: true
-};
+};*/
 
-server.use(cors(corsOptions));
 
 server.get('/', (req, res) => {
     res.send("working")}); 
@@ -30,6 +46,7 @@ server.use('/api/note', noteController);
 mongoose.Promise = global.Promise;
 //mongoose.connect(keys.mongoURI);
 mongoose.connect(process.env.mongo)
+//mongoose.connect('mongodb://localhost/auth')
   .then(() => {
    console.log('Database is connected');
   })
