c54b1569730a1342ad07b03e0928d81172191b27
AmyShackles <amyshackles@gmail.com>
2018-06-28 22:00:44 -0700

    Console.log

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -34,6 +34,7 @@ router.post('/login', (req, res) => {
                 .then(passwordMatch => {
                     if (passwordMatch) {
                         const token = generateToken(username)
+                        console.log(res)
                         res.status(200).json({ message: `Welcome to Lambda Take Note, ${username}!`, token})
                     } else {
                         res.status(401).send('invalid credentials')
