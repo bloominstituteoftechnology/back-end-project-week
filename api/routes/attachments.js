27c30d99788494e627bb542b54aaf9d172a8acc2
Danny Vail <danny_vail@outlook.com>
2018-06-28 23:37:58 -0400

    Restrict all attachment endpoints to authenticated users

--- api/routes/attachments.js
+++ api/routes/attachments.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
 const Attachment = require('../models/Attachment');
 const { sendErr, sendRes } = require('../utils/apiResponses');
+const { authenticate } = require('../../server/middleware');
 
 router
-  .post('/', (req, res) => {
+  .post('/', authenticate, (req, res) => {
     const newAttachment = req.body;
 
     Attachment.create(newAttachment)
@@ -14,12 +15,16 @@ router
         sendErr(res, err, 'The attachment could not be created.');
       });
   })
-  .delete('/:id', (req, res) => {
+  .delete('/:id', authenticate, (req, res) => {
     const { id } = req.params;
 
     Attachment.findByIdAndRemove(id)
       .then(deletedAttachment => {
-        sendRes(res, '200', deletedAttachment ? { _id: deletedAttachment._id } : null);
+        sendRes(
+          res,
+          '200',
+          deletedAttachment ? { _id: deletedAttachment._id } : null
+        );
       })
       .catch(err => {
         sendErr(res, err, `The attachment with id ${id} could not be removed.`);
