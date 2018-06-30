8a006d1619ac55f985c2ef6c9a30ea497f24bec2
Obo Agboghidi <oagboghi2@gmail.com>
2018-06-30 02:36:24 -0700

    fixing restricted middleware

--- backend/users/userController.js
+++ backend/users/userController.js
@@ -14,45 +14,46 @@ function generateToken(user){
     return jwt.sign(payload, secret, options)
 }
 
-const validateToken = (req, res, next) => {
-    // this piece of middleware is taking the token delivered up to the server and verifying it
-    // if no token is found in the header, you'll get a 422 status code
-    // if token is not valid, you'll receive a 401 status code
-    const token = req.headers.authorization;
-    if (!token) {
-        res
-            .status(422)
-            .json({ error: 'No authorization token found on Authorization header' });
-    } else {
-        jwt.verify(token, secret, (err, decoded) => {
-            if (err) {
-                res
-                    .status(401)
-                    .json({ error: 'Token invalid, please login', message: err });
-            } else {
-                // token is valid, so continue on to the next middleware/request handler function
-                next();
-            }
-        });
-    }
-};
-
-// function restricted(res, req, next){
+// const validateToken = (req, res, next) => {
+//     // this piece of middleware is taking the token delivered up to the server and verifying it
+//     // if no token is found in the header, you'll get a 422 status code
+//     // if token is not valid, you'll receive a 401 status code
 //     const token = req.headers.authorization;
-//     if(token){
-//         jwt.verify(token, secret, (err, decodedToken => {
-//             if(err){
-//                 res.status(401).json({ message: "Does not pass verification" })
+//     if (!token) {
+//         res
+//             .status(422)
+//             .json({ error: 'No authorization token found on Authorization header' });
+//     } else {
+//         jwt.verify(token, secret, (err, decoded) => {
+//             if (err) {
+//                 res
+//                     .status(401)
+//                     .json({ error: 'Token invalid, please login', message: err });
+//             } else {
+//                 // token is valid, so continue on to the next middleware/request handler function
+//                 next();
 //             }
-//             next();
-//         }))
-//     }else{
-//         res.status(401).json({ message: "Does not pass token" })
+//         });
 //     }
-// }
+// };
+
+function restricted(res, req, next){
+    const token = req.headers.authorization;
+    if(token){
+        jwt.verify(token, secret, (err, decodedToken => {
+            req.jwtPayload = decodedToken;
+            if(err){
+                res.status(401).json({ message: "Does not pass verification" })
+            }
+            next();
+        }))
+    }else{
+        res.status(401).json({ message: "Does not pass token" })
+    }
+}
 
 router
-    .get('/list', validateToken, (req, res) => {
+    .get('/list', restricted, (req, res) => {
             User
                 .find()
                 .select('username')
