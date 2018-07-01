5048a98adf657f1bfef7378af80e6e36bc0c4969
AJ Genung <DragonRydrz@me.com>
2018-07-01 00:11:07 -0400

    switched db to production db

--- secrets/config.js
+++ secrets/config.js
@@ -1,7 +1,8 @@
 const config = {
   // database: 'mongodb://localhost/lambdanotestesting',
-  database:
-    'mongodb://lambdanotes1:lambdanotes1@ds261429.mlab.com:61429/lambda-notes',
+  // database:
+  //   'mongodb://lambdanotes1:lambdanotes1@ds261429.mlab.com:61429/lambda-notes',
+  database: 'mongodb://138.197.69.65:27017/lambdaNotes',
   secret: 'Bend the knee!',
   salt: "Dragons are a real man's best friend",
 };
