2d13269f77ec391cfe26fff18fc320048104cb46
Obo Agboghidi <oagboghi2@gmail.com>
2018-07-01 02:55:36 -0700

    fixing authenticate

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
-const secret = 'Secret is yours';
+const mysecret = 'Secret is yours';
 
 require('dotenv').config();
 
@@ -18,27 +18,20 @@ server.use(express.json());
 
 
 
-const validateToken = (req, res, next) => {
-  // this piece of middleware is taking the token delivered up to the server and verifying it
-  // if no token is found in the header, you'll get a 422 status code
-  // if token is not valid, you'll receive a 401 status code
-  const token = req.headers.authorization;
-  if (!token) {
-    res
-      .status(422)
-      .json({ error: 'No authorization token found on Authorization header' });
-  } else {
-    jwt.verify(token, secret, (err, decoded) => {
-      if (err) {
-        res
-          .status(401)
-          .json({ error: 'Token invalid, please login', message: err });
-      } else {
-        // token is valid, so continue on to the next middleware/request handler function
-        next();
-      }
-    });
-  }
+const authenticate = (req, res, next) => {
+    // You won't need to change anything in this file here.
+    const token = req.get('Authorization');
+    if (token) {
+        jwt.verify(token, mysecret, (err, decoded) => {
+            if (err) return res.status(422).json(err);
+            req.decoded = decoded;
+            next();
+        });
+    } else {
+        return res.status(403).json({
+            error: 'No token provided, must be set on the Authorization Header'
+        });
+    }
 };
 
 const getNote = require('./backend/getNote/getNote.js');
@@ -47,10 +40,10 @@ const deleteNote = require('./backend/deleteNote/deleteNote.js')
 const editNote = require('./backend/editNote/editNote.js')
 const userLogin = require('./backend/users/userController.js')
 
-server.use('/api/get', validateToken, getNote);
-server.use('/api/create', validateToken, createNote);
-server.use('/api/delete', validateToken, deleteNote);
-server.use('/api/edit', validateToken, editNote);
+server.use('/api/get', authenticate, getNote);
+server.use('/api/create', authenticate, createNote);
+server.use('/api/delete', authenticate, deleteNote);
+server.use('/api/edit', authenticate, editNote);
 server.use('/api/user/', userLogin);
 
 
