d6f489a45d32504e451d3e6ac64aa7f9fc4b42dd
Obo Agboghidi <oagboghi2@gmail.com>
2018-06-30 18:32:49 -0700

    moving jwt to server.js

--- backend/users/userController.js
+++ backend/users/userController.js
@@ -14,45 +14,23 @@ function generateToken(user){
     return jwt.sign(payload, secret, options)
 }
 
-// const validateToken = (req, res, next) => {
-//     // this piece of middleware is taking the token delivered up to the server and verifying it
-//     // if no token is found in the header, you'll get a 422 status code
-//     // if token is not valid, you'll receive a 401 status code
-//     const token = req.headers.authorization;
-//     if (!token) {
-//         res
-//             .status(422)
-//             .json({ error: 'No authorization token found on Authorization header' });
-//     } else {
-//         jwt.verify(token, secret, (err, decoded) => {
-//             if (err) {
-//                 res
-//                     .status(401)
-//                     .json({ error: 'Token invalid, please login', message: err });
-//             } else {
-//                 // token is valid, so continue on to the next middleware/request handler function
-//                 next();
-//             }
-//         });
-//     }
-// };
 
-function restricted(req, res, next){
-    const token = req.headers.authorization;
-    console.log(token)
-    if(token){
-        console.log("if successful, run this block of code", token)
-        jwt.verify(token, secret, (err, decodedToken => {
-            req.jwtPayload = decodedToken;
-            if(err){
-                return res.status(401).json({ message: "Does not pass verification" })
-            }
-            next();
-        }))
-    }else{
-        return res.status(401).json({ message: "Does not pass token" })
-    }
-}
+// function restricted(req, res, next){
+//     const token = req.headers.authorization;
+//     console.log(token)
+//     if(token){
+//         console.log("if successful, run this block of code", token)
+//         jwt.verify(token, secret, (err, decodedToken => {
+//             req.jwtPayload = decodedToken;
+//             if(err){
+//                 return res.status(401).json({ message: "Does not pass verification" })
+//             }
+//             next();
+//         }))
+//     }else{
+//         return res.status(401).json({ message: "Does not pass token" })
+//     }
+// }
 
 router
     .get('/list', restricted, (req, res) => {
