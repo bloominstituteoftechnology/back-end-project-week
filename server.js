9cbc1bb4ff9b8d2f6f111aa7adbc10d052cc076d
Obo Agboghidi <oagboghi2@gmail.com>
2018-07-02 20:44:09 -0700

    update netlify

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ server.use(express.json());
 
 
 const authenticate = (req, res, next) => {
-    const token = req.headers.Authorization;
+    const token = req.get('Authorization');
     console.log(token);
     if (token) {
         jwt.verify(token, mysecret, (err, decoded) => {
