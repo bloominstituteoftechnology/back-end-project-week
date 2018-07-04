81f3aba89d155d4cca33e393ce937eeb1f17ebd6
Christina Kopecky <kopecky12112@gmail.com>
2018-07-04 09:57:57 -0700

    changes

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express");
+require("dotenv").config();
 const server = express();
 
 const database = "heroku_nq1462wx";
-const dbUser = "cmvnk";
-const dbPswd = "temp1234";
-const port = "25851";
+const dbUser = process.env.DBUSER;
+const dbPswd = process.env.DBPSWD;
+const port = process.env.PORT;
 
 const noteController = require("./Controllers/NoteController");
 const userController = require("./Controllers/UserController");
@@ -19,7 +20,7 @@ mongoose.connect(`mongodb://${dbUser}:${dbPswd}@ds1${port}.mlab.com:${port}/${da
         console.log(`Connected to ${database} database on mlab`)
     })
     .catch(err => {
-        console.log({Error: err.message, message: "Did you start an instance of Mongo?"});
+        console.log({Error: err.message, message: "Did you start an instance of Mongo? || have you checked dbUser and dbPswd?"});
     });
 
 //middleware
