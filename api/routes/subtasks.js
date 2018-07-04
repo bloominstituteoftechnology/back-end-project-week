845ae6b73856702f3e24a34a506abae8bd276b8c
Danny Vail <danny_vail@outlook.com>
2018-07-04 00:41:53 -0400

    Restrict POST for /api/subtasks - users can only create subtasks for tasks whos parent project they are members of

--- api/routes/subtasks.js
+++ api/routes/subtasks.js
@@ -2,18 +2,24 @@ const router = require('express').Router();
 const Subtask = require('../models/Subtask');
 const { sendErr, sendRes } = require('../utils/apiResponses');
 const { authenticate } = require('../middleware/authentication');
+const { isTaskAccessible } = require('../middleware/permissions');
 
 router
-  .post('/', authenticate, (req, res) => {
+  .post('/', authenticate, isTaskAccessible, (req, res) => {
     const newSubtask = req.body;
+    const authorized = req.taskAccessible;
 
-    Subtask.create(newSubtask)
-      .then(subtask => {
-        sendRes(res, '201', subtask);
-      })
-      .catch(err => {
-        sendErr(res, err, 'The subtask could not be created.');
-      });
+    if (authorized) {
+      Subtask.create(newSubtask)
+        .then(subtask => {
+          sendRes(res, '201', subtask);
+        })
+        .catch(err => {
+          sendErr(res, err, 'The subtask could not be created.');
+        });
+    } else {
+      sendErr(res, '403', 'User is not authorized to perform this action.');
+    }
   })
   .put('/:id', authenticate, (req, res) => {
     const { id } = req.params;
