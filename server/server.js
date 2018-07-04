97897b31c1ae7c73954c423257edac889ecce685
april <martinezapril56@yahoo.com>
2018-07-03 22:17:19 -0400

    working on  routes and endpoints

--- server/server.js
+++ server/server.js
@@ -19,26 +19,45 @@ const corsOptions = {
 server.use( cors() );
 server.use( express.json() );
 
+const Token = ( req, res, next ) =>
+{
+    const token = req.get( 'Authorization' );
+    if ( token )
+    {
+        jwt.verify( token, mysecret, ( err, decoded ) =>
+        {
+            if ( err ) return res.status( 422 ).json( err );
+            req.decoded = decoded;
+            next();
+        } );
+    } else
+    {
+        return res.status( 403 ).json( {
+            error: 'No token given'
+        } );
+    }
+}
+
 
 //where my information is getting imported from
 const getNote = require( './backend/getNote/getNote.js' );
 const createNote = require( './backend/createNote/createNote.js' )
 const deleteNote = require( './backend/deleteNote/deleteNote.js' )
 const editNote = require( './backend/editNote/editNote.js' )
+const userLogin = require('./backend/users/userController.js')
 
-
-const Notes = require( './backend/notesModel/notesModel.js' )
 
 //where my endpoints are coming in from
-server.use( '/api/get', getNote );
-server.use( '/api/create', createNote );
-server.use( '/api/delete', deleteNote );
-server.use( '/api/edit', editNote );
+server.use( '/api/get', Token, getNote ); 
+server.use( '/api/create', Token, createNote );
+server.use( '/api/delete', Token, deleteNote );
+server.use( '/api/edit', Token, editNote );
+server.use('/api/user/', userLogin)
  
 //this shows me me that my server is running
 server.get( '/', ( req, res ) =>
 {
-    res.send( '<h2>DB:${process.env.mongo}</h2>' );
+    res.send( 'DB:${process.env.mongo}' );
 } );
 
 // mongoose.Promise = global.Promise;
