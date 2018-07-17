ee0e83a82e88c37fffb1ef665f87725ee30807c6
Lisa Canini <three_nineteen@yahoo.com>
2018-07-16 21:07:48 -0700

    trello and heroku links added, setup complete

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const express = require('express')
 const PORT = process.env.PORT || 5000
 
 express()
-    .get('/', (req, res) => res.json({ hello: 'world!!!' }))
+    .get('/', (req, res) => res.json({ msg: '###--Connected--###' }))
     .listen(PORT, () => console.log(`Listening on ${PORT}`))
 
 
-// mongoose.connect('mongodb://LisaCee:TU4aFT_PRN@ds239681.mlab.com:39681/lambda-notes')
+mongoose
+    .connect('mongodb://LisaCee:TU4aFT_PRN@ds239681.mlab.com:39681/lambda-notes')
+    .then(() => {
+        console.log('Connected!!')
+    })
+    .catch(err => {
+        console.log(err);
+    })
