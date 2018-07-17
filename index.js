43dfa8d422ceeea43533237ed26a670e92c83ee9
Lisa Canini <three_nineteen@yahoo.com>
2018-07-16 20:17:39 -0700

    mlab db created

--- index.js
+++ index.js
@@ -4,3 +4,6 @@ const PORT = process.env.PORT || 5000
 express()
     .get('/', (req, res) => res.json({ hello: 'world' }))
     .listen(PORT, () => console.log(`Listening on ${PORT}`))
+
+
+// mongoose.connect('mongodb://LisaCee:TU4aFT_PRN@ds239681.mlab.com:39681/lambda-notes')
