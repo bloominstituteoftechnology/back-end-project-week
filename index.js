9c0b6c14b14d281bdbf5a427c32edca3ef62e56f
Lisa Canini <three_nineteen@yahoo.com>
2018-07-16 20:46:35 -0700

    initial commit, accidentally forked from wrong branch

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const PORT = process.env.PORT || 5000
+
+express()
+    .get('/', (req, res) => res.json({ hello: 'world!!!' }))
+    .listen(PORT, () => console.log(`Listening on ${PORT}`))
+
+
+// mongoose.connect('mongodb://LisaCee:TU4aFT_PRN@ds239681.mlab.com:39681/lambda-notes')
