fc98c8078dc3b87fdd3e25e86e86f94f23bd1582
Lisa Canini <three_nineteen@yahoo.com>
2018-07-17 19:06:27 -0700

    added server req to index

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // const express = require('express')
 const PORT = process.env.PORT || 5000;
 const mongoose = require('mongoose');
+const server = require('./server')
 
 // express()
 //     .get('/', (req, res) => res.json({ msg: '###--Connected--###' }))
