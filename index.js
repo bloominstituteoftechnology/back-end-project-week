2ec74a8a1378c1f9a8731ded7c53a8a6b1a89aca
Lisa Canini <three_nineteen@yahoo.com>
2018-07-17 18:49:55 -0700

    removed express and added mongoose to index

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const express = require('express')
+// const express = require('express')
 const PORT = process.env.PORT || 5000;
 const mongoose = require('mongoose');
 
-express()
-    .get('/', (req, res) => res.json({ msg: '###--Connected--###' }))
-    .listen(PORT, () => console.log(`Listening on ${PORT}`))
+// express()
+//     .get('/', (req, res) => res.json({ msg: '###--Connected--###' }))
+//     .listen(PORT, () => console.log(`Listening on ${PORT}`))
 
 
 mongoose
