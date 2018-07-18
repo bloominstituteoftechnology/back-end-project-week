73f5903bcc34c9eaa2a6bf6de83a7c2255a72119
Lisa Canini <three_nineteen@yahoo.com>
2018-07-17 18:47:48 -0700

    mongoose added

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 const express = require('express')
 const PORT = process.env.PORT || 5000;
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
 express()
     .get('/', (req, res) => res.json({ msg: '###--Connected--###' }))
     .listen(PORT, () => console.log(`Listening on ${PORT}`))
 
 
-// mongoose
-//     .connect('mongodb://LisaCee:TU4aFT_PRN@ds239681.mlab.com:39681/lambda-notes')
-//     .then(() => {
-//         console.log('Connected!!')
-//         server.listen(PORT, () => {
-//             console.log(`Listening on ${PORT}`)
-//         })
-//     })
-//     .catch(err => {
-//         console.log(err);
-//     })
+mongoose
+    .connect('mongodb://LisaCee:TU4aFT_PRN@ds239681.mlab.com:39681/lambda-notes')
+    .then(() => {
+        console.log('Connected!!')
+        server.listen(PORT, () => {
+            console.log(`Listening on ${PORT}`)
+        })
+    })
+    .catch(err => {
+        console.log(err);
+    })
