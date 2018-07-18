d264ccdee1044a606c3f87b7e59089146e2d1dbb
Lisa Canini <three_nineteen@yahoo.com>
2018-07-18 10:48:06 -0700

    3 tests written for note model

---
+++ tests/note.test.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+const Note = require('../models/Note');
+
+describe('The Note Model', () => {
+    beforeAll(() => {
+        return mongoose
+            .connect('mongodb://LisaCee:TU4aFT_PRN@ds141631.mlab.com:41631/lambdatest')
+            .then(() => console.log('\n=== connected to TEST DB ==='))
+            .catch(err => {
+                console.log('error connecting to TEST database');
+            });
+    });
+
+    afterAll(() => {
+        return mongoose
+            .disconnect()
+            .then(() => console.log('\n=== disconnected from TEST DB ==='));
+    });
+
+    afterEach(() => {
+        return Note.remove()
+    })
+
+    it('runs the tests', () => { });
+    it('contains a title field', async () => {
+        const titleNote = {
+            title: 'Note 1',
+            content: 'Note 1 content'
+        }
+
+        const newNote = await Note.create(titleNote);
+        expect(newNote.title).toBe('Note 1')
+    })
+    it('contains gives a content field', async () => {
+        const contentNote = {
+            title: 'Note Title',
+            content: 'Note Content'
+        }
+      
+        const newNote = await Note.create(contentNote);
+        expect(newNote.content).toBe('Note Content')
+    })
+
+});
