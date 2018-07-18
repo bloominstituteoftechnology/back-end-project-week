296e15cca85d59ba48e57456f22f4feb18425f24
Lisa Canini <three_nineteen@yahoo.com>
2018-07-17 20:28:45 -0700

    user schema and bcrypt started

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-// const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt')
 
 const User = new mongoose.Schema({
     email: {
@@ -19,15 +19,15 @@ const User = new mongoose.Schema({
     }
 })
 
-// User.pre('save', function (next) {
-//     bcrypt.hash(this.password, 11, (err, hash) => {
-//         if (err) {
-//             return next(err)
-//         } else {
-//             this.password = hash;
-//             return next()
-//         }
-//     })
-// })
+User.pre('save', function (next) {
+    bcrypt.hash(this.password, 11, (err, hash) => {
+        if (err) {
+            return next(err)
+        } else {
+            this.password = hash;
+            return next()
+        }
+    })
+})
 
 module.exports = mongoose.model('User', User)
