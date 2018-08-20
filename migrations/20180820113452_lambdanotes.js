cae0b529e0c92176fbc2ad7c45e3e2d2d557b187
Mariana Cerda <raymond_grc@yahoo.com>
2018-08-20 11:38:53 -0500

    Create index.js, knexfile.js, knex migrations

---
+++ migrations/20180820113452_lambdanotes.js
@@ -0,0 +1,12 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('notes', function (table){
+    table.increments();
+    table.string('title').notNullable();
+    table.string('content');
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.scheme.dropTable('notes')
+};
