1d7027a7ffa3b8ccfcdca9646bd9748d6c1932ef
cmok4290 <cmok4290@gmail.com>
2018-08-20 12:29:45 -0400

    added get endpoint and helper

--- data/helpers/index.js
+++ data/helpers/index.js
@@ -1,5 +1,22 @@
 const db = require('../db');
+let query = db('notes as t');
 
 module.exports = {
+    get: function (id) {
+        return query.then(notes => {
+            return notes.map(note => this.recordToBody(note));
+        });
+    },
+    add: function () {
 
+    },
+    edit: function () {
+
+    },
+    drop: function () {
+
+    },
+    recordToBody: function(record) {
+        return { ...record };
+    }
 }
--- notes/index.js
+++ notes/index.js
@@ -2,5 +2,13 @@ const express = require('express');
 const router = express.Router();
 const db = require('../data/helpers/index');
 
+router.get('/', async (req, res) => {
+    try {
+        const notes = await db.get();
+        res.status(200).json(notes);
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
 
 module.exports = router;
