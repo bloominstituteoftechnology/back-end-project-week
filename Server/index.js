1aaa0e81c61f9dd34bd1999b0a474ed2b5b5a4cb
Katie Gorbell <katiegorbell@gmail.com>
2018-08-20 12:49:26 -0500

    added request to receive a list of notes

--- Server/index.js
+++ Server/index.js
@@ -7,4 +7,13 @@ const port = 8000;
 server.use(express.json());
 
 
+server.get('/notes', (req, res) => {
+    db('notes')
+        .then(notes => {
+            res.status(200).json(notes)
+        })
+        .catch(err => res.status(500).json(err))
+})
+
+
 server.listen(port, () => console.log('\n==== API is running ====\n'));
