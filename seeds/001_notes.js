7fb72a7b2bfabb30f96209fb3a03672884b6a5b4
Joe Stanfield <jlstan06@hotmail.com>
2018-08-20 12:27:00 -0400

    database setup with migrations and seeds

---
+++ seeds/001_notes.js
@@ -0,0 +1,13 @@
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('notes').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('project').insert([
+        {id: 1, title:'Build a bear', content:'Assemble a bear from parts'},
+        {id: 2, title:'Make Dinner', content:'Make chick parm for family'},
+        {id: 3, title:'Wash laundry', content:'laundry has been piling up'}
+      ]);
+    });
+};
