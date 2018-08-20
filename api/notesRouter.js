a4e8de6175a783dc86ddf93e920734dde582d9f3
Simon <Simon_Freelance@hotmail.com>
2018-08-20 13:20:59 -0400

    Changed status code for POST for notes table

--- api/notesRouter.js
+++ api/notesRouter.js
@@ -13,10 +13,18 @@ router.get("/", (req, res, next) => {
       next(err);
     });
 });
+
 router.post("/", (req, res, next) => {
   db("notes")
     .insert(req.body)
     .then(response => {
+      res.status(codes.CREATED).json(response);
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
       res.status(codes.OK).json(response);
     })
     .catch(err => {
