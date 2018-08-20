609404d031ec41afe7e6f79730b2843e7a523904
Joe Stanfield <jlstan06@hotmail.com>
2018-08-20 12:58:52 -0400

    added endpoint for view and create as well as key logger and security

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
 const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const cors = require('cors')
 
 const db = require('./data/db.js');
 
 const server = express();
-
 server.use(express.json());
+server.use(helmet());
+server.use(morgan('dev'));
+server.use(cors());
 
 ///endpoints go here
+
+server.get('/notes', (req, res) => {
+    db('notes').then(note => {
+      res.status(200).json(note);
+    })
+    .catch(err => res.status(500).json(err));
+    
+  });
+
+  server.post('/notes', (req, res) => {
+    const { title, content } = req.body;
+    if (!title || !content )
+    res.status(400).json({ errorMessage: "Required fields"});
+    db.insert({ title, content }) 
+    .into("notes")
+    .then(note => res.status(201).json({title, content})) 
+    .catch(err => res.status(400).json({error: "Error posting"}))
+  })
 
 const port = 3300;
 server.listen(port, function() {
--- seeds/001_notes.js
+++ seeds/001_notes.js
@@ -4,7 +4,7 @@ exports.seed = function(knex, Promise) {
   return knex('notes').del()
     .then(function () {
       // Inserts seed entries
-      return knex('project').insert([
+      return knex('notes').insert([
         {id: 1, title:'Build a bear', content:'Assemble a bear from parts'},
         {id: 2, title:'Make Dinner', content:'Make chick parm for family'},
         {id: 3, title:'Wash laundry', content:'laundry has been piling up'}
