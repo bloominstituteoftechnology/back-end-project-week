5930c08e5c7f2d17c828979758d4afd9fd113100
TangledTessellations <mcevoya@oregonstate.edu>
2018-08-20 12:38:29 -0700

    Build basic endpoint for single note

--- api/routes/notes.js
+++ api/routes/notes.js
@@ -14,4 +14,20 @@ server.get('/get/all', (req,res, next) => {
         })
         .catch(next)
 })
+
+server.get('/get/:id', (req,res,next) => {
+    const { id } = req.params
+
+    if(!id){
+        res.status(404).json("ID Not found")
+    }
+    db('notes')
+        .select('id', 'title', 'textBody')
+        .where({id})
+        .first()
+        .then(note => {
+            res.status(200).json(note)
+        })
+        .catch(next)
+})
 module.exports = server
