792d5d73505d97e72fb3022e04b38e214b829eef
Austin Merando <ajmerando1@yahoo.com>
2018-08-20 14:15:42 -0700

    crud complete

--- server.js
+++ server.js
@@ -12,4 +12,38 @@ server.use(helmet());
 
 server.use('/api', apiRoutes);
 
+server.use((err, req, res, next) => {
+    switch (err.code) {
+        case 500:
+            res.status(500).send({
+                success: false,
+                data: undefined,
+                title: err.message,
+                description: 'Failed get',
+                recovery: 'Please check database'
+            })
+            break;
+        case 501:
+            res.status(501).send({
+                success: false,
+                data: undefined,
+                title: 'Bad database modification',
+                description: err.message,
+                recovery: 'Please check inputs'
+            })
+            break;
+        case 502:
+            res.status(502).send({
+                success: false,
+                data: undefined,
+                title: 'Removed Failed',
+                description: err.message,
+                recovery: 'Please check inputs'
+            })
+            break;
+        default:
+            res.status(404).send({ message: 'Something bad happened' })
+    }
+})
+
 server.listen(8000, () => console.log('API running on port 8000... *.*'));
