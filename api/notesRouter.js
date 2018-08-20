6f5ee82c43feac9373d8fe52f6d4b2daabeac5d2
Simon <Simon_Freelance@hotmail.com>
2018-08-20 13:08:47 -0400

    GET request for notes table completed

--- api/notesRouter.js
+++ api/notesRouter.js
@@ -4,4 +4,13 @@ const express = require("express");
 const db = require("../data/db");
 const router = express.Router();
 
+router.get("/", (req, res, next) => {
+  db("notes")
+    .then(response => {
+      res.status(codes.OK).json(response);
+    })
+    .catch(err => {
+      next(err);
+    });
+});
 module.exports = router;
