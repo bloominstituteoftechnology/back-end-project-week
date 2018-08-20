556eb72ee5d520268e39c22c3edfe9bbd6bbd09f
Sumayyah <sumayyah@zubairs-air.attlocal.net>
2018-08-20 15:35:56 -0700

    MVP completed 💖

--- index.js
+++ index.js
@@ -13,18 +13,54 @@ server.get('/notes', (req, res) => {
     });
 });
 
+server.get('/notes/:id', (req, res) => {
+  const { id } = req.params;
+  db('notes')
+    .where({ id })
+    .then((note) => {
+      res.status(200).json(note);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
+
 server.post('/notes', (req, res) => {
   const note = req.body;
   if (!note.title || !note.text)
     res.status(400).json({ errorMessage: 'Provide a title and text please' });
   db('notes')
     .insert(note)
-    .then((response) => {
-      res.status(201).json(response);
+    .then((ids) => {
+      const id = ids[0];
+      res.status(201).json({ id, ...note });
     })
     .catch((err) => {
       res.status(500).json(err);
     });
+});
+
+server.put('/notes/:id', (req, res) => {
+  const { id } = req.params;
+  const note = req.body;
+  db('notes')
+    .where({ id })
+    .update(note)
+    .then((note) => {
+      res.status(201).json(note);
+    })
+    .catch((err) => res.status(500).json(err));
+});
+
+server.delete('/notes/:id', (req, res) => {
+  const { id } = req.params;
+  db('notes')
+    .where({ id })
+    .del()
+    .then((response) => {
+      res.status(200).json(response);
+    })
+    .catch((err) => res.status(500).json(err));
 });
 
 const port = 5000;
