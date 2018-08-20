40cdbc48f62f2d03bc17ad22b16c9e8a9f3713ea
markdavidwilliams <markwilliams.aus@gmail.com>
2018-08-20 16:29:53 -0500

    added cors

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const apiRoutes = require('./api/apiRoutes');
+const cors = require('cors');
 
 const server = express();
 
+server.use(cors());
 server.use(express.json());
 server.use('/api', apiRoutes);
 
