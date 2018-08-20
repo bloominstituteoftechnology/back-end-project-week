bfc60b0d95cd1c808c478132a66fbd15373d57a7
cmok4290 <cmok4290@gmail.com>
2018-08-20 12:06:46 -0400

    server running

---
+++ index.js
@@ -0,0 +1,26 @@
+const cors = require('cors');
+const express = require('express');
+const helmet = require('helmet');
+
+// routers
+const noteRouter = require('./notes/index');
+
+const server = express();
+server.use(cors());
+server.use(express.json());
+server.use(helmet());
+
+server.use('/notes', noteRouter);
+
+server.get('/', (req, res) => {
+    res.status(200).send(`\n=== Web API Listening on http://localhost:${port} ===\n`);
+});
+
+server.use(function (req, res) {
+    res.status(404).json({ error: "Page Not Found" });
+});
+
+const port = 8000;
+server.listen(port, function () {
+    console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
+});
---
+++ notes/index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../data/helpers/index');
+
+
+module.exports = router;
