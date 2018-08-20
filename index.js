5ff618087be1d6968698c749050db55db0610141
Mariana Cerda <raymond_grc@yahoo.com>
2018-08-20 12:06:29 -0500

    add api route get all notes, add more dummy data

--- index.js
+++ index.js
@@ -7,8 +7,16 @@ server.use(express.json());
 
 const port = 8000;
 
-
-
+//Get all notes
+server.get('/api/notes', (req, res) => {
+  db.select().from('notes')
+  .then(response => {
+    res.json({"Message": response});
+  })
+  .catch(error => {
+   res.status(500).send({ error: "Server Error" })
+  })
+});
 
 
 
