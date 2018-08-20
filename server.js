830c0c2ccc92b6ef64812933bccb3aab24f3a004
Allen Hai <allenhai03@gmail.com>
2018-08-20 12:11:06 -0500

    write GET /notes handler with a few tests

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const express = require('express')
+const db = require('knex')(require('./knexfile').development)
 
 const server = express()
 server.use(express.json())
 
-server.listen(1234, () => console.log('... 1234 ...'))
+server.get('/notes', async (req, res) => {
+  try {
+    const notes = await db('notes')
+    res.status(200).json(notes)
+  } catch (e) {
+    next(e)
+  }
+})
+
+server.use((err, req, res, next) => {
+  res.status(500).json(err)
+})
+
+//server.listen(1234, () => console.log('... 1234 ...'))
 
 module.exports = server
--- server.spec.js
+++ server.spec.js
@@ -1,1 +1,21 @@
 const request = require('supertest')(require('./server'))
+
+describe('Display a list of notes', () => {
+  it('GET /notes should respond with a 200 status code', async () => {
+    const res = await request.get('/notes')
+    expect(res.status).toBe(200)
+  })
+  it('should send json with an array of notes with title and content fields', async () => {
+    const res = await request.get('/notes')
+    expect(res.type).toBe('application/json')
+    expect(res.body).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          id: expect.any(Number),
+          title: expect.any(String),
+          content: expect.any(String)
+        })
+      ])
+    )
+  })
+})
