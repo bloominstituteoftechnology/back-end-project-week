2e5144f7f6034b65e7975df37c418e4ae4228645
Kevin Tena <kevten09@gmail.com>
2018-08-20 16:01:13 -0500

    Added post endpoint to create a new note

--- index.js
+++ index.js
@@ -16,6 +16,24 @@ server.get('/notes', async (req, res) => {
     }
 })
 
+server.post('/notes', async (req,res) => {
+    let note = req.body;
+    if(!('title') in note){
+        res.status(400).send({ error: "Please provide a title for your note."});
+    }
+    else if(note.title.length > 30){
+        res.status(400).send({error: "Please limit the characters in the title to 30."});
+    }
+    try { 
+        await db.insert(note).into('notes');
+        res.status(201).json({msg: 'Note successfully created'});
+    }
+
+    catch(err){
+        res.status(500).json({error: 'Note could not be created'});
+    }
+})
+
 const port = 8000;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
