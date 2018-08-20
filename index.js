08feaacc7583ef4476430327ea0652cf8b1a76a8
CLN <cln@Sags-MacBook-Pro.local>
2018-08-20 15:54:17 -0400

    Added post and get to notes and its correctling adding new note with title and body to database,.

--- index.js
+++ index.js
@@ -9,6 +9,21 @@ server.get('/', (req, res) => {
     res.send('up and running... LambdaNotes')
 });
 
+server.get('/notes', (req, res) => {
+    db('notes').then(note => {
+        res.status(200).json(note)
+    }).catch(err => res.status(500).json(err))
+});
+
+server.post('/notes', (req, res) => {
+    const note = req.body;
+
+    db.insert(note).into('notes').then(notes =>{
+        const id = notes[0];
+        res.status(201).json({id, ...note})
+    }).catch(err => res.status(500).json(err))
+})
+
 const port = 6500;
 server.listen(port, function () {
     console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
