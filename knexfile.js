075af158d974a839d8220f557805fea1bd8630a4
Simon <Simon_Freelance@hotmail.com>
2018-08-20 12:52:06 -0400

    Renamed db file in knexfile

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
+      filename: './data/lambda-notes.sqlite3'
     },
     migrations: {
       directory: './data/migrations'
