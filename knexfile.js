41c1ea9550d2fb18c829319ed0d5681b4df1d1b7
Mark Hong <m4rkh0ng@gmail.com>
2018-08-20 19:17:07 -0400

    basic get for notes from db

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
+      filename: './data/back-end-project-data.sqlite3'
     },
     useNullasDefault: true
   },
--- server.js
+++ server.js
@@ -1,11 +1,43 @@
 const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+
+const db = require('./data/db');
+
+// const configureRoutes = require('./config/routes');
 
 const server = express();
+const corsOptions = {
+  // do this later
+};
 
 server.use(express.json());
+server.use(cors());
+
+// configureRoutes(server);
 
 server.get('/', (req, res) => {
   res.status(200).json({ hello: "world", project: "back-end-project" });
+})
+
+server.get('/notes', (req, res) => {
+  
+  db('notes')
+    .then(notes => {
+      res.status(200).json({notes});
+    })
+
+  // axios
+  //   .get(
+      
+  //   )
+  //   .then(response => {
+  //     res.status(200).json(response.data);
+  //   })
+  //   .catch(err => {
+  //     res.status(500).json({ message: 'Error getting notes.', error: err});
+  //   })
+
 })
 
 // GET | Return stored notes
@@ -15,4 +47,4 @@ server.get('/', (req, res) => {
 // })
 
 const port = process.env.PORT || 8888;
-server.listen(port, () => console.log("Node-Express API running on process.env.PORT || 8888. . ."));
+server.listen(port, () => console.log(`Node-Express API running on ${port}. . .`));
