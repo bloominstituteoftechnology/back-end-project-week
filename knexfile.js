e8297d9c92f0d0ffdcd1ef8437d5858d34fedc76
unknown <may.alex43@gmail.com>
2018-08-20 16:31:17 -0400

    made and tested endpoints for creating, deleting, updating, viewing notes

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
+      filename: './data/notes.sqlite3'
     },
     useNullAsDefault:true,
   },
--- server.js
+++ server.js
@@ -18,6 +18,56 @@ server.get('/notes', (req,res)=> {
     .catch(error=> res.status(500).json(error));
 });
 
+server.get('/notes/:id', (req, res)=> {
+    const {id}=req.params;
+    db('notes')
+    .where({id})
+    .then(notes=>{
+        res.status(200).json(notes);
+    })
+    .catch(error => res.status(500).json(error));
+});
+
+server.post('/notes', (req,res)=> {
+    const note=req.body;
+    db.insert(note)
+      .into('notes')
+      .then(ids=> {
+          const id=ids[0];
+          res.status(200).json(id, ...note);
+      })
+      .catch(error=> {
+          res.status(500).json(error);
+      }); 
+});
+
+server.put('/notes/:id', (req, res)=>{
+    const changes=req.body;
+    const {id}=req.params;
+    db('notes')
+    .where('id','=', id)
+    .update(changes)
+    .then(count=>{
+        res.status(200).json(count);
+    })
+    .catch(error=>{
+        res.status(500).json(error);
+    });
+});
+
+server.delete('/notes/:id', (req, res)=>{
+    const {id}=req.params;
+    db('notes')
+    .where({id})
+    .del()
+    .then(count=>{
+        res.status(200).json(count);
+    })
+    .catch(error=>{
+        res.status(500).json(error);
+    });
+});
+
 ////////////////////////////////////
 
 const port=7000;
