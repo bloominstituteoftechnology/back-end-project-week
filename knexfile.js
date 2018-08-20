1519137c8bd61c983c05657164df5af2e5ff0f24
Thomas Greenhalgh <thomasgreenhalgh@gmail.com>
2018-08-20 09:47:30 -0700

    basic package setup

---
+++ knexfile.js
@@ -0,0 +1,21 @@
+// Update with your config settings.
+
+module.exports = {
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/notes.sqlite3',
+    },
+    pool: {
+      afterCreate: (conn, cb) => {
+        conn.run('PRAGMA foreign_keys = ON', cb);
+      },
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations',
+      tableName: 'dbmigrations',
+    },
+    seeds: { directory: './data/seeds' },
+  },
+};
