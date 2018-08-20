edf0f95d04cbe6c45a481ee0640e1c4baf50e596
Mariana Cerda <raymond_grc@yahoo.com>
2018-08-20 12:30:34 -0500

    Add api status codes, add required fields in post method

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const port = 8000;
 server.get('/api/notes', (req, res) => {
   db.select().from('notes')
   .then(response => {
-    res.json({"Message": response});
+    res.status(200).json({"Message": response});
   })
   .catch(error => {
    res.status(500).send({ error: "Server Error" })
@@ -23,7 +23,7 @@ server.get("/api/notes/:id", (req, res) => {
   const { id } = req.params;
   db.select().from('notes').where('id', id)
     .then(response => {
-      res.json({"Message": response});
+      res.status(200).json({"Message": response});
     })
     .catch(response => {
       res.status(500).send({ error: "Server Error" });
@@ -33,12 +33,16 @@ server.get("/api/notes/:id", (req, res) => {
 //Create new note
 server.post("/api/notes", (req, res) => {
   const { title, content } = req.body;
+  if(!title || !content){
+    res.status(422).json({"Message": "Need title/content"})
+  }
+  else { 
   db.insert({"title": title,
               "content" : content})
               .into('notes')
-    .then(response => (res.json(response)))
+    .then(response => (res.status(200).json(response)))
     .catch(error => {
      res.status(500).send({ error: "Server Error" })
-   })
+   })}
 });
 server.listen(port, () => { console.log(`Server is running on port ${port}`)});
