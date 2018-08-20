1c21001352263e5844acd2f59fe55f1c594f50f5
Katie Gorbell <katiegorbell@gmail.com>
2018-08-20 16:17:29 -0500

    built request to view a signle note, working on resolving error

--- Server/index.js
+++ Server/index.js
@@ -15,6 +15,21 @@ server.get('/notes', (req, res) => {
         .catch(err => res.status(500).json(err))
 })
 
+server.get('/view-note/:id', (req, res) => {
+    const { id } = req.params;
+    db('notes')
+        .where({ id })
+        .first()
+        .then( note => {
+            if (note) {
+                res.status(200).json(note)
+            } else {
+                res.status(404).json({ message: 'the note with the specified id could not be found' })
+            }
+        })
+        .catch(err => res.status(500).json(err))
+})
+
 server.post('/new-note', (req, res) => {
     const note = req.body;
     db('notes')
@@ -29,6 +44,8 @@ server.post('/new-note', (req, res) => {
         })
         .catch( err =>  res.status(500).json(err))
 })
+
+server.put()
 
 
 server.listen(port, () => console.log('\n==== API is running ====\n'));
