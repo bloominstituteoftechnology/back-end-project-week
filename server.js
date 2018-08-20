c29f9e8fceba901763000283bcbd05bab4477f71
Allen Hai <allenhai03@gmail.com>
2018-08-20 17:48:01 -0500

    write POST /notes handler with a few tests

--- server.js
+++ server.js
@@ -4,19 +4,59 @@ const db = require('knex')(require('./knexfile').development)
 const server = express()
 server.use(express.json())
 
-server.get('/notes', async (req, res) => {
+server.post('/notes', async (req, res, next) => {
+  console.log('REQ BODY', req.body)
+  if (!req.body || !req.body.title || !req.body.content) {
+    next({
+      code: 400, 
+      message: 'please provide title and content fields'
+    })
+  }
+
+  try {
+    const ids = await db('notes').insert(req.body)
+    res.status(200).json({id: ids[0], ...req.body})
+  } catch (e) {
+    next({
+      code: 500,
+      message: e.message
+    })
+  }
+})
+
+server.get('/notes', async (req, res, next) => {
   try {
     const notes = await db('notes')
     res.status(200).json(notes)
   } catch (e) {
-    next(e)
+    next({
+      code: 500,
+      message: e.message
+    })
   }
 })
 
-server.use((err, req, res, next) => {
-  res.status(500).json(err)
+
+
+server.get('/notes/:id', async (req, res) => {
+  const id = +req.params.id
+  try {
+    const note = await db('notes')
+      .where('id', '=', id)
+    res.status(200).json(note)
+  } catch (e) {
+    next({
+      code: 500,
+      message: e.message
+    })
+  }
 })
 
-//server.listen(1234, () => console.log('... 1234 ...'))
+
+server.use((err, req, res, next) => {
+  res.status(err.code).json(err.message)
+})
+
+server.listen(1234, () => console.log('... 1234 ...'))
 
 module.exports = server
--- server.spec.js
+++ server.spec.js
@@ -19,3 +19,36 @@ describe('Display a list of notes', () => {
     )
   })
 })
+
+describe('Create a note with title and content', () => {
+  it('POST /notes should respond with a 400 status code if title and content body fields are missing from request', async () => {
+    const res = await request.post('/notes')
+    expect(res.status).toBe(400)
+  })
+  it('POST /notes should respond with a 200 status code if title and content body fields are provided', async () => {
+    const res = await request.post('/notes')
+      .send({title: 'yo', content: 'hello world'}) 
+      .set('Content-Type', 'application/json') 
+      
+    expect(res.status).toBe(200)
+  })
+  it('should send json with object containing id, title, and content of new note', async () => {
+    const res = await request.post('/notes')
+      .send({title: 'yo', content: 'hello world'})
+      .set('Content-Type', 'application/json')
+      expect(res.body).toEqual(
+        expect.objectContaining({
+          id: expect.any(Number),
+          title: expect.any(String),
+          content: expect.any(String)
+        })
+      )
+  })
+})
+
+describe('View an existing note', () => {
+  it('should return a 404 status code if note not found', async () => {
+    const res = await request.get('/notes/100')
+    expect(res.status).toBe(404)
+  })
+})
