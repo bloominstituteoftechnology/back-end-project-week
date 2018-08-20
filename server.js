a7241bad21b5e64fb009bc42b979c9803fafcb1e
Simon <Simon_Freelance@hotmail.com>
2018-08-20 12:27:14 -0400

    Basic setup of the server.js

---
+++ server.js
@@ -0,0 +1,29 @@
+const codes = require("./data/statusCodes");
+const express = require("express");
+const server = express();
+const noteRoutes = require("./api/notesRouter");
+server.use(express.json());
+
+server.use("/api/notes", noteRoutes);
+
+server.use((err, req, res, next) => {
+  err.code = err.code !== undefined ? err.code : codes.INTERNAL_SERVER_ERROR;
+  const errorInfo = {
+    status: err.code,
+    errorMessage: err.message,
+    success: false,
+    route: err.route
+  };
+  switch (errorInfo.code) {
+    case codes.BAD_REQUEST:
+      res.status(codes.BAD_REQUEST).json(errorInfo);
+      return;
+    case codes.NOT_FOUND:
+      res.status(codes.NOT_FOUND).json(errorInfo);
+      return;
+    default:
+      res.status(codes.INTERNAL_SERVER_ERROR).json(errorInfo);
+  }
+});
+const port = 8001;
+server.listen(port, (req, res) => console.log(`Port ${port} is in use`));
