7288ca803dbb689c7907b196074bcefabc3eae6d
chainchompa <chainchompa@gmail.com>
2018-08-20 16:56:48 -0400

    built endpoint to view all notes

--- index.js
+++ index.js
@@ -5,8 +5,18 @@ const server = express();
 server.use(express.json());
 
 server.get('/', (req, res) => {
-    res.send('API up and running!')
+  res.send('API up and running!')
+})
+
+server.get('/api/notes', (req, res) =>  {
+  db('notes')
+  .then(notes => {
+    res.status(200).json(notes);
   })
+  .catch(err => {
+    res.status(500).json({ error: "The notes could not be retrieved." })
+  })
+})
 
 const port = 8000;
 server.listen(port, function() {
