044e9cf86d895a804b9d8bce7167f97de02f27b3
Nicky Chen <nicky.chen0@gmail.com>
2018-08-20 13:21:04 -0700

    added delete, finish mvp

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 
 // Routes
 const NoteRoutes = require("./Routes/NoteRoutes.js");
@@ -7,14 +8,11 @@ const TagRoutes = require("./Routes/TagRoutes.js");
 // Middleware
 const server = express();
 server.use(express.json());
+server.use(cors());
 
 // Routing
 server.use("/api/notes", NoteRoutes);
 server.use("/api/tags", TagRoutes);
-
-server.use("/", (req, res) => {
-  res.send("server is up!!!");
-});
 
 server.listen(8000, () => {
   console.log(`\n=== Web API Listening on http://localhost:8000 === \n`);
