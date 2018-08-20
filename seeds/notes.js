ac0401ca2c228c2394ce6e47de5219241a2a6546
Kamry Bowman <kamry.bowman@gmail.com>
2018-08-20 10:02:17 -0600

    Seed database

---
+++ seeds/notes.js
@@ -0,0 +1,22 @@
+exports.seed = function (knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('notes')
+    .del()
+    .then(() => knex('notes').insert([
+      {
+        title: 'Stuff you learned',
+        textBody: 'There are so many important things you have learned.',
+        createdAt: '2018-08-01 5:45:12',
+      },
+      {
+        title: 'You never forget anything',
+        textBody: "There's a time and a place.",
+        createdAt: '2018-08-02 7:45:12',
+      },
+      {
+        title: 'Cigarettes Kill',
+        textBody: '98% of smokers are soldiers.',
+        createdAt: '2018-08-05 5:35:12',
+      },
+    ]));
+};
