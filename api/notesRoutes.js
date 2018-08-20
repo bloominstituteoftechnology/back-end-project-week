ff5384353761a553dc1ec7dca5dc6605bddd2b7d
markdavidwilliams <markwilliams.aus@gmail.com>
2018-08-20 15:28:02 -0500

    completed get single note by id

--- api/notesRoutes.js
+++ api/notesRoutes.js
@@ -13,4 +13,16 @@ router.get('/', (req, res, next) => {
     .catch(err => console.log(err))
 })
 
+router.get('/:id', (req, res, next) => {
+  const id = req.params.id;
+  db
+    .getNote(id)
+    .then(response => {
+      res
+        .status(200)
+        .json(response)
+    })
+    .catch(err => console.log(err))
+})
+
 module.exports = router;
