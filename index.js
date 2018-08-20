7baab64ee82a5b3f0a98c33bf1670eb89f0d8d81
Kevin Tena <kevten09@gmail.com>
2018-08-20 15:31:02 -0500

    Added get endpoint to retrieve list of notes from database

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ const server = express();
 
 server.use(express.json());
 
+server.get('/notes', async (req, res) => {
+    try{
+        const notes = await db.select().from('notes');
+        res.status(200).json(notes);
+    }
+    catch (err){
+        res.status(500).json({ error: 'Notes could not be retrieved'});
+    }
+})
 
 const port = 8000;
 server.listen(port, function() {
