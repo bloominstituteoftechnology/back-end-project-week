7a7e39dad473becf8cf20159209726a895fc1430
Sumayyah <sumayyah@zubairs-air.attlocal.net>
2018-08-20 15:40:34 -0700

    cors

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const db = require('./data/db');
 const express = require('express');
+const cors = require('cors');
 const server = express();
 server.use(express.json());
+
+server.use(cors());
 
 server.get('/notes', (req, res) => {
   db('notes')
