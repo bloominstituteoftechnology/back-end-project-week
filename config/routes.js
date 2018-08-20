058bcc2af99c61f26f9f14cc7df40df316e608ef
Kevin <brackkevin@gmail.com>
2018-08-20 16:01:57 -0500

    Add POST: note endpoint, GET: note by id endpoint

--- config/routes.js
+++ config/routes.js
@@ -3,6 +3,8 @@ const db = require("../database/dbConfig");
 module.exports = server => {
   server.get("/", root);
   server.get("/api/notes", get_notes);
+  server.post("/api/notes", post_notes);
+  server.get("/api/notes/:id", get_note_id);
 };
 
 const root = (req, res) => {
@@ -17,4 +19,31 @@ const get_notes = (req, res) => {
     .catch(error => {
       res.status(500).json(error.message);
     });
+};
+
+const post_notes = (req, res) => {
+  const note = req.body;
+  if (!note.title || !note.text_body) {
+    res
+      .status(400)
+      .json(
+        "You must send a title and text_body to create a note in the database"
+      );
+  } else {
+    db.insert(note)
+      .into("notes")
+      .then(ids => {
+        const id = ids[0];
+        res.status(201).json({ id, ...note });
+      })
+      .catch(error => res.status(500).json(error.message));
+  }
+};
+
+const get_note_id = (req, res) => {
+  const id = req.params.id;
+  db("notes")
+    .where("id", Number(id))
+    .then(note => res.status(200).json(note))
+    .catch(error => res.status(500).json(error.message));
 };
