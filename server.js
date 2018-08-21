98d7a1a8b57d7ca23a405515d978519424810608
unknown <may.alex43@gmail.com>
2018-08-21 18:20:20 -0400

    modified frontend to hookup with backend

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express= require('express');
 const db=require('./data/db');
 const server=express();
+const cors=require('cors');
 
 server.use(express.json());
+server.use(cors());
 
 server.get('/', (req, res) => {
     res.send('back-end project week');
