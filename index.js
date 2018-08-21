6c86eb14c05801b47127549c4666f6ed21d9146b
Wdifulvio523 <w.difulvio523@gmail.com>
2018-08-21 19:53:45 -0400

    minor changes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const db = require('./data/db');
+// const db = require('./data/db');
 const cors = require('cors'); 
 const knex = require('knex');
 const knexConfig = require('./knexfile');
