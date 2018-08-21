fbaa0154c91893528f5e46772b743b3dc7cc088a
Daniel Chamorro <dan@dcitllc.com>
2018-08-21 13:35:23 -0400

    Ran migration for notes table

--- migrations/20180821132438_notes_db.js
+++ migrations/20180821132438_notes_db.js
@@ -1,8 +1,20 @@
-
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable("notes", notes => {
+    notes.increments("_id");
+
+    notes
+      .string("tags")
+      .notNullable()
+      .defaultTo("");
+    notes.string("title").notNullable();
+    notes.string("textBody").notNullable();
+    notes
+      .int("__v")
+      .notNullable()
+      .defaultTo(0);
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.dropTableIfExists("notes");
 };
