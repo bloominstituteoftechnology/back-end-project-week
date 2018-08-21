d6e38514afeb2b54f9f3b933cdbf4557a2fe99cb
Ari Mercado <ari@Apples-Mac-3.local>
2018-08-20 20:01:55 -0700

    created register and login enpoints

--- index.js
+++ index.js
@@ -1,17 +1,105 @@
 const express = require('express');
 const cors = require('cors');
+const secret = 'secret';
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const db = require('./data/db');
 const server = express();
 
 server.use(express.json());
 server.use(cors({origin: 'http://localhost:3000', credentials:true}));
 
+function generateToken(user) {
+  const payload = {
+    username: user.username,
+  };
+
+  const options = {
+    expiresIn: '1h',
+    jwtid: '8728391',
+  };
+
+  return jwt.sign(payload, secret, options);
+}
+
 server.get('/', (req, res) => {
   res.send('Running....');
 });
 
-// ! ====================== GET  
-server.get('/api/notes', (req, res) => {
+//! middleware
+function protected(req, res, next) {
+  const token = req.headers.authorization;
+  console.log(token)
+  if (token) {
+    jwt.verify(token, secret, (err, decodedToken) => {
+      if (err) {
+        return res
+          .status(401)
+          .json({ error: 'Token is not valid' });
+      }
+      req.jwtToken = decodedToken;
+      next();
+    });
+  } else {
+    //console.log('err2',err)
+    return res.status(401).json({ error: 'Token is required' });
+  }
+}
+
+// ! ====================== NOTES ENDPOINTS
+
+//! register
+server.post('/api/register', (req, res) => {
+  const user = req.body;
+  const hash = bcrypt.hashSync(user.password, 14);
+  user.password = hash;
+  console.log(user)
+  db    
+    .insert(user)
+    .into('users')
+    .then(id => {
+      db('users') 
+        .then(users => {
+          console.log(users)
+          const user = users.pop();
+          const token = generateToken(user);
+          res.send(token);
+        })
+    })
+    .catch(err => console.log(err));
+});
+
+//! login
+server.post('/api/login', function(req, res) {
+  const credentials = req.body;
+  db('users')
+    .where({ username: credentials.username })
+    .first()
+    .then(function(user) {
+      if (user && bcrypt.compareSync(credentials.password, user.password)) {
+        const token = generateToken(user);
+        res.send(token);
+      } else {
+        return res.status(401).json({ error: 'Incorrect Username or Password' });
+      }
+    })
+    .catch(function(error) {
+      res.status(500).json({ error });
+    });
+});
+
+//! get users
+server.get('/api/users', protected, (req, res) => {
+  db('users')
+    .then(users => {
+      res.json(users);
+    })
+    .catch(err => res.send(err));
+});
+
+
+// ! ====================== NOTES ENDPOINTS
+server.get('/api/notes', protected, (req, res) => {
   db('notes')
     .then(notes => {
       if (notes.length === 0) {
