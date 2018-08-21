779fa25a52a21b978a73c77675b415aff5fd843e
sean chavez <seanchavez@seanchavezsMBP.home>
2018-08-21 12:37:12 -0700

    Implement POST a  new note endpoint

--- index.js
+++ index.js
@@ -9,6 +9,17 @@ server.get('/', (req, res) => {
   res.send('Sanity Check');
 });
 
+server.post('/api/notes', (req, res) => {
+  const note = req.body;
+  db.insert(note).into('notes')
+    .then(response => {
+      res.status(201).json(response);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
 server.listen(PORT, () => {
   console.log(`UP and RUNNING on ${PORT}`)
 });
