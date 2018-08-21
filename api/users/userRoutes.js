de229ed9b92e75905ed5ae9b4396ea00461855bd
DirupT <iamcooled@gmail.com>
2018-08-21 12:37:57 -0500

    updated register to use hashed password

--- api/users/userRoutes.js
+++ api/users/userRoutes.js
@@ -20,15 +20,17 @@ router.post('/register', loginPostCheck, async (req, res, next) => {
     const users = await usersDb.get();
     for (let i = 0; i < users.length; i++) {
         if (users[i].username.toLowerCase() === req.credentials.username.toLowerCase()) {
-            return next({ code: 400, errorMessage: 'There is already a user with that name.' })
+            return next({ code: 400, errorMessage: 'There is already a user with that name.' });
         }
     }
     try {
+        const hash = bcrypt.hashSync(req.credentials.password, 14);
+        req.credentials.password = hash;
         const user = await usersDb.register(req.credentials);
         const token = generateToken({ id: user.id, ...req.credentials });
         return res.status(201).json({ id: user.id, ...req.credentials, token });
     } catch (err) {
-        next({ code: 500, error: "Couldn't save the user to the database." })
+        next({ code: 500, error: "Couldn't save the user to the database." });
     }
 });
 
--- data/seeds/01-users.js
+++ data/seeds/01-users.js
@@ -5,9 +5,9 @@ exports.seed = function (knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('users').insert([
-        { username: 'DirupT', password: 'test' },
-        { username: 'test', password: 'test' },
-        { username: 'test1', password: 'test' },
+        { username: 'DirupT', password: '$2a$14$AeTR7LYM//vlj5ofsrYB2ub2faK5Wd4xv.dRvdk92yvfPgCBkfCVG' },
+        { username: 'test', password: '$2a$14$AeTR7LYM//vlj5ofsrYB2ub2faK5Wd4xv.dRvdk92yvfPgCBkfCVG' },
+        { username: 'test1', password: '$2a$14$AeTR7LYM//vlj5ofsrYB2ub2faK5Wd4xv.dRvdk92yvfPgCBkfCVG' },
       ]);
     });
 };
