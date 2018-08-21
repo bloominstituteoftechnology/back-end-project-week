9a6b9dced78e078f2a120fda1763b43ca751b3c8
Tai <bowlofryce@gmail.com>
2018-08-21 15:11:24 -0500

    finishing testing backend, working on frontend

--- data/routes/routes.js
+++ data/routes/routes.js
@@ -3,9 +3,9 @@ const db = require('../dbConfig.js');
 module.exports = server => {
     server.post('/api/createnotes', createNotes);
     server.get('/api/notes', getNotes);
-    server.get('/api/notes/:id', existNotes);
-    server.put('/api/editnotes', editNotes);
-    server.delete('/api/deletenotes', deleteNotes);
+    server.get('/api/:id/notes', existNotes);
+    server.put('/api/:id/editnotes', editNotes);
+    server.delete('/api/:id/deletenotes', deleteNotes);
 };
 
 function getNotes(req, res) {
@@ -20,13 +20,15 @@ function getNotes(req, res) {
 function existNotes(req, res) {
     const id = req.params.id;
 
-    let query = db('notes');
-    
-    if(id) {
-        query.where('id', id).first();
-    }
-
-    return query;
+    db('notes')
+     .where('id', id)
+     .first()
+     .then(response => {
+         res.status(201).json(response);
+     })
+     .catch(err => {
+         res.status(500).json({err});
+     })
 }
 
 function createNotes(req, res) {
@@ -44,6 +46,7 @@ const createnotes = req.body;
 };
 
 function editNotes(req, res) {
+    const id = req.params.id;
     const editNotes = req.body;
 
     db('notes')
@@ -59,10 +62,13 @@ function editNotes(req, res) {
 
 function deleteNotes(req, res) {
     const id = req.params.id;
-    
+
     db('notes')
      .where('id', id)
      .del()
+     .then(response => {
+        res.status(201).json(`Note ${id} delete successfully`);
+    })  
      .catch(err => {
         res.status(500).json({err});
     });
