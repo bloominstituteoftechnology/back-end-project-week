df750f50d109a91322b699a4107abb5753f8df1d
Simon <Simon_Freelance@hotmail.com>
2018-08-21 12:19:46 -0400

    Added a test  for GET /api/notes for the length of the array

--- server.spec.js
+++ server.spec.js
@@ -1,17 +1,21 @@
 const codes = require("./data/statusCodes");
 const request = require("supertest");
+const db = require("./data/db");
 const { paths, server } = require("./server");
 const { isObject } = require("./functions/testing-functions");
 
 describe("server.js", () => {
   describe("GET request for notes (/api/notes)", () => {
+    beforeAll(() => {
+      db.seed.run({
+        directory: "./data/seeds"
+      });
+    });
     it("should return a status code of 200 OK", async () => {
       const response = await request(server).get(paths.notes);
       expect(response.status).toBe(codes.OK);
     });
-    it("should return JSON", () => {
-
-    });
+    it("should return JSON", () => {});
     it("should return an array", async () => {
       const response = await request(server).get(paths.notes);
       expect(Array.isArray(response.body)).toBeTruthy();
@@ -21,6 +25,10 @@ describe("server.js", () => {
       response.body.forEach(noteObj => {
         expect(isObject(noteObj)).toBeTruthy();
       });
+    });
+    it("should return an array with 3 elements", async () => {
+      const response = await request(server).get(paths.notes);
+      expect(response.body.length).toBe(3);
     });
   });
 });
