5c5c067d08130b42f7f113e591d674ebc90006bb
Arthur Pisakhov <arthurpisakhov1@gmail.com>
2018-08-21 12:36:39 -0400

    Helpers for DB added

--- apiRoutes/noteRoute.js
+++ apiRoutes/noteRoute.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const db  = require('../data/db');
+const db = require('../data/helpers/noteDb');
 const router = express.Router();
 
 router.get('/', (req, res) => {
