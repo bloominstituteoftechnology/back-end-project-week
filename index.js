37b075ab3a3fa52aaf16d86fdd2a1e861b9a2430
JacobLayton <jlayjlay3@gmail.com>
2018-08-21 16:10:13 -0700

    Get route complete

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const db = require('./data/db');
 
-const port = 6000;
+const port = 4444;
 
 const server = express();
 
 server.use(express.json());
+
+server.get('/', (req, res) => {
+  res.send('The server is up and running.')
+});
+
+server.get('/api/notes', (req, res) => {
+  db('notes')
+  .then(notes => {
+    res.status(200).json(notes);
+  })
+  .catch(err => res.status(500).json(err));
+});
 
 server.listen(port, function() {
   console.log(`\n ==== Web API listening on http://localhost:${port} ==== \n`);
--- knexfile.js
+++ knexfile.js
@@ -6,39 +6,52 @@ module.exports = {
     client: 'sqlite3',
     connection: {
       filename: './data/backend.sqlite3'
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './migrations',
+      tableName: 'knex_migrations'
+    },
+    seeds: {
+      directory: './seeds',
     }
   },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
 };
+
+//   staging: {
+//     client: 'postgresql',
+//     connection: {
+//       database: 'my_db',
+//       user:     'username',
+//       password: 'password'
+//     },
+//     pool: {
+//       min: 2,
+//       max: 10
+//     },
+//     migrations: {
+//       directory: './migrations',
+//       tableName: 'knex_migrations'
+//     },
+//     seeds: {
+//       directory: './seeds',
+//     }
+//   },
+
+//   production: {
+//     client: 'postgresql',
+//     connection: {
+//       database: 'my_db',
+//       user:     'username',
+//       password: 'password'
+//     },
+//     pool: {
+//       min: 2,
+//       max: 10
+//     },
+//     migrations: {
+//       tableName: 'knex_migrations'
+//     }
+//   }
+
+// };
