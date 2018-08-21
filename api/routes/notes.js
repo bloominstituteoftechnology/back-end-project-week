c377134c246cc37637497d720e3c2e41575c236f
TangledTessellations <mcevoya@oregonstate.edu>
2018-08-21 09:10:33 -0700

    Handle edited tags and new tags

--- api/routes/notes.js
+++ api/routes/notes.js
@@ -10,6 +10,7 @@ server.get('/get/all', (req,res,next) => {
     db('notes')
         .select('*')
         .then(notes => {
+            notes.forEach(note => note.tags = note.tags ? note.tags.split(',').map(tag => tag.trim()) : [])
             res.status(200).json(notes)
         })
         .catch(next)
@@ -22,13 +23,14 @@ server.get('/get/:id', (req,res,next) => {
         res.status(404).json("ID Not found")
     }
     db('notes')
-        .select('id', 'title', 'textBody')
+        .select('*')
         .where({id})
         .first()
         .then(note => {
             if(!note){
                 res.status(404).json({error: true, message: "No note with that ID"})
             }else{
+                if(note.tags) note.tags = note.tags.split(',').map(tag => tag.trim())
                 res.status(200).json(note)
             }
             
@@ -41,6 +43,9 @@ server.post('/post', (req,res,next) => {
 
     if(!newNote.title || !newNote.textBody){
         res.status(418).json({error: true, message: "Missing title or textBody"})
+    }
+    if(newNote.tags){
+        newNote.tags = newNote.tags.join(',')
     }
 
     db('notes')
@@ -67,6 +72,8 @@ server.delete('/delete/:id', (req,res,next) => {
 server.put('/edit/:id', (req,res,next) => {
     const { id } = req.params
     const updated = req.body
+
+    updated.tags = updated.tags.join(',')
 
     db('notes')
         .where({id})
