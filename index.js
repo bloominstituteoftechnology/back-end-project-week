9dc609f1a989af14f36547e21339abe9d27975a4
Daniel Chamorro <dan@dcitllc.com>
2018-08-21 13:10:12 -0400

    Installed cors, express, sqlite3, db.js, knex, up and running

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const db = require("./data/db");
+const cors = require("cors");
+
+const server = express();
+
+server.use(express.json());
+server.use(cors());
+
+server.get("/", (req, res) => {
+  res.send("up and running...");
+});
+
+const port = 3300;
+server.listen(port, function() {
+  console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
+});
