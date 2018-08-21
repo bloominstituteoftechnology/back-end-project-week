27ab3fca5327fbbd3baddd3304debfdc36d55cb1
chainchompa <chainchompa@gmail.com>
2018-08-21 14:38:18 -0400

    added endpoint to create a new note

--- index.js
+++ index.js
@@ -18,6 +18,23 @@ server.get('/api/notes', (req, res) =>  {
   })
 })
 
+server.post('/api/notes', (req, res) => {
+  const { title, content } = req.body;
+  if (!title || !content) {
+    res.status(400).json({ error: 'Please provide a title and content for the note'})
+  }
+  db.insert({ title, content })
+  .into('notes')
+  .then(response => {
+    res.status(201).json(response)
+  })
+  .catch(err => {
+    res.status(500).json({ error: 'Could not create new note' })
+  })
+})
+
+
+
 const port = 8000;
 server.listen(port, function() {
     console.log(`\n===Web API listening on http://localhost:${port} ===\n`);
--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,41 @@
 // Update with your config settings.
 
 module.exports = {
-  development: {
+   development: {
     client: 'sqlite3',
     connection: {
-      filename: './data/data.sqlite3',
+      filename: './data/data.sqlite3'
     },
-    useNullAsDefault: true,
+    useNullAsDefault: true
   },
-};
+   staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+   production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+ };
