e303b0723c09a5d883e12968f95182b30a673a11
sean chavez <seanchavez@seanchavezsMBP.home>
2018-08-21 10:55:56 -0700

    Set up server; add nodemon; test server

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const server = express();
+server.use(express.json());
+const PORT = 8000;
+
+server.get('/', (req, res) => {
+  res.send('Sanity Check');
+});
+
+server.listen(PORT, () => {
+  console.log(`UP and RUNNING on ${PORT}`)
+});
