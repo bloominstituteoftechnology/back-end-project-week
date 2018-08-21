c4640a43d0acf012ba15fcf3f230b4013c02c5a4
Rachelle De Jesus Pestanas <rachelle.pestanas@gmail.com>
2018-08-21 17:25:56 -0500

    created & ran migrations

--- knexfile.js
+++ knexfile.js
@@ -9,6 +9,10 @@ module.exports = {
     },
     useNullAsDefault: true,
   },
+  migrations: {
+    directory: './data/migrations',
+    tableName: 'dbmigrations',
+  },
 
   staging: {
     client: 'postgresql',
