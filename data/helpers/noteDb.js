a6770626e11e90b3f42bceba653db4e593a4f976
Vu Cao <cdhvu@yahoo.com>
2018-08-21 13:50:26 -0700

    MVP Completed

--- data/helpers/noteDb.js
+++ data/helpers/noteDb.js
@@ -20,16 +20,19 @@ function get(id) {
         return notes
     });
 }
+
 function add(note) {
     return db('Notes')
     .insert(note)
     .then(ids => ids[0])
 }
+
 function update(id, note) {
     return db('Notes')
     .where('id', id)
     .update(note)
 }
+
 function remove(id) {
     return db('Notes')
     .where('id', id)
