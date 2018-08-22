039ae12220828752378995a56a2d5e6b71ac7b39
Daniel Chamorro <dan@dcitllc.com>
2018-08-22 00:29:14 -0400

    Still....

--- data/db.js
+++ data/db.js
@@ -1,3 +1,4 @@
+const environment = process.env.NODE_ENV || "development";
 const config = require("../knexfile");
 const knex = require("knex");
 
--- knexfile.js
+++ knexfile.js
@@ -12,10 +12,9 @@ module.exports = {
   staging: {
     client: "postgresql",
     connection: {
-      database: "dcm0572f53nf1t",
-      user: "zpuhklwvlraxkl",
-      password:
-        "62ecc2d3debd4c768f754dae97bdff080a5e2f922d692456f73054a135b2026a"
+      database: "my_db",
+      user: "username",
+      password: "password"
     },
     pool: {
       min: 2,
@@ -27,22 +26,27 @@ module.exports = {
   },
 
   production: {
-    client: "postgresql",
-    connection: {
-      database: "dcm0572f53nf1t",
-      user: "zpuhklwvlraxkl",
-      password:
-        "62ecc2d3debd4c768f754dae97bdff080a5e2f922d692456f73054a135b2026a",
-      DATABASE_URL:
-        "postgres://zpuhklwvlraxkl:62ecc2d3debd4c768f754dae97bdff080a5e2f922d692456f73054a135b2026a@ec2-50-16-196-138.compute-1.amazonaws.com:5432/dcm0572f53nf1t"
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: "knex_migrations"
-    }
-  },
-  useNullAsDefault: true
+    client: "pg",
+    connection: process.env.DATABASE_URL
+  }
+
+  // production: {
+  //   client: "postgresql",
+  //   connection: {
+  //     database: "dcm0572f53nf1t",
+  //     user: "zpuhklwvlraxkl",
+  //     password:
+  //       "62ecc2d3debd4c768f754dae97bdff080a5e2f922d692456f73054a135b2026a",
+  //     DATABASE_URL:
+  //       "postgres://zpuhklwvlraxkl:62ecc2d3debd4c768f754dae97bdff080a5e2f922d692456f73054a135b2026a@ec2-50-16-196-138.compute-1.amazonaws.com:5432/dcm0572f53nf1t"
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: "knex_migrations"
+  //   }
+  // },
+  // useNullAsDefault: true
 };
