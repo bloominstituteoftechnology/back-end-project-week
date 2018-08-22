4c4a23c6b2172594e390777733a590b1e695a87c
Luis <luis@Luiss-MacBook-Air.local>
2018-08-22 13:07:57 -0700

    update

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -155,21 +155,17 @@ const restricted = (req, res, next) => {
   let token = req.headers.authorization
   console.log('token innn Restricted ,', token)
   if (token) {
-    jtw.verify(
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTUzNDk2Nzc3MCwiZXhwIjoxNTM1MDU0MTcwfQ.kcmGddYpOQlvXEWWcS-Sm8L9XzxgZerh3bGmXpKowMk',
-      process.env.SECRET,
-      (err, decodedToken) => {
-        if (err) {
-          console.log('THERE WAS AN ERROR')
-          return res
-            .status(401)
-            .json({ error: 'you shall not pass!! - token invalid' })
-        }
-        console.log('decoded', decodedToken)
-        req.token = decodedToken
-        next()
+    jtw.verify(token, process.env.SECRET, (err, decodedToken) => {
+      if (err) {
+        console.log('THERE WAS AN ERROR')
+        return res
+          .status(401)
+          .json({ error: 'you shall not pass!! - token invalid' })
       }
-    )
+      console.log('decoded', decodedToken)
+      req.token = decodedToken
+      next()
+    })
   } else {
     console.log('NO HEREEEE')
     return res.status(401).json({ error: 'you shall not pass!! - no token' })
--- index.js
+++ index.js
@@ -5,5 +5,5 @@ require('./api/routes')(server)
 const port = process.env.PORT || 8000
 
 server.listen(port, () => {
-  console.log('\n=== API RUNNING... ===\n')
+  console.log(`\n=== API RUNNING...${port} ===\n`)
 })
