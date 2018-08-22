e9b2049dd2511a19a48f84aba001b46751dd34e7
Daniel Chamorro <dan@dcitllc.com>
2018-08-21 23:06:57 -0400

    API call returns encrypted, need to figure why

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ server.get("/", (req, res) => {
 
 //* Display List of notes
 server.get(
-  "https://back-end-project-week-lambda.herokuapp.com/all",
+  "postgres://zpuhklwvlraxkl:62ecc2d3debd4c768f754dae97bdff080a5e2f922d692456f73054a135b2026a@ec2-50-16-196-138.compute-1.amazonaws.com:5432/dcm0572f53nf1t/all",
   (req, res) => {
     db("notes")
       .then(notes => {
