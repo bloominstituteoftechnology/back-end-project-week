2bc688a7e96b81d460c6d955569e04896d5aa887
Terrance <tbarnett2020@gmail.com>
2018-08-22 13:11:57 -0400

    Added simple input functionality for removing/adding/etc notes

--- app.js
+++ app.js
@@ -2,21 +2,23 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const _ = require('lodash')
+const yargs = require('yargs')
 
 const notes = require('./notes.js')
 
-let command = process.argv[2]
+const argv = yargs.argv
+let command = argv._[0];
 console.log('Command:', command)
-console.log(process.argv)
+console.log('Yargs', argv);
 
 if (command === 'add') {
-  console.log('Adding new note')
+  notes.addNote(argv.title, argv.body)
 } else if (command === 'list') {
-  console.log('Listing all notes')
+  notes.getAll();
 } else if (command === 'read') {
-  console.log('Reading note')
+  notes.getNote(argv.title)
 } else if (command === 'remove') {
-  console.log('Removing note')
+  notes.removeNote(argv.title)
 } else {
   console.log('Command not recognized')
 }
--- notes.js
+++ notes.js
@@ -1,6 +1,26 @@
 console.log('Starting notes.js');
 
-module.exports.addNote = () => {
-  console.log('addNote');
-  return 'New note';
+let addNote = (title, body) => {
+  console.log('Adding note', title, body)
 };
+
+let getAll = () => {
+  console.log('Getting all notes',)
+};
+
+let getNote = (title) => {
+  console.log('Reading note',)
+};
+
+let removeNote = (title) => {
+  console.log('Removing note',)
+};
+
+
+
+module.exports = {
+  addNote,
+  getAll,
+  getNote,
+  removeNote
+}
