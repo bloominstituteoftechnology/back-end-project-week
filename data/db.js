b8ed07f194e84a0397f079adac515948ac1d9d8e
Daniel Chamorro <dan@dcitllc.com>
2018-08-22 00:10:36 -0400

    Still....

--- data/db.js
+++ data/db.js
@@ -1,15 +1,4 @@
 const config = require("../knexfile");
-// const knex = require("knex");
+const knex = require("knex");
 
-const options =
-  process.env.NODE_ENV === "production"
-    ? {
-        client: "pg",
-        connection: process.env.DATABASE_URL,
-        searchPath: ["public"]
-      }
-    : {};
-
-const knex = require("knex")(options);
-
-// module.exports = knex(config.production);
+module.exports = knex(config.production);
