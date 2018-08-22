17bdfd7ba862e7dc864a0e86bc0a4b492e6174e3
Rachelle De Jesus Pestanas <rachelle.pestanas@gmail.com>
2018-08-22 12:41:46 -0500

    implemented PUT

--- api/notesRoutes.js
+++ api/notesRoutes.js
@@ -41,4 +41,18 @@ router.post('/', (req, res, next) => {
         .catch(err => console.log(err))
 })
 
+// PUT (POSTMAN: OK)
+router.put('/:id', (req, res, next) => {
+    const id = req.params.id;
+    const note = req.body;
+    db
+      .editNote(id, note)
+      .then(response => {
+        res
+          .status(200)
+          .json(response)
+      })
+      .catch(err => console.log(err))
+  })
+
 module.exports = router;
