47cc8208c4342b6af6d738cc0cee576619341802
Luis <luis@Luiss-MacBook-Air.local>
2018-08-22 11:14:13 -0700

    deploy

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const server = require('express')()
 require('./api/middleware/')(server)
 require('./api/routes')(server)
 
-const port = 8000 || process.env.PORT
+const port = process.env.PORT || 8000
 
 server.listen(port, () => {
   console.log('\n=== API RUNNING... ===\n')
