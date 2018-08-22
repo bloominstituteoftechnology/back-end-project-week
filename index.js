e8da86b46da664b1abe94823fd036199413d8b7f
Rachelle De Jesus Pestanas <rachelle.pestanas@gmail.com>
2018-08-22 12:30:16 -0500

    implemented GET by ID

--- index.js
+++ index.js
@@ -6,12 +6,7 @@ const server = express();
 
 server.use(express.json());
 
-// ENDPOINTS
 server.get('/', (req, res) => {
     res.send('up and running...');
 });
 
-const port = 3300;
-server.listen(port, function() {
-    console.log('\n=== Web API Listening on http://localhost:${port} ===\n');
-});
