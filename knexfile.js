cc5d0c6cf2cfc61a774cc62d17306938441a315e
Daniel Chamorro <dan@dcitllc.com>
2018-08-21 22:36:34 -0400

    Migrating to Postgres for Heroku

--- knexfile.js
+++ knexfile.js
@@ -28,9 +28,10 @@ module.exports = {
   production: {
     client: "postgresql",
     connection: {
-      database: "my_db",
-      user: "username",
-      password: "password"
+      database: "dcm0572f53nf1t",
+      user: "zpuhklwvlraxkl",
+      password:
+        "62ecc2d3debd4c768f754dae97bdff080a5e2f922d692456f73054a135b2026a"
     },
     pool: {
       min: 2,
