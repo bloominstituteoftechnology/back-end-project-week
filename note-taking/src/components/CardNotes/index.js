8b7f8c9644af01acff3fe8ad4e6e4819a8f26fb6
Tai <bowlofryce@gmail.com>
2018-08-22 17:52:19 -0500

    I can get the data from the backend, still working on create, delete, edit

---
+++ note-taking/src/components/CardNotes/index.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import Axios from 'axios';
+import { Link } from 'react-router-dom';
+
+class CardNotes extends Component {
+  state = {
+    notes: [],
+    note: ''
+  }
+
+  handleSetData = data => this.setState({ notes: data });
+
+  componentDidMount() {
+    Axios
+     .get("http://localhost:8000/api/notes")
+     .then(response => {
+       console.log("GET RESPONSE: ", response);
+       this.setState({ notes: response.data });
+     })
+     .catch(err => {
+       console.log(err);
+     });
+  };
+
+  render() {
+    return(
+    <div>
+      <div>
+        {this.state.notes.title}
+        {this.state.notes.noteBody}
+      </div>
+      <Link to={`/${this.state.notes.id}/editnotes`}>
+      Edit Notes
+      </Link>
+    </div>
+    )
+  }
+}
+
+export default CardNotes;
