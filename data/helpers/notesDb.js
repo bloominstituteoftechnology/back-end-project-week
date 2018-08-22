fecb619e4bb048b99876c18f63db2898260a00bf
Rachelle De Jesus Pestanas <rachelle.pestanas@gmail.com>
2018-08-22 12:26:01 -0500

    established data access, implemented GET

---
+++ data/helpers/notesDb.js
@@ -0,0 +1,26 @@
+const db = require('../db');
+
+module.exports = {
+  getNotes: () => {
+    return db('notes');
+  },
+
+  getNote: id => {
+    id = Number(id);
+    return db('notes').where({ id }).first();
+  },
+
+   addNote: note => {
+    return db('notes').insert(note).then(ids => ({id: ids[0]}));
+  },
+
+  editNote: (id, note) => {
+    id = Number(id)
+    return db('notes').where({ id }).update(note);
+  },
+
+  deleteNote: id => {
+    id = Number(id);
+    return db('note').where({ id }).del();
+  }
+} 
