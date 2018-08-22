02ffcce70ff71fdea17bd1d60f6a578288b159c8
Tmbrwolf <timberwolf.eric@gmail.com>
2018-08-21 19:48:37 -0500

    Added some initial routes & starting to test

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
-const db = require('./db/dbConfig.js');
+const db = require('./db/dbConfig');
 
 const express = require('express');
 const server = express();
 
 server.use(express.json());
+
+server.get('/api/lnotes', (req, res) => {
+  db('lnotes')
+  .then(lnotes => {
+    res.status(200).json({lnotes});
+  })
+  .catch(error => {
+    res.status(500).json({error});
+  });
+})
+
+server.post('/api/lnotes', (req, res) => {
+  const newnote = req.body;
+  // if (!newnote.title || !newnote.content) {
+  //   res.status(400).json(`There was an error with unfilled blank or field`);
+  // }
+  db('lnotes')
+  .insert(newnote)
+  .then(response => {
+    res.status(201).json(`A new note has been successfully added!`);
+  })
+  .catch(error => {
+    res.status(500).json(`The notes API had an error while adding a note.`);
+  });
+});
+
+
 
 const port = 8000;
 
