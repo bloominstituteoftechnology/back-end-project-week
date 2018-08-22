cff8ebd2800689e2739e92cd4f3d640926357f13
Blake Fletcher <blakefletcher@Blakes-MacBook-Air.local>
2018-08-22 10:02:11 -0400

    Deployed to heroku

--- routes/apiRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const server = express.Router();
-
-const notesRoutes = require('./notesRoutes');
-
-
-server.use('/notes', notesRoutes);
-
-module.exports = server;
