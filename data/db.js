bac015664afeefae443f40a28e09076479f46e61
Daniel Chamorro <dan@dcitllc.com>
2018-08-22 00:03:43 -0400

    Still testing

--- data/db.js
+++ data/db.js
@@ -1,4 +1,15 @@
 const config = require("../knexfile");
-const knex = require("knex");
+// const knex = require("knex");
+
+const options =
+  process.env.NODE_ENV === "production"
+    ? {
+        client: "pg",
+        connection: process.env.DATABASE_URL,
+        searchPath: ["public"]
+      }
+    : {};
+
+const knex = require("knex")(options);
 
 module.exports = knex(config.production);
