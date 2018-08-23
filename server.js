3adb12c31a3035085aecfb8c40c8331343981495
DirupT <iamcooled@gmail.com>
2018-08-23 12:28:44 -0500

    small changes for heroku

--- server.js
+++ server.js
@@ -7,11 +7,15 @@ const cors = require('cors');
 
 const server = express();
 
+server.get('/', (req, res) => {
+    res.send('API is running!');
+});
+
 server.use(express.json());
 server.use(helmet());
 server.use(cors());
 server.use('/api', apiRoutes);
 server.use(errorHandler);
 
-const port = process.env.PORT || 8000; 
+const port = process.env.PORT || 8000;
 server.listen(port, () => console.log(`\n== API up on port ${port} ==\n`));
