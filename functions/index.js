d413f93dfb2dfd51a744e052161254a6b613d211
Tyson Gushiken <tysonism@gmail.com>
2018-08-23 05:01:45 -0700

    Refactor code into a Google Firebase project

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,17 @@
 const functions = require('firebase-functions');
+const cors = require('cors');
+const express = require('express');
+const router = require('./router');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const corsOptions = {
+  origin: true,
+  allowedHeaders: ['Content-Type', 'Authorization', 'Content-Length', 'X-Requested-With', 'Accept'],
+  methods: ['GET','POST','PUT','DELETE'],
+  optionsSuccessStatus: 200
+};
+
+const server = express();
+server.use(cors(corsOptions));
+server.use('/notes', router);
+
+exports.app = functions.https.onRequest(server);
