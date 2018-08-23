1da29e361efc773309f18fc7693e061d2812b100
Gabriel Duquette <gabrielduquette@gmail.com>
2018-08-23 13:25:56 -0400

    add comments

--- api/apiRoutes.js
+++ api/apiRoutes.js
@@ -5,13 +5,6 @@ const session = require('express-session');
 const notes = require('../data/helpers/noteHelpers');
 const users = require('../data/helpers/userHelpers');
 
-const userCheck = (req, res, next) => {
-  if (!req.body.username || !req.body.password || !req.body.noteOrdering) {
-    return res.status(400).json({ message: "All fields must be completed." });
-  }
-  next();
-};
-
 const noteCheck = (req, res, next) => {
   if (!req.body.title || !req.body.content) {
     return res.status(400).json({ message: "All fields must be completed." });
@@ -19,40 +12,7 @@ const noteCheck = (req, res, next) => {
   next();
 };
 
-router.post('/register', async (req, res) => {
-  try {
-    const credentials = req.body;
-    const hash = bcrypt.hashSync(credentials.password, 14);
-    credentials.password = hash;
-    await users.insert(credentials);
-    try {
-      const user = await users.get(credentials);
-      req.session.userId = user.id;
-      return res.status(201).json(user);
-    } catch (error) {
-      return res.status(404).json({ message: "User does not exist.", error: error.message });
-    }
-  } catch (error) {
-    return res.status(500).json({ message: "User could not be registered." });
-  }
-});
-
-router.post('/login', async (req, res) => {
-  try {
-    const credentials = req.body;
-    const user = await users.get(credentials);
-    if (user && bcrypt.compareSync(credentials.password, user.password)) {
-      req.session.userId = user.id;
-      return res.status(200).json({ message: `User ${user.id} is logged in.` });
-    } else {
-      return res.status(404).json({ message: "User does not exist." });
-    }
-  } catch (error) {
-    return res.status(500).json({ message: "An error occurred during login." });
-  }
-});
-
-router.put('/ordering', async (req, res) => {
+router.put('/ordering', async (req, res) => { // updates a user's noteOrdering
   try {
     const updatedNoteOrdering = await users.updateNoteOrdering(1, req.body);
     if (updatedNoteOrdering === 0) {
