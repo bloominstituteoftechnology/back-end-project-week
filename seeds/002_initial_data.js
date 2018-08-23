4265136471c61b5d7d1ad2bd3f393a4f7c6291f8
TangledTessellations <mcevoya@oregonstate.edu>
2018-08-23 10:36:13 -0700

    re-seed

--- seeds/002_initial_data.js
+++ seeds/002_initial_data.js
@@ -6,7 +6,7 @@ exports.seed = function(knex, Promise) {
       // Inserts seed entries
       return knex('notes').insert([
         { title: 'Learn React', textBody: 'Learning React really well would be super neat'},
-        { title: 'Learn Some kind of photo Editing software', textBody: 'I just need to cut some faces out of some images, it shouldnt be this hard'},
+        { title: 'Learn Some kind of photo Editing software', textBody: 'The world needs more emojis (maybe?)'},
         { title: 'Just, live your life, man', textBody: 'I guess this is going to happen no matter what you do'}
       ]);
     });
