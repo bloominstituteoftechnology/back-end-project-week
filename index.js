0c92c165889e11326e79363a73c8137bbc5d2d23
Tmbrwolf <timberwolf.eric@gmail.com>
2018-08-22 19:58:15 -0500

    Got GET & DELETE working - POST/PUT still not working :-/

--- index.js
+++ index.js
@@ -16,21 +16,40 @@ server.get('/api/lnotes', (req, res) => {
 })
 
 server.post('/api/lnotes', (req, res) => {
-  const newnote = req.body;
+  const { title, content } = req.body;
+  // const newnote = req.body;
   // if (!newnote.title || !newnote.content) {
   //   res.status(400).json(`There was an error with unfilled blank or field`);
   // }
-  db('lnotes')
-  .insert(newnote)
+  db
+  // .insert(newnote)
+  .insert({ title, content })
+  .into('lnotes')
+  // .then(noteid => {
+  //   const newid = noteid[0];
+  //   res.status(201).json({newid, ...newnote});
+  // })
   .then(response => {
-    res.status(201).json(`A new note has been successfully added!`);
+    //res.status(201).json(`A new note has been successfully added!`)
+    res.status(201).json(response);
   })
   .catch(error => {
     res.status(500).json(`The notes API had an error while adding a note.`);
   });
 });
 
-
+server.delete('/api/lnotes/:id', (req, res) => {
+  const { id } = req.params;
+  db('lnotes')
+  .where({id})
+  .del()
+  .then(potatosalad => {
+    res.status(201).json({potatosalad});
+  })
+  .catch(potatosalad => {
+    res.status(500).json({potatosalad});
+  });
+})
 
 const port = 8000;
 
