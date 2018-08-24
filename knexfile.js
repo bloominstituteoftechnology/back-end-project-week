1f57efa024793a257fad607a32b8ba75ff507550
Kamry Bowman <kamry.bowman@gmail.com>
2018-08-24 09:11:21 -0600

    add ssl=true query

--- knexfile.js
+++ knexfile.js
@@ -37,7 +37,7 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL,
+    connection: `${process.env.DATABASE_URL}?ssl=true`,
     pool: {
       min: 2,
       max: 10
