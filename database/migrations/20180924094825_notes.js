382ee9d601453eceda44fedd39670e6dab27742b
adamcglee <adamcglee@gmail.com>
2018-09-24 10:01:53 -0700

    trying to set up knex migrations, but it won't create the db file

---
+++ database/migrations/20180924094825_notes.js
@@ -0,0 +1,18 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('notes', notes => {
+      notes.increments();
+
+      notes.string('title', 70).notNullable().unique();
+
+      notes.string('note');
+
+      notes.boolean('editToggle').defaultTo(false);
+  });
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.table("notes", function(tbl) {
+        tbl.dropColumn("notes");
+      });
+};
