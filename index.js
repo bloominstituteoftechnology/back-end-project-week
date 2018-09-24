46d300ec3d019b9b8d23a50b5ac1a7167df5dbd9
francistse23 <wtse.ucsd@gmail.com>
2018-09-24 09:36:35 -0700

    Front end notes page showing 3 notes, but title and content are not displaying at the moment.

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const server = express();
+const knex = require('knex');
+const cors = require('cors');
+//db
+const dbConfig = require('./knexfile');
+const db = knex(dbConfig.development);
 
 server.use(express.json());
+server.use(cors());
 
+server.get('/get/all', async (req, res) => {
+    try {
+        const notes = await db('notes')
+        res.status(200).json( notes );
+    }
+    catch ( err ) {
+        res.status(500).json( err.message );
+    }
+})
 
 
 const port = 8000;
