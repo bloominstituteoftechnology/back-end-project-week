a660f192f72224d4eca7e5df7f008b9aa6f197fc
skennedy93 <sean47900@gmail.com>
2018-09-24 17:34:28 -0500

    issues resolved made mistake on postman not code, all endpoints done

--- index.js
+++ index.js
@@ -9,10 +9,13 @@ server.use(helmet());
 const dbConfig = require('./knexfile'); 
 const db = knex(dbConfig.development); 
 
+
+//------------------------------TEST------------------------------------
 server.get("/", (req, res) => {
     res.status(200).json({message: "Test"})
 })
 
+//------------------------------------GET LIST---------------------------
 server.get("/api/notes", (req, res) => {
     db('notes').then(notes => {
         res.status(200).json(notes); 
@@ -20,7 +23,7 @@ server.get("/api/notes", (req, res) => {
         res.status(500).json({error: "Cannot retrieve notes"})
     })
 });
-
+//----------------------------------GET INDIVIDUAL--------------------------------------
 server.get("/api/notes/:id", (req, res) => {
     const { id } = req.params; 
     db("notes")
@@ -30,7 +33,7 @@ server.get("/api/notes/:id", (req, res) => {
         res.status(500).json({error: "Cannot retrieve notes"})
     });
 })
-
+//-------------------------------------POST-------------------------------------------
 server.post('/api/notes', (req, res) => {
     const newNote = req.body;
     const { title, content } = req.body;
@@ -47,7 +50,7 @@ server.post('/api/notes', (req, res) => {
             res.status(500).json({error: "Cannot retrieve notes"})
    });
 })
-
+//-------------------------------------------DELETE-------------------------------------------
 server.delete("/api/notes/:id", (req, res) => {
     const { id } = req.params;
     db("notes")
@@ -60,5 +63,15 @@ server.delete("/api/notes/:id", (req, res) => {
         res.status(500).json(err);
       });
   });
+//----------------------------------------EDIT--------------------------------------------------
+  server.put('/api/notes/:id', (req, res) => {
+    const edit = req.body;
+    const {id} = req.params;
+     db('notes')
+        .where({id: id})
+        .update(edit)
+        .then(count => {res.status(200).json(count)})
+        .catch(err => {res.status(500).json(err)});
+});
 
 server.listen(3000, () => console.log("server listening at port 3000"));
