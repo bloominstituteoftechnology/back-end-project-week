05af043852140badf2f4dea2f50c74ccf57c8bb1
David Pok <david.pok@gmail.com>
2018-09-24 16:56:46 -0700

    more setup

--- knexfile.js
+++ knexfile.js
@@ -5,8 +5,15 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
+      filename: './db/lambda.sqlite3'
     }
+  },
+  useNullAsDefault: true,
+  migrations: {
+    directory: './db/migrations',
+  },
+  seeds: {
+    directory: './db/seeds',
   },
 
   staging: {
