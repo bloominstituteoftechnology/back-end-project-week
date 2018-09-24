fd1487e00bc3eb0725e4e3d74e87f877b4c5bb77
Katherine Gilligan <katia.gilligan88@gmail.com>
2018-09-24 09:45:10 -0600

    Added POST endpoint that creates new note with title and content requirements

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const express = require('express'); 
 const knex = require('knex'); 
+const Joi = require('joi'); 
 
+// Server setup using express
 const server = express(); 
 server.use(express.json()); 
 
+// Database setup using knex
 const dbConfig = require('./knexfile'); 
-const db = knex(dbConfig.development); 
+const db = knex(dbConfig.development);
 
-server.get("/", (req, res) => {
-    res.status(200).json({message: "This worked!"})
-}); 
+// Validation of request body using Joi
+const validateBody = body => {
+    const schema = {
+        title: Joi.string().min(3).max(35).required(), 
+        content: Joi.string().min(3).required()
+    }
+    return Joi.validate(body, schema); 
+}
 
 server.get("/api/notes", (req, res) => {
     db('notes').then(notes => {
@@ -31,6 +39,22 @@ server.get("/api/notes/:id", (req, res) => {
         res.status(500).json({Error: "Error accessing the data from the database"}); 
     }); 
 }); 
+
+server.post("/api/notes", (req, res) => {
+    const newNote = req.body;
+
+    // Calling Joi validation, if error return which missing requirements
+    const validationResult = validateBody(newNote);
+    if(validationResult.error){
+        return res.status(400).json(validationResult.error.details[0].message)
+    }
+
+    db('notes').insert(newNote).into('notes').then(id => {
+        res.status(201).json(id); 
+    }).catch(err => {
+        res.status(500).json({Error: "Error posting the data to the database", err})
+    })
+})
 
 
 server.listen(9000, () => console.log("Server listening at Port 9000")); 
