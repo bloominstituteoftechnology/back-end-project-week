196d430344bf3493a6cdb4b2ae467beac8b05773
“Kennedykid1995” <34872885+Kennedykid1995@users.noreply.github.com>
2018-09-24 12:10:00 -0500

    GET
    I can view all the notes and I can see and individual note by the id.

--- index.js
+++ index.js
@@ -15,6 +15,20 @@ server.get("/", (req, res)=> {
     res.send("API Running..."); 
 })
 
+server.get("/notes", (req, res)=>{
+    db('notes')
+    .then(notes => res.status(200).json(notes))
+    .catch(err => res.status(500).json(err)); 
+})
+
+server.get("/notes/:id", (req, res) => {
+    const { id } = req.params; 
+    db("notes")
+    .where({id})
+    .then(note => res.status(200).json(note))
+    .catch(err => res.status(500).json(err)); 
+})
+
 const port = 3001; 
 server.listen(port, function(){
     console.log("\n=== WEB API LISTENING 3001 ===\n")
