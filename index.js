3e7af959e659084e5e9279f5a71caf65c5732d22
vanceleon <vanceleon44@gmail.com>
2018-09-24 12:29:19 -0400

    latest project setup

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require('express');
-
+const knex = require("knex");
 const server = express();
+const dbConfig = require("./knexfile");
 
+const db = knex(dbConfig.development);
 
+server.use(express.json());
+
+//testing the server
 server.get('/', (req, res) => {
     res.send("Hello WOrld");
 });
+
+
 
 
 server.listen(8000, () => console.log("======API Running on Port 8000======="))
