9e5580036e3738645d76c0932869bda90e01c8b0
Erica Thompson <ericathompson.nc@gmail.com>
2018-09-24 11:15:41 -0700

    Seeds Added

---
+++ seeds/01-notes.js
@@ -0,0 +1,14 @@
+
+exports.seed = function(knex, Promise) {
+    // Deletes ALL existing entries
+    return knex('notes').del()
+        .then(function () {
+        // Inserts seed entries
+            return knex('notes').insert([
+                {id: 1, title: 'ToDo', content: 'work, work, work'},
+                {id: 2, title: 'ToEat', content: 'all the foods'},
+                {id: 3, title: 'Goals', content: 'work smart'},
+                {id: 4, title: 'Events', content: 'network'}
+        ]);
+    });
+};
