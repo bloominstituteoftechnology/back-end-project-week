144bde9dc636b80d3573cba290444cb80cb19c18
Katherine Gilligan <katia.gilligan88@gmail.com>
2018-09-24 09:24:27 -0600

    Added the GET endpoint for individual note access

--- index.js
+++ index.js
@@ -15,8 +15,21 @@ server.get("/api/notes", (req, res) => {
     db('notes').then(notes => {
         res.status(200).json(notes); 
     }).catch(err => {
-        res.status(500).json({error: "Error accessing the data from the database"})
+        res.status(500).json({error: "Error accessing the data from the database"}); 
     })
+}); 
+
+server.get("/api/notes/:id", (req, res) => {
+    const {id} = req.params; 
+    db('notes').where({id}).then(note => {
+        if(note.length > 0){
+            res.status(200).json(note); 
+        }else{
+            res.status(404).json({Error: "The ID used was not found within the database"}); 
+        }
+    }).catch(err => {
+        res.status(500).json({Error: "Error accessing the data from the database"}); 
+    }); 
 }); 
 
 
