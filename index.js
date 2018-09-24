e3fcf33fd91d80891b0483960532ddb14927d920
farste <sfargali@gmail.com>
2018-09-24 16:56:58 -0400

    Stephen Fargali View an existing note implemented

--- index.js
+++ index.js
@@ -2,27 +2,39 @@ const express = require("express");
 const knex = require("knex");
 const server = express();
 const dbConfig = require("./knexfile.js");
-const db = knex(dbConfig.development)
+const db = knex(dbConfig.development);
 server.use(express.json());
 
 server.get("/api/notes", (req, res) => {
-    db("notes")
+  db("notes")
     .select()
     .then(notes => {
-        res.status(200).json(notes);
+      res.status(200).json(notes);
     })
-    .catch(err => res.status(404).json({error: "no notes found"}))
-})
+    .catch(err => res.status(404).json({ error: "no notes found" }));
+});
+
+server.get("/api/notes/:id", (req, res) => {
+  const { id } = req.params;
+
+  db("notes")
+    .where("id", "=", id)
+    .select()
+    .then(notes => {
+      res.status(200).json(notes);
+    })
+    .catch(err => res.status(404).json({ error: "no notes found" }));
+});
 
 server.post("/api/notes", (req, res) => {
-    const {title, content} = req.body;
-    db.insert({title, content})
+  const { title, content } = req.body;
+  db.insert({ title, content })
     .into("notes")
     .then(notes => {
-        res.status(201).json(notes);
+      res.status(201).json(notes);
     })
     .catch(err => res.status(500).json(err));
-})
+});
 
 const port = 3300;
 server.listen(port, function() {
