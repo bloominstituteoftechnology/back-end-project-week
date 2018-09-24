ff56452d5e6c1520b314a7143e19e0476187bf92
johnoro <officialjohnorourke@gmail.com>
2018-09-24 16:50:06 -0400

    Altered cors to allow all origins.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const port = process.env.PORT || 8000;
 
 express()
-  .use(cors({ origin: 'https://notes-johnoro.surge.sh/' }))
+  .use(cors())
   .use(express.json())
   .use('/api', require('./api'))
   .listen(port, () => console.log(`listening on port ${port}`));
