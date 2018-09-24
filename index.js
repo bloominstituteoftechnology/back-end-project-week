1618a3dec994732b744b947ec1e6aafb75ca7e56
johnoro <officialjohnorourke@gmail.com>
2018-09-24 15:31:04 -0400

    Yet another attempt at making heroku work.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const port = process.env.port || 8000;
+const port = process.env.PORT || 8000;
 
 express()
   .use(cors())
