3479715f49157fec2901103bccaf2e324ed677f0
LaikaFusion <newrad4@gmail.com>
2018-09-24 12:09:00 -0400

    db added

---
+++ migrations/20180924120243_notes.js
@@ -0,0 +1,15 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable("notes", function(tbl) {
+    tbl.increments().primary();
+    tbl.string("title").notNullable();
+    tbl.string("body").notNullable();
+    tbl.string("checklist");
+    tbl.string("tags");
+    tbl.string("userID").notNullable();
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists("notes");
+};
