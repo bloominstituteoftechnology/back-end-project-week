9f0a7a9c34dd3728863440daa5436bc187fa25e6
Charlie Winslow <cwinslow22@yahoo.com>
2018-09-24 14:46:33 -0500

    completed the add note endpoint

--- index.js
+++ index.js
@@ -15,6 +15,23 @@ server.get('/', (req, res) => {
     res.send('API needs a break');
 });
 
+
+// add a note
+server.post('/api/notes', (req, res) => {
+    const newNote = req.body;
+    const { title, content } = req.body;
+    if (!title || !content) {
+        res.status(400).json({ errorMessage: 'A note title and content are required' });
+        return;
+    }
+    db.insert(newNote)
+        .into('notes')
+        .then(ids => {
+            res.status(201).json(ids);
+        })
+        .catch(err => res.status(500).json(err));
+});
+
 // get the list of notes
 server.get('/api/notes', (req, res) => {
     db('notes')
