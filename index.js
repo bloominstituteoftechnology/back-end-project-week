b13b1514a5de24e884679e00886f713a633cf92e
Erica Thompson <ericathompson.nc@gmail.com>
2018-09-24 11:43:28 -0700

    Note Post and Get Endpoints working

--- index.js
+++ index.js
@@ -12,6 +12,35 @@ res.send('API Running...');
 });
 
 
+server.post('/notes', (req, res) => {
+    const item = req.body;
+
+    db('notes').insert(item)
+        .then((ids)=> { 
+            res.status(201).json(ids);
+        })
+                .catch((fail) => {
+                    console.log(fail);
+                    res.status(500).json({ error: "There was an error while saving the note to the database." });
+                });
+});
+
+server.get('/notes', (req, res) => {
+    db('notes').then(item => {
+        res.status(200).json(item)
+    }).catch((fail) => {
+        console.log(fail);
+        res.status(500).json({ error: "There was an error while receiving the notes" });
+    })
+})
+
+
+
+
+
+
+
+
 
 
 const port = 5000;
