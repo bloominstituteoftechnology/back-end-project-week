ba3002f96c278a2e06e0fc9dfc331af8462ee2bd
Charles Cortinas <crcortinas@gmail.com>
2018-09-24 12:10:49 -0500

    GET and POST
    Implemented endpoints to get all notes, get notes by id, and insert new notes

--- index.js
+++ index.js
@@ -1,13 +1,58 @@
 const express = require("express");
 const helmet = require("helmet");
+const knex = require("knex");
+const dbConfig = require("./knexfile");
 
 const server = express();
+const db = knex(dbConfig.development);
 
 server.use(express.json());
 server.use(helmet());
 
 server.get("/", (req, res) => {
   res.status(200).json({ api: "running" });
+});
+
+server.get("/api/notes", (req, res) => {
+  db("notes")
+    .then(data => {
+      res.status(200).json(data);
+    })
+    .catch(err => {
+      res.status(404).json(err);
+    });
+});
+
+server.get("/api/notes/:id", (req, res) => {
+  db("notes")
+    .where({ id: req.params.id })
+    .then(data => {
+      if (data.length === 0) {
+        res.status(404).json({ error: "Note not found" });
+      } else {
+        res.status(200).json(data);
+      }
+    })
+    .catch(err => {
+      res.status(404).json(err);
+    });
+});
+
+server.post("/api/notes", (req, res) => {
+  const newNote = req.body;
+
+  if (newNote.title && newNote.contents) {
+    db("notes")
+      .insert(newNote)
+      .then(response => {
+        res.status(201).json(response);
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      });
+  } else {
+    res.status(400).json({ error: "Request missing one or more parameters" });
+  }
 });
 
 server.listen(8000, () => {
