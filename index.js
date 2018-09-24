c7a43e559ed8a16ca689115e783750e5d9e38f75
szincone <sawyerzincone@gmail.com>
2018-09-24 10:25:02 -0600

    added middleware, error handler, knexfile, and index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+"use strict";
+const express = require("express");
+const server = new express();
+
+// middleware
+const configureMiddleware = require("./middleware/middleware.js");
+configureMiddleware(server);
+
+const PORT = 7000;
+
+server.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
---
+++ middleware/middleware.js
@@ -0,0 +1,41 @@
+"use strict";
+// dependencies
+const express = require("express");
+// const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+// routes
+const noteRoutes = require("../routes/noteRoute.js");
+
+function errorHandler(err, req, res, next) {
+  console.log(err);
+  switch (err.code) {
+    case 404:
+      res.status(404).json({
+        message: "The requested note does not exist.",
+      });
+      break;
+    case 400:
+      res.status(400).json({
+        message: "There was an error regarding your input.",
+      });
+    case 406:
+      res.status(406).json({
+        message: "Missing title or content.",
+      });
+    default:
+      res.status(500).json({
+        message: "There was an error performing the required operation",
+      });
+      break;
+  }
+}
+
+module.exports = server => {
+  // server.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+  server.use(helmet());
+  server.use(express.json());
+  server.use(morgan("dev"));
+  server.use("/api/notes", noteRoutes);
+  server.use(errorHandler);
+};
