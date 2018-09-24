e7f6ac2c135a0069ce5a3138550e824c5be51815
Kairi <kairijb@gmail.com>
2018-09-24 11:19:05 -0500

    modified knex file

--- knexfile.js
+++ knexfile.js
@@ -1,44 +1,15 @@
-// Update with your config settings.
-
 module.exports = {
-
   development: {
-    client: 'sqlite3',
+    client: 'sqlite3', // tells knex that we're using the SQLite3 driver we installed via npm
     connection: {
-      filename: './dev.sqlite3'
+      filename: './lambda.sqlite3', // update this with the location of your database file
+    },
+    useNullAsDefault: true, // new configuration for SQLite
+    migrations: {
+      directory: './migrations',
+    },
+    seeds: {
+      directory: './seeds',
     }
   },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
 };
