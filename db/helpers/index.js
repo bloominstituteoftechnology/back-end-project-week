e2d7f9bd20e31011280da320e5ca7993e8d8dbf3
AI2EJAY <JHammett1314@gmail.com>
2018-09-24 11:59:26 -0500

    Update: -Seeds Created -Get & GetbyId endpoints completed with helper file

--- db/helpers/index.js
+++ db/helpers/index.js
@@ -1,8 +1,19 @@
-const knex = require('knex')
-const dbConfig = require('../../knexfile')
+const knex = require("knex");
+const dbConfig = require("../../knexfile");
 
 const db = knex(dbConfig.development);
 
 module.exports = {
-    
-}
+  getNotes: function(id) {
+    let query = db("notes");
+
+    if (id) {
+      return query
+        .where(id)
+        .first()
+        .then(note => note);
+    } else {
+      return query.then(notes => notes);
+    }
+  }
+};
--- index.js
+++ index.js
@@ -1,16 +1,37 @@
-const express = require('express')
-const db = require('./db/helpers')
-const server = express()
+const express = require("express");
+const db = require("./db/helpers");
+const server = express();
 
-server.use(express.json())
+server.use(express.json());
 
-server.get('/', (req, res) => {
-    res.send('Server Works')
-})
+server.get("/", (req, res) => {
+  res.send("Server Works");
+});
 
+server.get("/notes", (req, res) => {
+  db.getNotes()
+    .then(notes => {
+      res.status(200).json(notes);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+server.get("/notes/:id", (req, res) => {
+  const { id } = req.params;
+
+  db.getNotes({id})
+    .then(note => {
+      res.status(200).json(note);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
 
 port = 9000;
 server.listen(
   port,
-  console.log(`\n ===> Server is running on port:${port} <=== \n`)
+  console.log(`\n ===> Server is running on port: ${port} <=== \n`)
 );
