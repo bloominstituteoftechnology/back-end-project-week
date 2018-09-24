d8da544fd716c6f60d3b96664a9f759de9cfca69
Kairi <kairijb@gmail.com>
2018-09-24 11:22:51 -0500

    created index.js file with imports

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const helmet = require('helmet');
+const knex = require('knex')
+const cors = require('cors')
+const server = express();
+const bcrypt = require('bcryptjs');
+const dbConfig = require('./knexfile')
+const jwt = require('jsonwebtoken')
+const session = require('express-session');
+
+const db = knex(dbConfig.development);
+
+
+server.use(session(sessionConfig))
+server.use(express.json());
+server.use(helmet());
+server.use(cors())
+
+
+
+const port = 3500;
+server.listen(port, function () {
+    console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
+});
