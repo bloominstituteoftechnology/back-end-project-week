540ce80f25467a668f5a5a9b509fd058dd0bb05a
adamcglee <adamcglee@gmail.com>
2018-09-24 10:57:09 -0700

    added code for display, view existing, create, and edit endpoints

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const cors = require('cors');
-const knex = require('knex');
+const express = require("express");
+const cors = require("cors");
+const knex = require("knex");
 
-const dbConfig = require('./knexfile');
+const dbConfig = require("./knexfile");
 
 const db = knex(dbConfig.development);
 
@@ -11,13 +11,49 @@ const server = express();
 server.use(express.json());
 server.use(cors());
 
-server.get('/', (req, res) => {
-    db('notes').select('title', 'note', 'editToggle').then(notes => {
-        res.status(200).json(notes)
+server.get("/", (req, res) => {
+  db("notes")
+    .then(notes => {
+      res.status(200).json(notes);
     })
     .catch(err => res.status(500).json(err));
-})
+});
+
+server.post("/form", (req, res) => {
+  const note = req.body;
+
+  db.insert(note)
+    .into("notes")
+    .then(ids => {
+      res.status(201).json(ids);
+    })
+    .catch(err => res.status(500).json(err));
+});
+
+server.get("/note/:id", (req, res) => {
+  db("notes")
+    .where({ id: req.params.id })
+    .then(note => {
+      res.status(200).json(note);
+    })
+    .catch(err => res.status(500).json(err));
+});
+
+server.put("/note/:id", (req, res) => {
+  const note = req.body;
+  db("notes")
+    .where({ id: req.params.id })
+    .update(note)
+    .then(note => {
+      if (note) {
+        res.status(200).json({ message: "Note Successfully Updated!" });
+      } else {
+        res.status(404).json({ message: "No Note with that ID was found." });
+      }
+    })
+    .catch(err => res.status(500).json({ message: "Update Failed!" }));
+});
 
 module.exports = {
-    server,
-  };
+  server
+};
