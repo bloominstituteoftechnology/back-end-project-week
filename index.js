033d7547859a93ad5bb0f74ec9d5a4a66e7183ec
Katherine Gilligan <katia.gilligan88@gmail.com>
2018-09-24 09:12:49 -0600

    Added the GET endpoint to display all notes

--- index.js
+++ index.js
@@ -4,9 +4,20 @@ const knex = require('knex');
 const server = express(); 
 server.use(express.json()); 
 
+const dbConfig = require('./knexfile'); 
+const db = knex(dbConfig.development); 
+
 server.get("/", (req, res) => {
     res.status(200).json({message: "This worked!"})
-})
+}); 
+
+server.get("/api/notes", (req, res) => {
+    db('notes').then(notes => {
+        res.status(200).json(notes); 
+    }).catch(err => {
+        res.status(500).json({error: "Error accessing the data from the database"})
+    })
+}); 
 
 
 server.listen(9000, () => console.log("Server listening at Port 9000")); 
