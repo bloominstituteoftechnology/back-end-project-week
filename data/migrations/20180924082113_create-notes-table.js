2c1a6bf2cf2eb8935aa0c02160042df8deee9da5
Jun Hyuk Kim <junhyukkeem@gmail.com>
2018-09-24 08:23:40 -0700

    initialize application, initialize database

---
+++ data/migrations/20180924082113_create-notes-table.js
@@ -0,0 +1,11 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('notes', function(table) {
+    table.increments();
+    table.string('title').notNullable();
+    table.string('content').notNullable();
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('notes');
+};
