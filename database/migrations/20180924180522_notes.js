8ddab0700c71c7a426b9d35dcbe6200d1a2ca675
Synch3 <jonsimo@aol.com>
2018-09-24 19:58:13 -0400

    Database created

--- database/migrations/20180924180522_notes.js
+++ database/migrations/20180924180522_notes.js
@@ -1,3 +1,11 @@
 exports.up = function(knex, Promise) {};
+return knex.schema.createTable("notes", notes => {
+  notes.increments();
+
+  notes.string("title", 128).notNullable();
+
+  notes.string("body", 2000);
+});
 
 exports.down = function(knex, Promise) {};
+return knex.schema.dropTable("notes");
