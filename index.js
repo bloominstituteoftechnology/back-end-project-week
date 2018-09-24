f175e9061f76ae6ed59e4e851c3ee66dd23ef31e
JoCatherine Griggs <jocafrin@gmail.com>
2018-09-24 16:03:46 -0400

    GET set up

--- index.js
+++ index.js
@@ -3,16 +3,15 @@ const db = require("./data/db");
 const server = express();
 server.use(express.json());
 
-
-
-
-
-
-
-
-
-
-
+server.get("/notes", async (req, res) => {
+  try {
+    const notes = await db.select().from("notes");
+    res.status(200).json(notes);
+  } catch (err) {
+    console.log('/notes GET error:', err);
+    res.status(500).send({ error: "Unable to retrieve notes. Please try again later." });
+  }
+});
 
 const port = 8000;
 server.listen(port, function() {
