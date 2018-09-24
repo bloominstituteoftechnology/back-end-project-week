598ad7eb8af387de2ffa0136b31ae602d3e7b0a6
keegn <keegan.t.burkett@gmail.com>
2018-09-24 11:19:57 -0500

    project setup

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+// const bcrypt = require("bcryptjs");
+// const jwt = require("jsonwebtoken");
+
+// const db = require("./database/dbConfig.js");
+
+const server = express();
+
+server.use(express.json());
+server.use(cors());
+
+
+
+server.get("/", (req, res) => {
+    res.send("Server is humming along nicely.");
+  });
+  
+  server.listen(3300, () => console.log("\nrunning on port 3300\n"));
+  
