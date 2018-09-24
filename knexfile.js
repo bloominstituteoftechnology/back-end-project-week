40c2101bb6148a790542ee8b9bdcb77b96d353bf
Joseph Stossmeister <joseph.stossmeister@gmail.com>
2018-09-24 14:45:38 -0400

    Another attempt to get postgres working on heroku

--- knexfile.js
+++ knexfile.js
@@ -21,9 +21,8 @@ module.exports = {
   },
   production: {
     client: 'postgresql',
-    connection: {
-      database: process.env.DATABASE_URL,
-    },
+    connection: process.env.DATABASE_URL,
+    ssl: true,
     pool: {
       min: 2,
       max: 10
