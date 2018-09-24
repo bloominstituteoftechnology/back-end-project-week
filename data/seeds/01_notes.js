9af613776e8a70bef09b14bf40f20870dde4ad79
William <kwontaewoongah@gmail.com>
2018-09-24 14:13:34 -0700

    made seed file

--- data/seeds/01_notes.js
+++ data/seeds/01_notes.js
@@ -1,15 +1,13 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('notes').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
+      return knex('notes').insert([
         {title:'Dr. Strange', textBody:'Keeper of the Time Stone'},
         {title:'Thor', textBody: 'God of Thunder'},
         {title:'Captain America', textBody:'The First Avenger'}
       ]);
     });
 };
-
-
--- knexfile.js
+++ knexfile.js
@@ -2,8 +2,14 @@ module.exports = {
     development: {
       client: 'sqlite3',
       connection: {
-        filename: '.data/db.sqlite3', 
+        filename: './data/db.sqlite3', 
       },
       useNullAsDefault: true,
+      migrations:{
+        directory:'./data/migrations'
+    },
+    seeds:{
+        directory:'./data/seeds'
+    }
     },
   };
