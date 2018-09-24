ab05f92e8a62009f55271bf6b60c60de2c900634
“Kennedykid1995” <34872885+Kennedykid1995@users.noreply.github.com>
2018-09-24 11:50:27 -0500

    Migrations and Seeds
    I have made the migration and seed files and also have finished up the content within them.

--- knexfile.js
+++ knexfile.js
@@ -8,38 +8,13 @@ module.exports = {
       filename: './data/lambda.sqlite3'
     }, 
     useNullAsDefault: true,
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
     migrations: {
-      tableName: 'knex_migrations'
+      directory: './data/migrations',
+      tableName: 'dbmigrations',
+    },
+    seeds: {
+      directory: './data/seeds'
     }
   },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
 
 };
