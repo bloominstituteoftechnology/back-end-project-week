b0cb3e6e601bcb21ecbc0dac9f8850907bff80aa
Carey <careycbaldwin@gmail.com>
2018-09-24 11:29:16 -0400

    created and configured knex file

--- knexfile.js
+++ knexfile.js
@@ -5,40 +5,15 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
-    }
-  },
+      filename: './db/notes.sqlite3'
+    },
+    useNullAsDefault: true,
 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      directory: '.db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds',
+    },
   },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
 };
