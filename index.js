39233d0b096888bf8ec3e6480f20df78a48df8ca
francistse23 <wtse.ucsd@gmail.com>
2018-09-24 09:51:46 -0700

    Create note works.

--- index.js
+++ index.js
@@ -9,10 +9,23 @@ const db = knex(dbConfig.development);
 server.use(express.json());
 server.use(cors());
 
+//GET ALL NOTES
 server.get('/get/all', async (req, res) => {
     try {
         const notes = await db('notes')
         res.status(200).json( notes );
+    }
+    catch ( err ) {
+        res.status(500).json( err.message );
+    }
+})
+
+//POST A NEW NOTE
+server.post('/create', async (req, res) => {
+    const note = req.body;
+    try {
+        const newNote = await db.insert(note).into('notes');
+        res.status(201).json( newNote );
     }
     catch ( err ) {
         res.status(500).json( err.message );
