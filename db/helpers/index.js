cfbf6a33dda17cb8c7aec60105c3d1fe7a968772
Trevor <trevorfehrman@gmail.com>
2018-09-24 13:07:38 -0700

    Get all implemented, data structure problem overcome

--- db/helpers/index.js
+++ db/helpers/index.js
@@ -2,6 +2,21 @@ const db = require("knex")(require("../../knexfile").development);
 
 module.exports = {
 	getNotes() {
-		return db("notes");
+		// let notes = db("notes")
+		// 	.join("tags", "tags.note_id", "notes.id")
+		// 	.select(
+		// 		"notes.id",
+		// 		"notes.title",
+		// 		"notes.textBody",
+		// 		"tags.tag",
+		// 		"tags.note_id",
+		// 	);
+
+		// return notes;
+		let notes = db("notes");
+		let tags = db("tags");
+		return Promise.all([notes, tags]).then(results => {
+			return results;
+		});
 	},
 };
--- routes/restrictedRoutes.js
+++ routes/restrictedRoutes.js
@@ -5,14 +5,32 @@ const helpers = require("../db/helpers/index");
 router.get("/", function(req, res) {
 	helpers
 		.getNotes()
-		.then(notes => {
-			return res.json({
-				error: false,
-				message: notes,
+		.then(data => {
+			let notesArr = data[0];
+			let tagsArr = data[1];
+
+			let notesTags = notesArr.map(note => {
+				let tags = tagsArr
+					.map(tagEl => {
+						if (tagEl.note_id === note.id) {
+							return tagEl.tag;
+						}
+					})
+					.filter(el => {
+						return el !== undefined;
+					});
+				// console.log({ ...note, tags });
+				return { ...note, tags };
 			});
+
+			// console.log(notesTags);
+			// console.log(notesArr, tagsArr);
+			res.json({ error: false, message: notesTags });
 		})
 		.catch(err => {
 			console.log(err);
 			res.status(500).json({ error: true, message: "Server Error" });
 		});
 });
+
+module.exports = router;
