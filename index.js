6ee228f6e74102a99694d287adb44b7ed3790503
cubsfan46350 <cubsfan.46350@msn.com>
2018-09-24 16:35:42 -0500

    added index file

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const server = express();
+const knex = require('knex');
+const bcrypt = require('bcryptjs');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+server.listen(3300, console.log('Listening on Port 3300'));
