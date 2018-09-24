4ee76df113a2820ea4a5988445e81db002192def
dasgma <dasgrigoma@gmail.com>
2018-09-24 10:19:00 -0700

    Installed cors and connected to the front-end-project app

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const helmet = require('helmet');
+const cors = require('cors');
 
 const db = require('./db/helpers/notesHelper');
 
@@ -7,6 +8,10 @@ const server = express();
 
 server.use(helmet());
 server.use(express.json());
+server.use(cors());
+
+
+
 
 server.get('/', (req, res) => {
     res.send('API running....')
