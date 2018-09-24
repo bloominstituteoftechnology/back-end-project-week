01e6a3bcae7329a65c6cba54563e46be170ff169
Carey <careycbaldwin@gmail.com>
2018-09-24 12:53:09 -0400

    some refactoring using helper functions

---
+++ db/helpers.js
@@ -0,0 +1,37 @@
+const knex = require('knex');
+const knexConfig = require('../knexfile.js');
+const db = knex(knexConfig.development);
+
+module.exports = {
+  find,
+  findById,
+  insert,
+  update,
+  remove,
+};
+
+function find() {
+  return db('notes');
+}
+
+function findById(id) {
+  return db('notes').where({ id: Number(id) });
+}
+
+function insert(note) {
+  return db('notes')
+    .insert(note)
+    .then(ids => ({ id: ids[0] }));
+}
+
+function update(id, note) {
+  return db('notes')
+    .where('id', Number(id))
+    .update(note);
+}
+
+function remove(id) {
+  return db('notes')
+    .where('id', Number(id))
+    .del();
+}
--- server.js
+++ server.js
@@ -2,8 +2,7 @@ const express = require('express');
 const helmet = require('helmet');
 const knex = require('knex');
 
-const dbConfig = require('./knexfile');
-const db = knex(dbConfig.development);
+const db = require('./db/helpers.js');
 
 const server = express();
 
@@ -14,25 +13,53 @@ server.get('/', (req,res) => {
     res.send('API for notetaking app is running')
 });
 
+//POST a new note
 server.post('/api/notes', (req,res) => {
     const note = req.body;
 
     db
     .insert(note)
-    .into('notes')
-    .then(ids => {
-        res.status(201).json(ids);
+    .then(response => {
+        res.status(201).json(response);
     })
     .catch(err => res.status(500).json(err));
+    return;
 })
 
+//GET all notes
 server.get('/api/notes', (req,res) => {
-    db('notes')
+    db
+    .find()
     .then(notes => {
         res.status(200).json(notes);
     })
     .catch(err => res.status(500).json(err));
+    return;
 });
+
+//GET an individual note
+server.get('/api/notes/:id', (req,res) => {
+
+    const id = req.params.id;
+
+    db
+    .findById(id)
+    .then(note => {
+        if(note.length === 0){
+            res.status(404).json({message:  "could not find the corresponding note"})
+            return;
+        }
+        res.json(note);
+    })
+    .catch(error => {
+        res.status(500).json({message: 'Error looking for post'});
+    })
+});
+
+//PUT changes on a note - edit
+
+
+//DELETE an existing note
 
 
 server.listen(9000);
