0471210f4cffeb2508c4051bb835173b3783006e
keegn <keegan.t.burkett@gmail.com>
2018-09-24 15:41:43 -0500

    edit existing note working

--- api/routes/noteRoutes.js
+++ api/routes/noteRoutes.js
@@ -6,55 +6,79 @@ const router = express.Router();
 
 // GET
 router.get("/", (req, res) => {
-    db("notes")
-      .then(notes => {
-        res.status(200).json(notes);
-      })
-      .catch(err => {
-        res.status(500).json({ error: "The notes could not be retrieved." });
-      });
-  });
-  
+  db("notes")
+    .then(notes => {
+      res.status(200).json(notes);
+    })
+    .catch(err => {
+      res.status(500).json({ error: "The notes could not be retrieved." });
+    });
+});
+
 // GET BY ID
 router.get("/:id", (req, res) => {
-    const { id } = req.params;
+  const { id } = req.params;
+  db("notes")
+    .where({ id: id })
+    .then(note => {
+      if (note.length === 0) {
+        res.status(404).json({
+          message: "The note with the specified ID does not exist."
+        });
+      } else {
+        return res.status(200).json(note);
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ error: "The note could not be retrieved." });
+    });
+});
+// end
+
+// POST
+router.post("/", (req, res) => {
+  const note = req.body;
+  if (!note) {
+    return res.status(406).json({
+      errorMessage: "Please provide a title for the note."
+    });
+  } else {
     db("notes")
-      .where({ id: id })
-      .then(note => {
-        if (note.length === 0) {
-          res.status(404).json({
-            message: "The note with the specified ID does not exist.",
-          });
-        } else {
-          return res.status(200).json(note);
-        }
+      .insert(note)
+      .into("notes")
+      .then(notes => {
+        res.status(201).json({ message: "Note successfully added." });
       })
       .catch(err => {
-        res.status(500).json({ error: "The note could not be retrieved." });
+        res.status(500).json({ error: "The note could not be added." });
       });
-  });
-  // end
+  }
+});
+// end POST
 
-  // POST
-router.post("/", (req, res) => {
-    const note = req.body;
-    if (!note) {
-      return res.status(400).json({
-        errorMessage: "Please provide a title for the note.",
+// PUT
+router.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const updatedNote = req.body;
+  if (!updatedNote) {
+    return res.status(406).json({
+      errorMessage: "Please provide a title for the note."
+    });
+  } else {
+    db("notes")
+      .where("id", id)
+      .update({
+        title: updatedNote.title,
+        textBody: updatedNote.textBody
+      })
+      .then(notes => {
+        res.status(200).json({ message: "Note successfully modified." });
+      })
+      .catch(err => {
+        res.status(500).json({ error: "The note could not be updated." });
       });
-    } else {
-      db("notes")
-        .insert(note)
-        .into("notes")
-        .then(notes => {
-          res.status(201).json({ message: "Note successfully added." });
-        })
-        .catch(err => {
-          res.status(500).json({ error: "The note could not be added." });
-        });
-    }
-  });
-  // end POST
-  
+  }
+});
+// end PUT
 
-  module.exports = router;
+module.exports = router;
