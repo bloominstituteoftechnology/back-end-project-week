2490f9e51b26d900df6d4b5251ae2b0e14c8285e
Katherine Gilligan <katia.gilligan88@gmail.com>
2018-09-23 21:27:53 -0600

    Added knex migration and seeding files for notes, db fully setup and ready to go

---
+++ db/migrations/20180923203848_notes.js
@@ -0,0 +1,12 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('notes', tbl => {
+        tbl.increments(); 
+        tbl.string('Title').notNullable(); 
+        tbl.string('Content').notNullable(); 
+    })
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTable('notes'); 
+};
--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express'); 
+const knex = require('knex'); 
 
 const server = express(); 
 server.use(express.json()); 
+
+server.get("/", (req, res) => {
+    res.status(200).json({message: "This worked!"})
+})
+
 
 server.listen(9000, () => console.log("Server listening at Port 9000")); 
