a1dc8e06381c50135bc22764383247307d7e3931
Charlie Winslow <cwinslow22@yahoo.com>
2018-09-24 14:18:41 -0500

    completed endpoint to get list of notes

--- index.js
+++ index.js
@@ -15,6 +15,14 @@ server.get('/', (req, res) => {
     res.send('API needs a break');
 });
 
+// get the list of notes
+server.get('/api/notes', (req, res) => {
+    db('notes')
+        .then(notes => {
+            res.status(200).json(notes);
+        })
+        .catch(err => res.status(500).json(err));
+});
 
 
 const port = 8000;
