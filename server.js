ebc718ec2b8f4d6eb5f69739344a2a9e66b8d5f8
Joseph Stossmeister <joseph.stossmeister@gmail.com>
2018-09-24 15:04:38 -0400

    fixing cors to allow app to use backend -believe it was working for awhile, just only through postman

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const apiRoutes = require("./api");
 const server = express();
 server.use(express.json());
 server.use(helmet());
-server.use(cors({}));
+server.use(cors());
 
 server.use("/api", apiRoutes);
 
