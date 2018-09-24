8ad543c5c6fa586563ca775e4b76673c7ce7ec5a
bri79319 <bri79319@gmail.com>
2018-09-24 14:00:37 -0400

    writing out endpoints

--- db/seeds/01-seeds.js
+++ db/seeds/01-seeds.js
@@ -1,10 +1,10 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('notes').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
+      return knex('notes').insert([
         {id: 1, title: 'Back-End-Project', content: 'Successful!'},
         {id: 2, title: 'Notes/Back-End', content: 'Look at all these notes.'},
         {id: 3, title: 'Nice one!', content: 'High five!!'}
--- index.js
+++ index.js
@@ -14,30 +14,63 @@ server.use(cors());
 
 //=================GET ENDPOINT===============//
 server.get("/note/get/all", (req, res) => {
-
+  db("notes")
+    .then(notes => {
+      res.status(200).json(notes);
+    })
+    .catch(err => {
+      console.log("Error with GET ALL: ", err);
+      res.status(500).json({ Error: "Notes cannot be retrieved" });
+    });
 });
 //=================GET ENDPOINT===============//
 
 //=================GET ENDPOINT===============//
 server.get("/note/get/:id", (req, res) => {
-
+  const id = req.params.id;
+  db("notes")
+    .select()
+    .where({ id })
+    .then(note => {
+      res.status(200).json(note);
+    })
+    .catch(err => {
+      console.log("Error with GET ID: ", err);
+      res.status(500).json({ Error: "Note with ID cannot be retrieved" });
+    });
 });
 //=================GET ENDPOINT===============//
 
 //=================POST ENDPOINT===============//
 server.post("/note/create", (req, res) => {
+  const newNote = req.body;
 
+  db.insert(newNote)
+    .into("notes")
+    .then(ids => {
+      res.status(201).json(ids);
+    })
+    .catch(err => {
+      console.log("Error with POST CREATE: ", err);
+      res.status(500).json({ Error: "Pass in a title and content" });
+    });
 });
 //=================POST ENDPOINT===============//
 
 //=================PUT ENDPOINT===============//
 server.put("/note/edit/:id", (req, res) => {
+    const changes = req.body;
+    const { id } = req.params;
 
+    db("notes")
+      .where({ id })
+      .update(changes)
+      .then(count => {
+          
+      })
 });
 //=================PUT ENDPOINT===============//
 
 //=================DELETE ENDPOINT===============//
-server.delete("/note/delete/:id", (req, res) => {
-
-});
+server.delete("/note/delete/:id", (req, res) => {});
 //=================DELETE ENDPOINT===============//
