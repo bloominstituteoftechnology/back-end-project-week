0fd0fe6fe82a50a6bb2ea66aae4a9d196e2cead6
Sania Parekh <sparekh@Sanias-MacBook.local>
2018-09-24 16:07:29 -0500

    Endpoint: View and existing note

--- index.js
+++ index.js
@@ -26,6 +26,38 @@ server.get('/api/notes', (req, res) => {
     })
 });
 
+//view an existing note
+server.get('/api/notes/:id', (req, res) => {
+    const { id } = req.params;
+    db('notes')
+    .where({ id })
+    .then(note => {
+        if (note) {
+            res.status(200).json(note);
+        }
+        else {
+            res.status(404).json({error: 'The Note with this ID is not found'});
+        }
+    })
+    .catch(error => res.status(500).json({ error: 'Cannot retrieve note information'}));
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const port = 6000;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
