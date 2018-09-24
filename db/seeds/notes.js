4cf4497163d23172b77ebb1c0b5a5c94db524191
LaikaFusion <newrad4@gmail.com>
2018-09-24 12:14:56 -0400

    added seed file

---
+++ db/seeds/notes.js
@@ -0,0 +1,9 @@
+exports.seed = function(knex, Promise) {
+  return knex('notes').del()
+    .then(function () {
+      return knex('notes').insert([
+        { title: 'Pizza', body: 'Testestest', checklist:'[{checked: true, name:"string"}]', tags:'["string"]', userID:0},
+        { title: 'Orange', body: 'Testestest1111', userID:0}
+      ]);
+    });
+};
