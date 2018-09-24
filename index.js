e625f606202c0c00856f48b29332ccc083a8117f
skennedy93 <sean47900@gmail.com>
2018-09-24 11:40:23 -0500

    notes migration

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-const express = require('express')
+const express = require('express');
+const knex = require('knex');
 
 const server = express();
 server.use (express.json())
 
+const dbConfig = require('./knexfile'); 
+const db = knex(dbConfig.development); 
 
+server.get("/", (req, res) => {
+    res.status(200).json({message: "Test"})
+})
 
 server.listen(3000, () => console.log("server listening at port 3000"));
