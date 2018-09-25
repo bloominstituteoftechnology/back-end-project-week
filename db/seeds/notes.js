1adab901bfdc58827942294d0092656624886ea1
Katherine Gilligan <katia.gilligan88@gmail.com>
2018-09-25 10:26:21 -0600

    Fixed seeding to display correct tags

--- db/seeds/notes.js
+++ db/seeds/notes.js
@@ -14,18 +14,18 @@ exports.seed = function(knex, Promise) {
           title: "Notes about React",
           content:
             "React is a UI render component library. It uses components to built out a UI.", 
-          tags: "#React, #UI, #Component, #Programming, #Javascript"
+          tags: "#React #UI #Component #Programming #Javascript"
         },
         {
           title: "Notes about Node.js",
           content:
             "Node.js is a Javascript runtime. Node is single-threaded and non-blocking. It contains a large ecosystem of libraries called NPM modules.", 
-          tags: "#Node, #Programming, #Javascript"
+          tags: "#Node #Programming #Javascript"
         },
         {
           title: "Things to get Done Today",
           content: "Clean laundry, wash dishes, go grocery shopping", 
-          tags: "#Todo, #Chores"
+          tags: "#Todo #Chores"
         }
       ]);
     });
