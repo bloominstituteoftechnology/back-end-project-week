9fc2f121189e0f1c57d488e8a71c7370f90e9d3a
sca4cs <shannon.atkinson.ca@gmail.com>
2018-09-25 16:07:30 -0400

    added routes for authentication

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const helmet = require('helmet');
 const knex = require('knex');
 const cors = require('cors');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const dbConfig = require('./knexfile');
 
@@ -13,8 +15,79 @@ server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
+//auth
+const secret = 'secret';
+
+function generateToken(user) {
+  const payload = {
+    username: user.username,
+    department: user.department
+  };
+  const options = {
+    expiresIn: '1h',
+    jwtid: '12345',
+  };
+  return jwt.sign(payload, secret, options);
+}
+
+function protected(req, res, next) {
+  const token = req.headers.authorization;
+  if (token) {
+    jwt.verify(token, secret, (err, decodedToken) => {
+      if (err) {
+        res.status(401).json({ message: 'Invalid Token' });
+      } else {
+        req.user = { username: decodedToken.username, department: decodedToken.department };
+        next();
+      }
+    });
+  } else {
+    res.status(401).json({ message: 'no token provided' });
+  }
+}
+
+server.post('/api/register', (req, res) => {
+    const creds = req.body;
+    const hash = bcrypt.hashSync(creds.password, 10);
+    creds.password = hash;
+    db('users')
+      .insert(creds)
+      .then(ids => {
+        const id = ids[0];
+        db('users')
+          .where({ id })
+          .first()
+          .then(user => {
+            const token = generateToken(user);
+            res.status(201).json({ id: user.id, token });
+          })
+          .catch(err => {
+            console.log(err);
+            res.status(500).send(err)});
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).send(err)});
+  });
+  
+  server.post('/api/login', (req, res) => {
+    const creds = req.body;
+    db('users')
+      .where({ username: creds.username })
+      .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(creds.password, user.password)) {
+          const token = generateToken(user);
+          res.status(200).json({ token });
+        } else {
+          res.status(401).json({ message: 'You shall not pass!' });
+        }
+      })
+      .catch(err => res.status(500).send(err));
+  });
+
 //endpoints
-server.post('/api/notes', (req, res) => {
+server.post('/api/notes', protected, (req, res) => {
     const note = req.body;
     if (!note.title || !note.textBody) {
         res.status(400).json({ error: "Please provide a title and body for the note." })
@@ -27,7 +100,7 @@ server.post('/api/notes', (req, res) => {
         .catch(err => res.status(500).json({ error: "There was an error saving the note." }))
 });
 
-server.get('/api/notes', (req, res) => {
+server.get('/api/notes', protected, (req, res) => {
     db('notes')
     .then(notes => {
         res.status(200).json(notes);
@@ -35,7 +108,7 @@ server.get('/api/notes', (req, res) => {
     .catch(err => res.status(500).json(err));
 });
 
-server.get('/api/notes/:id', (req, res) => {
+server.get('/api/notes/:id', protected, (req, res) => {
     const {id} = req.params;
     db('notes').where({ id: id }).first()
     .then(async note => {
@@ -48,7 +121,7 @@ server.get('/api/notes/:id', (req, res) => {
     .catch(err => res.status(500).json(err));
 });
 
-server.put('/api/notes/:id', (req, res) => {
+server.put('/api/notes/:id', protected, (req, res) => {
     const {id} = req.params;
     const note = req.body;
     if (!note.title || !note.textBody) {
@@ -65,7 +138,7 @@ server.put('/api/notes/:id', (req, res) => {
         .catch(err => res.status(500).json(err));
 });
 
-server.delete('/api/notes/:id', (req, res) => {
+server.delete('/api/notes/:id', protected, (req, res) => {
     const {id} = req.params;
     db('notes').where({ id: id }).del()
     .then(count => {
