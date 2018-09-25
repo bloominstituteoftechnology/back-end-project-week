8f30c084b78c36900b50d088429d6e99cf52433a
cubsfan46350 <cubsfan.46350@msn.com>
2018-09-25 14:42:57 -0500

    added endpoint to view list of notes

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const db = knex(dBConfig.development);
 server.use(express.json());
 
 //Endpoints
-server.post('/api/addNote', (res, req) => {
+server.post('/api/addNote', (req, res) => {
     const newNote = req.body;
     db('notes')
         .insert(newNote)
@@ -17,8 +17,16 @@ server.post('/api/addNote', (res, req) => {
             res.status(201).json(res)
         })
         .catch(err => res.status(500).send(err));
-})
+});
 
+server.get('/api/notes', (req, res) => {
+    db('notes')
+        .select('id', 'title', 'textBody')
+        .then(notes => {
+            res.json(notes);
+        })
+        .catch(err => res.send(err));
+});
 
 
 
