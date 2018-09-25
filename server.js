9ebf2464cbe118eb283ce9844ae915d19440776a
David Pok <david.pok@gmail.com>
2018-09-25 15:44:02 -0700

    adds put

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const helmet = require("helmet");
 const knex = require("knex");
+const cors = require("cors");
 
 const dbConfig = require("./knexfile");
 
@@ -19,7 +20,7 @@ server.post("/api/notes", (req, res) => {
   if (!req.body.title || !req.body.content) {
     res.status(400).json({ error: "needs both title and content" });
   } else {
-    db.insert(note)
+    db.insert(req.body)
       .into("notes")
       .then(ids => {
         res.status(201).json(ids);
@@ -48,6 +49,26 @@ server.get("/api/notes/:id", (req, res) => {
       }
     })
     .catch(err => res.status(500).json(err));
+});
+
+server.put("/api/notes/:id", (req, res) => {
+  const { id } = req.params;
+  if (!req.body.title || !req.body.content) {
+    res.status(400).json({ error: "needs both title and content" });
+  } else
+    db("notes")
+      .where({ id: id })
+      .update(req.body)
+      .then(count => {
+        if (count) {
+          res.status(200).json({ message: "updated!" });
+        } else {
+          res.status(404).json({ message: "no note with that ID" });
+        }
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      });
 });
 
 const port = 8000;
