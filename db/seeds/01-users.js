c8a6ed8a45b78ef94f6466a4a18cfed8b171b3c6
Alberto-Funk <laptopreneur@gmail.com>
2018-09-25 15:27:39 -0700

    Update seeds

--- db/seeds/01-users.js
+++ db/seeds/01-users.js
@@ -3,7 +3,7 @@ exports.seed = function(knex, Promise) {
   return knex('users').del()
     .then(function () {
       return knex('users').insert([
-        {username: 'Alberto Funk', password: 'You will never know'}
+        {username: 'INIT', password: 'You will never know'}
       ]);
     });
 };
--- db/seeds/03-tags.js
+++ db/seeds/03-tags.js
@@ -5,11 +5,7 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('tags').insert([
-        {tag_title: 'test', noteId: 1},
-        {tag_title: 'test2', noteId: 2},
-        {tag_title: 'test3', noteId: 2},
-        {tag_title: 'test4', noteId: 3},
-        {tag_title: 'test5', noteId: 4}
+        {tag_title: 'INIT', noteId: 1}
       ]);
     });
 };
