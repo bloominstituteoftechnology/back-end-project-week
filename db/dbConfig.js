23ed3e2c2ebcda6805b17af39fa34d1c8e633163
kamalnrf <kamalnrf@hotmail.com>
2018-09-25 18:43:35 +0530

    0

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,4 +1,6 @@
 const env = process.env.NODE_ENV || 'development';
 const config = require('../knexfile')[env];
+console.log('~~~~~~~~~~~~~~~')
+console.log(config)
 
 module.exports = require('knex')(config);
--- knexfile.js
+++ knexfile.js
@@ -14,18 +14,16 @@ module.exports = {
     }
   },
   production: {
-    client: 'postgresql',
-    connection: {
-      database: process.env.DATABASE_URL || 'my_db',
-      user:     'uegefrgadgkgcv',
-      password: '48a51e273b0fc599dbdd1476d7bbfcf430f6dc10a284b1c7b9d4d77d29f3c4e0'
-    },
+    client: 'pg',
+    debug: true,
+    connection: process.env.DATABASE_URL || 'postgres://uegefrgadgkgcv:48a51e273b0fc599dbdd1476d7bbfcf430f6dc10a284b1c7b9d4d77d29f3c4e0@ec2-107-21-98-165.compute-1.amazonaws.com:5432/dlhrvbdjg7ads',
     migrations: {
       directory: "./db/migrations",
       tableName: "dbMigrations"
     },
     seeds: {
       directory: './db/seeds',
-    }
+    },
+    ssl: true
   }
 };
