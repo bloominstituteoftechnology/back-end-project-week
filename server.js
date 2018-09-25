aa9dc8990096f783eab70a5a1bdc6e67a74eaa2d
Kyle Meltzer <jimmy13@Kyles-MacBook-Pro.local>
2018-09-25 10:37:08 -0500

    modifying endpoints to work with front end

--- server.js
+++ server.js
@@ -87,7 +87,15 @@ server.put("/api/notes/:id", (req, res) => {
     .where({ id })
     .update(changes)
     .then(count => {
-      res.status(200).json(count);
+      console.log(count);
+      db("notes")
+        .then(notes => {
+          res.status(200).json(notes);
+        })
+        .catch(err => {
+          console.log("error", err);
+          res.status(500).json({ error: "The notes could not be retrieved." });
+        });
     })
     .catch(err => {
       console.log("error", err);
