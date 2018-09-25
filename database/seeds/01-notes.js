45d87378b715d8087ad3c6e180c15c8951ef55fb
Synch3 <jonsimo@aol.com>
2018-09-25 13:18:05 -0400

    Seeded

--- database/seeds/01-notes.js
+++ database/seeds/01-notes.js
@@ -12,19 +12,19 @@ exports.seed = function(knex, Promise) {
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
         },
         {
-          id: 1,
+          id: 2,
           title: "Is this just fantasy?",
           body:
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
         },
         {
-          id: 1,
+          id: 3,
           title: "Caught in a landslide",
           body:
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
         },
         {
-          id: 1,
+          id: 4,
           title: "No escape from reality",
           body:
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
