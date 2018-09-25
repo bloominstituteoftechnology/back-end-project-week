38c391ab78f7482876a5397c388d48bb21b01d1a
“Kennedykid1995” <34872885+Kennedykid1995@users.noreply.github.com>
2018-09-25 10:42:38 -0500

    All Endpoints are working
    Finished the last three endpoints DELETE POST and PUT, they all work checked in Postman.

--- index.js
+++ index.js
@@ -29,6 +29,51 @@ server.get("/notes/:id", (req, res) => {
     .catch(err => res.status(500).json(err)); 
 })
 
+server.post("/notes", (req, res) => {
+    const note = req.body; 
+    const {title} = req.body; 
+    const {content} = req.body; 
+    if(!title & !content){
+        res
+            .status(400)
+            .json({errorMessage: "note needs a title and content"});
+    }
+    db.insert(note)
+        .into('notes')
+        .then(ids => {
+            res.status(201).json(ids);
+        })
+        .catch(err => res.status(500).json(err))
+});
+
+server.delete('/notes/:id', (req, res) => {
+    const { id } = req.params; 
+    db("notes")
+    .where({id})
+    .del()
+    .then(count => {
+        res.status(200).json(count); 
+    })
+    .catch(err => {
+        res.status(500).json(err);
+    });
+})
+
+server.put("/notes/:id", (req, res) => {
+    const changes = req.body;
+    const {id} = req.params;
+
+    db("notes")
+        .where({id})
+        .update(changes)
+        .then(count => {
+            res.status(200).json(count);
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        });
+});
+
 const port = 3001; 
 server.listen(port, function(){
     console.log("\n=== WEB API LISTENING 3001 ===\n")
