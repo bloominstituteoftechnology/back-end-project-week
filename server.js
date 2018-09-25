e69789fd6ecfed7c370f4f0377d330de6084f3ce
David Pok <david.pok@gmail.com>
2018-09-25 14:43:55 -0700

    adds GET endpoint

--- server.js
+++ server.js
@@ -15,7 +15,15 @@ server.get("/", (req, res) => {
   res.send("connected");
 });
 
+server.get("/api/notes", (req, res) => {
+  db("notes")
+    .then(notes => {
+      res.status(200).json(notes);
+    })
+    .catch(err => res.status(500).json(err));
+});
+
 const port = 8000;
 server.listen(port, function() {
-  console.log(`\n=API ON ${port}=`);
+  console.log(`\n=API ON ${port}=\n`);
 });
