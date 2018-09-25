c128f5fea07a72a51aa004b7f7ac1f0b8e55d941
Synch3 <jonsimo@aol.com>
2018-09-25 11:32:35 -0400

    EOD Monday

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
+const knex = require("knex");
+const bcrypt = require("bcryptjs");
+
+const dbConfig = require("./knexfile");
+
+const db = knex(dbConfig.development);
 
 const server = express();
 
@@ -10,6 +16,15 @@ server.use(helmet());
 
 server.get("/", (req, res) => {
   res.send("This server is fully operational.");
+});
+
+server.get("/api/notes", (req, res) => {
+  db("notes")
+    .select("id", "title", "body")
+    .then(notes => {
+      res.join(notes);
+    })
+    .catch(err => res.send(err));
 });
 
 server.listen(9000);
--- knexfile.js
+++ knexfile.js
@@ -9,21 +9,5 @@ module.exports = {
     useNullAsDefault: true,
     migrations: { directory: "./database/migrations" },
     seeds: { directory: "./database/seeds" }
-  },
-
-  production: {
-    client: "postgresql",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password"
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: "knex_migrations"
-    }
   }
 };
