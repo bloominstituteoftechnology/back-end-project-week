bd012097d2640661b315a383302eadb1e3fdd43f
szincone <sawyerzincone@gmail.com>
2018-09-25 16:18:13 -0600

    updated cors url request address

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -31,7 +31,12 @@ function errorHandler(err, req, res, next) {
 }
 
 module.exports = server => {
-  server.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+  server.use(
+    cors({
+      credentials: true,
+      origin: "https://secret-bastion-74251.herokuapp.com",
+    }),
+  );
   server.use(helmet());
   server.use(express.json());
   server.use(morgan("dev"));
