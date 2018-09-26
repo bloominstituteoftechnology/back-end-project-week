62dc8e898ee056e42aa0c9cb46088970fba7c9cd
Erica Thompson <ericathompson.nc@gmail.com>
2018-09-25 19:59:31 -0700

    Better MVP

--- notes-app/src/Components/EditNote/EditNote.js
+++ notes-app/src/Components/EditNote/EditNote.js
@@ -4,80 +4,6 @@ import axios from 'axios';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-
-class DelayLink extends React.Component {
-    static propTypes = {
-      /**
-       * Milliseconds to wait before registering the click.
-       */
-      delay:        PropTypes.number,
-      /**
-       * Called after the link is clicked and before the delay timer starts.
-       */
-      onDelayStart: PropTypes.func,
-      /**
-       * Called after the delay timer ends.
-       */
-      onDelayEnd:   PropTypes.func
-    };
-  
-    static defaultProps = {
-      delay:        0,
-      onDelayStart: () => {},
-      onDelayEnd:   () => {}
-    };
-  
-    static contextTypes = Link.contextTypes;
-  
-    constructor(props) {
-      super(props);
-      this.timeout = null;
-    }
-  
-    componentWillUnmount() {
-      if (this.timeout) {
-        clearTimeout(this.timeout);
-      }
-    }
-  
-    /**
-     * Called when the link is clicked
-     *
-     * @param {Event} e
-     */
-    handleClick = (e) => {
-      const { replace, to, delay, onDelayStart, onDelayEnd } = this.props;
-      const { history } = this.context.router;
-  
-      onDelayStart(e, to);
-      if (e.defaultPrevented) {
-        return;
-      }
-      e.preventDefault();
-  
-      this.timeout = setTimeout(() => {
-        if (replace) {
-          history.replace(to);
-        } else {
-          history.push(to);
-        }
-        onDelayEnd(e, to);
-      }, delay);
-    };
-  
-    render() {
-      const props = Object.assign({}, this.props);
-      delete props.delay;
-      delete props.onDelayStart;
-      delete props.onDelayEnd;
-  
-      return (
-        <Link {...props} onClick={this.handleClick} />
-      );
-    }
-  }
-
-
 class EditNote extends Component {
     
     state = {
@@ -85,11 +11,6 @@ class EditNote extends Component {
         title: '',
         content: ''
     }
-
-
-    
-
-
 
 
 
