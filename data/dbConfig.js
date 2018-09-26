0b3891ffa0e5f1e61290960a434fb2984ff31f0e
Kyle Meltzer <jimmy13@Kyles-MacBook-Pro.local>
2018-09-26 13:00:41 -0500

    modified dbConfig

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,5 +1,7 @@
 const knex = require('knex');
 
-const knexConfig = require('../knexfile.js');
+const dbEngine = process.env.DB || 'development';
 
-module.exports = knex(knexConfig.development);
+const knexConfig = require('../knexfile.js')[dbEngine];
+
+module.exports = knex(knexConfig);
