51cf5ebd2b88b0073389e2caa39ba0547abe5972
francistse23 <wtse.ucsd@gmail.com>
2018-09-26 13:05:46 -0700

    Pagination is functioning. Need to style for interface design consistency.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+// require('dotenv').config();
 const express = require('express');
 const server = express();
 const knex = require('knex');
@@ -159,5 +159,5 @@ server.delete('/delete/:id', async (req, res) => {
     }
 });
 
-const port = process.env.PORT || 8000
+const port = 8000
 server.listen(port, () => console.log(`===Server is running on port ${port}===`));
