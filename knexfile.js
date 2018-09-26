fc93bb6a8fd0e64182b409871cfd6b156e24e3e9
sca4cs <shannon.atkinson.ca@gmail.com>
2018-09-26 13:53:41 -0400

    config knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,20 +1,42 @@
 require('dotenv').config();
 
-const dbConnection = process.env.DATABASE_URL;
+const localPg = {
+  host: 'localhost',
+  database: 'lambda',
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+};
+const dbConnection = process.env.DATABASE_URL || localPg;
 
 module.exports = {
-
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './data/lambda.sqlite3'
+      filename: './db/lambda.sqlite3',
     },
-    useNullAsDefault: true,
     migrations: {
-      directory: './data/migrations',
+      tableName: 'knex_migrations',
+      directory: './db/migrations',
     },
     seeds: {
-      directory: './data/seeds',
+      directory: './db/seeds',
+    },
+    useNullAsDefault: true,
+  },
+
+  production: {
+    client: 'pg',
+    connection: dbConnection,
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: './db/migrations',
+    },
+    seeds: {
+      directory: './db/seeds',
     },
   },
 };
