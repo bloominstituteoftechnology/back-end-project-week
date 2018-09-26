5a427fa327137a0ff6400bbba4775909b0c02efb
AI2EJAY <JHammett1314@gmail.com>
2018-09-26 12:52:06 -0500

    test

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,11 @@
 // Update with your config settings.
 require("dotenv").config();
+const localPg = {
+  host: 'localhost',
+  database: 'lambda',
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+};
 const dbConnection = process.env.DATABASE_URL;
 module.exports = {
   development: {
