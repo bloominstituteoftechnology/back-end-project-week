a2727126314edd1fd0b3c0047b604e5bd939b8ad
Kyle Meltzer <jimmy13@Kyles-MacBook-Pro.local>
2018-09-26 12:42:24 -0500

    added production settings in knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,7 @@
 // Update with your config settings.
+require("dotenv").config();
+
+const dbConnection = process.env.DATABASE_URL;
 
 module.exports = {
   development: {
@@ -7,6 +10,20 @@ module.exports = {
       filename: "./data/lambda.sqlite3"
     },
     useNullAsDefault: true,
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    }
+  },
+  production: {
+    client: "pg",
+    connection: dbConnection,
+    pool: {
+      min: 2,
+      max: 10
+    },
     migrations: {
       directory: "./data/migrations"
     },
