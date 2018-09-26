fb10333a433d35d84ff5e813751f95dc42d708db
AI2EJAY <JHammett1314@gmail.com>
2018-09-26 12:54:42 -0500

    saved migration

--- db/migrations/20180926105809_addTabs.js
+++ db/migrations/20180926105809_addTabs.js
@@ -1,8 +1,11 @@
 
 exports.up = function(knex, Promise) {
-  
+    return knex.schema.createTable('notes', tbl => {
+        tbl.string('tabs', 64)
+   
+    })
 };
 
 exports.down = function(knex, Promise) {
-  
+    return knex.schema.dropTable('notes')
 };
