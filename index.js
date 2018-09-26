a58213ad784fd554fa6f579cd6c15a4cf3a42e78
Grant Reighard <grant.reighard@gmail.com>
2018-09-26 15:41:31 -0400

    Added dotenv package and PORT variable.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+require('dotenv').config();
+
 const server = require('./server');
 
-const port = process.env.PORT || 5020;
+const port = process.env.PORT || 5000;
 
 server.listen(port, () => console.log(`\n === Server listening on port ${port} === \n`));
