407b7365a45faf54c842a56f0ae6f5c5501219e1
jordan-massingill <jordan_massingill@hotmail.com>
2018-09-26 12:52:29 -0500

    knexfile edits

--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,12 @@
 // Update with your config settings.
 require('dotenv').config();
-const dbConnection = process.env.DATABASE_URL;
+const localPg = {
+  host: 'localhost',
+  database: 'lambda',
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+};
+const dbConnection = process.env.DATABASE_URL || localPg;
 module.exports = {
 
   development: {
