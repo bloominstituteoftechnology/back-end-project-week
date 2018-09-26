97cd79fae1f9069535c36e29fbefb275d7c9ec97
Rachel DiCesare <racheldicesare@missrachelthedeveloper.com>
2018-09-26 14:29:21 -0400

    heroku take 1000

--- knexfile.js
+++ knexfile.js
@@ -1,53 +1,40 @@
 // Update with your config settings.
-require('dotenv').config();
-const localPg = {
-  host: 'localhost',
-  database: 'lambda',
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-};
+require("dotenv").config();
+
 const dbConnection = process.env.DATABASE_URL;
 
 module.exports = {
-
   development: {
-    client: 'sqlite3',
+    client: "sqlite3",
     connection: {
       filename: './dev.sqlite3'
     },
-    useNullAsDefault: true
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
+    useNullAsDefault: true,
     migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'pg',
-    connection: 'dbConnection'
-  },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
+      directory: "./data/migrations"
     },
     seeds: {
-      directory: './seeds',
+      directory: "./data/seeds"
+    }
+  },
+  production: {
+    client: "pg",
+    connection: dbConnection,
+    pool: {
+      min: 2,
+      max: 10
     },
-  };
+    migrations: {
+      directory: "./migrations"
+    },
+    seeds: {
+      directory: "./seeds"
+    }
+  }
+};
+
+
+
+ 
 
 
