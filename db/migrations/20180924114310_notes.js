4dd71f62aa75cb502ec51bac5a26d3874b98f4f0
Grant Reighard <grant.reighard@gmail.com>
2018-09-27 14:45:57 -0400

    Revert "Changed migration file types to text from string."
    This reverts commit 6d2be7abe458725899046fdc9298713712a2114f.

--- db/migrations/20180924114310_notes.js
+++ db/migrations/20180924114310_notes.js
@@ -3,12 +3,12 @@ exports.up = function(knex, Promise) {
     return knex.schema.createTable('notes', function(tbl) {
         tbl.increments();
 
-        tbl.text('title', 128)
+        tbl.string('title', 128)
         .notNullable();
 
-        tbl.text('textBody');
+        tbl.string('textBody');
 
-        tbl.text('tags');
+        tbl.string('tags');
     })
 };
 
