6d2be7abe458725899046fdc9298713712a2114f
Grant Reighard <grant.reighard@gmail.com>
2018-09-26 20:26:07 -0400

    Changed migration file types to text from string.

--- db/migrations/20180924114310_notes.js
+++ db/migrations/20180924114310_notes.js
@@ -3,12 +3,12 @@ exports.up = function(knex, Promise) {
     return knex.schema.createTable('notes', function(tbl) {
         tbl.increments();
 
-        tbl.string('title', 128)
+        tbl.text('title', 128)
         .notNullable();
 
-        tbl.string('textBody');
+        tbl.text('textBody');
 
-        tbl.string('tags');
+        tbl.text('tags');
     })
 };
 
