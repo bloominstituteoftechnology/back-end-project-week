58ec9a13185a0ca260e514024139aea022d743a9
bri79319 <bri79319@gmail.com>
2018-09-27 11:16:35 -0400

    configuration of package json and knex file

--- knexfile.js
+++ knexfile.js
@@ -1,17 +1,33 @@
 // Update with your config settings.
 module.exports = {
-
   development: {
     client: 'sqlite3',
     connection: {
       filename: './db/lambda.sqlite3'
     },
-    useNullAsDefault: true,
     migrations: {
+      tableName: "notes",
       directory: "./db/migrations",
     },
     seeds: {
       directory: "./db/seeds"
-    }
+    },
+    useNullAsDefault: true,
+  },
+
+  production: {
+    client: 'pg',
+    connection: dbConnection,
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "notes",
+      directory: "./db/migrations",
+    },
+    seeds: {
+      directory: "./db/seeds",
+    },
   },
 };
