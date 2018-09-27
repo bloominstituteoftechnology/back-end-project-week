54042e688fe0450f9a01a9c3d18488e83dd36e53
rverdi642 <rverdi642@gmail.com>
2018-09-26 19:53:22 -0500

    Richard Verdier - MVP complete

--- backend/index.js
+++ backend/index.js
@@ -43,19 +43,38 @@ server.get('/', (req, res) => {
     if (!title || !content) return res.status(400).json({ message: "Title and message are required" })
     db('notes')
       .insert({ title, content })
-      .then(result => res.status(201).json({ title, content }))
+      .then(res => res.status(201).json({ title, content }))
       .catch(() => res.status(500).json({ message: "Note could not be saved" }))
   })
   
-  server.get('/api/notes/:id',   (req, res) => {
+  // server.get('/api/notes/:id',   (req, res) => {
+  //   const { id } = req.params;
+  //   db('notes')
+  //     .where({ id })
+  //     .then(note => {
+  //       note = note.filter(note => note.username === req.headers.username);
+  //       res.status(200).json(note);
+  //     })
+  //     .catch((err) => res.status(500).json(err));
+  // });
+
+  server.get("/api/notes/:id", (req, res) => {
     const { id } = req.params;
-    db('notes')
-      .where({ id })
+     console.log({id})
+    db("notes")
+      .where({ id: id })
       .then(note => {
-        note = note.filter(note => note.username === req.headers.username);
-        res.status(200).json(note);
+        if (note.id === 0) {
+          res.status(404).json({
+            message: "The note with the specified ID does not exist."
+          });
+        } else {
+          return res.status(200).json(note);
+        }
       })
-      .catch((err) => res.status(500).json(err));
+      .catch((err) => {
+        res.status(500).json({ error: "The note could not be retrieved." });
+      });
   });
   
   server.delete('/api/notes/:id',   (req, res) => {
@@ -64,11 +83,26 @@ server.get('/', (req, res) => {
       .where({ id })
       .del()
       .then(id => {
+        if( id === 0) {
+          return res.status(404).json({err:" The note with that ID is not found "});
+        } else {
         res.status(200).json({ id });
-      })
+        }})
       .catch((err) => res.status(500).json(err));
   });
   
+  // server.put('/api/notes/:id',   (req, res) => {
+  //   const { id } = req.params;
+  //   const { title, content } = req.body;
+  //   db('notes')
+  //     .where({ id })
+  //     .update({ title, content })
+  //     .then(id => {
+  //       res.status(200).json(id);
+  //     })
+  //     .catch((err) => res.status(500).json(err));
+  // });
+
   server.put('/api/notes/:id',   (req, res) => {
     const { id } = req.params;
     const { title, content } = req.body;
@@ -76,8 +110,11 @@ server.get('/', (req, res) => {
       .where({ id })
       .update({ title, content })
       .then(id => {
+        if( id === 0) {
+          return res.status(404).json({err:" The note with that ID is not found "});
+        } else {
         res.status(200).json(id);
-      })
+      }})
       .catch((err) => res.status(500).json(err));
   });
   
