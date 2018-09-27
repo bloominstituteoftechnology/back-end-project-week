92251af9bc1b2513ffcb01430563b44db818c9f9
codejoncode <jonathanjholloway@gmail.com>
2018-09-27 11:27:37 -0500

    Attempting deployment setting up knex file and env file.

--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,13 @@
 // Update with your config settings.
 require('dotenv').config();
-const localPg = {}
+const localPg = {
+  host: 'localhost',
+  database: 'lambda',
+  user: "jonathanH",
+  password: 'pass',
+  /*user: process.env.DB_USER
+    password: process.env.DB_pass   */
+};
 const dbConnection = process.env.DATABASE_URL || localPg;
 console.log(`\n\n ${dbConnection}\n\n`);
 
@@ -38,36 +45,21 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
+    client: 'pg',
+    connection: dbConnection,
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      tableName: 'knex_migrations',
+      directory: "./data/migrations",
+    },
+    seeds: {
+      directory: './data/seeds',
     }
   }
 
 };
 
-// production: {
-//   client: "pg",
-//   connection: dbConnection,
-//   pool: {
-//     min: 2, 
-//     max: 10
-//   }, 
-//   migrations: {
 
-//   },
-//   seeds: {
-
-//   }
-
-
-// }
