f84a4fcbb6ee85296b9e26f81702e066be4b7f19
sca4cs <shannon.atkinson.ca@gmail.com>
2018-09-27 13:49:38 -0400

    configure cors for netlify

--- server.js
+++ server.js
@@ -5,10 +5,14 @@ const cors = require('cors');
 const configureRoutes = require('./db/routes/notes');
 
 const server = express();
+const corsOptions = {
+  credentials: true,
+  origin: "https://nifty-almeida-2f92e2.netlify.com"
+};
 
 server.use(helmet());
 server.use(express.json());
-server.use(cors());
+server.use(cors(corsOptions));
 
 configureRoutes(server);
 
