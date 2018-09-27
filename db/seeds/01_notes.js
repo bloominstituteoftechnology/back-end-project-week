d37989049c48394e8f87fc22cbeecdf50a17c64a
mkerbleski <mkerbles@gmail.com>
2018-09-27 09:00:41 -0600

    change userId to 2 to match user seeds

--- db/seeds/01_notes.js
+++ db/seeds/01_notes.js
@@ -6,7 +6,7 @@ exports.seed = function(knex, Promise) {
       // Inserts seed entries
       return knex('notes').insert([
         { title: 'note #1', textBody: 'note #1 text body lorem ipsum blah blab bling', tags: 'tag1, tag1a', userId: 1},
-        { title: 'note #2', textBody: 'note #2 text body lorem ipsum blah blab bling', tags: 'tag2, tag2a', userId: 4},
+        { title: 'note #2', textBody: 'note #2 text body lorem ipsum blah blab bling', tags: 'tag2, tag2a', userId: 2},
         { title: 'note #3', textBody: 'note #3 text body lorem ipsum blah blab bling', tags: 'tag3a, tag3c', userId: 3}
       ]);
     });
