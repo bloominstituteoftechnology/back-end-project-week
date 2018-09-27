919164cf319556b80459088bcf31c8dbf53d1438
William <kwontaewoongah@gmail.com>
2018-09-27 15:02:32 -0700

    moved stuff

--- authenticate/middleware.js
+++ authenticate/middleware.js
@@ -1,6 +1,5 @@
 const jwt=require('jsonwebtoken');
 const secret='I see dead people.';
-const Joi=require('joi');
 
 function authenticate(req,res,next) {
     const token=req.headers.authorization;
@@ -16,21 +15,6 @@ function authenticate(req,res,next) {
         })
     } else {
         res.status(401).json({error:'No token provided must be provided on authorization header'});
-    }
-}
-function validateNewUserCred (req,res,next){
-    const newUser=req.body;
-    const schema={
-        username:Joi.string.min(3),
-        password:Joi.string.min(3)
-    }
-    const {error,value}=Joi.validate(newUser,schema);
-    
-    if (error===null) {
-        next();
-    }
-    else {
-        res.status(500).json(err);
     }
 }
 module.exports={
