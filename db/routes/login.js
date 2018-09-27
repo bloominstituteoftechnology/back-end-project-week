6a04b258265c8ae542c0f686bbba03d3dccaca76
sca4cs <shannon.atkinson.ca@gmail.com>
2018-09-27 18:55:33 -0400

    take out unnecessary code

--- db/routes/login.js
+++ db/routes/login.js
@@ -2,30 +2,6 @@ const db = require('../../dbConfig.js');
 const bcrypt = require('bcryptjs');
 const { generateToken } = require('./middleware.js');
 
-// function register (req, res) {
-//     const creds = req.body;
-//     const hash = bcrypt.hashSync(creds.password, 10);
-//     creds.password = hash;
-//     db('users')
-//       .insert(creds)
-//       .then(ids => {
-//         const id = ids[0];
-//         db('users')
-//           .where({ id })
-//           .first()
-//           .then(user => {
-//             const token = generateToken(user);
-//             res.status(201).json({ id: user.id, token });
-//           })
-//           .catch(err => {
-//             console.log(err);
-//             res.status(500).send(err)});
-//       })
-//       .catch(err => {
-//         console.log(err);
-//         res.status(500).send(err)});
-//   };
-
 function register (req, res) {
   const creds = req.body;
   const hash = bcrypt.hashSync(creds.password, 10);
@@ -38,14 +14,12 @@ function register (req, res) {
       .first()
       .then(user => {
         const token = generateToken(user);
-        res.status(201).json({ id: user.id, token });
+        res.status(201).json({ token });
       })
       .catch(err => {
-        console.log(err);
         res.status(500).send(err)});
     })
     .catch(err => {
-      console.log(err);
       res.status(500).send(err)});
 };
   
