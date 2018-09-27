1d7d299bb5519c66ed269a56915d2ded2fa98a43
David Pok <david.pok@gmail.com>
2018-09-27 10:48:29 -0700

    minor changes

--- db/migrations/20180925142618_nodes_table.js
+++ db/migrations/20180925142618_nodes_table.js
@@ -2,9 +2,9 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable("notes", function(table) {
     table.increments();
 
-    table.string("title", 128).notNullable();
+    table.string("title", 30).notNullable();
 
-    table.string("content").notNullable();
+    table.string("content", 128).notNullable();
   });
 };
 
