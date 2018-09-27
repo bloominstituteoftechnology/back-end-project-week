9a23caa54d5f7268b82fc7f01d194b397ba14611
Munsu <dark_specineff2002@yahoo.com>
2018-09-27 13:37:32 -0700

    mvp

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
-const db = require('./db/dbConfig.js');
+const db = require("./db/dbConfig.js");
 
 const server = express();
 
@@ -11,77 +11,87 @@ server.use(cors());
 // endpoints
 
 // make a new note
-server.post('/api/create', (req, res) => {
-    console.log(req.body);
-    const info = req.body;
-    db('notes')
-        .insert(info)
-        .then(ids => {
-        const id = ids[0];
+server.post("/api/create", (req, res) => {
+  console.log(req.body);
+  const info = req.body;
+  db("notes")
+    .insert(info)
+    .then(ids => {
+      const id = ids[0];
 
-        db('notes')
-            .where({ id })
-            .first()
-            .then(note => {
-                res.status(201).json({ id: note.id });
-            })
-            .catch(err => res.status(500).send(err));
+      db("notes")
+        .where({ id })
+        .first()
+        .then(note => {
+          res.status(201).json({ id: note.id });
         })
         .catch(err => res.status(500).send(err));
+    })
+    .catch(err => res.status(500).send(err));
 });
 
 // see list of notes
-server.get('/api/notes', (req, res) => {
-    db('notes')
-        .then(notes => {
-            res.status(200).json(notes);
-        })
-        .catch(err => {
-            res.status(500).json({ errMsg: 'Database could not retrieve info' })
-        });
+server.get("/api/notes", (req, res) => {
+  db("notes")
+    .then(notes => {
+      res.status(200).json(notes);
+    })
+    .catch(err => {
+      res.status(500).json({ errMsg: "Database could not retrieve info" });
+    });
 });
 
 // see specific note by id
-server.get('/api/notes/:id', (req, res) => {
-    const { id } = req.params;
-    db('notes')
-        .get(id)
-        .then(notes => {
-            if (notes) {
-                res.status(200).json(notes);
-            } else {
-                res.status(404).json({ errMsg: `The note with the id:${id} is not found` });
-            }
-        })
-        .catch(err => res.status(500).json({ errMsg: 'Database could not retrieve info' }));
+server.get("/api/notes/:id", (req, res) => {
+  const { id } = req.params;
+  db("notes")
+    .get(id)
+    .then(notes => {
+      if (notes) {
+        res.status(200).json(notes);
+      } else {
+        res
+          .status(404)
+          .json({ errMsg: `The note with the id:${id} is not found` });
+      }
+    })
+    .catch(err =>
+      res.status(500).json({ errMsg: "Database could not retrieve info" })
+    );
 });
 
 // edit note by id
-server.put('/api/edit/:id', (req, res) => {
-    db('notes')
-        .where({ id:req.params.id } )
-        .update(req.body)
-        .then((note) => {
-            res.status(201).json(note);
-        })
-        .catch((fail) => {
-            console.log(fail);
-            res.status(404).json({ message: "The note with the specified ID does not exist."});
-        });
-})
-
-// delete note by id
-server.delete('/api/delete/:id', (req, res) => {
-    db('notes')
-        .where({ id:req.params.id })
-        .delete()
-        .then((note) => {
-            res.status(201).json(note);
-        })
-        .catch((fail) => {
-            console.log(fail);
-            res.status(404).json({ message: "The note with the specified ID didn't delete."});
-        });
+server.put("/api/edit/:id", (req, res) => {
+  db("notes")
+    .where({ id: req.params.id })
+    .update(req.body)
+    .then(note => {
+      res.status(201).json(note);
+    })
+    .catch(fail => {
+      console.log(fail);
+      res
+        .status(404)
+        .json({ message: "The note with the specified ID does not exist." });
+    });
 });
 
-server.listen(5000, () => console.log('\nrunning on port 5000\n'));
+// delete note by id
+server.delete("/api/delete/:id", (req, res) => {
+  console.log(req.params.id, typeof req.params.id);
+  const { id } = req.params;
+  db("notes")
+    .where({ id })
+    .delete()
+    .then(note => {
+      res.status(201).json(note);
+    })
+    .catch(fail => {
+      console.log(fail);
+      res
+        .status(404)
+        .json({ message: "The note with the specified ID didn't delete." });
+    });
+});
+
+server.listen(5000, () => console.log("\nrunning on port 5000\n"));
