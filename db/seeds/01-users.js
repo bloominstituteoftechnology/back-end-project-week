7a2e55cb28ff434034b01d4dcc10fe103b32888f
kamalnrf <kamalnrf@hotmail.com>
2018-09-27 22:54:38 +0530

    fix

--- db/seeds/01-users.js
+++ db/seeds/01-users.js
@@ -1,10 +1,10 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('Users').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
+      return knex('Users').insert([
         {id: 1, username: 'ka', password: 'sd'},
         {id: 2, username: 'k', password: 'i'},
         {id: 3, username: 'a', password: 'b'}
