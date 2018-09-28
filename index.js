85e2bccb52de1976178747189169cea432478a99
William <kwontaewoongah@gmail.com>
2018-09-28 13:48:45 -0700

    changed port configuration

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+require('dotenv').config();
 const server=require('./server.js');
 
-const port=9000;
+const port=process.env.PORT||9000;
 
 server.listen(port,()=>console.log(`\n===Running on port ${port}===\n`));
