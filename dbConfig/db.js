71cb10c3afe07679031111303f0bc85a456d2c9f
William <kwontaewoongah@gmail.com>
2018-09-28 14:17:45 -0700

    changed dbConfig file

--- dbConfig/db.js
+++ dbConfig/db.js
@@ -1,4 +1,4 @@
-const dbEngine='development';
-const config=require('../knexfile.js')[dbEngine];
+const dbEngine = process.env.DB || 'development';
+const config = require('../knexfile.js')[dbEngine];
 
 module.exports=require('knex')(config);
