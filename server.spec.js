874b9d3e6900cd623901c7ffcb73690c7791b52c
William <kwontaewoongah@gmail.com>
2018-09-30 01:54:13 -0700

    added tests for authenticate routes

--- server.spec.js
+++ server.spec.js
@@ -43,7 +43,7 @@ describe('server.js',()=>{
             })
             expect(response.status).toEqual(500);
         })
-        it('should return an error with status code 404 when an object is missing a required parameter(s) for POST request',async()=>{
+        it('should return an error with status code 404 when an object is missing parameter(s) for POST request',async()=>{
             const response=await(request(server)).post('/note/create').send(
                 {title:'Chronicles of Narnia',
                 user_id:1
@@ -78,8 +78,10 @@ describe('server.js',()=>{
         it('should return an array of all the notes for a specific user_id',async()=>{
             const user_id=1;
             const response =await(request(server)).get(`/note/get/all/${user_id}`);
-            const expectedResponse=[{id:1,title:'Chronicles of M',textBody:'Written by M',tags:null,user_id:1},{id:2,title:'Chronicles of Z',
-            textBody:'Written by Z',tags:null,user_id:1},{id:3,title:'Chronicles of Za',textBody:'Written by Za',tags:null,user_id:1}]
+            const expectedResponse=
+            [{id:1,title:'Chronicles of M',textBody:'Written by M',tags:null,user_id:1},
+            {id:2,title:'Chronicles of Z',textBody:'Written by Z',tags:null,user_id:1},
+            {id:3,title:'Chronicles of Za',textBody:'Written by Za',tags:null,user_id:1}]
             expect(response.body).toEqual(expectedResponse);
         })
     })
@@ -131,7 +133,7 @@ describe('note edit by id route',()=>{
         .send({title:'Warrior 3',textBody:'Henchmen of Thor'});
         expect(response.body).toEqual(1);
     })
-    it('shoudl return a count of 0 if the note with specified id does not exist',async()=>{
+    it('should return a count of 0 if the note with specified id does not exist',async()=>{
         const note_id=5;
         const response=await(request(server)).put(`/note/edit/${note_id}`)
         .send({title:'Warrior 3',textBody:'Henchmen of Thor'});
@@ -158,6 +160,66 @@ describe('note delete by id route',()=>{
         const note_id=3;
         const response=await(request(server)).delete(`/note/delete/${note_id}`);
         expect(response.body).toEqual(0);
+    })
+})
+describe('/api/register route',()=>{
+    it('should return a 400 status code if passed in parameters do not meet required length of 3 characters',async()=>{
+        const response=await(request(server)).post('/api/register').send({username:'pi',password:'za'});
+        expect(response.status).toEqual(400);
+    })
+    it('should return a 201 status code if passed in parameters meet required length of 3 characters',async()=>{
+        const response=await(request(server)).post('/api/register').send({username:'piz',password:'zas'});
+        expect(response.status).toEqual(201);
+    })
+    it('should return a 500 status code if passed in object contains a username that is already in the table',async()=>{
+        const response=await(request(server)).post('/api/register').send({username:'piz',password:'zas'});
+        expect(response.status).toEqual(500);
+    })
+    it('should return a json object',async()=>{
+        const response=await(request(server)).post('/api/register').send({username:'sar',password:'din'});
+        expect(response.type).toEqual('application/json');
+    })
+})
+describe('api/login route',()=>{
+    it('should return a 200 status code if passed in parameters of username and password are same as that in table',async()=>{
+        const response=await(request(server)).post('/api/login').send({username:'sar',password:'din'});
+        expect(response.status).toEqual(200);
+    })
+    it('should return a 500 status code if passed in parameters of username and password do not meet any in table',async()=>{
+        const response=await(request(server)).post('/api/login').send({username:'stuff',password:'none'});
+        expect(response.status).toEqual(500);
+    })
+    it('should return a json object',async()=>{
+        const response=await(request(server)).post('/api/login').send({username:'sar',password:'din'});
+        expect(response.type).toEqual('application/json');
+    })
+    it('should return a 401 status code if password does not match but username does on table',async()=>{
+        const response=await(request(server)).post('/api/login').send({username:'sar',password:'dine'});
+        expect(response.status).toEqual(401);
+    })
+})
+describe('/api/notes route',()=>{
+    it('should return a 401 status code if passed in token is invalid.',async()=>{
+        const response=await(request(server)).get('/api/notes').send({headers:{authorization:'',id:3}});
+        expect(response.status).toEqual(401);
+    })
+    it('should return a JSON object',async()=>{
+        const response=await(request(server)).get('/api/notes').send({headers:{authorization:'',id:3}});
+        expect(response.type).toEqual('application/json');
+    })
+    it('should return an array containing notes that the logged in user has if any.',async()=>{
+        const response=await(request(server)).post('/api/login').send({username:'sar',password:'din'});
+        if (response){
+        async()=>{
+            const secondResponse=await(request(server)).get('/api/notes').send({
+            headers:{
+                Authorization:response.body.token,
+                id:response.body.user_id
+            }
+        })
+        expect(secondResponse.body).toEqual([]);
+    }
+    }
     })
 })
 })
