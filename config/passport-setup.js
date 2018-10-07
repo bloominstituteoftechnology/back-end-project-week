320ae89af2ffbc44b0fbaf90ee0462dd08a3881d
Sukhada Gholba <sukhadagholba@gmail.com>
2018-10-06 18:16:34 -0700

    updated

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,11 +1,12 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
+require('dotenv').load();
 
 passport.use(
  new GoogleStrategy({
  	callbackURL:'/api/users/google',
-	clientID: googleID,
-	clientSecret: googleSecret
+	clientID: process.env.clientID,
+	clientSecret: process.env.clientSecret
  
  },()=>{
  
