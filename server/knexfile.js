09c914336fdb028cebd3e722503054144cc8c3c6
verydecent <wnjhwng@gmail.com>
2018-10-29 11:20:56 -0700

    updates knexfile.js

--- server/knexfile.js
+++ server/knexfile.js
@@ -8,37 +8,12 @@ module.exports = {
       filename: './dev.sqlite3'
     }
   },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+  useNullAsDefault: true,
+  migrations: {
+    directory: './data/migrations'
   },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+  seeds: {
+    directory: './data/seeds'
   }
 
 };
