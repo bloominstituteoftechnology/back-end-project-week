29a015d3928501a373214369fc7b9a48d924bc00
Kevin Sooter <kevin.m.sooter@gmail.com>
2018-10-29 12:32:03 -0500

    index file set up

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const db = require('./data/db');
+const server = express();
+
+server.use(express.json());
+
+const port = 6000;
+server.listen(port, function() {
+  console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
+});
