c1bc400c1bdf93b65514d9071fe220c35068633b
Marious Davis <mlancelot88@gmail.com>
2018-10-29 17:04:56 -0500

    fixed params in seeds

--- data/seeds/01-seeds.js
+++ data/seeds/01-seeds.js
@@ -1,10 +1,10 @@
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex("table_name")
+  return knex("notes")
     .del()
     .then(function() {
       // Inserts seed entries
-      return knex("table_name").insert([
+      return knex("notes").insert([
         { id: 1, title: "Success", body: "Way to go champ!" },
         {
           id: 2,
