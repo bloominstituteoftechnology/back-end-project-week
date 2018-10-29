9709d6ddc03be99e77d462e1bad1d636832f0f05
Terrance Danckert <tbarnett2020@gmail.com>
2018-10-29 16:17:19 -0400

    Notes Migration

--- db/migrations/20181029151921_notes.js
+++ db/migrations/20181029151921_notes.js
@@ -1,8 +1,11 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('notes', function(tbl) {
+    tbl.increments();
+    tbl.string('title', 255).notNullable();
+    tbl.text('textBody').notNullable();
+  });
 };
-
-exports.down = function(knex, Promise) {
-  
+ exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('notes');
 };
