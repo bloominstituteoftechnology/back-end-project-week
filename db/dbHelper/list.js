8d63e73ca8e3cbe9e64a428dcea97877396fe2e1
Csillag61 <csillagok@gmail.com>
2018-10-29 16:39:36 -0400

    db helpers are done

---
+++ db/dbHelper/list.js
@@ -0,0 +1,26 @@
+const db = require('../dbConfig')
+
+module.exports = {
+    async getListsByUser(userID) {
+        return await db('List').where({ user_id: userID }).select()
+    },
+
+    async getLists() {
+        return await db('List').select()
+    },
+
+    async addList(listData) {
+        return await db('List').insert({
+            list_name: listData.name,
+            description: listData.description,
+            user_id: listData.userId
+        })
+    },
+
+    async addNotes(listId, notesId) {
+        return await db('Lists_Notes').insert({
+            list_id: listId,
+            notes_id: notesId
+        })
+    }
+}
