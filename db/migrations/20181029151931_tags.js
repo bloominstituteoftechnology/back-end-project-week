2848fa7416b1e4c8e66b45359a213732b1fcc641
Terrance Danckert <tbarnett2020@gmail.com>
2018-10-29 16:21:31 -0400

    Tag Migration Created

--- db/migrations/20181029151931_tags.js
+++ db/migrations/20181029151931_tags.js
@@ -1,8 +1,15 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('tags', function(tbl) {
+    tbl.increments();
+    tbl.string('tag_name', 255).notNullable();
+    tbl.integer('note_id')
+      .unsigned()
+      .notNullable()
+      .references('notes.id')
+      .onDelete('CASCADE');
+  });
 };
-
-exports.down = function(knex, Promise) {
-  
+ exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('tags');
 };
