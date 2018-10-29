819d381b17229fef5c9fcfcc9448d9efe9ff9a6d
Terrance Danckert <tbarnett2020@gmail.com>
2018-10-29 16:39:28 -0400

    Added Notes Seed

---
+++ db/seeds/notes.js
@@ -0,0 +1,40 @@
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('notes')
+    .truncate()
+    .then(function() {
+      // Inserts seed entries
+      return knex('notes').insert([
+        {
+          title: 'Joe Dirt',
+          textBody:
+            `Joe Dirt: So you're gonna tell me that you dont have no Black Cats, no Roman Candles, no Screaming Meemies? 
+
+            Kickin' Wing: No. 
+            
+            Joe Dirt: Oh come on man, you don't got no Ladyfingers, Buzz Bottles, Snicker Bombs, Church Burners, Finger Blasters, Gut Busters, Zippedy-doodas, Crap Flappers? 
+            
+            Kickin' Wing: No, I don't. 
+            
+            Joe Dirt: You're gonna stand there, owning a fireworks stand and tell me you dont have any Whistling Bungholes, no Spleen Splitters, Whisker Biscuits, Honkey Lighters, Hüsker Düs, Hüsker Don'ts, Cherry Bombs, Nipsy Dazers, with or without the Scooter Stick, or one single Whistling Kitty-chaser? 
+            
+            Kickin' Wing: No. 
+            
+            Joe Dirt: Why? 
+            
+            Kickin' Wing: Because Snakes and Sparklers are the only ones I like.`,
+        },
+        {
+          title: 'Gucci Mane',
+          textBody:
+            `Don't get lost in the sauce`,
+        },
+        {
+          title: 'Drake',
+          textBody:
+            'KiKi do you love me?',
+        },
+      ]);
+    });
+};
