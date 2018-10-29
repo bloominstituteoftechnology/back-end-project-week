4f1e5eb4bb88ebc07c5c74c6ac27aa0b913e8808
Carlos Vargas <cornielleandres@gmail.com>
2018-10-29 09:43:27 -0400

    Create GET /notes/ endpoint.

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -3,9 +3,12 @@ const noteDb	= require('../data/models/noteDb.js');
 
 const router	= express.Router();
 
-// sanity check
+// return list of all notes
 router.get('/', (req, res) => {
-	return res.status(200).json('Server is running.');
+	return noteDb
+		.get()
+		.then(notes => res.status(200).json(notes))
+		.catch(err => res.status(500).json({ error: `Server could not retrieve notes: ${ err }`}));
 });
 
 module.exports = router;
