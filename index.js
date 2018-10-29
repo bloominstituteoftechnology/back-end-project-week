925aa92807d036c9c381c1d252c5fb8f69f90187
Akshay Gadkari <akshay.gadkari@gmail.com>
2018-10-29 13:17:43 -0700

    create all endpoints

--- index.js
+++ index.js
@@ -13,9 +13,64 @@ server.get('/', (req, res) => {
     res.send('it\'s alive');
 });
 
+server.get('/api/notes', (req, res) => {
+    db('notes')
+	.then(notes => {
+	    res.status(200).json(notes);
+	})
+	.catch(err => res.status(500).json(err));
+});
+
+server.get('/api/notes/:id', async (req, res) => {
+    const {id} = req.params;
+    const note = await db('notes')
+	  .wehre({id})
+	  .first();
+    if(note) {
+	res.status(200).json(note);
+    } else {
+	res.status(404).json({message: 'zoo not found'});
+    }
+});
+
+server.post('/api/notes', (req, res) => {
+    const note = req.body;
+    db.insert(note)
+	.into('notes')
+	.then(ids => {
+	    res.status(201).json(ids[0]);
+	})
+	.catch(err => res.status(500).json(err));
+});
+
+server.delete('/api/notes/:id', (req, res) => {
+    const {id} = req.params;
+    db('notes')
+	.where({id})
+	.del()
+	.then(count => {
+	    if (!count || count < 1) {
+		res.status(404).json({message: 'No records found to remove'});
+	    } else {
+		res.status(200).json(count);
+	    }
+	})
+	.catch(err => res.status(500).json(err));
+});
+
+server.put('/api/notes/:id', (req, res) => {
+    const {id} = req.params;
+    const changes = req.body;
+    db('notes')
+	.where({id})
+	.update(changes)
+	.then(count => {
+	    res.status(200).json(count);
+	})
+	.catch(err => res.status(500).json(err));
+});
 
 const port = 3300;
 server.listen(port, function() {
     console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
 });
-
