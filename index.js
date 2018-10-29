dbf99079b1524dc8305cb777916ec9a0435d5966
Kevin Sooter <kevin.m.sooter@gmail.com>
2018-10-29 14:15:50 -0500

    Added POST route

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const db = require('./data/db');
 const server = express();
-
 server.use(express.json());
 
 server.get('/notes', async (req, res) => {
@@ -13,6 +12,26 @@ server.get('/notes', async (req, res) => {
     res
       .status(500)
       .send({ error: 'Unable to retrieve notes. Please try again later.' });
+  }
+});
+
+server.post('/notes', async (req, res) => {
+  let note = req.body;
+  if (!'title' in note) {
+    res.status(400).send({ error: 'Your note must have a title.' });
+  } else if (note.title.length > 40) {
+    res
+      .status(400)
+      .send({ error: 'Titles must be shorter than 40 characters.' });
+  }
+  try {
+    await db.insert(note).into('notes');
+    res.status(201).json({ msg: 'Your note was successfully saved.' });
+  } catch (err) {
+    console.log('/notes POST error:', err);
+    res
+      .status(500)
+      .send({ error: 'Your note could not be saved. Please try again later.' });
   }
 });
 
