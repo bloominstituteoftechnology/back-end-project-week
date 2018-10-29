0aa5376b455063d641a863568938f5b2ee70da94
verydecent <wnjhwng@gmail.com>
2018-10-29 13:07:36 -0700

    adds Get note/notes endpoints

--- server/route/handlers.js
+++ server/route/handlers.js
@@ -6,7 +6,21 @@ router.get('/', (req, res) => {
   helpers
     .getNotes()
       .then(notes => {
+        console.log(notes);
         res.status(200).json(notes);
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      });
+});
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  helpers
+    .getNote(id)
+      .then(id => {
+        console.log(id);
+        res.status(200).json({ id });
       })
       .catch(err => {
         res.status(500).json(err);
--- server/route/helpers.js
+++ server/route/helpers.js
@@ -3,12 +3,17 @@ const knexConfig = require('../knexfile.js');
 const db = knex(knexConfig.development);
 
 
-
 getNotes = () => {
   return db('note_table');
 };
 
+getNote = (id) => {
+  return db('note_table')
+            .where({ note_id: id})
+}
+
 // export 
 module.exports = {
-  getNotes
+  getNotes,
+  getNote
 };
