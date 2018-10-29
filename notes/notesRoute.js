6a014faf16d05df82fb8f32db007393c06967f98
m-j-hacker <mjhacker@gmail.com>
2018-10-29 12:36:28 -0500

    Added get all notes endpoint

--- notes/notesRoute.js
+++ notes/notesRoute.js
@@ -1,5 +1,16 @@
 const express = require('express');
 
+const notes = require('./notesModel.js');
+
 const router = express.Router();
+
+router.get('/', (req, res) => {
+    notes
+        .find()
+        .then(notes => {
+            res.status(200).json(notes);
+        })
+        .catch(err => res.status(500).json(err));
+});
 
 module.exports = router;
