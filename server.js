19017af21f7cc80d80ec024f5ac741d297ed2d74
Marious Davis <mlancelot88@gmail.com>
2018-10-29 14:44:16 -0500

    established server.js for connection to front end of project

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+
+const configureRoutes = require("./config/routes");
+
+const server = express();
+const corsOptions = {
+  // If you're moving onto the stretch problem you'll need to set this obj with the appropriate fields
+  // ensure that your client's URL/Port can achieve a Handshake
+  // then pass this object to the cors() function
+};
+
+server.use(express.json());
+server.use(cors());
+
+configureRoutes(server);
+
+module.exports = {
+  server
+};
