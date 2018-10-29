b64d54ad55342cb7780e889c572804527ab9e8d1
Marious Davis <mlancelot88@gmail.com>
2018-10-29 14:49:56 -0500

    minor syntax error fixed

--- config/routes.js
+++ config/routes.js
@@ -5,10 +5,12 @@ const db = require("../data/dbConfig");
 const { authenticate, generateToken } = require("./authentication");
 
 // ---- API Endpoints for Testing ----
-server.get("/api/all", all);
-server.post("/api/create", create);
-sever.get("/api/view/:id", view);
-sever.put("/api/edit/:id", edit);
+module.exports = server => {
+  server.get("/api/all", all);
+  server.post("/api/create", create);
+  sever.get("/api/view/:id", view);
+  sever.put("/api/edit/:id", edit);
+};
 
 // ---- Retrieve ALL Notes. GET Endpoint ----
 server.get("/api/all", (req, res) => {
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-const configureRoutes = require("./config/routes");
+const configureRoutes = require("./config/routes.js");
 
 const server = express();
 const corsOptions = {
