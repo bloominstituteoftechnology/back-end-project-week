55f81329310257332224d8e7a26be2691dd6ae5f
m-j-hacker <mjhacker@gmail.com>
2018-10-29 12:32:21 -0500

    Created notesRoute for /api/notes endpoints

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const helmet = require('helmet');
 
+const notesRoute = require('./notes/notesRoute.js');
+
 const server = express();
 
 server.use(helmet());
@@ -10,6 +12,8 @@ server.use(express.json());
 server.get('/', (req, res) => {
     res.send("It's allliiiiiive!!");
 });
+
+server.use('/api/notes', notesRoute);
 
 const port = 3333;
 server.listen(port, () => console.log(`***API running on ${port}***`));
