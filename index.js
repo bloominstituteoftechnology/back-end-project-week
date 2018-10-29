9df6c0bfa9c6e958decc334070f3c41f98191263
lyang9 <lyang9@buffalo.edu>
2018-10-29 13:47:36 -0400

    Pulled in dependencies, knexConfig

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const cors = require('cors');
+const knex = require('knex');
+
+const knexConfig = require('./knexfile');
+const db = knex(knexConfig.development);
+
+const server = express();
+
+server.use(express.json());
+server.use(cors());
