7f0af1a2baa015918b31f5cf8668c6e9c2034337
Kevin Sooter <kevin.m.sooter@gmail.com>
2018-10-29 14:03:58 -0500

    Added GET route

--- index.js
+++ index.js
@@ -4,6 +4,18 @@ const server = express();
 
 server.use(express.json());
 
+server.get('/notes', async (req, res) => {
+  try {
+    const notes = await db.select().from('notes');
+    res.status(200).json(notes);
+  } catch (err) {
+    console.log('/notes GET error:', err);
+    res
+      .status(500)
+      .send({ error: 'Unable to retrieve notes. Please try again later.' });
+  }
+});
+
 const port = 6000;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
