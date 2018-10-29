add8eb43086c645cc450cfc737bf9a46d43b3764
lyang9 <lyang9@buffalo.edu>
2018-10-29 14:17:53 -0400

    server.get display a list of notes

--- index.js
+++ index.js
@@ -15,6 +15,16 @@ server.get('/', (req, res) => {
   res.send('It is working!');
 });
 
+server.get('/api/notes', (req, res) => {
+  db('notes')
+    .then(notes => {
+      res.status(200).json(notes);
+    })
+    .catch(err => {
+      res.status(500).json({ error: 'The notes information could not be retrieved.', err });
+    })
+});
+
 // listening port
 const port = 5000;
 server.listen(port, function() {
