ee17f7c8b20fd9c7d1a93afa814ab0898a9e3bd2
m-j-hacker <mjhacker@gmail.com>
2018-10-29 12:23:42 -0500

    Ran knex migration to create notes table

--- data/migrations/20181029120010_01_notes_table.js
+++ data/migrations/20181029120010_01_notes_table.js
@@ -1,8 +1,16 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('notes', function(tbl) {
+      tbl.increments();
+
+      tbl.string('title').notNullable();
+
+      tbl.unique('title');
+
+      tbl.string('contents');
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+    return knex.schema.dropTableIfExists('notes');
 };
