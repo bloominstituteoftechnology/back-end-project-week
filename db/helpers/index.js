52b609a93fab47b0695d3b661d45d24319c76805
Terrance Danckert <tbarnett2020@gmail.com>
2018-10-30 14:14:53 -0400

    GET Single Note

--- db/helpers/index.js
+++ db/helpers/index.js
@@ -6,12 +6,12 @@ module.exports = {
    return db('notes')
    .select('note_id as id', 'title', 'textBody', 'tag_name as tags')
      .join('tags', 'notes.id', 'tags.note_id');
- },
+  },
  addNote(note) {
   return db('notes')
     .insert(note)
     .then(([id]) => id);
-},
+  },
 
 addNoteWithTags(note, tags) {
   let noteID = null;
@@ -21,6 +21,8 @@ addNoteWithTags(note, tags) {
       return this.addTags(tags, id);
     })
     .then(() => noteID);
-},
-
+  },
+getOneNote(id) {
+  return this.getAllNotes().where('notes.id', id);
+  },
 };
--- routes/notes.js
+++ routes/notes.js
@@ -2,22 +2,39 @@
 const router = require('express').Router();
 const helpers = require('../db/helpers');
 
+// *****  ***** //
+function tagReduce(acc, next) {
+  if (acc[next.id]) {
+    acc[next.id].tags = [].concat(acc[next.id].tags).concat(next.tags);
+  } else {
+    acc[next.id] = next;
+    acc[next.id].tags = next.tags ? [next.tags] : [];
+  }
+  return acc;
+}
+
+// #################### GET #################### //
+
 // ******* GET ******* //
  router.route('/').get(async (req, res, next) => {
   //  accumulator can be an intimidating term, so you can think of it like the current state of the array as we're applying the logic on the callback's invocations/ Google Fu
   let notes = await helpers.getAllNotes();
-  notes = notes.reduce((ac, next) => {
-    if (ac[next.id]) {
-      ac[next.id].tags = [].concat(ac[next.id].tags).concat(next.tags);
-    } else {
-      ac[next.id] = next;
-      ac[next.id].tags = next.tags ? [next.tags] : [];
-    }
-    return ac;
-  }, {});
+  notes = notes.reduce(tagReduce, {});
   res.status(200).json(Object.values(notes));
 });
 
+// ******* GET Single Note ******* //
+router.get('/:id', async (req, res, next) => {
+  let note = await helpers.getOneNote(Number(req.params.id));
+  note = Object.values(note.reduce(tagReduce, {}))[0];
+  if (!note)
+    return res.json({
+      error: `I'm not calling you a liar but....that ID doesn't exist`,
+    });
+  res.status(200).json(note);
+});
+
+// #################### POST #################### //
 router.post('/', async (req, res, next) => {
   const { title, textBody, tags } = req.body;
    if (!title || !textBody)
