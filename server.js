ba65f09dd63818ef279ca033040c5b63858718f5
Evan Carlstrom <evan.carlstrom@gmail.com>
2018-10-30 14:15:17 -0400

    added cors to server.js

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const db = require('./data/db');
+const cors = require('cors');
 
 const server = express();
 server.use(express.json());
+server.use(cors());
 
 // server operations
 
