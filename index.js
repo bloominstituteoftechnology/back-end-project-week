1f89812be38e4fa978d1149cf8a1095bd981d672
Keiran <keirankozlowski@gmail.com>
2018-10-30 13:33:28 -0400

    POST request works.

--- index.js
+++ index.js
@@ -12,3 +12,29 @@ server.use(helmet());
 
 const port = 8000;
 server.listen(port, () => console.log(`API running on port ${port}`));
+
+// sanity check
+server.get('/', (request, response) => {
+    response.send('Server initialized.');
+});
+
+// POST request
+server.post('/api/notes', (request, response) => {
+    const title = request.body.title;
+    const content = request.body.content;
+    const newNote = { title, content };
+
+    db
+        .insert(newNote)
+        .into('notes')
+        .then(ids => {
+            return response
+                .status(201)
+                .json(ids[0]);
+        })
+        .catch(() => {
+            return response
+                .status(500)
+                .json({ Error: "There was an error while saving the note" })
+        });
+});
