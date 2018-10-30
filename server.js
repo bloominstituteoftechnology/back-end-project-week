b0118de2230443d69edbdafe79d41378d9dd51b0
Marious Davis <mlancelot88@gmail.com>
2018-10-30 15:40:59 -0500

    generated corsOptions. enabled whitelist for front-end to read back-end

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 const express = require("express");
 const cors = require("cors");
-const path = require("path");
-
 const configureRoutes = require("./config/routes.js");
 
 const server = express();
+
+var whitelist = [
+  "http://localhost:5500/api/all",
+  "http://localhost:5500/api/create",
+  "http://localhost:5500/api/view/:id",
+  "http://localhost:5500/api/edit/:id",
+  "http://localhost:5500/api/delete/:id"
+];
 const corsOptions = {
-  // If you're moving onto the stretch problem you'll need to set this obj with the appropriate fields
-  // ensure that your client's URL/Port can achieve a Handshake
-  // then pass this object to the cors() function
+  origin: function(origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  }
 };
 
 server.use(express.json());
@@ -17,5 +27,6 @@ server.use(cors());
 configureRoutes(server);
 
 module.exports = {
-  server
+  server,
+  corsOptions
 };
