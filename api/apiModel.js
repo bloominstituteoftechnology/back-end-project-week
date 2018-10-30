598bfa641a354dffff78128a089a46a214d16b29
Tristen Neu <tristenneu@Tristens-iMac.local>
2018-10-30 13:58:37 -0500

    Changed Database

--- api/apiModel.js
+++ api/apiModel.js
@@ -1,6 +1,6 @@
-const knex = require('knex');
-const knexConfig = require('../knexfile');
-const db = knex(knexConfig.development);
+const environment = process.env.ENVIRONMENT || 'development';
+const knexConfig = require('../knexfile')[environment];
+const db = require('knex')(knexConfig);
 
 module.exports = {
     find,
--- knexfile.js
+++ knexfile.js
@@ -33,18 +33,16 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    ssl: true,
+    useNullAsDefault: true,
     migrations: {
-      tableName: 'knex_migrations'
+      tableName: 'knex_migrations',
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
     }
   }
 
