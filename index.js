240d32ca775381635d8b086127082fd240fb7e6d
duckets615 <33557104+duckets615@users.noreply.github.com>
2018-10-30 10:18:19 -0400

    index requires setup, migration, server

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const helmet = require('helmet');
+const knex = require('knex')
+const cors = require('cors')
+const server = express();
+const bcrypt = require('bcryptjs');
+const dbConfig = require('./knexfile')
+const session = require('express-session');
+ const db = knex(dbConfig.development);
+ server.use(session(sessionConfig))
+server.use(express.json());
+server.use(helmet());
+server.use(cors())
+
+
+ const port = 8000;
+server.listen(port, function () {
+    console.log(`\n=*= Backend Project Week Rolling On ${port} =*=\n`);
+});
