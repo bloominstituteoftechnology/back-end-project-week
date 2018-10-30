43320210aa0d86b2e64ba5a0163e8b860bb3d76c
Evan Carlstrom <evan.carlstrom@gmail.com>
2018-10-30 01:41:00 -0400

    added basic GET request for all notes, currently failing as expected

--- index.js
+++ index.js
@@ -4,6 +4,19 @@ const db = require('./data/db');
 const server = express();
 server.use(express.json());
 
+// simple GET request
+
+server.get('/api/notes', (req, res) => {
+  db('notes')
+    .then(notes => {
+      res.status(200).json(notes);
+    })
+    .catch(err => {
+      console.log(err.message);
+      res.status(500).json({ error: "Unable to retrieve notes at this time." });
+    });
+});
+
 // server instantiation
 
 const port = 8000;
