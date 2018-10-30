0b7356bb4ba736183a74bf3d7106bf7c75155ea5
Terrance Danckert <tbarnett2020@gmail.com>
2018-10-30 16:37:02 -0400

    Fixed Back-End Errors with Routes

--- routes/notes.js
+++ routes/notes.js
@@ -4,30 +4,30 @@ const helpers = require('../db/helpers');
 const _ = require('lodash');
 
 // *****  ***** //
-function tagReduce(acc, next) {
-  if (acc[next.id]) {
-    acc[next.id].tags = [].concat(acc[next.id].tags).concat(next.tags);
+function reducerTag(ac, next) {
+  //  accumulator can be an intimidating term, so you can think of it like the current state of the array as we're applying the logic on the callback's invocations/ Google Fu
+  if (ac[next.id]) {
+    ac[next.id].tags = [].concat(ac[next.id].tags).concat(next.tags);
   } else {
-    acc[next.id] = next;
-    acc[next.id].tags = next.tags ? [next.tags] : [];
+    ac[next.id] = next;
+    ac[next.id].tags = next.tags ? [next.tags] : [];
   }
-  return acc;
+  return ac;
 }
 
 // #################### GET #################### //
 
 // ******* GET ******* //
- router.route('/').get(async (req, res, next) => {
-  //  accumulator can be an intimidating term, so you can think of it like the current state of the array as we're applying the logic on the callback's invocations/ Google Fu
+router.get('/', async (req, res, next) => {
   let notes = await helpers.getAllNotes();
-  notes = notes.reduce(tagReduce, {});
+  notes = notes.reduce(reducerTag, {});
   res.status(200).json(Object.values(notes));
 });
 
 // ******* GET Single Note ******* //
 router.get('/:id', async (req, res, next) => {
   let note = await helpers.getOneNote(Number(req.params.id));
-  note = Object.values(note.reduce(tagReduce, {}))[0];
+  note = Object.values(note.reduce(reducerTag, {}))[0];
   if (!note)
     return res.json({
       Error: `I'm not calling you a liar but....that ID doesn't exist`,
@@ -38,9 +38,9 @@ router.get('/:id', async (req, res, next) => {
 // #################### POST #################### //
 router.post('/', async (req, res, next) => {
   const { title, content, tags } = req.body;
-   if (!title || !content)
-    return res.json({ Error: 'Stop forgetting things' });
-   let id = null;
+  if (!title || !content)
+  return res.json({ Error: 'Stop forgetting things' });
+  let id = null;
   if (_.isArray(tags)) {
     id = await helpers.addNoteWithTags({ title, content }, tags);
   } else {
@@ -53,20 +53,20 @@ router.post('/', async (req, res, next) => {
 
 router.put('/:id', async (req, res) => {
   let { title, content, tags } = req.body;
-   if (!title && !content && !Array.isArray(tags))
-    return res.json({ Error: `So you think I CAN JUST WORK WITH ONLY ONE THING FILLED OUT?!! THINK AGAIN...redo it please` });
-   if (_.isArray(tags)) {
+  if (!title && !content && !Array.isArray(tags))
+  return res.json({ Error: `So you think I CAN JUST WORK WITH ONLY ONE THING FILLED OUT?!! THINK AGAIN...redo it please` });
+  if (_.isArray(tags)) {
     try {
       let ids = await helpers.updateTags(tags, Number(req.params.id));
     } catch (err) {
       return res.json({ Error: `I'm not calling you a liar but....that ID doesn't exist` });
     }
   }
-   let objUpdater = { title, content };
-    objUpdater = _.omitBy(objUpdater, _.isUndefined);
-    let count = await helpers.updateNote(objUpdater, Number(req.params.id));
-      if (count === 0) return res.json({ Error: `I'm not calling you a liar but....that ID doesn't exist` });
-    res.status(200)
+  let objUpdater = { title, content };
+  objUpdater = _.omitBy(objUpdater, _.isUndefined);
+  let count = await helpers.updateNote(objUpdater, Number(req.params.id));
+  if (count === 0) return res.json({ Error: `I'm not calling you a liar but....that ID doesn't exist` });
+  res.status(200)
     .json({ Message: `The note that had the id of ${req.params.id} has been updated...peacefully` });
 });
 
@@ -78,7 +78,7 @@ router.delete('/:id', async (req, res, next) => {
     return res.json({
       Error: `I'm not calling you a liar but....that ID doesn't exist`,
     });
-   res.status(200).json({
+  res.status(200).json({
     Message: `The note that had the id of ${req.params.id} has been destroyed...peacefully`,
   });
 });
