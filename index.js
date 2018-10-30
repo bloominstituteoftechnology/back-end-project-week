164c3bfca81c40db091f633a6ebb0a02fe51f66a
Evan Carlstrom <evan.carlstrom@gmail.com>
2018-10-30 02:04:32 -0400

    added GET request for individual note via ID check

--- index.js
+++ index.js
@@ -17,6 +17,27 @@ server.get('/api/notes', (req, res) => {
     });
 });
 
+// GET request for an individual note
+
+server.get('/api/notes/:id', (req, res) => {
+  const { id } = req.params;
+
+  db('notes')
+    .where('id', id)
+    .then(check => {
+      if (!check.length) {
+        res
+          .status(401)
+          .json({ error: 'Invalid ID! Please try again.' });
+      }
+      res.status(200).json(check)
+    })
+    .catch(err => {
+      console.log(err.message);
+      res.status(500).json({ error: 'The note could not be retrieved.' });
+    });
+});
+
 // server instantiation
 
 const port = 8000;
