9e72eb6d7da1567b755c276940391ef2cdfc2edb
Evan Carlstrom <evan.carlstrom@gmail.com>
2018-10-30 01:51:03 -0400

    created a basic seed file, GET request now displays data correctly

--- data/seeds/notes_seed.js
+++ data/seeds/notes_seed.js
@@ -1,13 +1,16 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('notes')
+    .truncate()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('notes').insert([
+        {id: 1, title: 'Test Note 1', content: 'Test note 1.'},
+        {id: 2, title: 'Test Note 2', content: 'Test note 2.'},
+        {id: 3, title: 'Test Note 3', content: 'Test note 3.'},
+        {id: 4, title: 'Test Note 4', content: 'Test note 4.'},
+        {id: 5, title: 'Very Creative Names So Far!', content: 'COMBO BREAKER'}
       ]);
     });
 };
