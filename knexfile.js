a75c3d7dfa0d4f7cd882832c047afde950e35b95
Csillag61 <csillagok@gmail.com>
2018-10-31 11:50:34 -0400

    schema

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,6 @@
 // Update with your config settings.
 
 module.exports = {
-  
   development: {
     client: "sqlite3",
     connection: {
@@ -19,8 +18,13 @@ module.exports = {
   production: {
     client: "pg",
     debug: true,
-    connection:
-      process.env.DATABASE_URL ,
+    searchPath: "database_schema",
+    connection: {
+      database: process.env.DATABASE_NAME,
+      user: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      url: process.env.DATABASE_URL
+    },
     migrations: {
       directory: "./db/migrations",
       tableName: "dbMigrations"
@@ -30,5 +34,4 @@ module.exports = {
     },
     ssl: true
   }
-
 };
