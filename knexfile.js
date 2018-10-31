eac61e09306b666e59c9280c7f6d2c47293784fb
Huthman King <Huff@Huthmans-MacBook-Pro.local>
2018-10-31 17:19:56 -0400

    Knex tweak

--- knexfile.js
+++ knexfile.js
@@ -10,21 +10,15 @@ module.exports = {
     },
     seeds: {
       directory: './data/seeds'
-    }
-  },
-  production: {
-    client: 'pg',
-    connection: process.env.DATABASE_URL,
-    ssl: true,
-    pool: {
-      min: 2,
-      max: 10
     },
-    migrations: {
-      directory: './data/migrations'
-    },
-    seeds: {
-      directory: './data/seeds'
+    production: {
+      client: 'pg',
+      connection: dbCOnnection,
+      ssl: true,
+      pool: {
+        min: 2,
+        max: 10
+      }
     }
   }
 };
