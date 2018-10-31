4d977510859448ec45fcdcb12d51a7c727b85902
duckets615 <33557104+duckets615@users.noreply.github.com>
2018-10-31 17:57:48 -0400

    create note

--- notes/notesRouters.js
+++ notes/notesRouters.js
@@ -25,7 +25,7 @@ router.put('/api/edit/:id', (req, res) => {
 })
 
 // DELETE delete existing note
-router.delete('/api/view/:id/delete', (req, res) => {
+router.delete('/api/:id/delete', (req, res) => {
     const {id} = req.params
 
     db('notes').where({id}).del().then(note => {
@@ -36,4 +36,15 @@ router.delete('/api/view/:id/delete', (req, res) => {
     })
 })
 
+// POST create note
+router.post('/api/create', (req, res) => {
+    const note = req.body
+
+    db.insert(note).into('notes').then(note => {
+        res.status(200).json({message: 'note created'})
+    }).catch(err => {
+        console.log(err)
+        res.status(500).json({message: 'Could not create note'})
+    })
+})
 module.exports = router;
