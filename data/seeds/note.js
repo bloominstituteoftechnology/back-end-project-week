cb98f96618dafa3a840e11ea20d76ac62a01a402
Kevin Sooter <kevin.m.sooter@gmail.com>
2018-10-31 18:48:46 -0500

    Fixed seeds file and ran migrations

--- data/seeds/note.js
+++ data/seeds/note.js
@@ -3,7 +3,7 @@ exports.seed = function(knex, Promise) {
     .del()
     .then(function() {
       return knex('notes').insert([
-        { id: 1, title: 'Here is a note', text: 'Try me' }
+        { id: 1, title: 'Here is a note', content: 'Try me' }
       ]);
     });
 };
