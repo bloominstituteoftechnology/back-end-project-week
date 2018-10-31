82756dbf24e9e01664fd791f625a57cbd2bec680
Huthman King <Huff@Huthmans-MacBook-Pro.local>
2018-10-31 17:00:01 -0400

    Updated Knexfile

--- knexfile.js
+++ knexfile.js
@@ -11,5 +11,20 @@ module.exports = {
     seeds: {
       directory: './data/seeds'
     }
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    ssl: true,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    }
   }
 };
