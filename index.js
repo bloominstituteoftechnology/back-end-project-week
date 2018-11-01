e2cea103d25dca2a99973d915869f3b804bfbd0c
duckets615 <33557104+duckets615@users.noreply.github.com>
2018-11-01 04:51:34 -0400

    view all notes

--- index.js
+++ index.js
@@ -55,7 +55,11 @@ server.post('/api/create', (req, res) => {
         res.status(500).json({message: 'Could not create note'})
     })
 })
-
+// GET view all notes
+server.get('/api/notes', (req, res) => {
+    db('notes').then(notes => res.status(200).json(notes))
+    .catch(err => res.status(500).json(err))
+});
 
  const port = 8000;
 server.listen(port, function () {
