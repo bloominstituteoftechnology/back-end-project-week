debff864abc7c01488666587be3fe86cc74a247b
Kevin Sooter <kevin.m.sooter@gmail.com>
2018-11-01 11:28:02 -0500

    Added another note

--- data/seeds/note.js
+++ data/seeds/note.js
@@ -3,7 +3,8 @@ exports.seed = function(knex, Promise) {
     .del()
     .then(function() {
       return knex('notes').insert([
-        { id: 1, title: 'Here is a note', content: 'Try me' }
+        { id: 1, title: 'Here is a note', content: 'Try me' },
+        { id: 2, title: 'Hey', content: 'Whats up?' }
       ]);
     });
 };
