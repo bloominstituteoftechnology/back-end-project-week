78384e2c757b9de69192fc0b89278ad921a3a253
Tsai Huang <tsaihuangsd@gmail.com>
2018-11-01 00:58:50 -0700

    remove migration tableName to see if it gets rid of heroku error code 42P01

--- knexfile.js
+++ knexfile.js
@@ -30,7 +30,7 @@ module.exports = {
       max: 10,
     },
     migrations: {
-      tableName: 'knex_migrations',
+      // tableName: 'knex_migrations',
       directory: './data/migrations'
     },
     seeds: {
