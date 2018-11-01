a4bd220785debc59f55e1b08fae9d061de267e29
Csillag61 <csillagok@gmail.com>
2018-11-01 14:15:41 -0400

    register fix

--- spec/auth.spec.js
+++ spec/auth.spec.js
@@ -1,21 +1,21 @@
-// const request = require("supertest");
-// const server = require("../index");
+const request = require("supertest");
+const server = require("../index");
 
-// describe("Auth Route", () => {
-//   describe("POST /api/auth/register", () => {
-//     let res;
-//     beforeAll(async () => {
-//       res = await request(server)
-//         .post("/api/auth/register")
-//         .send({
-//           username: `csillag${Math.random()}`,
-//           password: "matek"
-//         })
-//         });
+describe("Auth Route", () => {
+  describe("POST /api/auth/register", () => {
+    let res;
+    beforeAll(async () => {
+      res = await request(server)
+        .post("/api/auth/register")
+        .send({
+          username: `csillag${Math.random()}`,
+          password: "matek"
+        })
+        });
 
-//       it("should check for response status", () => {
-//         expect(res.status).toBe(200);
-//       });
-//     });
-//   });
+      it("should check for response status", () => {
+        expect(res.status).toBe(200);
+      });
+    });
+  });
 
