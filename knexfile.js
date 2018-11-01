b3d74a361b97a8255137954fb063ad69bd7bd858
mparedes003 <michelle.g.paredes@gmail.com>
2018-11-01 09:18:13 -0700

    Made db dynamic by adding code in knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,11 @@
-// Update with your config settings.
+require('dotenv').config();
+const localPg = {
+  host: 'localhost',
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+};
+const dbConnection = process.env.DATABASE_URL || localPg;
 
 module.exports = {
   development: {
@@ -6,12 +13,29 @@ module.exports = {
     connection: {
       filename: './data/db.sqlite3',
     },
-    useNullAsDefault: true,
     migrations: {
       directory: './data/migrations',
     },
     seeds: {
       directory: './data/seeds',
     },
+    useNullAsDefault: true,
+  }, 
+  
+    // adding this for deployment
+  production: {
+    client: 'pg', // pg dependency in use
+    connection: dbConnection, // could be an object or a string
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: './db/migrations',
+    },
+    seeds: {
+      directory: './db/seeds',
+    },  
   },
 };			
