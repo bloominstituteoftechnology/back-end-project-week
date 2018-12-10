33d0c8e29c37f30d82f04195c3b3308cf174098e
Jake Thomas <jmaxt12@gmail.com>
2018-12-10 12:29:51 -0700

    CRUD done

--- index.js
+++ index.js
@@ -11,12 +11,80 @@ server.use(express.json());
 
 
 
+//------------Notes----------------
 
+// GET
 
+server.get("/api/notes", (req, res) => {
+    db("notes")
+      .then(notes => {
+          res.status(200).json(notes)
+      })
+      .catch(err => {
+          res.status(500).json(err)
+      });
+  });
 
+  
+  // GET BY ID
+  
+  server.get("/api/notes/:id", (req, res) => {
+    const { id } = req.params;
+    db("notes")
+      .where({ id: id})
+      .then(notes => {
+          res.status(200).json(notes)
+      })
+      .catch(err => {
+          res.status(500).json(err)
+      });
+  });
+  
 
+// POST
 
+server.post("/api/notes", (req, res) => {
+    const notes = req.body;
 
+    db("notes")
+      .insert(notes)
+      .into("notes")
+      .then(ids => {
+          res.status(201).json({ids})
+      })
+      .catch(err => {
+          res.status(500).json({ error: err })
+      });
+  });
+
+  // UPDATE
+
+server.put("/api/notes/:id", (req, res) => {
+    const changes = req.body;
+    const {id} = req.params;
+    db("notes")
+      .where({ id: id })
+      .update(changes)
+      .then(count => {
+          res.status(200).json(count)
+      })
+      .catch(err => res.status(500).json({ error: err }));
+  });
+
+// DELETE
+
+server.delete("/api/notes/:id", (req, res) => {
+    const { id } = req.params;
+    db("notes")
+      .where({id:id})
+      .del()
+      .then(ids => {
+          res.status(200).json(ids)
+      })
+      .catch(err => {
+          res.status(500).json({ error: err })
+      });
+  });
 
 
 
--- knexfile.js
+++ knexfile.js
@@ -2,7 +2,7 @@
 module.exports = {
   development: {
     client: 'sqlite3',
-    connection: { filename: './data/auth.sqlite3' }, 
+    connection: { filename: './data/notes.sqlite3' }, 
     useNullAsDefault: true, 
     migrations: {
       directory: './data/migrations',
