0d62130ed7957aa652f6fdf2cfb9350a09590b90
Rahul Desai <rd825@georgetown.edu>
2018-12-10 11:07:16 -0500

    Wrote all endpoints except PUT

--- api/server.js
+++ api/server.js
@@ -13,10 +13,77 @@ server.get('/', (req, res) => res.send({API: "live"}));
 server.get('/note/get/all', async (req, res) => {
     try {
         const notes = await notesDb.getAll();
-        res.status(200).json(notes)
+        res.status(200).json(notes);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// GET by id endpoint 
+server.get('/note/get/:id', async (req, res) => {
+    const {id} = req.params;
+    try {
+        const note = await notesDb.getById(id);
+        if (!note) {
+            res.status(404).json({message: "Nonexistent note"})
+        } else {
+            res.status(200).json(note)
+        }
     } catch (err) {
         res.status(500).json(err);
     }
 })
 
-module.exports = server;
+// POST endpoint
+server.post('/note/create', async (req, res) => {
+    const {title, textBody} = req.body;
+    try {
+        if (!title || !textBody) {
+            res.status(422).json({message: "Please fill out all fields"});
+        } else {
+            let response = await notesDb.insert(req.body)
+            res.status(201).json(response)
+        }
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+// PUT endpoint 
+server.put('note/edit/:id', async (req, res) => {
+    const {id} = req.params;
+    const {title, textBody} = req.body;
+    try {
+        if (!title || !textBody) {
+            res.status(422).json({message: "Please fill out all fields"});
+        } else {
+            const note = await notesDb.getById(id);
+            if (!note) {
+                res.status(404).json({message: "Nonexistent note"})
+            } else {
+                let response = await notesDb.insert(req.body)
+                res.status(201).json(response)
+            }
+        }
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+// DELETE endpoint
+server.delete('/note/delete/:id', async (req, res) => {
+    const {id} = req.params;
+    try {
+        const note = await notesDb.getById(id);
+        if (!note) {
+            res.status(404).json({message: "Nonexistent note"})
+        } else {
+            const removal = await notesDb.remove(id);
+            res.status(200).json(removal)
+        }
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+module.exports = server;    
