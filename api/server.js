4d022f0087a69f6aaab7ab1a3c348ee2d6d53d33
shawn <shawnantonucci2018@gmail.com>
2018-12-10 09:33:23 -0800

    added route to view note by id

--- api/server.js
+++ api/server.js
@@ -1,26 +1,67 @@
-const express = require('express');
-const knex = require('knex');
+const express = require("express");
+const knex = require("knex");
 
-const notes = require('../note.js')
+const notes = require("../note.js");
 
-const knexConfig = require('../knexfile.js');
+const knexConfig = require("../knexfile.js");
 const db = knex(knexConfig.development);
 
 const server = express();
 server.use(express.json());
 
-server.get('/', (req, res) => {
-    res.status(200).json({ message: 'api: up' });
-})
+server.get("/", (req, res) => {
+  res.status(200).json({ message: "api: up" });
+});
 
-server.get('/notes', (req, res) => {
-    notes
-        .getNotes()
-        .then(note => {
-            res.status(200).json(note);
-        }).catch(error => {
-            res.status(500).json({ error: 'Cannot find the note' });
+server.get("/notes", (req, res) => {
+  notes
+    .getNotes()
+    .then(note => {
+      res.status(200).json(note);
+    })
+    .catch(error => {
+      res.status(500).json({ error: "Cannot find the note" });
+    });
+});
+
+server.post("/addNote", (req, res) => {
+  const { title, content } = req.body;
+  const note = { title, content };
+
+  if (!note) {
+    return res
+      .status(422)
+      .sendDate({
+        Message: "Please provide Title and Content."
+      });
+  }
+  notes
+    .addNote(note)
+    .then(ids => {
+      res
+        .status(201)
+        .json({
+          title: note.title,
+          content: note.content
         });
+    })
+    .catch(error => {
+      res.status(405).json({ error: "Cannot add a new note." });
+    });
+});
+
+server.get('/notes/:id', (req, res) => {
+    const { id } = req.params;
+
+    db('notes').where({ id }).then(note => {
+        if (note.length !== 0) {
+            res.status(200).jsonp(note);
+        } else {
+            res.status(404).jsonp({ message: 'The note with the specified ID does not exist'});
+        }
+    }).catch(error => {
+        res.status(500).jsonp({ error: 'Cant get notes data'});
+    })
 })
 
 module.exports = server;
