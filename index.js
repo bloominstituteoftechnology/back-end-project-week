41d652966bd0cc684050814eddde7c9ba4cdcb9c
AM_Myrick <amyrick.820@gmail.com>
2018-12-10 12:47:26 -0500

    adds create, read, and update functionality to server

--- index.js
+++ index.js
@@ -17,6 +17,60 @@ server.get("/", (req, res) => {
     res.status(200).json({api: "running"});
 })
 
+server.get("/api/notes", (req, res) => {
+    db("notes")
+        .then(notes => res.status(200).json(notes))
+        .catch(err => res.status(500).json({error: err}))
+})
+
+server.get("/api/notes/:id", (req, res) => {
+    let { id } = req.params;
+    console.log(id)
+
+    db("notes")
+        .where({id})
+        .then(note => res.status(200).json(note))
+        .catch(err => res.status(500).json({error: err}))
+})
+
+server.post("/api/notes", (req, res) => {
+    let { title, content } = req.body;
+
+    if (!title) {
+        return res.status(405).json({error: "Please enter a title."});
+    }
+    if (!content) {
+        return res.status(405).json({error: "Please enter some content in the note."});
+    }
+
+    db("notes")
+        .insert({ title, content })
+        .then(id => res.status(200).json(id))
+        .catch(err => res.status(500).json({error: err}))
+})
+
+server.put("/api/notes/:id", (req, res) => {
+    let { title, content } = req.body;
+    let { id } = req.params;
+
+    if (!title) {
+        return res.status(405).json({error: "Please enter a title."});
+    }
+    if (!content) {
+        return res.status(405).json({error: "Please enter some content in the note."});
+    }
+
+    db("notes")
+        .where({id})
+        .update({ title, content })
+        .then(count => {
+            count ?
+            res.status(200).json(count) :
+            res.status(400).json({error: "Please enter a valid id"});
+        })
+        .catch(err => res.status(500).json({error: err}))
+})
+
 const port = 9001;
 
 server.listen(port, function() {
