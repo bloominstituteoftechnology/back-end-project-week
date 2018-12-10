17e0d95b1f2381c0b2eecccfba6d915c6be39104
unknown <brandongrych@gmail.com>
2018-12-10 13:05:53 -0600

    endpoints working

--- api/server.js
+++ api/server.js
@@ -15,64 +15,57 @@ server.get('/', (req, res) =>{
 
 server.get('/api/notes', (req, res) => {
   db('notes')
-    .then(zoos => res.status(200).json(zoos))
-    .catch(err => res.status(500).json(err));
-});
-
-server.get('/api/notes/:noteId', (req, res) => {
-  const { noteId } = req.params;
-  db('notes')
-    .then(notes => res.status(200).json(ntoes))
-    .catch(err => res.status(500).json(err));
+  .then(notes => res.status(200).json(notes))
+  .catch(err => res.status(500).json(err));
 });
 
 server.post('/api/notes', (req, res) => {
-  const note = req.body;
+const note = req.body;
 
-  db('notes')
-    .insert(note)
-    .then(ids => {
-      res.status(201).json(ids);
-    })
-    .catch(err => {
-      res.status(500).json({ message: 'error adding note', err });
-    });
+db('notes')
+  .insert(note)
+  .then(ids => {
+    res.status(201).json(ids);
+  })
+  .catch(err => {
+    res.status(500).json({ message: 'error adding note', err });
+  });
 });
 
 server.get('/api/notes/:noteId', (req, res) => {
-  const { noteId } = req.params;
+const { noteId } = req.params;
 
-  db('notes')
-    .where({ id: noteId })
-    .then(note => {
-      res.status(201).json({ note });
-    })
-    .catch(err => res.status(500).json(err));
+db('notes')
+  .where({ _id: noteId })
+  .then(note => {
+    res.status(201).json({ note });
+  })
+  .catch(err => res.status(500).json(err));
 });
 
 server.put('/api/notes/:noteId', (req, res) => {
-  const changes = req.body;
-  const { noteId } = req.params;
+const changes = req.body;
+const { noteId } = req.params;
 
-  db('notes')
-    .where({ id: noteId })
-    .update(changes)
-    .then(count => {
-      res.status(200).json({ count });
-    })
-    .catch(err => res.status(500).json(err));
+db('notes')
+  .where({ _id: noteId })
+  .update(changes)
+  .then(count => {
+    res.status(200).json({ count });
+  })
+  .catch(err => res.status(500).json(err));
 });
 
 server.delete('/api/notes/:noteId', (req, res) => {
-  const { noteId } = req.params;
+const { noteId } = req.params;
 
-  db('notes')
-    .where({ id: noteId })
-    .del()
-    .then(count => {
-      res.status(200).json({ count });
-    })
-    .catch(err => res.status(500).json(err));
+db('notes')
+  .where({ _id: noteId })
+  .del()
+  .then(count => {
+    res.status(200).json({ count });
+  })
+  .catch(err => res.status(500).json(err));
 });
 
 module.exports = server;
