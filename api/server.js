f1335300cfaed2d57709164754d0f15818581071
KHollobaugh <khollobaugh@gmail.com>
2018-12-10 12:28:31 -0500

    added get by id endpoint

--- api/server.js
+++ api/server.js
@@ -6,12 +6,15 @@ const db = knex(knexConfig.development);
 
 server.use(express.json());
 
+//Test if server is running
+
 server.get('/', (req, res) => {
     res.status(200).json({
         api: 'server is running!'
     })
 })
 
+//GET a list of notes
 
 server.get('/api/notes', (req, res) => {
     db('notes')
@@ -20,6 +23,17 @@ server.get('/api/notes', (req, res) => {
         message: 'failed to get notes'
     }))
 })
+
+//GET notes by id
+
+server.get('/api/notes/:id', (req, res) => {
+    const { id } = req.params;
+    db('notes')
+    .where({ id: id })
+    .then(notes => res.status(200).json(notes))
+    .catch(error => res.status(500).json({ message: "can't find note by that id"}))
+})
+
 
 
 module.exports = server;
