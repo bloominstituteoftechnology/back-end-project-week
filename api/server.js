d8bc2afa54ce4892a88d162c6ca2cef904817dfe
Kat Fries <katherynejf@gmail.com>
2018-12-10 13:15:10 -0500

    added endpoint for getting all notes and created titleTextChecker for correct posts

--- api/server.js
+++ api/server.js
@@ -6,6 +6,7 @@ const db = knex(knexConfig.development);
 
 
 // Add middleware nameChecker
+const titleTextChecker = require('../middleware/titleTextChecker.js')
 
 const server = express();
 server.use(express.json());
@@ -30,9 +31,9 @@ server.use(express.json());
 
 // NOTES
 // POST: .insert() .into
-server.post('/CreateNewView', (req, res) => {
+server.post('/CreateNewView', titleTextChecker, (req, res) => {
   const { title, textBody } = req.body;
-
+  // const noteTitle = { title };
   db('notes')
     .insert({ title, textBody })
     .then(ids => {
@@ -46,13 +47,24 @@ server.post('/CreateNewView', (req, res) => {
     })
 })
 
-
-
-
-
-
-
+// GET (assumes .select())
 server.get('/', (req, res) => {
+
+  db('notes')
+    //.select()
+    .then(notes => res.status(200).json(notes))
+    .catch(err => res.status(500).json({
+      err
+    }));
+});
+
+
+
+
+
+
+
+server.get('/api', (req, res) => {
   res.json({ api: 'Lambda Notes (Backend): running' });
 });
 
