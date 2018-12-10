fa3602fecd8f3a8c1a82f370c77e041960963220
jarheadm249 <36177264+jarheadm249@users.noreply.github.com>
2018-12-10 12:43:13 -0500

    Server Running

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const knex = require('knex');
+const knexConfig = require('./knexfile.js');
+const db = knex(knexConfig.development);
+
+const server = express();
+server.use(express.json());
+server.use(cors());
+
+server.get('/', (req, res) => {
+  res.send('Server is Running!');
+});
+
+
+const port = 3300;
+server.listen(port, function() {
+  console.log(`\n=== Server listening on port ${port} ===\n`);
+});
