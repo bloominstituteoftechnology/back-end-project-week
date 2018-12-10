8b65c85bbf43e4500ac15f1667d2801a94ff860e
theryanclausen <theryanclausen@gmail.com>
2018-12-10 14:49:36 -0700

    get and post

--- config/route.js
+++ config/route.js
@@ -1,18 +1,44 @@
 const express = require("express");
 const route = express.Router();
+const knex = require("knex");
+const knexConfig = require("../knexfile");
 
-route.get("/", (req, res) => {
-  res.status(200).json("list all notes");
+const db = knex(knexConfig.development);
+const sendErr = (res, stat = 500, message = "Server error") => {
+  return res.status(stat).json({ error: message });
+};
+
+route.get("/", async (req, res) => {
+  try {
+    const notes = await db("notes");
+    return res.status(200).json(notes);
+  } catch (err) {
+    return sendErr(res);
+  }
 });
 
-route.get("/:id", (req, res) => {
+route.get("/:id", async (req, res) => {
   const { id } = req.params;
-
-  res.status(200).json(`note ${id}`);
+  try {
+    const note = await db("notes").where({ id });
+    return res.status(200).json(note);
+  } catch (err) {
+    return sendErr(res);
+  }
 });
 
 route.post("/", async (req, res) => {
-  //new note
+    const {title, textBody} = req.body;
+    if (!title || !textBody){
+        return sendErr(res, 404, 'must include title and text body')
+    }
+    try {
+        const id = await db('notes').insert({... req.body})
+        const newNote = await db('notes').where({id})
+        return res.status(201).json(newNote)
+    }catch (err){
+        return sendErr(res)
+    }
 });
 
 route.put("/:id", (req, res) => {
