1ba3191a70f6ed098d448d91fbb9763da0b7b8ac
sean-one <basement365@gmail.com>
2018-12-10 13:56:39 -0800

    added seeds for some testing content

--- api/server.js
+++ api/server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const server = express();
 
+const db = require('../data/dbConfig');
+
 server.use(express.json());
 
-server.get('/', (req, res) => {
-    res.status(200).json({ message: 'its a start!' })
+server.get('/', async (req, res) => {
+    const notes = await db('notes');
+    res.status(200).json(notes);
 })
+
+server.get('/')
 
 module.exports = server;
