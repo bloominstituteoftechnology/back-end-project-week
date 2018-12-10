bbc9023d543cfffe7322bce619d4aecb6f3c403f
Carlos Lantigua <clantigua954@gmail.com>
2018-12-10 15:21:13 -0500

    added knex to server side

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,44 +1,12 @@
-// Update with your config settings.
-
 module.exports = {
-
-  development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
+	development: {
+		client: 'sqlite3',
+		connection: { filename: './data/lambdanotes.sqlite3' },
+		useNullAsDefault: true,
+		migrations: {
+			directory: './data/migrations',
+			tableName: 'dbmigrations'
+		},
+		seeds: { directory: './data/seeds' }
+	}
 };
