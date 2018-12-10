96936a7621c70d16ec00ec0528f1213aebf09f44
MEBonham <programmer@mckaybonham.com>
2018-12-10 12:12:43 -0700

    Implemented GET (one note) endpoint; not working

--- api/server.js
+++ api/server.js
@@ -42,4 +42,21 @@ server.get("/api/notes", (req, res) => {
         });
 });
 
+server.get("/api/notes/:id", (req, res) => {
+    const { noteId } = req.params;
+    db("notes")
+        .where("notes.id", noteId)
+        .then(notesArray => {
+            if (!notesArray.length) {
+                res.status(404).json({ error: "The note with the specified ID was not found." });
+            } else {
+                const result = notesArray[0];
+                res.status(200).json(result);
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ message: "Error retrieving note.", err });
+        });
+});
+
 module.exports = server;
