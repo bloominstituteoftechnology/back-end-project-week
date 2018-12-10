046e116960609da871ad9c5b54fc754518d4eabb
sarahtennis <sarahtennis@Sarahs-Air.fios-router.home>
2018-12-10 14:20:17 -0500

    allows for unique user registration with password hashing

--- data/helpers/usersHelper.js
+++ data/helpers/usersHelper.js
@@ -1,10 +1,26 @@
 const db = require('../dbConfig.js');
 
+const bcrypt = require('bcryptjs');
+
 module.exports = {
-    getUsers
+    getUsers,
+    registerUser
 };
 
-// return all notes in database
+// return all users in database
 function getUsers() {
     return db('users');
-}
+};
+
+// registers new user if valid, returns new id
+function registerUser(user) {
+    const newUser = user;
+    const hash = bcrypt.hashSync(newUser.password, 14);
+
+    newUser.username = newUser.username.toLowerCase(); // username NOT case sensitive
+    newUser.password = hash;
+
+    return db('users')
+        .insert(newUser)
+        .then(id => { return { id: id[0] }});
+};
--- routers/usersRouter.js
+++ routers/usersRouter.js
@@ -1,8 +1,29 @@
+require('dotenv').config();
+
 const express = require('express');
+
+const jwt = require('jsonwebtoken');
 
 const usersDb = require('../data/helpers/usersHelper.js');
 
 const router = express.Router();
+
+// generates jwt
+const generateToken = user => {
+    const payload = {
+        subject: user.id,
+        username: user.username,
+        department: user.department
+    };
+
+    const secret = process.env.JWT_SECRET;
+
+    const options = {
+        expiresIn: '1h',
+    };
+
+    return jwt.sign(payload, secret, options);
+};
 
 // [GET] /api/users
 router.get('', (req, res) => {
@@ -13,6 +34,22 @@ router.get('', (req, res) => {
         .catch(err => {
             res.status(500).json({ message: 'Error retrieving users' });
         });
+});
+
+// [POST] /api/users/register
+router.post('/register', (req, res) => {
+    const newUser = req.body;
+    usersDb.registerUser(newUser)
+        .then(id => {
+            res.status(201).json(id);
+        })
+        .catch(err => {
+            if (err.errno === 19 && err.code === 'SQLITE_CONSTRAINT') {
+                res.status(409).json({ message: 'Username already exists' });
+            } else {
+                res.status(500).json({ message: 'Error registering new user' });
+            }
+        })
 });
 
 module.exports = router;
