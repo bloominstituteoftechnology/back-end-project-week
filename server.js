47e86b470a4e6af4a06b22c6cc98d139146022e3
Barbara Hunter <commandandcode@gmail.com>
2018-12-10 17:10:59 -0500

    push

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+
+const server = express();
+
+server.use(express.json());
+server.use(helmet());
+server.use(morgan('dev'));
+server.use(cors());
+
+const notesRoute = require('./routes/notesRoute');
+server.use('/notes', notesRoute);
+
+//sanity check endpoint
+server.get('/', (req, res) => {
+  res.send({ message: 'Alive!' })
+});
+
+module.exports = server;
