af93fade41393dbadcdfae8321aadfa45edec3b0
Carlos Lantigua <clantigua954@gmail.com>
2018-12-10 16:01:13 -0500

    created notes table and routes

---
+++ backend/config/routes.js
@@ -0,0 +1,31 @@
+const db = require('../data/dbConfig');
+
+module.exports = (server) => {
+	server.get('api/notes', getNotes);
+	server.get('/api/note/:id', getNote);
+	server.post('/api/createnote', createNote);
+	server.put('/api/editnote', editNote);
+	server.delete('/api/removenote', removeNote);
+};
+
+function getNotes(req, res) {
+	db('notes')
+		.select('*')
+		.then((note) => {
+			res.status(200).json(note);
+		})
+		.catch((err) => res.send(err));
+}
+
+function getNote(req, res) {
+	const note = req.params;
+	db('notes')
+		.where({ id: note.id })
+		.select('*')
+		.then((note) => {
+			res.json(note);
+		})
+		.catch((err) => {
+			res.status(500).json({ message: 'Error retrieving that note', err });
+		});
+}
---
+++ backend/data/migrations/20181210154957_notes.js
@@ -0,0 +1,10 @@
+exports.up = function(knex, Promise) {
+	return knex.schema.createTable('notes', (tbl) => {
+		tbl.increments();
+		tbl.string('title', 30).notNullable();
+		tbl.text('content').notNullable();
+		tbl.timestamp('created_at').defaultTo(knex.fn.now());
+	});
+};
+
+exports.down = function(knex, Promise) {};
