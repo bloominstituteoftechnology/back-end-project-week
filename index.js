306c34774916e744ee0163c6d4331a90c13256f3
Yusuf Nafey <yusufnafey@yahoo.com>
2018-12-10 11:59:33 -0800

    get all notes endpoint

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const server = require('./server.js');
 
 const port = process.env.PORT || 3000;
 server.listen(port, () => {
-    console.log(`Listening on port ${port}`);
+    console.log(`*****\nServer running on port ${port}\n*****`);
 });
--- server.js
+++ server.js
@@ -7,8 +7,20 @@ const cors = require('cors');
 const server = express();
 server.use(express.json());
 
+// server test
 server.get('/', (req, res) => {
     res.send({ API: 'is live' });
+});
+
+// get all notes
+server.get('/api/notes/all', (req, res) => {
+    db('notes')
+        .then(notes => {
+            res.status(200).json(notes);
+        })
+        .catch(error => {
+            res.status(500).json({ message: 'could not find notes', error });
+        });
 });
 
 module.exports = server;
