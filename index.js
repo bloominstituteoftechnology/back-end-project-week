b76562f7f11321a560cd325dc771df4fed2ea85b
Matthew Wright <matthewbwright@gmail.com>
2018-12-10 10:37:20 -0700

    creates server

---
+++ index.js
@@ -0,0 +1,6 @@
+const server = require('./api/server.js');
+const port = process.env.PORT || 9000;
+
+server.listen(port, () =>
+  console.log(`\n** welcome to the server running on ${port} **\n`)
+);
