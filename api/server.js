d239d3db8e67d3e82db82290329a6dd6e6b93e36
shawn <shawnantonucci2018@gmail.com>
2018-12-10 13:29:07 -0800

    setup google register

--- api/server.js
+++ api/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const knex = require("knex");
-var cors = require('cors')
+const cors = require("cors");
 
 const notes = require("../note.js");
 
@@ -10,6 +10,7 @@ const db = knex(knexConfig.development);
 const server = express();
 server.use(express.json());
 server.use(cors());
+
 
 server.get("/", (req, res) => {
   res.status(200).json({ message: "api: up" });
@@ -31,60 +32,69 @@ server.post("/addNote", (req, res) => {
   const note = { title, content };
 
   if (!note) {
-    return res
-      .status(422)
-      .sendDate({
-        Message: "Please provide Title and Content."
-      });
+    return res.status(422).sendDate({
+      Message: "Please provide Title and Content."
+    });
   }
   notes
     .addNote(note)
     .then(ids => {
-      res
-        .status(201)
-        .json({
-          title: note.title,
-          content: note.content
-        });
+      res.status(201).json({
+        title: note.title,
+        content: note.content
+      });
     })
     .catch(error => {
       res.status(405).json({ error: "Cannot add a new note." });
     });
 });
 
-server.get('/notes/:id', (req, res) => {
-    const { id } = req.params;
+server.get("/notes/:id", (req, res) => {
+  const { id } = req.params;
 
-    db('notes').where({ id }).then(note => {
-        if (note.length !== 0) {
-            res.status(200).json({ note });
-        } else {
-            res.status(404).json({ message: 'The note with the specified ID does not exist'});
-        }
-    }).catch(error => {
-        res.status(500).json({ error: 'Cant get notes data'});
-    })
-})
-
-server.delete('/notes/:id', (req, res) => {
-    const { id } = req.params;
-
-    db('notes').where({ id }).del().then(note => {
-        res.status(200).json(note);
-    }).catch(error => {
-        res.status(500).json({ error: 'Cant delete note'});
-    })
-})
-
-server.put('/notes/:id', (req, res) => {
-    const changes = req.body;
-    const { id } = req.params;
-
-    db('notes').where({ id }).update(changes).then(note => {
+  db("notes")
+    .where({ id })
+    .then(note => {
+      if (note.length !== 0) {
         res.status(200).json({ note });
-    }).catch(error => {
-        res.status(500).json({ error: 'cannot update the note'})
+      } else {
+        res
+          .status(404)
+          .json({ message: "The note with the specified ID does not exist" });
+      }
     })
-})
+    .catch(error => {
+      res.status(500).json({ error: "Cant get notes data" });
+    });
+});
+
+server.delete("/notes/:id", (req, res) => {
+  const { id } = req.params;
+
+  db("notes")
+    .where({ id })
+    .del()
+    .then(note => {
+      res.status(200).json(note);
+    })
+    .catch(error => {
+      res.status(500).json({ error: "Cant delete note" });
+    });
+});
+
+server.put("/notes/:id", (req, res) => {
+  const changes = req.body;
+  const { id } = req.params;
+
+  db("notes")
+    .where({ id })
+    .update(changes)
+    .then(note => {
+      res.status(200).json({ note });
+    })
+    .catch(error => {
+      res.status(500).json({ error: "cannot update the note" });
+    });
+});
 
 module.exports = server;
--- index.js
+++ index.js
@@ -1,4 +1,26 @@
 const server = require('./api/server.js');
+const express = require("express");
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys.js');
+require('./models/User.js');
+require('./services/passport.js');
+
+mongoose.connect(keys.mongoURI);
+
+server.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+
+server.use(passport.initialize());
+server.use(passport.session());
+
+require('./routes/authRoutes.js')(server);
+
 const PORT = process.env.PORT || 7000;
 
 server.listen(PORT, () => console.log(`\n---<Server Running On ${PORT}>---\n`));
