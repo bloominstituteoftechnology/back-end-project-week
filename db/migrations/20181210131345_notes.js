8506577296711afde02b3f36ecc45fe11eb76ba3
Katie Cleary <katiecleary@airmaster.lan>
2018-12-10 13:21:41 -0500

    created MVP notes migration file

---
+++ db/migrations/20181210131345_notes.js
@@ -0,0 +1,29 @@
+exports.up = function(knex, Promise) {
+    // makes the changes to the database
+    return knex.schema.createTable('notes', function(tbl) {
+        // primary key
+        tbl.increments(); 
+    
+        // other fields - note title and content
+        tbl
+        .string('title', 255)
+        .notNullable()
+        tbl
+        .string('content', 3000)
+        .notNullable()
+        
+        /*/foreign id --- may use later for tags 
+        tbl
+        .integer('cohort_id')
+        .unsigned()
+        .references('id')
+        .inTable('cohorts_table');*/
+    
+      });
+    };
+
+
+exports.down = function(knex, Promise) {
+  // undo the changes to the database (it's called rolling back changes)
+  return knex.schema.dropTableIfExists('notes');
+};
