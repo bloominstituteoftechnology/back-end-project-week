c864e141e29889090526f00fe9e83cf5e3b16085
ssmodish <smodish@hotmail.com>
2018-12-10 16:22:26 -0500

    api complete, adjusting front-end

--- api/server.js
+++ api/server.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const knex = require('knex');
 
-const knexConfig = require('../knexfile.js');
-const db = knex(knexConfig.development);
 const server = express();
 
 server.use(express.json());
+
+const notes = require('./routes/notes.js');
+server.use('/notes', notes);
 
 server.get('/', (req, res) => {
   res.status(200).json({ api: 'operational' });
---
+++ data/seeds/noteSEEDS.js
@@ -0,0 +1,30 @@
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('notes').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('notes').insert([
+        {
+          id: "1",
+          title: "this is an edited note",
+          textBody: "my notehhhhhlllllllll",
+        },
+        {
+          id: "2",
+          title: "Title",
+          textBody: "Cobnnnnnnnnnntent",
+        },
+        {
+          id: "3",
+          title: "bbbbbbbb",
+          textBody: "nnnnnn",
+        },
+        {
+          id: "4",
+          title: "mmmmmmmmmmmmmm",
+          textBody: "kkkkkkkkkkkkk",
+        }
+      ]);
+    });
+};
