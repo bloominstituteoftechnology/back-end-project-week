97751a50ddfb62c0f3d1e37a6078a86c271793a9
Andre Myrick <amyrick.820@gmail.com>
2018-12-10 18:39:31 -0500

    creates authentication middleware

---
+++ middleware.js
@@ -0,0 +1,39 @@
+const jwt = require('jsonwebtoken');
+const jwtKey = require('./_secrets/keys').jwtKey;
+
+function authenticate(req, res, next) {
+  const token = req.get('Authorization');
+
+  if (token) {
+    jwt.verify(token, jwtKey, (err, decoded) => {
+      if (err) return res.status(401).json(err);
+
+      req.decoded = decoded;
+
+      next();
+    });
+  } else {
+    return res.status(401).json({
+      error: 'No token provided, must be set on the Authorization Header',
+    });
+  }
+}
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username,
+  };
+
+  const secret = jwtKey;
+  const options = {
+    expiresIn: '1h',
+  };
+
+  return jwt.sign(payload, secret, options);
+}
+
+module.exports = {
+  authenticate,
+  generateToken
+};
