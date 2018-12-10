7c835004ae15dafbd3762a27bea7efc97908902b
Hunter315 <hunter.mckay.smith@gmail.com>
2018-12-10 16:12:22 -0700

    GET notes endpoint created

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const middlewareConfig = require('./middleware/middlewareConfig.js');
+const knex = require('knex');
+const knexConfig = require('./knexfile.js');
+const db = knex(knexConfig.development);
 // Initializes the server
 const server = express();
 // Middleware setup
@@ -7,4 +10,16 @@ middlewareConfig(server);
 // Endpoints
 server.get("/", (req, res) => res.send("API UP"));
 
+//=========GET NOTES==========
+server.get('/api/notes', async (req, res) => {
+    db('notes')
+      .then(notes => res.status(200).json(notes))
+      .catch(error => {
+        res
+          .status(500)
+          .json({ message: 'There was an error getting the notes', error });
+      });
+  });
+
 module.exports = server;
+
