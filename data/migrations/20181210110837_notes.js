ae65dc3644890ee1f58f52a9675819b7eb63f249
Steven O'Neill <oneillsteven957@gmail.com>
2018-12-10 11:25:14 -0600

    completed migration
    I deleted the seed because I didn't think it was needed

--- data/migrations/20181210110837_notes.js
+++ data/migrations/20181210110837_notes.js
@@ -1,8 +1,13 @@
-
 exports.up = function(knex, Promise) {
-  
+    return knex.schema.createTable('notes', tbl => {
+        tbl.increments();
+
+        tbl.string('title', 128).notNullable();
+
+        tbl.text('content').notNullable();
+    });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists('notes');
 };
