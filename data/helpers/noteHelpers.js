374b7e9d2b2f52df83d110a83a09b0afb0b7b2d6
Jordan Stoddard <jjsincorporated@gmail.com>
2018-12-10 10:53:02 -0800

    created helper functions for routes

---
+++ data/helpers/noteHelpers.js
@@ -0,0 +1,29 @@
+const db = require('../dbConfig.js')
+
+module.exports = {
+
+    get: id => {
+        const query = db('notes')
+
+        if(id) {
+            query.where('id', Number(id))
+            .first()
+        }
+        return query
+    },
+    create: note => {
+        return db('notes')
+        .insert(note)
+        .then(ids => ({id: ids[0]}))
+    },
+    update: (id, note) => {
+        return db('notes')
+        .where('id', id)
+        .update(note)
+    },
+    remove: id => {
+        return db('notes')
+        .where('id', id)
+        .del()
+    }
+}
