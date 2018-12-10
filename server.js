a63bed29610815d1acea459a00a4c1095b79d6fe
TRIF3X <thewizardofcrypto@gmail.com>
2018-12-10 10:19:06 -0600

    display list of notes end point done

--- server.js
+++ server.js
@@ -1,5 +1,22 @@
 const express = require('express');
 const server = express();
 server.use(express.json());
+const knex = require('knex')
+const knexConfig = require('./knexfile.js')
+const db = knex(knexConfig.development)
+const cors = require('cors')
+server.use(cors())
+
+
+server.get('/notes', (req, res) => {
+
+    db('notes')
+        .then(notes => {
+            res.status(200).json(notes)
+        })
+        .catch(err => {
+            res.status(500).json({ message: 'Server error', err })
+        })
+})
 
 module.exports = server;
