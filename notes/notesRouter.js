4d95d32d2c7cc764cd4c59f8ad593e4031eaeab6
Stublink <chrisrey95@gmail.com>
2018-12-10 12:54:49 -0500

    added end point to edit an existing note

--- notes/notesRouter.js
+++ notes/notesRouter.js
@@ -9,6 +9,7 @@ router.get('/', (req, res) => {
 router.get('/get/all', getAllNotes);
 router.get('/get/:id', getNoteById);
 router.post('/create', createNote);
+router.put('/edit/:id', editNote);
 
 
 module.exports = router;
@@ -18,7 +19,6 @@ async function getAllNotes(req, res) {
     const notes = await db('notes');
     res.status(200).json(notes);
 }
-
 
 async function getNoteById(req, res) {
     const id = req.params.id;
@@ -39,3 +39,28 @@ async function createNote(req, res) {
 
     res.status(201).json({success: note[0]})
 }
+
+async function editNote(req, res) {
+    const id = req.params.id;
+    const newData = req.body;
+
+    if (!newData && !newData.title && !newData.textBody && !newData.tags) {
+        res.status(422).json({error: "Note Title or Note TextBody is required"});
+        return;
+    }
+
+    const updatedNote = await db('notes')
+        .where('id', '=', id)
+        .update({...newData});
+    
+    console.log(updatedNote);
+    if (updatedNote > 0) {
+        const note = await db('notes').where('id', '=', id).first();
+        res.status(201).json(note);
+        return
+    } else {
+        res.status(500).json({error: "Error updating note"});
+    }
+
+}
+
