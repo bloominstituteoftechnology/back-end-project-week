e71f99e8f0330a0d45d017b355e310e71677935e
Jordan Stoddard <jjsincorporated@gmail.com>
2018-12-10 12:10:36 -0800

    .delete endpoint for notes db completed

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -24,43 +24,66 @@ route.get("/:id", (req, res) => {
       if (!note) {
         res.status(404).json({ message: `Note with id ${id} does not exist.` });
       }
-     res.status(200).json(note)
+    //   if (note) {
+    //     db("tags as t").then(tags => {
+    //       note.tags = tags;
+    //       res.status(200).json(note);
+    //     });
+    //   } else {
+    //       res.status(404).json({ message: "note not found" });
+    //     }
+    res.status(200).json(note)
     })
     .catch(err => res.status(500).json({ message: `Error: ${err}` }));
 });
 
+
 //Endpoint for adding a note note
 
 route.post("/", (req, res) => {
-  const { title, textBody } = req.body;
-  if (!title || !textBody) {
-    res
-      .status(400)
-      .json({ Error: `Please provide title and textBody to add a new note.` });
-  }
-  noteHelp
-    .create({ title, textBody })
-    .then(ids => {
-      res
-        .status(200)
-        .json(`Successfully added a new note with the id ${ids.id}`);
-    })
-    .catch(err => res.status(500).json({ message: `Error: ${err}` }));
+const { title, textBody } = req.body;
+if (!title || !textBody) {
+res
+  .status(400)
+  .json({ Error: `Please provide title and textBody to add a new note.` });
+}
+noteHelp
+.create({ title, textBody })
+.then(ids => {
+  res
+    .status(200)
+    .json(`Successfully added a new note with the id ${ids.id}`);
+})
+.catch(err => res.status(500).json({ message: `Error: ${err}` }));
 });
 
 // Endpoint for updating a note
 
 route.put('/:id', (req, res) => {
+const {id} = req.params
+const {title, textBody} = req.body
+noteHelp.update(id, {title, textBody})
+.then(success => {
+    if(success === 0) {
+        res.status(404).json({message: `Note with id of ${id} does not exist.`})
+    }
+    res.status(200).json({message: `Success! Note edited.`})
+})
+.catch(err => res.status(500).json({message: `Error: ${err}`}))
+})
+
+
+// Endpoint for deleting a note
+
+route.delete('/:id', (req, res) => {
     const {id} = req.params
-    const {title, textBody} = req.body
-    noteHelp.update(id, {title, textBody})
+    noteHelp.remove(id)
     .then(success => {
         if(success === 0) {
             res.status(404).json({message: `Note with id of ${id} does not exist.`})
         }
-        res.status(200).json({message: `Success! Note edited.`})
+        res.status(200).json({message: `Success! Note deleted.`})
     })
     .catch(err => res.status(500).json({message: `Error: ${err}`}))
 })
-
 module.exports = route;
