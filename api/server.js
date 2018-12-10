556aea4786e1338124eb9d2343de9e7316cc919c
bummings <eabummings@gmail.com>
2018-12-10 14:44:38 -0500

    added get notes and post note endpoints

--- api/server.js
+++ api/server.js
@@ -1,6 +1,30 @@
 const express = require('express');
 const server = express();
+const db = require('../data/dbConfig');
 
 server.use(express.json());
+
+// R O O T
+server.get('/', (req, res) => {
+  res.send('This is the API root route.');
+});
+
+// G E T   A L L   N O T E S
+server.get('/api/notes', (req, res) => {
+  db('notes')
+    .then(note => res.status(200).json(note))
+    .catch(err => res.status(500).json(err));
+});
+
+// P O S T
+server.post('/api/notes', (req, res) => {
+  const { title, content } = req.body;
+  db('notes')
+    .insert({ title, content })
+    .then(() => {
+      res.status(201).json({ message: 'successfully added note' });
+    })
+    .catch(err => res.send(err));
+});
 
 module.exports = server;
