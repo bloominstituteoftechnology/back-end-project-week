12f489cf2e5382022d1748e114fe8514b472c9d1
Brad Mortensen <b.mort86@gmail.com>
2018-12-10 10:34:53 -0700

    Adds Migrations and Database

--- data/migrations/20181210093054_notes.js
+++ data/migrations/20181210093054_notes.js
@@ -1,8 +1,15 @@
 
-exports.up = function(knex, Promise) {
-  
+exports.up = function (knex, Promise) {
+  //makes changes to the database
+  return knex.schema.createTable('notes', function (tbl) {
+    //primary key
+    tbl.increments('_id');
+    //other fields
+    tbl.string('title', 120).notNullable();
+    tbl.string('textBody', 1000);
+  })
 };
 
-exports.down = function(knex, Promise) {
-  
+exports.down = function (knex, Promise) {
+  return knex.schema.dropTableIfExists('notes');
 };
