18ab55f21eaad9d5f16630363d3702e8313388eb
Michael Checo <checomichael2@gmail.com>
2018-12-10 11:30:58 -0500

    seeded test data

---
+++ data/seeds/001.js
@@ -0,0 +1,11 @@
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('notes').truncate()
+    .then(function () {
+      // Inserts seed entries
+      return knex('notes').insert([
+        {title: 'My test note', content: 'This is my test note.'},
+      ]);
+    });
+};
