53371156fcd217cdf20a2488d9de3460eaa03eaf
lajawanti <myemail>
2018-12-10 14:20:06 -0500

    POST endpoint : to create new note

--- config/routes.js
+++ config/routes.js
@@ -8,8 +8,8 @@ const db = require('../database/dbConfig.js');
 module.exports = server => {
     server.get('/', serverRunning);
     server.get('/api/notes', getNotes); //Display a list of notes.
-    //server.post('/api/notes', createNote) //Create a note with a title and content
-    //server.get('/api/notes/:id', viewSingleNote);//View an existing note.
+    server.get('/api/notes/:id', viewSingleNote);//View an existing note.
+    server.post('/api/notes', createNote) //Create a note with a title and content
     //server.put('/api/notes/:id', updateNote);//Edit an existing note.
     //server.delete('/api/notes/:id', deleteNote);//Delete an existing note.
 }
@@ -31,3 +31,33 @@ function getNotes(req, res) {
 
             })
 }
+
+
+//===== NOTES GET BY ID ROUTE '/api/notes/:id' ============
+function viewSingleNote(req, res) {
+    db('notes')
+             .where({id : req.params.id})
+             .then(note => {
+                    res.status(200).json(note); 
+              })
+             .catch(error => {
+                    res.status(500).json({error : 'The data could not be retrieved'})
+              }) 
+}
+
+//===== POST to create new note ROUTE '/api/notes' ============
+function createNote(req, res) {
+    if(req.body) {
+        db('notes')
+                .insert(req.body)
+                .then(id => {
+                        res.status(201).json(id);
+                 })
+                .catch(error => {
+                        res.status(500).json({message : 'error creating note',error});
+                 })
+    } else {
+        res.status(422).json({message : 'Need correct data to create note..'})
+    }
+}
+ 
