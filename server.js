7bdf9b41e3c7e5267f6088cb7b04e6ac4b94b61a
Drewsup123c <drewsup123c@gmail.com>
2018-12-10 09:56:50 -0700

    created get endpoint to display all notes

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const cors = require('cors');
+const db = require('./database/dbConfig.js');
 
 const server = express();
 
 server.use(express.json());
 server.use(cors());
 
-
+server.get('/note/get/all',(req,res)=>{
+    db('notes')
+    .then(notes => {
+        if(notes){
+            res.status(200).json(notes);
+        }else{
+            res.status(200).send("No notes in database")
+        }
+    })
+    .catch(err => {
+        res.status(500).json({Error : err})
+    })
+})
 
 module.exports = {
     server,
