7d54958e6da31355e5480efbbdabbd71a6fde70a
bkwillia052 <bkwillia052@gmail.com>
2018-12-10 11:50:00 -0600

    DELETE method functional

--- api/server.js
+++ api/server.js
@@ -63,7 +63,14 @@ server.use(express.json())
 });
 
  server.delete('/note/delete/:id', async (req, res) => {
-
+    const id = req.params.id;
+    try{
+        const response = await db('notes').where({id}).del()
+        res.status(200).json(response)
+    }
+    catch(err){
+        res.status(500).json({message: 'An error occured while processing the data.'})
+    }
 });
 
 
