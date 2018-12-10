3fd78f72d0240940e0173b31da9f30c50f34dcc6
Brandon Vilaychith <blvilaychith@outlook.com>
2018-12-10 14:02:16 -0600

    Heroku test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const server = require('./server.js');
 
 // Sets up the port for the server
-const PORT = process.env.PORT || 9000;
+const PORT = 9000;
 
 server.listen(PORT, () => {
   console.log(`=== Server is listening on port ${PORT} ===`);
