99b37477c381491a383d068ed1b410f1627b1b83
Brandon Vilaychith <blvilaychith@outlook.com>
2018-12-10 11:11:36 -0600

    get notes endpoint created

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 // Imports
 const express = require('express');
 const middlewareConfig = require('./middleware/middlewareConfig.js');
+const knex = require('knex');
+const knexConfig = require('./knexfile.js');
+const db = knex(knexConfig.development);
 // Initializes the server
 const server = express();
 // Middleware setup
@@ -9,4 +12,22 @@ middlewareConfig(server);
 // Endpoints
 server.get('/', (req, res) => res.send('API UP'));
 
+server.get('/api/notes', async (req, res) => {
+  db('notes')
+    .then(notes => res.status(200).json(notes))
+    .catch(error => {
+      res
+        .status(500)
+        .json({ message: 'There was an error getting the zoos', error });
+    });
+});
 module.exports = server;
+
+// try {
+//   const notes = await db('notes');
+//   res.status(200).json(notes);
+// } catch (error) {
+//   res
+//     .status(500)
+//     .json({ message: 'There was an error getting the zoos', error });
+// }
