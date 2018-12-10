4af7aaa3991a57da1656abecfae4ba5c725068ed
Elvis Ibarra <ielvisd@gmail.com>
2018-12-10 14:30:57 -0800

    Added Post route

--- notes/notesRouter.js
+++ notes/notesRouter.js
@@ -2,13 +2,12 @@ const express = require('express');
 const router = express.Router();
 const db = require('./notesModel');
 
-// VideoGame endpoints
+// Notes endpoints
 router.get('/api/', (req, res) => {
   res.status(200).send('Server Listens and Obeys');
 });
 
 router.get('/api/notes/all', async (req, res) => {
-  // console.log('Why is this not working?');
   try {
     const notes = await db.getAll();
     res.status(200).json(notes);
@@ -17,23 +16,21 @@ router.get('/api/notes/all', async (req, res) => {
   }
 });
 
-// router.post('/api/games/', async (req, res) => {
-//   const GameData = req.body;
-//   console.log(req.body);
-//   if (!GameData.title || !GameData.genre) {
-//     res.status(422).json({ errorMessage: 'Please provide a title and a genre for your videogame.' });
-//   } else if (!db.findByTitle(GameData.title)) {
-//     res.status(405).json({ errorMessage: 'Duplicate Game Titles Not Allowed' });
-//   } else {
-//     try {
-//       const newGame = await db.insert(GameData);
-//       res.status(201).json(newGame);
-//     } catch (error) {
-//       res
-//         .status(500)
-//         .json({ error: 'There was an error while saving the videogame to the database. The error is ', error });
-//     }
-//   }
-// });
+router.post('/api/notes/', async (req, res) => {
+  const NoteData = req.body;
+  console.log(req.body);
+  if (!NoteData.title || !NoteData.content) {
+    res.status(422).json({ errorMessage: 'Please provide a title and/or content for your note.' });
+  } else if (!db.findByTitle(NoteData.title)) {
+    res.status(405).json({ errorMessage: 'Duplicate Note Titles Not Allowed' });
+  } else {
+    try {
+      const newGame = await db.insert(NoteData);
+      res.status(201).json(newGame);
+    } catch (error) {
+      res.status(500).json({ error: 'There was an error while saving the note to the database. The error is ', error });
+    }
+  }
+});
 
 module.exports = router;
