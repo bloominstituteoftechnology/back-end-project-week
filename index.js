0235629e43defbfd628ea7ac78f6f708c19a26ed
Wanda <doubleuevans@gmail.com>
2018-12-10 15:18:45 -0600

    display notes; add/create note

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+//const bcrypt = require('bcryptjs');
+//const jwt = require('jsonwebtoken');
 
 const db = require('./database/dbConfig.js');
 
@@ -10,6 +10,46 @@ const server = express();
 
 server.use(express.json());
 server.use(cors());
+
+/* //FUNCTIONS
+function generateToken(note) {
+  const payload = {
+    subject: note.id,
+    title: note.title,
+    text: note.note_text, //added manually here; normally would come from db
+  }
+
+  //const secret = 'afoiu2389u_caiocja;l3?vu80vnqa909jk&claksma';
+  const secret = process.env.JWT_SECRET; //create the dotenv
+
+  const options = {
+    expiresIn: '1h',
+  }
+  return jwt.sign(payload, secret, options)
+} */
+
+// ENDPOINTS
+//get notes
+server.get('/api/notes', (req, res) => {
+  db('notes')
+  .then(notes => res.status(200).json(notes))
+  .catch(err => res.status(500).json(err));
+})
+
+//addnote
+server.post('/api/notes', (req, res) => {
+  const note = req.body;
+
+  db('notes')
+  .insert(note)
+  .then(ids => {id: ids[0]})
+  .then(note => {
+    res.status(201).json({message: 'Note Added', note});
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Error inserting note', err });
+    });
+  });
 
 
 // server working?
