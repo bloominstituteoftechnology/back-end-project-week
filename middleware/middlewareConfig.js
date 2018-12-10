cf52a34ceed5e8bed6ef0aeaf63359d878b30c97
Hunter315 <hunter.mckay.smith@gmail.com>
2018-12-10 16:17:02 -0700

    added cors to project

--- middleware/middlewareConfig.js
+++ middleware/middlewareConfig.js
@@ -1,9 +1,11 @@
 const morgan = require("morgan");
 const helmet = require("helmet");
 const express = require("express");
+const cors = require("cors");
 const middlewareConfig = server => {
   server.use(express.json());
   server.use(morgan("dev"));
   server.use(helmet());
+  server.use(cors());
 };
 module.exports = middlewareConfig;
