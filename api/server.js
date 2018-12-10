6b75b66a505e7a8d0590cf5fed265ce1d249ac94
Kat Fries <katherynejf@gmail.com>
2018-12-10 12:47:16 -0500

    added notes db and post to it receiving an id back

--- api/server.js
+++ api/server.js
@@ -1,17 +1,50 @@
 const express = require('express');
+const knex = require('knex');
+const knexConfig = require('../knexfile.js');
+const db = knex(knexConfig.development);
 
 
 
-
-
+// Add middleware nameChecker
 
 const server = express();
 server.use(express.json());
 
 
+// TABLE SCHEMA
+//  "id": 
+//  "tags": ["tag", "otherTag"],
+//  "title": "Note Title",
+//  "textBody": "Note Body",
+
+// ENDPOINTS
+
+// endpoints here
+
+//  Get All: "/" 
+//  Post: '/CreateNewView'
+//  Get Note by id '/note/:id'
+//  Edit Note by id = '/note/edit/:id'
+//  Delete Note by id = '/note/delete/:id'
 
 
+// NOTES
+// POST: .insert() .into
+server.post('/CreateNewView', (req, res) => {
+  const { title, textBody } = req.body;
 
+  db('notes')
+    .insert({ title, textBody })
+    .then(ids => {
+      res.status(201).json({ id: ids[0] });
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: 'Error inserting',
+        err
+      })
+    })
+})
 
 
 
@@ -24,21 +57,6 @@ server.get('/', (req, res) => {
 });
 
 module.exports = server;
-/**
- * exact path="/" 
- * exact path='/CreateNewView'
- * exact path = '/note/:id'
- * path = '/note/edit/:id'
- * path = '/note/delete/:id'
- * 
- * 
- * 
- **/
 
-// ```js
-// {
-//   "tags": ["tag", "otherTag"],
-//   "title": "Note Title",
-//   "textBody": "Note Body",
-// }
-// ```
+
+
---
+++ data/migrations/20181210123401_01-notes.js
@@ -0,0 +1,19 @@
+
+exports.up = function(knex, Promise) {
+          // make changes to the database
+          return knex.schema.createTable('notes', table => {
+
+            // primary key
+            // by default it generates an id field and make it autoincrement and the primary key. Default tbl.increments('id');
+            table.increments();
+        
+            // other fields
+            table.string('title', 255);
+            table.string('textBody', 255);
+            table.timestamps(true, true);
+          })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('notes');
+};
