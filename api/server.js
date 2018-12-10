091628fe935c7e9db3141c04a0d10b2a08084cc7
MEBonham <programmer@mckaybonham.com>
2018-12-10 11:58:33 -0700

    Added CORS

--- api/server.js
+++ api/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 
 const server = express();
 
 server.use(express.json());
+server.use(cors());
 
 const db = require("../data/dbConfig.js");
 
@@ -10,5 +12,26 @@ const db = require("../data/dbConfig.js");
 server.get("/", (req, res) => {
     res.status(200).json({ api: "Up and running" });
 });
+
+// CRUD Endpoints for Lambda Notes
+server.post("/api/notes", (req, res) => {
+    const newPost = {
+        textBody: "",
+        tags: [],
+        ...req.body
+    };
+    if (!newPost.title) {
+        res.status(400).json({ error: "Note requires a valid title." });
+    } else {
+        db("notes")
+            .insert(newPost)
+            .then(idReturned => {
+                res.status(201).json(idReturned);
+            })
+            .catch(err => {
+                res.status(500).json({ message: "Cannot post note to database.", err });
+            });
+    }
+})
 
 module.exports = server;
