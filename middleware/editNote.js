bdd758dd39765bf8852b041d2bd808b29623a95f
Joe Bugajski <yogurtmystery@gmail.com>
2018-12-10 15:36:04 -0600

    -endpoints complete, for now

--- middleware/editNote.js
+++ middleware/editNote.js
@@ -2,11 +2,58 @@ const express = require('express');
 const router = express.Router();
 const db = require('../data/dbConfig');
 
-router.put('/', (req, res) => {
-  const note = req.body;
-  if (note.title.length && note.content.length !== 0) {
-    console.log(note);
-  }
+router.put('/:id', (req, res) => {
+  const id = req.params.id;
+  const changes = req.body;
+  db.update(id, changes)
+    .then(count => {
+      if (count) {
+        res
+          .status(200)
+          .json({
+            message: `Note number ${id} updated`
+          });
+      } else {
+        res
+          .status(404)
+          .json({
+            message: "Cannot edit a note that doesn't even exist!"
+          })
+      }
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json({
+          message: 'Server error! Could not update that note...',
+          error
+        })
+    });
+});
+
+router.delete('/:id', (req, res) => {
+  db.remove(req.params.id)
+    .then(count => {
+      if (count) {
+        res
+          .status(200)
+          .json({ message: `Note number ${req.params.id} deleted`})
+      } else {
+        res
+          .status(404)
+          .json({
+            message: `We couldn't delete note number ${req.params.id}, make sure you have the right number`
+          })
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({
+          message: `Sorry, we had some trouble deleting note number ${req.params.id}!`,
+          err
+        })
+    })
 })
 
 module.exports = router;
