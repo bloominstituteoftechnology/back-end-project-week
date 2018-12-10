22a88b26bdec590ddd29e9dfb48c042eb9d8ce35
IAN CAMERON <iancameron@IANs-MacBook-Pro.local>
2018-12-10 15:45:01 -0700

    debugs routes

--- config/routes.js
+++ config/routes.js
@@ -1,40 +1,43 @@
 //const axios = require('axios');
 //const bcrypt = require('bcryptjs');
 const db = require('../database/dbConfig.js');
-const { server } = require('../server.js');
 
 
 //import middleware
 
-// module.exports = {
+module.exports = server => {
+    server.get('/notes', getNotes);
+    server.post('/notes', createNote);
+    server.get('/notes/:id', getNoteById);
+    server.put('/notes/:id', editNote);
+    server.delete('/notes/:id', deleteNote);
+}
 
-// }
-
-server.get('/notes', (req, res) => {
+const getNotes = (req, res) => {
     db('notes')
-        .then(res => res.status(200).res.json(res.data))
+        .then(notes => res.status(200).json(notes.data))
         .catch(err => res.status(500).json({ message: 'Error getting notes' }))
-});
+};
 
-server.post('/notes', (req, res) => {
+const createNote = (req, res) => {
     const { title, textBody } = req.body;
     const newNote = { title, textBody };
     db('notes')
         .insert(newNote)
         .then(id => res.status(201).json(id))
         .catch(err => res.status(500).json({ message: 'Error creating note' }))
-});
+};
 
-server.get('/notes/:id', (req, res) => {
+const getNoteById = (req, res) => {
     const { id } = req.params;  //id in {}?
     db('notes')
         .where({ id: id})
         .get(note)
         .then(note => res.status(201).json(note))
         .catch(err => res.status(500).json({ message: 'Error getting note' }))
-});
+};
 
-server.put('/notes/:id', (req, res) => {
+const editNote = (req, res) => {
     const changes = req.body;
     const { id } = req.params;
     db('notes')
@@ -42,13 +45,13 @@ server.put('/notes/:id', (req, res) => {
         .update(changes)
         .then(count => res.status(200).json({ count }))
         .catch(err => res.status(500).json({ message: 'Error editing note' }))
-});
+};
 
-server.delete('/notes/:id', (req, res) => {
+const deleteNote = (req, res) => {
     const { id } = req.params;
     db('notes')
         .where({ id: id })
         .del()
         .then(count => res.status(200).json({ count }))
         .catch(err => res.status(500).json({ message: 'Error deleting note' }))
-});
+};
--- database/dbConfig.js
+++ database/dbConfig.js
@@ -1,4 +1,4 @@
-const knex = require(knex);
+const knex = require('knex');
 
 const knexConfig = require('../knexfile.js');
 
