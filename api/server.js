b994f73705a7ab4db28f785d7bdd44f0d535dd52
bkwillia052 <bkwillia052@gmail.com>
2018-12-10 11:43:55 -0600

    POST and PUT methods functional

--- api/server.js
+++ api/server.js
@@ -24,11 +24,42 @@ server.use(express.json())
 });
 
  server.post('/note/create', async (req, res) => {
+     const note = {title, content} = req.body;
 
+     try{
+        const response = await db('notes').insert(note);
+        res.status(200).json(response) 
+        
+    }
+
+     catch(err){
+        res.status(500).json({message: 'An error occured while processing the data.'})
+     }
 });
 
  server.put('/note/edit/:id', async (req, res) => {
-
+    const id = req.params.id;
+    const {title, content} = req.body;
+    try{
+        if(title && content){
+            const updated = await db('notes').where({id}).update({title, content})
+            res.status(200).json(updated)
+             }
+        else if(title && !content){
+                const updated = await db('notes').where({id}).update({title})
+                res.status(200).json(updated)
+            }
+        else if(!title && content){
+            const updated = await db('notes').where({id}).update({content})
+            res.status(200).json(updated)
+            }
+        else{
+            res.status(400).json({message: 'Please submit the proper inputs.'})
+        } 
+    }
+    catch(err){
+        res.status(500).json({message: 'An error occured while processing the data.'})
+    }
 });
 
  server.delete('/note/delete/:id', async (req, res) => {
