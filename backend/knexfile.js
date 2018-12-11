5eae5e1ce5471c4c618b7857786330f77f703396
Lola <lolahef@gmail.com>
2018-12-11 17:15:48 -0500

    Adds database URL

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -34,16 +34,14 @@ module.exports = {
   production: {
     client: 'postgresql',
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      database: process.env.DATABASE_URL
     },
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      directory: './data/migrations',
     }
   }
 
