f8da77a0da00a682087805f52a7536e920bd7685
MEBonham <programmer@mckaybonham.com>
2018-12-11 12:40:45 -0700

    Prepared for heroku deployment?

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+require("dotenv").config();
+
 const server = require("./api/server.js");
 
-const port = 9000;
+let port = process.env.PORT;
+if (port == null || port == "") {
+    port = 9000;
+}
 
 server.listen(port, () => console.log(`\n** server up on port ${port} **\n`));
