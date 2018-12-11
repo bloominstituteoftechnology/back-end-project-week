6aaa93e9274ffaffc26a5d7d7cbebd63a65d459f
txiong000 <tkx422@gmail.com>
2018-12-11 16:31:32 -0600

    all endpoints done

--- route/notesEndpoint.js
+++ route/notesEndpoint.js
@@ -12,5 +12,59 @@ route.get('/', (req, res) => {
       })
 })
 
+route.get('/:id', (req, res) => {
+    const { id } = req.params;
+    db('notes')
+      .where({ id: id })
+      .first()
+      .then(note => {
+          if(note){
+              res.status(200).json(note)
+          } else {
+              res.status(401).json({message: 'Error note does not exist'})
+          }
+      })
+      .catch(err => {
+          res.status(404).json({message: 'Can not access note'})
+      })
+})
+
+route.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    db('notes')
+      .where({ id: id })
+      .del()
+      .then(count => {
+          if(count){
+              res.status(200).json({message: 'Delete success'})
+          } else {
+              res.status(401).json({message: 'Error note does not exist'})
+          }
+      })
+      .catch(err => {
+          res.status(404).json({message: 'Can not access note'})
+      })
+})
+
+route.post('/add', (req, res)=> {
+    const notes = req.body;
+    db('notes')
+      .insert(notes)
+      .then(note => {
+          console.log(note)
+        res.status(201).json(note);
+      }).catch(err => res.status(404).json({message: 'Error can not add new note'}))
+})
+
+route.put('/:id', (req, res) => {
+    const { id } = req.params;
+    db('notes')
+      .where({ id: id })
+      .update(req.body)
+      .then(note => {
+          res.status(201).json(note)
+      }).catch(err => res.status(404).json({message: 'Unable to update note'}))
+})
+
 
 module.exports = route;
