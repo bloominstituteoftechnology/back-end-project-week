3c11e568f9d4a2d440989f0feffe0520fadd814b
Brandon Vilaychith <blvilaychith@outlook.com>
2018-12-11 12:56:28 -0600

    update

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const server = require('./server.js');
 const PORT = process.env.PORT || 9000;
 
 server.listen(PORT, () => {
-  console.log(`=== Server running on port ${PORT} ===`);
+  console.log(`=== Server running on ports ${PORT} ===`);
 });
