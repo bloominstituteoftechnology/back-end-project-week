d359b3bd494e5b691a81d97c817b86dd02052ca6
Daniel Ferrer <danfrrr13@gmail.com>
2018-12-11 11:55:03 -0500

    adding headers middleware

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ const server = express();
 
 server.use(express.json());
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 // hard coded configuration object
 conf = {
     // look for PORT environment variable,
