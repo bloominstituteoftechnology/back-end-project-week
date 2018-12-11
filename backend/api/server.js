a35932facf6f3ad59e6efcf98f71b8b3dda7e81c
Lola <lolahef@gmail.com>
2018-12-11 00:51:08 -0500

    Adds prod CORS

--- backend/api/server.js
+++ backend/api/server.js
@@ -2,7 +2,7 @@ const notes = require('../data/notesModel.js')
 const express = require('express')
 const server = express()
 const cors = require('cors')
-server.use(cors({ origin: 'http://localhost:8080', credentials: true }))
+server.use(cors({ origin: 'http://lolahef.surge.sh/', credentials: true }))
 server.use(express.json())
 
 server.get('/', (req, res) => {
