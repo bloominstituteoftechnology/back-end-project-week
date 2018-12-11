f0a1d227f54954801ecf700de13c20f7d9d2c181
Hargobind Atwal <ethaipath@gmail.com>
2018-12-11 12:58:45 -0800

    removed nodemon from start script

--- middleware.js
+++ middleware.js
@@ -4,8 +4,8 @@ const helmet = require('helmet')
 const  cors = require('cors')
 module.exports = server => {
   server.use(morgan('dev'))
+  // server.use(cors({ origin: 'localhost:9000' }))
   server.use(cors())
   server.use(helmet())
   server.use(express.json())
-  // server.use(cors({ origin: 'localhost:3000' }))
 }
