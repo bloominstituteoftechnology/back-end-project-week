bb8e7ad510fcc69f61144535af96f22fb9b47c4d
2thumbstech <2thumbstech@gmail.com>
2018-12-11 10:11:19 -0700

    Endpoints

---
+++ migrations/20181210174012_note.js
@@ -0,0 +1,15 @@
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable("notes", function(tbl) {
+      //primary key
+      tbl.increments();
+  
+      tbl.string("title", 128).notNullable();
+      tbl.string("description", 255);
+  
+      tbl.boolean("complete").defaultTo(0);
+    });
+  };
+  
+  exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists("notes");
+  };
