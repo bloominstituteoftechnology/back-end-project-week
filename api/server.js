26d8549c598a8fc5ccd0fab88fe744ddb18c07ae
unknown <quan.nguyen199712@gmail.com>
2018-12-11 12:16:19 -0500

    added cors

--- api/server.js
+++ api/server.js
@@ -1,6 +1,8 @@
 const express= require('express');
 const server= express();
+const cors= require('cors');
 server.use(express.json());
+server.use(cors());
 
 const knex= require('knex')
 const knexConfig= require('../knexfile');
