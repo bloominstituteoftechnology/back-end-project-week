ec911bef134ca1b13f8c8c04429e03c9c5bafbb8
Rahul Desai <rd825@georgetown.edu>
2018-12-11 18:04:14 -0500

    trying to get postgres running

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const server = require('./api/server');
-
+ 
 const port = process.env.PORT || 9000;
 server.listen(port, () => console.log(`Port listening on http://localhost:${port}`));
--- knexfile.js
+++ knexfile.js
@@ -12,4 +12,14 @@ module.exports = {
       directory: './data/seeds',
     },
   },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+        directory: './data/migrations',
+    },
+    seeds: {
+        directory: './data/seeds',
+    },
+  }
 };
