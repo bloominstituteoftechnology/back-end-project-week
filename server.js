53f6818a8e93df9ac507fb91fba82cf9b1ac1293
Jordan Stoddard <jjsincorporated@gmail.com>
2018-12-11 10:04:20 -0800

    Added auth routes and cors config

--- server.js
+++ server.js
@@ -1,20 +1,35 @@
+require('dotenv').config()
 const express = require('express')
 const helmet = require('helmet')
 const cors = require('cors')
+const cookie = require('cookie-session')
+
+
 
 // Routes
 const noteRoutes = require('./routes/noteRoutes')
+const authRoutes = require('./routes/authRoutes.js')
 
 // Initialize Server
 const server = express()
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    credentials: true
+}
 
 // Middleware
 server.use(express.json())
 server.use(helmet())
-server.use(cors())
+server.use(cors(corsOptions))
+server.use(cookie({
+    name: 'login_cookie',
+    secret: process.env.JWT_SECRET,
+    maxAge: 1000 * 60 * 60
+}))
 
 // End Point Routes
 server.use('/api/notes', noteRoutes)
+authRoutes(server)
 
 
 
