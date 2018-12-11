23123ccd3c250645b927361ee3579906efcd37de
Brad Mortensen <b.mort86@gmail.com>
2018-12-10 17:30:59 -0700

    Changes to Server Routes

--- api/server.js
+++ api/server.js
@@ -17,7 +17,7 @@ server.get('/', (req, res) => {
 
 server.get('/api/notes', (req, res) => {
   db('notes')
-    .then(notes => res.status(200).json({notes}))
+    .then(notes => res.status(200).json(notes))
     .catch(err => res.status(500).json(err));
 });
 
