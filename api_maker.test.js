ad547f2055ea9d7c31c628e78f8446a20b8bbe6c
JacobABrennan <jacobabrennan@gmail.com>
2018-12-11 18:06:42 -0500

    Outline apiMaker tests

--- api_maker.test.js
+++ api_maker.test.js
@@ -15,26 +15,43 @@ describe('Test API Maker', () => {
 
     //-- Get All -------------------------------------
     describe.skip('Test Get All Endpoint', function () {
-        
+        // Responds with status 200
+        // Responds with JSON
+        // Responds with an array
+        // Responds with all entries
     });
 
     //-- Get by ID -----------------------------------
     describe.skip('Test Get by ID Endpoint', function () {
-        
+        // Responds with status 200
+        // Responds with JSON
+        // Responds with Entry
+        // Handles unknown IDs (404)
     });
     
     //-- Create --------------------------------------
     describe.skip('Test Create Endpoint', function () {
-        
+        // Responds with status 201
+        // Responds with JSON
+        // Responds with Entry
+        // Handles malformed data (422)
     });
     
     //-- Delete --------------------------------------
     describe.skip('Test Delete Endpoint', function () {
-
+        // Responds with status 200
+        // Responds with JSON
+        // Responds with Entry
+        // Handles unknown IDs (404)
+        // Actually Deletes Entry
     });
     
     //-- Update --------------------------------------
     describe.skip('Test Update Endpoint', function () {
-
+        // Responds with status 200
+        // Responds with JSON
+        // Responds with Entry
+        // Handles unknown IDs (404)
+        // Actually Updates Entry
     });
 });
