de48e93370c67053c7363be79bcbd7c0de5bec2f
Lola <lolahef@gmail.com>
2018-12-11 00:45:41 -0500

    Add heroku env port

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,3 @@
 const server = require('./api/server.js')
 
-server.listen(3300, () => console.log('\n~servin up on port 3300~\n'))
+server.listen(process.env.PORT || 3300, () => console.log('\n~servin up on port 3300~\n'))
