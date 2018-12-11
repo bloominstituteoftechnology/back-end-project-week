dd05e660f262f2bcfa5e8090afca0e4575e957d0
DaftBeowulf <ajmboosh@gmail.com>
2018-12-11 17:25:33 -0600

    frontend implementation of tags complete. wrote backend process for secure registration and login via bcrypt and hashed passwords being stored in the users table of the database

--- api/server.js
+++ api/server.js
@@ -1,24 +1,90 @@
+require("dotenv").config();
+
 // import dependencies
 const express = require("express");
 const cors = require("cors");
+const helmet = require("helmet");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 // internal imports
 const notesRouter = require("../notes/notesRouter");
 const tagsRouter = require("../tags/tagsRouter");
+const db = require("../data/dbConfig");
+
 // init server
 const server = express();
 
 //middleware
+server.use(helmet());
 server.use(cors());
 server.use(express.json());
 
-// sanity check
-server.get("/", (req, res) => {
-  res.status(200).json({ api: "running" });
-});
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username
+  };
+  const secret = process.env.JWT_SECRET;
+  const options = {
+    expiresIn: "1h"
+  };
+  return jwt.sign(payload, secret, options);
+}
 
 // endpoints for notes via router
 server.use("/api/notes", notesRouter);
 // endpoints for tags via router
 server.use("/api/tags", tagsRouter);
+
+// register new user
+server.post("/api/register", (req, res) => {
+  const user = req.body;
+
+  if (!user.username || !user.password) {
+    res.status(422).json({
+      message: "Fill out both username and password fields before registering."
+    });
+  } else {
+    const hash = bcrypt.hashSync(user.password, 14);
+    user.password = hash;
+    db("users")
+      .insert(user)
+      .then(id => res.status(201).json(id))
+      .catch(err =>
+        res
+          .status(500)
+          .json({ error: "Error while registering new user: ", err })
+      );
+  }
+});
+
+// login
+server.post("/api/login", (req, res) => {
+  const creds = req.body;
+
+  if (!creds.username || !creds.password) {
+    res
+      .status(422)
+      .json({ message: "Fill all fields before attempting to log in" });
+  } else {
+    db("users")
+      .where({ username: creds.username })
+      .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(creds.password, user.password)) {
+          const token = generateToken(user);
+          res.status(200).json({ token });
+        } else {
+          res.status(401).json({
+            message: "Access denied, either password or username are incorrect"
+          });
+        }
+      })
+      .catch(err =>
+        res.status(500).json({ error: "Error during login attempt: ", err })
+      );
+  }
+});
+
 module.exports = server;
