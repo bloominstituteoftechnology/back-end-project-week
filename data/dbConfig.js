5c6b9748adf4850c8848a07ee9b268b4f1532264
Lola <lolahef@gmail.com>
2018-12-11 18:27:57 -0500

    Fixes missing env var for Knex

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,3 +1,3 @@
 const knex = require('knex');
-const config = require('../knexfile.js')[process.env];
+const config = require('../knexfile.js')[process.env.ENVIRONMENT];
 module.exports = knex(config);
