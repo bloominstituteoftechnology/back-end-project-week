8fd2fac85f38edde95541caf8ef201751278da72
Steven O'Neill <oneillsteven957@gmail.com>
2018-12-11 12:41:11 -0600

    using the router on the server

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require('express');
 const cors = require('cors');
 const server = express();
 const notesRouter = require('./routers/notesRouter.js');
+const registerRouter = require('./routers/registerRouter.js');
 
 server.use(express.json());
 server.use(cors());
 
+server.use('/api/register', registerRouter);
 server.use('/api/notes', notesRouter);
 
 server.get('/', (req, res) => {
