8720cae2e5cfea6c8c00b4024b060eb69ff7feac
Brandon Vilaychith <blvilaychith@outlook.com>
2018-12-11 13:19:59 -0600

    Heroku db set up

--- knexfile.js
+++ knexfile.js
@@ -10,5 +10,14 @@ module.exports = {
     migrations: {
       directory: './data/migrations'
     }
+  },
+  production: {
+    client: 'postgresql',
+    connection: {
+      database:
+        'postgres://cycqcgrxyznfok:ecc1784ecc3573eb3df95815c18eefc4063c7b73153feaee2e5e6f3da49548cc@ec2-54-163-245-64.compute-1.amazonaws.com:5432/d12idh0ar7rmnq',
+      user: 'username',
+      password: 'password'
+    }
   }
 };
