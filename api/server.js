f47faf0e1f422c9ca011f29000933e2b8629664b
Elvis Ibarra <ielvisd@gmail.com>
2018-12-10 16:24:35 -0800

    Added CORS

--- api/server.js
+++ api/server.js
@@ -1,7 +1,15 @@
 const express = require('express');
 
+var express = require('express');
+
+var cors = require('cors');
+
+// Then use it before your routes are set up:
+
+
 const server = express();
 
+server.use(cors());
 server.use(express.json());
 
 const notesRouter = require('../notes/notesRouter.js');
