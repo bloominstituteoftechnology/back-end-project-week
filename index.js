983fa35a3d34b60ac5649efad2c9ab5fc638907f
Daniel Ferrer <danfrrr13@gmail.com>
2018-12-11 11:59:14 -0500

    another try

--- index.js
+++ index.js
@@ -7,11 +7,13 @@ const server = express();
 
 server.use(express.json());
 
-server.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+server.all('*', function(req, res, next) {
+    var origin = req.get('origin'); 
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
-  });
+});
 
 // hard coded configuration object
 conf = {
