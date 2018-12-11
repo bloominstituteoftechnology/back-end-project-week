a897f4127b8f73207f3c1678b2129e32245f8e5f
bummings <eabummings@gmail.com>
2018-12-11 17:21:16 -0500

    configuring environment variables for postgres

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,11 @@
-// Update with your config settings.
+require('dotenv').config();
+const localPg = {
+  host: 'localhost',
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS
+};
+const dbConnection = process.env.DATABASE_URL || localPg;
 
 module.exports = {
   development: {
@@ -8,6 +15,21 @@ module.exports = {
     },
     useNullAsDefault: true,
     migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    }
+  },
+  production: {
+    client: 'pg',
+    connection: dbConnection,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations',
       directory: './data/migrations'
     },
     seeds: {
