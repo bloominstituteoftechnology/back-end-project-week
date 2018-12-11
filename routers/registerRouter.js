e5ae2c48e904cabccc9dfd23f40de6bf4d611c3e
Steven O'Neill <oneillsteven957@gmail.com>
2018-12-11 12:38:18 -0600

    created register router

---
+++ routers/registerRouter.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bcrypt = require('bcryptjs');
+
+const db = require('../database/dbConfig.js');
+
+const router = express.Router();
+
+router.post('/', (req, res) => {
+    const creds = req.body;
+  
+    const hash = bcrypt.hashSync(creds.password, 12);
+  
+    creds.password = hash;
+  
+    db('users')
+      .insert(creds)
+      .then(ids => {
+        res.status(201).json(ids);
+      })
+      .catch(err => res.status(500).json({message: `An error occured: ${err}`}))
+  });
+
+  module.exports = router;
