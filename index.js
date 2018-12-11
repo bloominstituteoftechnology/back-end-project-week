69a352f55753ec1b0381841247595f39f0aa530e
Daniel Ferrer <danfrrr13@gmail.com>
2018-12-11 11:57:30 -0500

    minor typo

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const server = express();
 
 server.use(express.json());
 
-app.use(function(req, res, next) {
+server.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
