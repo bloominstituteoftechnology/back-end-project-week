045684fd53db30840e83b8e250297a286a63381c
Jordan Stoddard <jjsincorporated@gmail.com>
2018-12-12 13:56:30 -0800

    fix /:id get

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -32,12 +32,11 @@ route.get("/:id", authenticate, (req, res) => {
           console.log('||||||TAGS HERE||||||', tags)
           note.tags = tags;
           console.log('|||||||NOTE HERE |||||||', note)
-          res.status(200).json(note);
+          return res.status(200).json(note);
         });
       } else {
           res.status(404).json({ message: "note not found" });
         }
-    res.status(200).json(note)
     })
     .catch(err => res.status(500).json({ message: `Error: ${err}` }));
 });
