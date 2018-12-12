5175109a6307c1360de3c5bcf955628ddb4b6b32
shawn <shawnantonucci2018@gmail.com>
2018-12-12 08:31:51 -0800

    refactore

--- api/server.js
+++ api/server.js
@@ -7,11 +7,6 @@ const notes = require("../note.js");
 const knexConfig = require("../knexfile.js");
 const db = knex(knexConfig.development);
 
-const server = express();
-server.use(express.json());
-server.use(cors());
-
-const port = process.env.PORT || 5000;
 
 server.get("/", (req, res) => {
   res.send(`API running on port: ${port}`);
