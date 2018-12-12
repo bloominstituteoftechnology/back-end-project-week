065c7d87e55b30ea110e708c59dae02e6434c0ba
Michael Checo <checomichael2@gmail.com>
2018-12-12 11:25:26 -0500

    committed out endpoints and added one simple get request

--- index.js
+++ index.js
@@ -8,66 +8,69 @@ const server = express();
 server.use(express.json());
 server.use(cors());
 
-server.get('/api/notes', (req, res) => {
-    db('notes')
-    .then(notes => {
-        res.status(200).json(notes)
-    })
-    .catch(err => {
-        res.status(500).json(err)
-    })
+server.get('/', (req, res) => {
+    res.send(`running on port : ${port}`)
 })
+// server.get('/api/notes', (req, res) => {
+//     db('notes')
+//     .then(notes => {
+//         res.status(200).json(notes)
+//     })
+//     .catch(err => {
+//         res.status(500).json(err)
+//     })
+// })
 
-server.get('/api/notes/:id', (req, res) => {
-    const { id } = req.params;
-    db('notes')
-    .where({id})
-    .then(note => {
-        res.status(200).json(note)
-    })
-    .catch(err => {
-        res.status(500).json(err)
-    })
-})
+// server.get('/api/notes/:id', (req, res) => {
+//     const { id } = req.params;
+//     db('notes')
+//     .where({id})
+//     .then(note => {
+//         res.status(200).json(note)
+//     })
+//     .catch(err => {
+//         res.status(500).json(err)
+//     })
+// })
 
-server.post('/api/notes', (req, res) => {
-    const note  = req.body;
-    db('notes')
-    .insert(note)
-    .then(ids => {
-        res.status(201).json(ids)
-    })
-    .catch(err => {
-        res.status(500).json(err)
-    })
-})
+// server.post('/api/notes', (req, res) => {
+//     const note  = req.body;
+//     db('notes')
+//     .insert(note)
+//     .then(ids => {
+//         res.status(201).json(ids)
+//     })
+//     .catch(err => {
+//         res.status(500).json(err)
+//     })
+// })
 
-server.put('/api/notes/:id', (req, res) => {
-    const { id } = req.params;
-    const changes = req.body;
-    db('notes')
-    .where({ id })
-    .update(changes)
-    .then(count => {
-        res.status(200).json(count)
-    })
-    .catch(err => {
-        res.status(500).json(err)
-    })
-})
+// server.put('/api/notes/:id', (req, res) => {
+//     const { id } = req.params;
+//     const changes = req.body;
+//     db('notes')
+//     .where({ id })
+//     .update(changes)
+//     .then(count => {
+//         res.status(200).json(count)
+//     })
+//     .catch(err => {
+//         res.status(500).json(err)
+//     })
+// })
 
-server.delete('/api/notes/:id', (req, res) => {
-    const { id } = req.params;
-    db('notes')
-    .where({ id })
-    .del()
-    .then(count => {
-        res.status(200).json(count)
-    })
-    .catch(err => {
-        res.status(500).json(err)
-    })
-})
+// server.delete('/api/notes/:id', (req, res) => {
+//     const { id } = req.params;
+//     db('notes')
+//     .where({ id })
+//     .del()
+//     .then(count => {
+//         res.status(200).json(count)
+//     })
+//     .catch(err => {
+//         res.status(500).json(err)
+//     })
+// })
 
 
 
