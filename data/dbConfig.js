d070c95087c0d95bf847cd8ea3732fe34439ee49
Hargobind Atwal <ethaipath@gmail.com>
2018-12-12 09:49:16 -0800

    heroku enviorment vars

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,3 +1,3 @@
-const dbEnviorment = process.env.DB || 'development'
+const dbEnviorment = process.env.DB_ENVIRONMENT || 'development'
 const knexConfig = require('../knexfile')[dbEnviorment]
 module.exports = require('knex')(knexConfig)
--- data/seeds/notes.js
+++ data/seeds/notes.js
@@ -1,13 +1,13 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('notes').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('notes').insert([
+        {title: 'first', content: 'hey'},
+        {title: 'second', content: 'sup'},
+        {title: 'third', content: 'yo'},
       ]);
     });
 };
