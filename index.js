e4d966de7a31ed394b2729fdfab5973bcf6b7580
bkwillia052 <bkwillia052@gmail.com>
2018-12-12 11:52:57 -0600

    Heroku config

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const PORT = process.env.PORT || 8888;
 
 server.get('/', (req, res)=>{
-    res.send(`Server is running on ${PORT}`)
+    res.send(`Server is running on ${PORT} and dev env is ${process.env.DB_ENVIRONMENT}`)
 });
 server.listen(PORT, ()=>{
     console.log(`Server listening on Port ${PORT}`)
