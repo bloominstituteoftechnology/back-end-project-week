30638a5db75188945fc1c64f72f982824d3db9be
jaklundun <govi.escudero@gmail.com>
2018-12-12 01:54:44 -0800

    POST for notes and actions now complete.

--- index.js
+++ index.js
@@ -45,4 +45,66 @@ server.get('/api/notes/:id', (req, res) => {
   });
 
 
+  // POST notes: create a note with title && content
+
+server.post('/api/notes', async (req, res) => {
+    const note = req.body;
+    
+    if (!note.title || !note.content) {
+        const err = "Please provide title and content for note."; 
+        res.status(400).json({err});
+        return
+    }
+
+    try {
+        await db('notes').insert(note);
+    
+    } catch (err) {
+            res.status(500).json({err: "Error while saving to database."});
+            return      
+    }
+        res.status(201).json(note);
+        return
+    });
+
+// POST Action: saves new action to database. Will need ID to work.
+
+server.post('/api/actions/:id', async (req, res) => {
+    const { id } = req.params; 
+    const action = req.body;
+    const charLimit = 188;
+    let addAction;
+  
+    if (!action.project_id || action.project_id !== id || action.project_id === "" ) {
+        const err = "Please provide correct project ID."; 
+        res.status(400).json({ err });
+        return
+    } 
+  
+    if (!action.description|| action.description==="" || !action.notes || action.notes===""  ) {
+        const err = "Please input a note and description for the project."; 
+        res.status(400).json({ err });
+        return
+    }  
+
+    if (action.description.length > charLimit) {
+        const err = "Please provide description under 188 characters."; 
+        res.status(400).json({ err });
+        return
+    }  
+
+    try {
+        addAction = await db('actions').insert(action)
+
+    } catch (err) {
+        console.log(err)
+            res.status(500).json({ error: "Error while trying to save to database." });
+            return      
+    }
+
+    res.status(201).json(addAction);
+        return
+});
+
+    
 server.listen(8888, () => console.log('\nrunning on port 8888\n'));
