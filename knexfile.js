0c6ecae2b5a8fa0ac03494fccfc8d0b1a11b4015
Chiboba <briangfelix@gmail.com>
2018-12-12 11:52:53 -0800

    Heroku

--- knexfile.js
+++ knexfile.js
@@ -50,7 +50,11 @@ module.exports = {
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      tableName: 'knex_migrations',
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds'
     }
   }
 
