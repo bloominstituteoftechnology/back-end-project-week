8471862c7bf41197505784d820d3cc650640ef04
sarahtennis <sarahtennis@Sarahs-Air.fios-router.home>
2018-12-12 17:18:18 -0500

    changes longtext to text in notes migration for postgres

--- data/migrations/20181210121830_notes.js
+++ data/migrations/20181210121830_notes.js
@@ -3,7 +3,7 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('notes', tbl => {
     tbl.increments();
     tbl.string('title', 128).notNullable();
-    tbl.longtext('textBody');
+    tbl.text('textBody');
     tbl.string('tags', 255);
     tbl.integer('user_id').unsigned().notNullable();
     tbl.foreign('user_id').references('id').inTable('users').onDelete('CASCADE');
