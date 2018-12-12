25a098bc6c5b61e5d22da032b08e669c63043720
koennecker <koenneckerb@gmail.com>
2018-12-12 13:46:46 -0700

    Add postgres config for deployment.

--- server/knexfile.js
+++ server/knexfile.js
@@ -15,4 +15,16 @@ module.exports = {
       directory: "./database/seeds"
     }
   },
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: "knex_migrations",
+      directory: "./database/migrations"
+    }
+  }
 };
