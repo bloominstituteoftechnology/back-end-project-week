a5f8e899a0212368c0d679ed760391e1e2d12439
mikaelacurrier <mikaela.currier@gmail.com>
2018-12-12 11:38:29 -0500

    fixed port issue - was declared twice

--- server.js
+++ server.js
@@ -3,5 +3,4 @@ const server = require('./api/index.js');
 
 const port = process.env.PORT || 9000;
 
-const port = 8000;
 server.listen(port, () => console.log(`\n Server is running on port ${port}\n`))
