2a59bc83301ca80b6dbe33a267ba22a297fb9ad2
zacharyarney <zachary.arney@gmail.com>
2018-12-11 18:07:43 -0600

    still figuring out pg

--- knexfile.js
+++ knexfile.js
@@ -33,7 +33,7 @@ module.exports = {
   },
 
   production: {
-    client: 'pg',
+    client: 'postgresql',
     connection: process.env.DATABASE_URL,
     migrations: {
       tableName: 'knex_migrations'
