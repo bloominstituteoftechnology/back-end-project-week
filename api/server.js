76bb759909b6f400b61b99a65525cb5e23112b3c
MEBonham <programmer@mckaybonham.com>
2018-12-12 14:37:45 -0700

    simplifying json(idReturned)

--- api/server.js
+++ api/server.js
@@ -27,7 +27,7 @@ server.post("/api/notes", (req, res) => {
             .insert(newPost)
             .then(idReturned => {
                 console.log(idReturned);
-                res.status(201).json({ success: idReturned[0] });
+                res.status(201).json(idReturned);
             })
             .catch(err => {
                 res.status(500).json({ message: "Cannot post note to database.", err });
