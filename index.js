19ba6f8efc716a65bc8d51a336c3c4229b52cb22
Michael Checo <checomichael2@gmail.com>
2018-12-12 11:34:44 -0500

    .env config

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors')
 const knex = require('knex')
 const knexConfig = require('./knexfile')
-// require('dotenv').config()
+require('dotenv').config()
 const db = knex(knexConfig.development)
 const server = express();
 server.use(express.json());
