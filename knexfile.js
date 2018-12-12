d77942d37260343454b3cf18184980d16057f132
Matthew Wright <matthewbwright@gmail.com>
2018-12-12 10:57:46 -0700

    points in the right direction

--- knexfile.js
+++ knexfile.js
@@ -33,10 +33,10 @@ module.exports = {
       },
       migrations: {
         tableName: 'knex_migrations',
-        directory: './db/migrations'
+        directory: './data/migrations'
       },
       seeds: {
-        directory: './db/seeds'
+        directory: './data/seeds'
       }
     }
   }
