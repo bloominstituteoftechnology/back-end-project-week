56abe727cd75107da890c53c67b54c61a913879b
Lola <lolahef@gmail.com>
2018-12-12 14:42:49 -0500

    Fixes notesModel

--- data/notesModel.js
+++ data/notesModel.js
@@ -7,8 +7,9 @@ async function get(id) {
 }
 
 async function add(note) {
-    const [id] = await db('notes').insert(note)
-    return db('notes').where({ id }).first()
+    const id = await db('notes').insert(note)
+    // return db('notes').where({ id }).first()
+    return get(id)
 }
 
 async function update(id, changes) {
