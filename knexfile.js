033159786e20eda7b3ff3556588520c4589cdedf
Brian G <brian.gregorius@gmail.com>
2018-12-12 18:20:54 -0500

    all changes were for postgres db

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,12 @@
 // Update with your config settings.
+const localPgConnection = {
+  host: 'localhost',
+  database: 'lambda',
+  user: 'bg',
+  pass: 'pass',
+};
+
+const dbConnection = process.env.DATABASE_URL || localPgConnection;
 
 module.exports = {
 
@@ -8,6 +16,20 @@ module.exports = {
       filename: './database/notes.sqlite3'
     },
     useNullAsDefault: true,
+    migrations: {
+      directory: './database/migrations'
+    },
+    seeds: {
+      directory: './database/seeds'
+    },
+  },
+  production: {
+    client: 'pg',
+    connection: dbConnection,
+    pool: {
+      min: 2,
+      max: 10,
+    },
     migrations: {
       directory: './database/migrations'
     },
