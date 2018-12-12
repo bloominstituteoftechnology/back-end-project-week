424d488156cc5b1d1aebac614cce002bfa6b7ae8
Yanrong Xu <xuyanrong@icloud.com>
2018-12-12 13:11:04 -0500

    something wrong with  my database

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -1,4 +1,5 @@
-
-const knexConfig = require('../knexfile.js')['production'];
+// const dbEngine = process.env.DB || 'development'
+const dbEnvironment = process.env.DB_ENVIRONMENT || 'development';
+const knexConfig = require('../knexfile.js')[dbEnvironment];
 
 module.exports = require('knex')(knexConfig);
