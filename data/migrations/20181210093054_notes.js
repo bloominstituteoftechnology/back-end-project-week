5e9ef867fc72cb8ab699ca75c1506f4a8edf6ef4
Brad Mortensen <b.mort86@gmail.com>
2018-12-12 16:44:39 -0700

    updates DB

--- data/migrations/20181210093054_notes.js
+++ data/migrations/20181210093054_notes.js
@@ -3,7 +3,7 @@ exports.up = function (knex, Promise) {
   //makes changes to the database
   return knex.schema.createTable('notes', function (tbl) {
     //primary key
-    tbl.increments('_id');
+    tbl.increments();
     //other fields
     tbl.string('title', 120).notNullable();
     tbl.string('textBody', 1000);
--- data/seeds/001-notes.js
+++ data/seeds/001-notes.js
@@ -6,63 +6,63 @@ exports.seed = function(knex, Promise) {
       // Inserts seed entries
       return knex('notes').insert([
         {
-          "_id": 1,
+          "id": 1,
           "title": "Leontine",
           "textBody": "Reide"
         }, {
-          "_id": 2,
+          "id": 2,
           "title": "Scotti",
           "textBody": "Swanton"
         }, {
-          "_id": 3,
+          "id": 3,
           "title": "Normand",
           "textBody": "Kinnar"
         }, {
-          "_id": 4,
+          "id": 4,
           "title": "Lise",
           "textBody": "Mushart"
         }, {
-          "_id": 5,
+          "id": 5,
           "title": "Sig",
           "textBody": "Bims"
         }, {
-          "_id": 6,
+          "id": 6,
           "title": "Philippe",
           "textBody": "Heiton"
         }, {
-          "_id": 7,
+          "id": 7,
           "title": "Stirling",
           "textBody": "Jakucewicz"
         }, {
-          "_id": 8,
+          "id": 8,
           "title": "Laure",
           "textBody": "Gude"
         }, {
-          "_id": 9,
+          "id": 9,
           "title": "Agosto",
           "textBody": "Rapi"
         }, {
-          "_id": 10,
+          "id": 10,
           "title": "Teodora",
           "textBody": "Vasyushkhin"
         }, {
-          "_id": 11,
+          "id": 11,
           "title": "Hercules",
           "textBody": "Liddall"
         }, {
-          "_id": 12,
+          "id": 12,
           "title": "Carl",
           "textBody": "De Caroli"
         }, {
-          "_id": 13,
+          "id": 13,
           "title": "Tomaso",
           "textBody": "Dugmore"
         }, {
-          "_id": 14,
+          "id": 14,
           "title": "Aidan",
           "textBody": "Claricoats"
         }, {
-          "_id": 15,
+          "id": 15,
           "title": "Katherina",
           "textBody": "Ancketill"
         }
