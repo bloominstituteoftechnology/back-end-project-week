a232008df7550774093f31aad032ae2c1ed1a4dd
Hargobind Atwal <ethaipath@gmail.com>
2018-12-12 10:55:03 -0800

    added knex-cleaner

--- data/seeds/notes.js
+++ data/seeds/notes.js
@@ -1,13 +1,8 @@
 
-exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('notes').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('notes').insert([
-        {title: 'first', content: 'hey'},
-        {title: 'second', content: 'sup'},
-        {title: 'third', content: 'yo'},
-      ]);
-    });
+exports.seed = function(knex) {
+    return knex('notes').insert([
+      {title: 'first', content: 'hey'},
+      {title: 'second', content: 'sup'},
+      {title: 'third', content: 'yo'},
+    ]);
 };
