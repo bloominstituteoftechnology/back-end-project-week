9f630955fb596191242002275c688c3c96a4c05e
shawn <shawnantonucci2018@gmail.com>
2018-12-12 14:36:35 -0800

    fixed port

--- lambdaNotes/src/index.js
+++ lambdaNotes/src/index.js
@@ -1,18 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
-import reduxThunk from "redux-thunk";
+// import { Provider } from "react-redux";
+// import { createStore, applyMiddleware } from "redux";
+// import reduxThunk from "redux-thunk";
 
 import App from "./App";
-import reducers from "./reducers";
+// import reducers from "./reducers";
 
-const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+// const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+    <App />,
   document.querySelector("#root")
 );
 
