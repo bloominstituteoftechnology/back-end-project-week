7be2b113f87d4659b9fb7ec5dee585880ce29291
ssmodish <smodish@hotmail.com>
2018-12-12 13:41:10 -0500

    edits for heroku

--- data/config.js
+++ data/config.js
@@ -1,4 +1,4 @@
-const dbEnvironment = process.env.DB_ENVIRONMENT || 'development';
+const dbEnvironment = process.env.DB_ENVIRONMENT || 'production';
 const config = require('../knexfile.js')[dbEnvironment];
 
 module.exports = require('knex')(config);
