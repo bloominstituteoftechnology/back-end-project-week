689cbd2c3ed0708b00e0943b94cf80d8be6e0e4d
shawn <shawnantonucci2018@gmail.com>
2018-12-12 14:18:01 -0800

    accepted change

--- lambdaNotes/src/setupProxy.js
+++ lambdaNotes/src/setupProxy.js
@@ -3,9 +3,5 @@ const proxy = require("http-proxy-middleware");
 module.exports = app => {
   app.use(proxy('/auth/google', { target: "http://localhost:5000" }));
   app.use(proxy('/api/*', { target: "http://localhost:5000" }));
-<<<<<<< HEAD
-  app.use(proxy('*', { target: "http://localhost:5000/api/notes" }));
-=======
-  app.use(proxy('*', { target: "http://localhost:5000/api/notes" }));
->>>>>>> ec42d54d5138396f4c4f13696a786619343425db
+  app.use(proxy('/*', { target: "http://localhost:5000/api/notes" }));
 };
