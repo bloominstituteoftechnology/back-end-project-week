990fded3c049a9a44d15d99d1c58881d3633bf7b
Brandon Vilaychith <blvilaychith@outlook.com>
2018-12-12 14:02:06 -0600

    update

--- knexfile.js
+++ knexfile.js
@@ -1,3 +1,13 @@
+require('dotenv').config();
+
+const localPgConnection = {
+  host: 'localhost', // address to find the db server
+  database: 'lambda',
+  user: 'brandon',
+  password: 'pass'
+};
+
+const dbConnection = process.env.DATABASE_URL || localPgConnection;
 // Update with your config settings.
 module.exports = {
   development: {
@@ -7,6 +17,18 @@ module.exports = {
     },
     useNullAsDefault: true,
     migrations: {
+      directory: './data/migrations'
+    }
+  },
+  production: {
+    client: 'pg',
+    connection: dbConnection, // can be and object or a string
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations',
       directory: './data/migrations'
     }
   }
