d9d1ec43cdd49748c0ca41ecfb2312850d9b14ef
Michael Checo <checomichael2@gmail.com>
2018-12-12 12:38:45 -0500

    configures db environment

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,11 @@
 // Update with your config settings.
-
+const localPgConnection = {
+  host: 'localhost',
+  database: 'lambda',
+  user: 'michael',
+  password: 'pass'
+}
+const dbConnection = process.env.DATABASE_URL || localPgConnection
 module.exports = {
 
   development: {
@@ -14,5 +20,20 @@ module.exports = {
     seeds: {
       directory: './data/seeds'
     }
-  }
+  },
+  production: {
+    client: 'pg',
+    connection: dbConnection,
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
+  },
 };
