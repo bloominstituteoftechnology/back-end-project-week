7bca200c028942a4e5dc1201df34196b9f81eb1b
jaklundun <govi.escudero@gmail.com>
2018-12-11 22:51:14 -0800

    GET for display list of notes and view existing note complete.

--- index.js
+++ index.js
@@ -21,5 +21,28 @@ server.get('/', (request, response) => {
     response.send("Let's QiGongGO!")
 });
 
+// GET Notes: display list of notes
+
+server.get('/api/notes', (req, res) => {
+    db('notes') 
+    .then(notes=> { 
+      res.status(200).json(notes);
+    }) 
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({ error: "User notes not available." });
+    });
+});
+
+// View an existing note.
+
+server.get('/api/notes/:id', (req, res) => {
+    const { id } = req.params; 
+    db('notes')
+    .where({ id })
+    .then(notes => res.status(200).json(notes))
+    .catch(err => res.status(500).json(err));
+  });
+
 
 server.listen(8888, () => console.log('\nrunning on port 8888\n'));
