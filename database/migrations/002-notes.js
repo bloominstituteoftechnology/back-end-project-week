e960f2dfbfc5b054b36ea652675732487c1dd650
koennecker <koenneckerb@gmail.com>
2018-12-12 15:42:22 -0700

    Modify migration again.

---
+++ database/migrations/002-notes.js
@@ -0,0 +1,16 @@
+exports.up = knex =>
+  knex.schema.createTable("notes", notes => {
+    notes.increments();
+    notes
+      .integer("user_id")
+      .unsigned()
+      .notNullable()
+      .references("id")
+      .inTable("users");
+    notes.string("title", 128).notNullable();
+    notes.text("content").notNullable();
+    notes.timestamp("created").defaultTo(knex.fn.now());
+    notes.timestamp("modified").defaultTo(knex.fn.now());
+  });
+
+exports.down = knex => knex.schema.dropTableIfExists("notes");
