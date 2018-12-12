2ad6e87267448a5237bf082caf97d98cac0b3e36
adamhinckley <adamhinckley@mac.com>
2018-12-12 11:00:29 -0700

    adds update to knexfile

--- knexfile.js
+++ knexfile.js
@@ -30,10 +30,10 @@ module.exports = {
     },
     migrations: {
       tableName: "knex_migrations",
-      directory: "./db/migrations"
+      directory: "./database/migrations"
     },
     seeds: {
-      directory: "./db/seeds"
+      directory: "./database/seeds"
     }
   }
 };
