b2bc1334dbbd46ec032f06f1dfeb529e129c30c9
Angelo Deleon <rudy209@comcast.net>
2018-12-12 10:42:14 -0800

    Changed dbConfig

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,9 +1,8 @@
-const dbEnviroment = process.env.DB_ENVIROMENT || 'development';
+const dbEnviroment = process.env.DB_ENVIROMENT || 'production';
 
 // const config = require('../knexfile.js')['development'];
 
 const config = require('../knexfile.js')[dbEnviroment];
-// const config = require
 
 module.exports = require('knex')(config);
 
