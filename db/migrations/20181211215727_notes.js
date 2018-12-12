3ae16ad0bc1b28dcba628ccf3d0804c55f1de53a
jaklundun <govi.escudero@gmail.com>
2018-12-11 22:00:23 -0800

    Configured notes migration.

--- db/migrations/20181211215727_notes.js
+++ db/migrations/20181211215727_notes.js
@@ -1,8 +1,19 @@
-
 exports.up = function(knex, Promise) {
-  
-};
+   
+    return knex.schema.createTable('notes', function(tbl) {
+        
+        tbl.increments(); // pk
+    
+        tbl // data fields and attributes
+        .string('title', 288)
+        .notNullable()
+        tbl
+        .string('content', 3333)
+        .notNullable()
+        
+      });
+    };
 
-exports.down = function(knex, Promise) {
-  
+ exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('notes'); // undo === Rollback
 };
