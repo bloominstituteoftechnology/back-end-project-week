fedc1b5a5e3021b6bff531fd531cee0f758463d4
sean-one <basement365@gmail.com>
2018-12-12 08:10:17 -0800

    update

--- api/server.js
+++ api/server.js
@@ -18,13 +18,24 @@ server.get('/api/notes', async (req, res) => {
 
 server.get('/api/notes/:id', async (req, res) => {
     const id = req.params.id;
-    const note = await db('notes').where({ id: id})
-    res.status(200).json(note);
+    const note = await db('notes').where({ id: id}).first();
+    res.status(200).json({note});
 })
 
-server.delete('/api/notes/:id', async (req, res) => {
-    const id = req.params.id;
-    res.status(200).json(id);
+server.post('/api/notes/new', async (req, res) => {
+    const note = req.body;
+    if (!note.title || !note.textBody) {
+        res.status(400).json({ message: 'you did something wrong' })
+    } else {
+        const noteId = await db('notes').insert({ title: note.title, textBody: note.textBody });
+        res.status(200).json(noteId);
+    }
+
 })
+
+// server.delete('/api/notes/:id', async (req, res) => {
+//     const id = req.params.id;
+//     res.status(200).json(id);
+// })
 
 module.exports = server;
