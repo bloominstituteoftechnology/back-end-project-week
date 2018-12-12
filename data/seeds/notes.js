b3a0446742c73c608fddcaf1b90e1373c3b4d3a1
Matthew Wright <matthewbwright@gmail.com>
2018-12-12 11:55:18 -0700

    uses cleanup

--- data/seeds/notes.js
+++ data/seeds/notes.js
@@ -1,13 +1,8 @@
 exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('notes')
-    .truncate()
-    .then(function() {
-      // Inserts seed entries
-      return knex('notes').insert([
-        { title: 'Test Note 1', body: 'This is test note 1' },
-        { title: 'Test Note 2', body: 'This is test note 2' },
-        { title: 'Test Note 3', body: 'This is test note 3' }
-      ]);
-    });
+  // Inserts seed entries
+  return knex('notes').insert([
+    { title: 'Test Note 1', body: 'This is test note 1' },
+    { title: 'Test Note 2', body: 'This is test note 2' },
+    { title: 'Test Note 3', body: 'This is test note 3' }
+  ]);
 };
