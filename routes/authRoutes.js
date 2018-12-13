e3825b27ccdacb1a1bd61b1cae7fa1f7e6dbeebc
shawn <shawnantonucci2018@gmail.com>
2018-12-13 13:16:47 -0800

    fixed routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -10,7 +10,7 @@ module.exports = app => {
     "/auth/google/callback", 
     passport.authenticate("google"),
     (req, res) => {
-      res.redirect("/api/notes");
+      res.redirect("/");
     }
   );
 
