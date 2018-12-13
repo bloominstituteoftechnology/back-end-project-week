70a7d5a4a5fb73980f7812777b399c87e97b188a
Yanrong Xu <xuyanrong@icloud.com>
2018-12-13 13:07:38 -0500

    change the token to response

--- config/routes.js
+++ config/routes.js
@@ -70,7 +70,7 @@ function getNote(req, res) {
     db('notes')
         .where({username: token.username})
         .then(response => {
-            res.status(200).json(token)
+            res.status(200).json(response)
         })
         .catch(err => {
             res.status(500).json({message: 'Error fetching notes'})
