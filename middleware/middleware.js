545855ac69fffbd5b171bb32920c589701d71562
adamhinckley <adamhinckley@mac.com>
2018-12-13 11:53:09 -0700

    removes middleware that didn't work

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -36,10 +36,3 @@ function protected(req, res, next) {
     res.status(401).json({ message: "no token provided" });
   }
 }
-
-function allowCrossDomain(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  next();
-}
