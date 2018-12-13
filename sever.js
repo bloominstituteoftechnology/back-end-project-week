d1f73736d056b30a35512d8756e9aa9d632dc4b2
David247365 <ds051128@gmail.com>
2018-12-13 14:39:51 -0600

    Making port Dynamic

--- sever.js
+++ sever.js
@@ -17,100 +17,100 @@ server.get("/", (req, res) => {
   res.status(200).json({ message: `Up and running on port: ${port}` });
 });
 
-// Grab the data from the notes table
+// // Grab the data from the notes table
 
-server.get("/notes/get/all", (req, res) => {
-  db("notes")
-    .then(notes => {
-      res.status(200).json(notes);
-    })
-    .catch(err => {
-      res.status(500).json({ message: "There was an error, please try again" });
-    });
-});
+// server.get("/notes/get/all", (req, res) => {
+//   db("notes")
+//     .then(notes => {
+//       res.status(200).json(notes);
+//     })
+//     .catch(err => {
+//       res.status(500).json({ message: "There was an error, please try again" });
+//     });
+// });
 
-// Creates a note and places the data into the notes table
-server.post("/notes", (req, res) => {
-  const { title, message } = req.body;
-  if (!title || !message) {
-    res.status(422).json({ message: "Please include a title and message" });
-  } else {
-    db("notes")
-      .insert(req.body)
-      .then(id => {
-        res.status(201).json(id);
-      })
-      .catch(err => {
-        res.status(500).json({
-          message: "There was an error, please try again."
-        });
-      });
-  }
-});
+// // Creates a note and places the data into the notes table
+// server.post("/notes", (req, res) => {
+//   const { title, message } = req.body;
+//   if (!title || !message) {
+//     res.status(422).json({ message: "Please include a title and message" });
+//   } else {
+//     db("notes")
+//       .insert(req.body)
+//       .then(id => {
+//         res.status(201).json(id);
+//       })
+//       .catch(err => {
+//         res.status(500).json({
+//           message: "There was an error, please try again."
+//         });
+//       });
+//   }
+// });
 
-// View individual notes by id
+// // View individual notes by id
 
-server.get("/notes/get/:id", (req, res) => {
-  const { id } = req.params;
-  db("notes")
-    .where("id", id)
-    .then(note => {
-      if (!note.length) {
-        res
-          .status(404)
-          .json({ message: "Could not find note with specified Id" });
-      } else {
-        res.status(200).json(note);
-      }
-    })
-    .catch(err => {
-      res.status(500).json({ message: "There was an error, please try again" });
-    });
-});
+// server.get("/notes/get/:id", (req, res) => {
+//   const { id } = req.params;
+//   db("notes")
+//     .where("id", id)
+//     .then(note => {
+//       if (!note.length) {
+//         res
+//           .status(404)
+//           .json({ message: "Could not find note with specified Id" });
+//       } else {
+//         res.status(200).json(note);
+//       }
+//     })
+//     .catch(err => {
+//       res.status(500).json({ message: "There was an error, please try again" });
+//     });
+// });
 
-// Edit individual notes
+// // Edit individual notes
 
-server.put("/note/edit/:id", (req, res) => {
-  const changes = req.body;
-  const { id } = req.params;
+// server.put("/note/edit/:id", (req, res) => {
+//   const changes = req.body;
+//   const { id } = req.params;
 
-  if (changes.title === "" || changes.message === "") {
-    res.status(400).json({ message: "Please Include a title and message" });
-  } else {
-    db("notes")
-      .where({ id })
-      .update(changes)
-      .then(count => {
-        res.status(200).json({ message: `Updated ${count} notes` });
-      })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ message: "There was an error, please try again" });
-      });
-  }
-});
+//   if (changes.title === "" || changes.message === "") {
+//     res.status(400).json({ message: "Please Include a title and message" });
+//   } else {
+//     db("notes")
+//       .where({ id })
+//       .update(changes)
+//       .then(count => {
+//         res.status(200).json({ message: `Updated ${count} notes` });
+//       })
+//       .catch(err => {
+//         res
+//           .status(500)
+//           .json({ message: "There was an error, please try again" });
+//       });
+//   }
+// });
 
-// Deletes individual notes by their id
+// // Deletes individual notes by their id
 
-server.delete("/notes/:id", (req, res) => {
-  const { id } = req.params;
+// server.delete("/notes/:id", (req, res) => {
+//   const { id } = req.params;
 
-  db("notes")
-    .where({ id })
-    .del()
-    .then(count => {
-      if (count === 0) {
-        res
-          .status(404)
-          .json({ message: "Could not find note with specified id" });
-      } else {
-        res.status(200).json({ message: `deleted: ${count} note` });
-      }
-    })
-    .catch(err => {
-      res.status(500).json({ message: "There was an error, please try again" });
-    });
-});
+//   db("notes")
+//     .where({ id })
+//     .del()
+//     .then(count => {
+//       if (count === 0) {
+//         res
+//           .status(404)
+//           .json({ message: "Could not find note with specified id" });
+//       } else {
+//         res.status(200).json({ message: `deleted: ${count} note` });
+//       }
+//     })
+//     .catch(err => {
+//       res.status(500).json({ message: "There was an error, please try again" });
+//     });
+// });
 
 module.exports = server;
