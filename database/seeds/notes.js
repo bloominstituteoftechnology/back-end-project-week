0d10ba3a650239516672861e4f974ca574f2edfb
adamhinckley <adamhinckley@mac.com>
2018-12-13 10:52:46 -0700

    removes id from seeds

--- database/seeds/notes.js
+++ database/seeds/notes.js
@@ -1,17 +1,14 @@
 exports.seed = function(knex) {
   return knex("notes").insert([
     {
-      id: 1,
       noteTitle: "Note One",
       noteBody: "new note"
     },
     {
-      id: 2,
       noteTitle: "Note Two",
       noteBody: "new note"
     },
     {
-      id: 3,
       noteTitle: "Note Three",
       noteBody: "new note"
     }
--- database/seeds/users.js
+++ database/seeds/users.js
@@ -1,7 +1,6 @@
 exports.seed = function(knex) {
   return knex("users").insert([
     {
-      id: 1,
       username: "Adam",
       password: "pass"
     }
