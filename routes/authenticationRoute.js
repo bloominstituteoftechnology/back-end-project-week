a3a1e96fd91d7a55f76d9e1912006e879d340bb4
kyranmccann <kyran.mccann@gmail.com>
2018-12-12 16:13:05 -0800

    scaffolding authentication

---
+++ routes/authenticationRoute.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const axios = require('axios');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const db = require('../data/dbConfig');
+
+const { authenticate } = require('./middlewares');
+
+const jwtKey = process.env.JWT_KEY;
+
+const router = express.Router();
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username,
+  };
+
+  const options = {
+    expiresIn: '1h',
+  };
+
+  return jwt.sign(payload, jwtKey, options);
+}
+
+router.post('/signup', authenticate, (req, res) => {
+    const creds = req.body;
+    const hash = bcrypt.hashSync(creds.password, 12);
+    creds.password = hash;
+
+    db('users')
+      .insert(creds)
+      .then(id => {
+        const user = db('users').where({ id: id }).first();
+        const token = generateToken(user)
+        res.status(201).json({ message: 'thanks for signing up', token });
+      })
+      .catch(err => res.json(err));
+})
+
+router.post('/signin', (req, res) => {
+  const creds = req.body;
+
+  db('users')
+    .where({ username: creds.username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(creds.password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({ message: 'welcome back!', token });
+      } else {
+        res.status(401).json({ message: 'she doesn\'t even go here' });
+      }
+    })
+    .catch(err => res.json(err));
+})
