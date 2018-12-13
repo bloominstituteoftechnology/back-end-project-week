e95d23a5a382729d1bff84216338d6996167e6ae
Stublink <chrisrey95@gmail.com>
2018-12-13 15:31:29 -0500

    .....

--- database/seeds/01-users.js
+++ database/seeds/01-users.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcrypt');
 exports.seed = function(knex, Promise) {
   return knex('users').insert([
     {
-      userId: 1, 
+      userId: 0, 
       username: 'rey',
       password: bcrypt.hashSync('pass', 8)
     },
