e93b225ec2fb0d442de8b1549f185cf2f5ca71ea
Stublink <chrisrey95@gmail.com>
2018-12-13 15:34:03 -0500

    heroku knex sqlite3 to postgres debugging

--- database/seeds/02-notes.js
+++ database/seeds/02-notes.js
@@ -1,8 +1,8 @@
 exports.seed = function(knex, Promise) {
   return knex('notes').insert([
-    {noteId: 0, title: 'this is a note title 1', textBody: 'this is the note text body 1', user: 1},
-    {noteId: 1, title: 'this is a note title 2', textBody: 'this is the note text body 2', user: 1},
-    {noteId: 2, title: 'this is a note title 3', textBody: 'this is the note text body 3', user: 1},
-    {noteId: 3, title: 'this is a note title 4', textBody: 'this is the note text body 4', user: 1},
+    {noteId: 0, title: 'this is a note title 1', textBody: 'this is the note text body 1', user: 0},
+    {noteId: 1, title: 'this is a note title 2', textBody: 'this is the note text body 2', user: 0},
+    {noteId: 2, title: 'this is a note title 3', textBody: 'this is the note text body 3', user: 0},
+    {noteId: 3, title: 'this is a note title 4', textBody: 'this is the note text body 4', user: 0},
   ]);
 };
