104d2d972c68fc5cffbd815e2ddb65c2a064b944
SabrinaGear <sabrinagear.ft@gmail.com>
2018-12-16 17:01:55 -0600

    change config to production

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,4 +1,4 @@
-const dbEnvironment = process.env.DB_ENVIRONMENT || "development";
+const dbEnvironment = process.env.DB_ENVIRONMENT || "production";
 
 const knex = require("knex");
 const knexConfig = require("../knexfile");
