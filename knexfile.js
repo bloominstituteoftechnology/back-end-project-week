523a87d28c9afbb490712cdd7d5564befbeb9cd9
SabrinaGear <sabrinagear.ft@gmail.com>
2018-12-16 17:28:18 -0600

    debugging

--- knexfile.js
+++ knexfile.js
@@ -24,11 +24,7 @@ module.exports = {
   },
   production: {
     client: "pg",
-    connection: {
-      database: process.env.DATABASE_URL
-      // user: process.env.DB_USER,
-      // password: process.env.DB_PASSWORD
-    },
+    connection: dbConnection + "?ssl=true",
     pool: {
       min: 2,
       max: 10
