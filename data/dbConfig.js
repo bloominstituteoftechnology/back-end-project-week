81c78581af0efdf93e8946b3139233b318281cc3
SabrinaGear <sabrinagear.ft@gmail.com>
2018-12-16 17:16:36 -0600

    test run

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,6 +1,4 @@
 const dbEnvironment = process.env.DB_ENVIRONMENT || "development";
-
 const knex = require("knex");
 const knexConfig = require("../knexfile");
-console.log(dbEnvironment);
 module.exports = knex(knexConfig[dbEnvironment]);
