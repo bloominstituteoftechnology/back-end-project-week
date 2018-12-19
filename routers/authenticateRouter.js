8cf00505b31b4a6c4da28b3f1941156435f4d508
sarahtennis <sarahtennis@Sarahs-MacBook-Air.local>
2018-12-19 11:20:27 -0600

    adds protected middleware to authenticated route

--- routers/authenticateRouter.js
+++ routers/authenticateRouter.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 
+const protected = require('../middleware/protected.js');
+
 // [GET] /api/authenticated
 // returns true if valid token, used for front end
 router.get('/authenticated', protected, (req, res) => {
