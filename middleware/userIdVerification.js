08982e2109094989f980f2c24d6892b0df46c9b6
sarahtennis <sarahtennis@Sarahs-MacBook-Air.local>
2018-12-19 12:13:23 -0600

    fix

--- middleware/userIdVerification.js
+++ middleware/userIdVerification.js
@@ -1,7 +1,6 @@
 module.exports = async (req, res, next) => {
     const decodedToken = req.decodedToken;
-    console.log(decodedToken);
-    if(Number(req.params.id) === decodedToken){
+    if(Number(req.params.id) === decodedToken.subject){
         next();
     } else {
         res.status(401).json({ message: 'Get outta here!' });
