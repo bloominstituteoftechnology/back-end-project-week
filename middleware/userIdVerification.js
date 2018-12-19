025c6cbe29e31583ef31594ba246d7f13446c628
sarahtennis <sarahtennis@Sarahs-MacBook-Air.local>
2018-12-19 11:42:15 -0600

    testing

--- middleware/userIdVerification.js
+++ middleware/userIdVerification.js
@@ -1,4 +1,5 @@
 module.exports = async (req, res, next) => {
+    console.log(req.params.id);
     const decodedToken = req.decodedToken;
     if(req.params.id === decodedToken.id){
         next();
