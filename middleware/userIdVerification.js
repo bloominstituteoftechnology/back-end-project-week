21861070360f76792f841de312ea15c44e44a817
sarahtennis <sarahtennis@Sarahs-MacBook-Air.local>
2018-12-19 11:50:45 -0600

    parses req params to Number

--- middleware/userIdVerification.js
+++ middleware/userIdVerification.js
@@ -1,7 +1,6 @@
 module.exports = async (req, res, next) => {
-    console.log(req.params.id);
     const decodedToken = req.decodedToken;
-    if(req.params.id === decodedToken.id){
+    if(Number(req.params.id) === decodedToken.id){
         next();
     } else {
         res.status(401).json({ message: 'Get outta here!' });
