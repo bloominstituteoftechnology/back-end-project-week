434e4f0bdfe7d09a2c72e8178eb9a8c37e399f07
Elvis Ibarra <ielvisd@gmail.com>
2018-12-20 18:37:37 -0800

    more route test

--- api/server.js
+++ api/server.js
@@ -41,12 +41,12 @@ server.use(cors(corsOptions));
 // server.use('/restricted/', authenticate);
 
 //Enable Cross Origin Requests Might make this limited to my own Domain if it works
-// server.use(function(req, res, next) {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//   next();
-// });
+server.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  // res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 const notesRouter = require('../notes/notesRouter.js');
 const registerRouter = require('../notes/registerRouter.js');
