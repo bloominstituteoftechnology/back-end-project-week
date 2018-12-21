06be72fde2e81dc8031c09c2a66642d397ea661e
Elvis Ibarra <ielvisd@gmail.com>
2018-12-20 17:31:16 -0800

    testing router.all

--- notes/notesRouter.js
+++ notes/notesRouter.js
@@ -4,14 +4,14 @@ const db = require('./notesModel');
 const knex = require('../data/dbConfig.js');
 var cors = require('cors');
 
-router.use(cors());
-
 router.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
+
+router.all('*', cors());
 
 // Notes endpoints
 router.get('/notes/', (req, res, next) => {
