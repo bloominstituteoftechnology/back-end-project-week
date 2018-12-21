19b9e30aab80a06d3c27e83775aeaeb4dc5a5e33
Elvis Ibarra <ielvisd@gmail.com>
2018-12-20 18:26:11 -0800

    trying cors options with whitelist

--- notes/notesRouter.js
+++ notes/notesRouter.js
@@ -2,18 +2,18 @@ const express = require('express');
 const router = express.Router();
 const db = require('./notesModel');
 const knex = require('../data/dbConfig.js');
-var cors = require('cors');
+// var cors = require('cors');
 
-router.use(cors());
+// router.use(cors());
 
-router.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
+// router.use(function(req, res, next) {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//   next();
+// });
 
-router.all('*', cors());
+// router.all('*', cors());
 
 // Notes endpoints
 router.get('/notes/', (req, res, next) => {
