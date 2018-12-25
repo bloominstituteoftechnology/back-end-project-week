2a6c1e7840e090e61ebc2c80768c6a7579f415e2
Josue Peralta <jperal77@gmail.com>
2018-12-25 15:58:59 -0600

    a post request to /api/notes now returns the new id

--- server.js
+++ server.js
@@ -1,78 +1,82 @@
-const express = require("express");
-const cors = require("cors");
-const knex = require("./db/config.js");
+const express = require('express');
+const cors = require('cors');
+const knex = require('./db/config.js');
 
 const server = express();
 const port = process.env.PORT || 9000;
 server.use(express.json());
 server.use(cors());
 
-server.get("/", (req, res) => {
+server.get('/', (req, res) => {
   res.send(`API running on port: ${port}`);
 });
 
-server.get("/api/notes", (req, res) => {
-  knex("notes")
+server.get('/api/notes', (req, res) => {
+  knex('notes')
     .then(notes => {
       res.status(200).json(notes);
     })
     .catch(err => {
-      res.status(500).json({ message: "Error retrieving notes" });
+      res.status(500).json({ message: 'Error retrieving notes' });
     });
 });
 
-server.get("/api/notes/:id", (req, res) => {
+server.get('/api/notes/:id', (req, res) => {
   const { id } = req.params;
-  knex("notes")
-    .where("id", id)
+  knex('notes')
+    .where('id', id)
     .then(note => {
       res.status(200).json(note);
     })
     .catch(err => {
-      res.status(500).json({ message: "Error getting project" });
+      res.status(500).json({ message: 'Error getting project' });
     });
 });
 
-server.post("/api/notes", (req, res) => {
+server.post('/api/notes', (req, res) => {
   const note = req.body;
+
   knex
     .insert(note)
-    .into("notes")
+    .into('notes')
     .then(response => {
-      res.status(201).json({ message: "New note successfully created" });
+      res
+        .status(201)
+        .json({ id: response[0], message: 'New note successfully created' });
     })
     .catch(err => {
-      res.status(500).json({ message: "Error creating note" });
+      res.status(500).json({ message: 'Error creating note' });
     });
 });
 
-server.put("/api/notes/:id", (req, res) => {
+server.put('/api/notes/:id', (req, res) => {
   const { id } = req.params;
   const data = req.body;
-  knex("notes")
+  knex('notes')
     .where({ id })
     .update(data)
     .then(count => {
+      console.log(count);
       if (count < 1) {
-        res.status(400).json({ message: "ID not found" });
+        res.status(400).json({ message: 'ID not found' });
       }
-      res.status(200).json({ message: "Note updated" });
+      res.status(200).json({ message: 'Note updated' });
     })
     .catch(err => {
       res.status(500).json(err);
     });
 });
 
-server.delete("/api/notes/:id", (req, res) => {
+server.delete('/api/notes/:id', (req, res) => {
   const { id } = req.params;
-  knex("notes")
+  knex('notes')
     .where({ id })
     .del()
     .then(count => {
       if (count < 1) {
-        res.status(400).json({ message: "Note not found" });
+        res.status(400).json({ message: 'Note not found' });
       }
-      res.status(200).json({ message: "Note deleted" });
+      res.status(200).json({ message: 'Note deleted' });
     })
     .catch(err => {
       res.status(500).json(err);
