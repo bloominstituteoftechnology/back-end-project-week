9f209e29df8b3d8f26aa79a8e01232c4355e434c
theryanclausen <theryanclausen@gmail.com>
2018-12-25 12:59:13 -0700

    seed change.

--- data/seeds/001notes.js
+++ data/seeds/001notes.js
@@ -5,8 +5,9 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('notes').insert([
-        {title: 'Project week', textBody:'Here we go again'},
-        {title: 'Finish back end', textBody:'That can happen today'}
+        {title: 'Welcome to Bubble Notes', textBody:'A Ryan Clausen Creation'},
+        {title: 'Front end:', textBody:'React 16.7.0-alpha.2. Global state Management with reactN.'},
+        {title: 'Back end:', textBody:'Node/express server using knex with a sqlite3 database.'}
       ]);
     });
 };
