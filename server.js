438646fbd175bf7b01baf6be61f8e33b0504a178
Carey <careycbaldwin@gmail.com>
2018-12-27 08:41:29 -0500

    changed CORS settings

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const server = express();
 
 server.use(helmet());
 server.use(express.json());
-server.use(cors({origin: 'http://localhost:3000'})); //app is running on port 3000
+server.use(cors({origin: 'https://lambda-note-taking-app.herokuapp.com'})); 
 
 
 
