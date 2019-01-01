38b714e9bfee13d5e3c9bdce6da199261bf9274f
Sumayyah Asgar <sasgar04@gmail.com>
2018-12-31 17:15:28 -0800

    deleted old db

--- seeds/001_notes.js
+++ seeds/001_notes.js
@@ -1,7 +1,7 @@
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
   return knex('notes')
-    .del()
+    .truncate()
     .then(function() {
       // Inserts seed entries
       return knex('notes').insert([
