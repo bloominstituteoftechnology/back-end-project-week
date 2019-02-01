84d6f9171bbc83927509cbab75e4c8f790395d87
SabrinaGear <sabrinagear.ft@gmail.com>
2019-02-01 02:12:28 -0600

    revert

--- api/server.js
+++ api/server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const middleware = require("./middleware/config");
 const noteRouter = require("./routes/noteRouter.js");
-const authRouter = require("./routes/authRouter");
 
 const server = express();
 
@@ -10,7 +9,6 @@ middleware(server);
 
 //routes
 server.use("/notes", noteRouter);
-server.use("/auth", authRouter);
 
 server.get("/", (req, res) => {
   res.status(200).json({ message: "server is running" });
