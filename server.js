7fb34a683e8c0e3c8ee29b589d8c4624bb4cbc0c
Robert Billington <robertbillington@Roberts-MacBook-Pro.local>
2019-02-12 12:06:22 -0600

    Get and get by id requests written

--- server.js
+++ server.js
@@ -8,6 +8,37 @@ middleware(server);
 server.use(express.json());
 
 
+//Get request for all notes
+server.get('/', async(req,res) =>{
+    const notes = await db('games');
+    try{
+        res.status(200).json(notes);
+    }catch(err){
+        res.status(500).json({message: 'Could not get notes data'});
+    }
+});
+
+//Get request for note by id
+server.get('/:id' , (req, res) =>{
+    const {id} = req.params;
+    db('games').where('id', id)
+        .then(rows =>{
+            res.status(200).json(rows);
+        })
+        .catch(() =>{
+            res.status(500).json({message: 'Could not fetch not with specified id'})
+        });
+});
+
+//Post request for creating note
+
+
+//Delete request
+
+
+//Put request
+
+
 server.listen(port, () =>{
     console.log(`Server is up and running on port ${port}`)
 });
