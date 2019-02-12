09ac52915fe0e24a0101ed9f4ff32067d0580c5f
cferrell2 <cferrell12182@gmail.com>
2019-02-11 22:52:09 -0500

    create and configure knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -6,6 +6,13 @@ module.exports = {
     client: 'sqlite3',
     connection: {
       filename: './data/notes.sqlite3'
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
     }
   },
 
