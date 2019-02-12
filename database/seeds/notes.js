8e70bfbb13d64e88b0276432b8e66dd2e01d659d
TheGameFreak720 <luis.alvarez@commithub.com>
2019-02-11 22:19:31 -0500

    created seeds for notes

--- database/seeds/notes.js
+++ database/seeds/notes.js
@@ -1,13 +1,13 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('notes').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('notes').insert([
+        {id: 1, title: 'FlatBush Zombies', description: 'I love their rap', user_id: 1},
+        {id: 2, title: 'Build a house', description: 'You play minecraft idk', user_id: 2},
+        {id: 3, title: `I'm Billy`, description: 'Hyuck Billy is my name', user_id: 3}
       ]);
     });
 };
