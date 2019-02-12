d764652c51476fb579fbc0f9943340323439b56f
Jennifer King <jenbellpgh@gmail.com>
2019-02-12 00:15:16 -0500

    Added framework for route handler

---
+++ routes/Notes.js
@@ -0,0 +1,23 @@
+// Base requires:
+const express = require('express');
+const router = express.Router();
+
+// App requires:
+
+
+/* ---------- Endpoints: ---------- */
+
+// GET list of notes
+
+// GET single note
+
+// POST new note
+
+// PUT edits single note
+
+// DELETE single note
+
+
+
+/* ---------- Export ---------- */
+module.exports = router;
