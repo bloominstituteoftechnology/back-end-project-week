1630c30acc6fc39b7b02cbfee6072f4e6b482a36
iepoch <staclem@live.com>
2019-02-11 20:43:01 -0700

    Firts Commit - - Most of Boiler Plate is Setup

---
+++ knexfile.js
@@ -0,0 +1,20 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/lbNotes.db3'
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations',
+      tableName: 'dbmigrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    }
+  },
+
+};
