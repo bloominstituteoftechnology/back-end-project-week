e68776c7fed0a928b518ceff0d6eafc8461a9439
Mitchell Robles <mitchellr92@live.com>
2019-02-11 20:58:44 -0800

    set up very basic server

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+const PORT = 2200;
+
+server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+});
