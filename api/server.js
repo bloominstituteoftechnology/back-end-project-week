afb81b6dfc2d347f6b36cb47f68d175b4c14642a
rlmclaughlin <rlmclaughlin@yahoo.com>
2019-02-12 01:05:57 -0600

    created GET by ID and Delete endpoints

--- api/server.js
+++ api/server.js
@@ -20,12 +20,34 @@ server.get('/api/notes', (req, res) => {
   })
 })
 
+server.get('/api/notes/:id', (req, res) => {
+    const {id} = req.params
+    db('notes').where({id}).then(content => {
+        res.status(200).json(content)
+    })
+    .catch(error => { res.status(400).json({ error: "There was an error locating the user"})
+  })
+})
+
 server.post('/api/notes', (req, res) => { 
     const body = req.body
-    db('notes').insert(body).then(id => {
-        res.status(201).json(id)
+    if(!body.textBody || !body.title){
+        res.status(401).send("Missing a title or the text-body ")
+    } else {
+        db('notes').insert(body).then(id => {
+          res.status(201).json(id) 
+        })
+        .catch( error => { res.status(400).json({error: "Unable to post note"})})
+    }    
+})
+
+server.delete('/api/notes/:id', (req, res) => {
+    const {id} = req.params
+    db('notes').where({id}).del().then( ids => {
+        res.status(200).json(ids)
     })
-    .catch( error => { res.status(400).json({error: "Unable to post note"})})
+    .catch( error => { res.status(400).json({ error: "We were unable to delete the user"})
+  })
 })
 
 module.exports = {
