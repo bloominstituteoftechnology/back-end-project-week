a7c0b09650ff27e63249f66eda8f794a671f80c7
Michael Trew <michaeltrew@Michaels-iMac.attlocal.net>
2019-02-11 23:29:33 -0500

    server init

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const server = express();
+const PORT = process.env.port || 5000;
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+
+server.use(
+  express.json(),
+  morgan('tiny'),
+  helmet()
+)
+
+
+server.listen(PORT, () => {
+  console.log(`Server Is Live On Port ${PORT}`)
+});
