c7db7f4c22ef8339d8ce3446456ccc19e51bd2ce
paulapivat <paul.apivat@gmail.com>
2019-02-12 16:05:09 +0700

    setup GET request to display all notes

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const knex = require('knex');
+const dbConfig = require('./knexfile.js');
+
+const server = express();
+const db = knex(dbConfig.development);
+const PORT = 3000;
+
+server.use(express.json());  //body parser middleware
+
+
+
+server.get('/notes', (req, res) => {
+    db('notes')
+    .then(rows => {
+        res.json(rows)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({err: 'Failed to retrieve Notes'})
+    })
+});
+
+server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+})
