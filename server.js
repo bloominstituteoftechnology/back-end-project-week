5aaf1ea860b9785cc18665072efb17f91b3c6100
Matthew Meitl <darksamus6@gmail.com>
2019-02-11 21:24:27 -0600

    added more boiler plate

--- server.js
+++ server.js
@@ -2,8 +2,24 @@ const express = require("express");
 const server = express();
 server.use(express.json());
 
+server.get("/notes", (req, res) => {
+    
+  });
 
+server.get("/notes/:id", (req, res) => {
+    
+});
 
+server.post("/notes", (req, res) => {
+    
+});
 
+server.put("/notes/:id", (req, res) => {
+    
+});
+
+server.delete("/notes/:id", (req, res) =>{
+
+});
 
 module.exports = server; 
