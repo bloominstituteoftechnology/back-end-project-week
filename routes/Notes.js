1d11c5be6b4b8c00dde2c9d5415b988797e20e36
Jennifer King <jenbellpgh@gmail.com>
2019-02-12 00:57:49 -0500

    Added route handler for /note/get/all and tested with Postman

--- routes/Notes.js
+++ routes/Notes.js
@@ -7,15 +7,28 @@ const notes = require('../data/helpers/noteModel');
 
 /* ---------- Endpoints: ---------- */
 
-// GET list of notes
+// GET list of notes: /note/get/all
+router.get( '/get/all', (req, res) => {
+  notes.get()
+    .then( (list) => {
+      res.json(list);
+    })
+    .catch( (err) => {
+      res.status(500).json({ error: "Note information could not be retrieved." });
+    })
+  // end-note
+});
 
-// GET single note
+// GET single note: /note/get/id
 
-// POST new note
 
-// PUT edits single note
+// POST new note: /note/create
 
-// DELETE single note
+
+// PUT edits single note: /note/edit/id
+
+
+// DELETE single note: /note/delete/id
 
 
 
