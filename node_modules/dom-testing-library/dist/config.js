ab154c0d9508f52e1bb27e3d8fa24e384080a278
Robert Billington <robertbillington@Roberts-MacBook-Pro.local>
2019-02-12 11:12:29 -0600

    Robert Billington, FSWPT2

---
+++ node_modules/dom-testing-library/dist/config.js
@@ -0,0 +1,33 @@
+"use strict";
+
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.configure = configure;
+exports.getConfig = getConfig;
+
+var _extends2 = _interopRequireDefault(require("@babel/runtime/helpers/extends"));
+
+// It would be cleaner for this to live inside './queries', but
+// other parts of the code assume that all exports from
+// './queries' are query functions.
+let config = {
+  testIdAttribute: 'data-testid'
+};
+
+function configure(newConfig) {
+  if (typeof newConfig === 'function') {
+    // Pass the existing config out to the provided function
+    // and accept a delta in return
+    newConfig = newConfig(config);
+  } // Merge the incoming config delta
+
+
+  config = (0, _extends2.default)({}, config, newConfig);
+}
+
+function getConfig() {
+  return config;
+}
