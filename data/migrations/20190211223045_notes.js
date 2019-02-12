7d93220e3494428e881678167ac6c43d489cb09c
rlmclaughlin <rlmclaughlin@yahoo.com>
2019-02-11 22:39:36 -0600

    populated migrations file and added database

--- data/migrations/20190211223045_notes.js
+++ data/migrations/20190211223045_notes.js
@@ -1,8 +1,13 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('notes', table => {
+      table.increments(); 
+      table.string('tags')
+      table.string("title").notNullable(); 
+      table.string("textBody").notNullable(); 
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists('notes')
 };
