0abb91c5e4e723492bf23e6ae0c3a1e35f1a2cdb
rlmclaughlin <rlmclaughlin@yahoo.com>
2019-02-12 00:33:15 -0600

    added a GET and POST endpoint

--- api/server.js
+++ api/server.js
@@ -8,9 +8,25 @@ const server = express();
 const dbConfig = require('../knexfile')
 const db = knex(dbConfig.development)
 
-
 server.use(express.json(), helmet(), cors())
 
+
+
+server.get('/api/notes', (req, res) => { 
+  db('notes').then( notes => {
+      res.status(200).json(notes)
+  })
+  .catch( error => { res.status(400).json({error: "we've encountered an error"})
+  })
+})
+
+server.post('/api/notes', (req, res) => { 
+    const body = req.body
+    db('notes').insert(body).then(id => {
+        res.status(201).json(id)
+    })
+    .catch( error => { res.status(400).json({error: "Unable to post note"})})
+})
 
 module.exports = {
     server
