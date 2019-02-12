0f93a1d0b8cc55f48cc39b62c497878f46ae1ec1
Robert Billington <robertbillington@Roberts-MacBook-Pro.local>
2019-02-12 13:10:05 -0600

    Created post and delete HTTP request, working properly with Postman

--- server.js
+++ server.js
@@ -31,10 +31,33 @@ server.get('/notes/:id' , (req, res) =>{
 });
 
 //Post request for creating note
-
+server.post('/notes', (req, res) =>{
+    const newNote = req.body;
+    if(newNote.title && newNote.content){
+        db('notes').insert(newNote)
+        .then(ids =>{
+            res.status(201).json(ids)
+        })
+        .catch(() =>{
+            res.status(500).json({message: 'Could not add new note'})
+        })
+    }else{
+        res.status(404).json({message: 'Missing note title or content'})
+    }
+    
+})
 
 //Delete request
-
+server.delete('/notes/:id', (req, res) =>{
+    const {id} = req.params;
+    db('notes').where('id', id).del()
+        .then(rowCount =>{
+            res.status(200).json(rowCount)
+        })
+        .catch(() =>{
+            res.status(500).json({message: 'Could not delete note with specified id'})
+        })
+})
 
 //Put request
 
