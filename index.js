7fc9c17baff1e3ec2f323302346c219af425b8db
mglittleton <michaelglittleton@gmail.com>
2019-02-11 23:23:36 -0500

    Express is set up in index.js and ready for endpoints to be created

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 const express = require('express');
+const knex = require('knex');
+
+const dbConfig = require('./knexfile');
+const db = knex(dbConfig.development);
+const PORT = 4700;
+const server = express();
+
+server.use(express.json());
+
+
+
+server.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`)
+})
