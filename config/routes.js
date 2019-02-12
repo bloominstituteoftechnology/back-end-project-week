e665eaf89cb8911b68a0e499ec5e74b4b0a6bc89
TheGameFreak720 <luis.alvarez@commithub.com>
2019-02-11 23:21:43 -0500

    created post route in routes

--- config/routes.js
+++ config/routes.js
@@ -3,6 +3,7 @@ const notesDB = require('../database/helpers/noteDb');
 
 module.exports = server => {
     server.get('/', getAllNotes);
+    server.post('/', addNote);
 }
 
 const getAllNotes = async (req, res) => {
@@ -12,4 +13,15 @@ const getAllNotes = async (req, res) => {
     } catch(e) {
         res.status.send(e);
     }
-}
+};
+
+const addNote = async (req, res) => {
+    const note = req.body;
+
+    if(note.title && note.description && note.user_id) {
+        const posted = await notesDB.add(note);
+        res.status(201).json(posted);
+    } else {
+        res.status(401).json({ message: 'The note is missing data' })
+    }
+};
