cd5e149f13c0e3bee1641f4a9fd4e25093a3466f
Matthew Meitl <darksamus6@gmail.com>
2019-02-11 21:03:32 -0600

    added initial test

--- server.js
+++ server.js
@@ -2,9 +2,7 @@ const express = require("express");
 const server = express();
 server.use(express.json());
 
-server.get("/tasks", (req, res) => {
-  res.status(200).json(db);
-});
+
 
 
 
---
+++ server.spec.js
@@ -0,0 +1,9 @@
+const request = require("supertest");
+const server = require("./server.js");
+
+describe("server.js", () => {
+    it("run the tests", () => {
+      expect(true).toBeTruthy();
+    })
+
+})
