90a01e53e3c7bc017e3d42f29468cfe5f8d582b6
TheGameFreak720 <luis.alvarez@commithub.com>
2019-02-11 23:10:46 -0500

    added new test suite that checks if add returns correct status message

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -1,8 +1,25 @@
 const request = require('supertest');
 const { server } = require('../api/server');
+const db = require('../database/dbConfig');
 
 
 describe('The route handlers', () => {
+    describe('Post /', () => {
+
+        afterEach(async () => {
+            await db('notes').truncate();
+            await db.seed.run();
+        });
+
+        it('responds with 201 if body is correct', async () => {
+            const body = {title: 'Coding is fun', description: 'Diandra made me do TDD. SOS', user_id: 3}
+            const response = await request(server).post('/').send(body);
+
+            expect(response.status).toBe(201);
+            db('notes').truncate();
+        });
+    });
+
     describe('Get /', () => {
         it('responds with 200', async () => {
             const response = await request(server).get('/');
