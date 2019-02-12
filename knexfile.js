39c36e5eae4d906b929bae3f37cce965ca559bfc
Abdiel Fernandez <fernandezabdiel@gmail.com>
2019-02-11 23:36:46 -0500

    added useNullAsDefault to the knex file

--- knexfile.js
+++ knexfile.js
@@ -7,6 +7,7 @@ module.exports = {
     connection: {
       filename: './data/notes_dev.sqlite3'
     },
+    useNullAsDefault: true,
     migrations: {
       directory: "./data/migrations"
     },
@@ -20,6 +21,7 @@ module.exports = {
     connection: {
       filename: './data/test.sqlite3'
     },
+    useNullAsDefault: true,
     migrations: {
       directory: './data/migrations',
     },
