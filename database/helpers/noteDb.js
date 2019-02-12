8d612a9f4ad30711766575937e852835d66deaf6
TheGameFreak720 <luis.alvarez@commithub.com>
2019-02-11 23:18:47 -0500

    added add method on db helper

--- database/helpers/noteDb.js
+++ database/helpers/noteDb.js
@@ -3,5 +3,9 @@ const db = require('../dbConfig');
 module.exports = {
     get: function() {
         return db('notes');
+    },
+
+    add: function(note) {
+        return db('notes').insert(note);
     }
 }
