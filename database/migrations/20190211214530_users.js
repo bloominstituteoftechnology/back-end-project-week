08d0f4d38354a8f4dfa775b674a273fcd97a3425
TheGameFreak720 <luis.alvarez@commithub.com>
2019-02-11 21:55:27 -0500

    created user schema

--- database/migrations/20190211214530_users.js
+++ database/migrations/20190211214530_users.js
@@ -1,8 +1,12 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('users', table => {
+      table.increments();
+      table.string(name, 50);
+      table.string(email);
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists('users');
 };
