d545478daaae1168678f856ff9b9e2de8941fff8
Jennifer King <jenbellpgh@gmail.com>
2019-02-12 00:39:32 -0500

    Added code to noteModel.js (db Helpers)

--- data/helpers/noteModel.js
+++ data/helpers/noteModel.js
@@ -1,12 +1,32 @@
 const db = require('../dbConfig');
 
 module.exports = {
-  get: (id) => {},
+  get: (id) => {
+    if( id ) {
+      // Will return the note with the specified ID
+      return db('notes').where('noteId', id).first();
+    } else {
+      // Will return a list of all notes
+      return db('notes');
+    }
+  },
 
-  insert: (id) => {},
+  insert: (note) => {
+    // Create a new note and return the ID
+    return db('notes').insert(note);
+  },
 
-  update: (id, newNote) => {},
+  update: (id, newNote) => {
+    // Edit note and return new note.
+    return db('notes').where('noteId', id)
+      .update(newNote)
+      .then( ([id]) => this.get(id) );
+  },
 
-  remove: (id) => {}
+  remove: (id) => {
+    // Delete the note
+    return db('notes').where('noteId', id)
+      .del();
+  }
 
 };
