c8cfc5cd865eabd827a88f4471828bd7422f5329
Joseph Thompson <joe.thompson838@gmail.com>
2019-02-12 10:05:53 -0500

    attempting to connect server to heroku

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const SERVER = require("./api/server");
 const PORT = process.env.PORT;
 
+SERVER.get("/", (req, res) => {
+  res.send("Server Active");
+});
 SERVER.listen(PORT, () => {
   console.log(`Listening on PORT: ${PORT}`);
 });
