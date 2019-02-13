edf4a892401cf5d51efc3e8dc0dc65576684963a
Michael Trew <michaeltrew@Michaels-iMac.attlocal.net>
2019-02-12 21:28:14 -0500

    finished users table schema

--- data/migrations/20190211235751_posts_table.js
+++ data/migrations/20190211235751_posts_table.js
@@ -1,6 +1,11 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('users', users => {
+    users.increments();
+    users.string('name').notNullable();
+    users.string('email').notNullable();
+    users.string('username').notNullable().unique();
+  })
 };
 
 exports.down = function(knex, Promise) {
