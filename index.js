2abc4e4c79da3bd935b7e535c32f10b3f4df8deb
Phinehas <kohler.nai@gmail.com>
2019-02-12 22:02:18 -0700

    fixed problem where getting a note that doesn't exist returned an empty array instead of a message

--- index.js
+++ index.js
@@ -36,7 +36,11 @@ server.post('/api/notes', (req, res) => {
 server.get('/api/notes', (req, res) => {
     db.find()
         .then(notes => {
-            res.json(notes)
+            if(notes > 0) {
+                res.json(notes)
+            } else {
+                res.json({ message: "There are currently no notes"})
+            }
         })
         .catch(err => {
             res.status(500)
@@ -44,9 +48,43 @@ server.get('/api/notes', (req, res) => {
         })
 })
 
+server.get('/api/notes/:id', (req, res) => {
+    const { id } = req.params;
+    db.findById(id)
+        .then(note => {
+            if(note === 1) {
+                res.json(note);
+            } else {
+                res.status(404)
+                    .json({ message: 'The note with the specified ID does not exist' })
+            }
+        })
+        .catch(err => {
+            res.status(500)
+                .json({ message: "The note information could not be returned" })
+        })
+
+})
+
 //--------------------Update-------------------------
 
 //--------------------Destroy------------------------
+
+server.delete('/api/notes/:id', (req, res) => {
+    const { id } = req.params;
+    db.remove(id)
+        .then(count => {
+            if(count) {
+                res.json({ message: "Note was successfully deleted"})
+            } else {
+                res.json({ message: "The post with the specified ID does not exist"})
+            }
+        })
+        .catch( err => {
+            res.status(500)
+                .json({ message: "The post could not be removed"})
+        })
+})
 
 server.listen(PORT, () => {
     console.log(`Server is listening on PORT ${PORT}`)
