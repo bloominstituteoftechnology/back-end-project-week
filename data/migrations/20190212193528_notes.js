467e5e78e11606768c9aa68d75841000dfa088ac
vy3191 <venkyafhs@gmail.com>
2019-02-12 20:12:33 -0500

    Added seeds to the database

--- data/migrations/20190212193528_notes.js
+++ data/migrations/20190212193528_notes.js
@@ -3,7 +3,7 @@ exports.up = function(knex, Promise) {
    return knex.schema.createTable('notes', function (table) {
           table.increments();
           table.string('title').notNullable();
-          table.string('description', 1000).notNullable();
+          table.string('content', 1000).notNullable();
    });
 };
 
