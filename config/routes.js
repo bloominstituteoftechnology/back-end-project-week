5db76043a6136b898e8ae51fc495eaa25b35da17
TheGameFreak720 <luis.alvarez@commithub.com>
2019-02-12 21:37:24 -0500

    created put route

--- config/routes.js
+++ config/routes.js
@@ -4,17 +4,9 @@ const notesDB = require('../database/helpers/noteDb');
 module.exports = server => {
     server.get('/', getAllNotes);
     server.post('/', addNote);
+    server.put('/:id', updateNote);
     server.delete('/:id', deleteNote);
 }
-
-const getAllNotes = async (req, res) => {
-    try {
-        const notes = await notesDB.get();
-        res.status(200).send(notes);
-    } catch(e) {
-        res.status.send(e);
-    }
-};
 
 const addNote = async (req, res) => {
     const note = req.body;
@@ -26,6 +18,28 @@ const addNote = async (req, res) => {
         res.status(401).json({ message: 'The note is missing data' })
     }
 };
+
+const getAllNotes = async (req, res) => {
+    try {
+        const notes = await notesDB.get();
+        res.status(200).send(notes);
+    } catch(e) {
+        res.status.send(e);
+    }
+};
+
+const updateNote = async(req, res) => {
+    const { id } = req.params;
+    const newNote = req.body;
+    newNote.id = id;
+
+    if(note.title && note.description && note.user_id) {
+        const updated = await notesDB.update(id, newNote);
+        res.status(200).json(updated);
+    } else {
+        res.status(401).json({ message: 'The note is missing data' });
+    }
+}
 
 const deleteNote = async (req, res) => {
     const { id } = req.params;
