107b5f1b41b36b697cb6763ab9c34309efed49fe
Abdiel Fernandez <fernandezabdiel@gmail.com>
2019-02-12 23:52:13 -0500

    created tests fot the API

---
+++ notes/notesModel.js
@@ -0,0 +1,29 @@
+const db = require('../data/dbConfig');
+
+const getAllNotes = async () => {
+
+}
+
+const getNoteByID = async () => {
+
+}
+
+const createNote = async () => {
+
+}
+
+const updateNote =  async () => {
+
+}
+
+const deleteNote = async () => {
+
+}
+
+module.exports = {
+  getAllNotes,
+  getNoteByID,
+  createNote,
+  updateNote,
+  deleteNote,
+}
