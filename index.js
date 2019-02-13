b55bb7fe9593ed7d705c6c2e4d85064e63cdfed7
Mitchell Robles <mitchellr92@live.com>
2019-02-12 19:55:41 -0800

    configured knex in index.js and created get by id functionality. Also set up basic structure for endpoints

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const knex = require('knex');
+const dbConfig = require('./knexfile.js');
+const db = knex(dbConfig.development);
 
 const server = express();
 
@@ -6,7 +9,33 @@ server.use(express.json());
 
 const PORT = 2200;
 
+server.post('/api/notes', (req, res) => {
 
+});
+
+server.get('/api/notes', (req, res) => {
+
+});
+
+server.get('/api/notes/:id', (req, res) => {
+    const { id } = req.params;
+    console.log(req.params);
+    db('notes').where('id', id)
+        .then(note => {
+            res.status(200).json(note);
+        })
+        .catch(err => {
+            res.status(500).json({ message: 'Failed to get note' })
+        })
+});
+
+server.put('/api/notes/:id', (req, res) => {
+
+});
+
+server.delete('/api/notes/:id', (req, res) => {
+
+});
 
 server.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
