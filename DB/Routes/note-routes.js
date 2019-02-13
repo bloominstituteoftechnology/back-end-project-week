c20484c2b3eee079adb7e21a1799c44d7dc0d698
Lorenzo Evans <lorenzo.evans94@gmail.com>
2019-02-12 21:44:24 -0500

    Starting to add boiler-plate for users auth login/register and notes.

---
+++ DB/Routes/note-routes.js
@@ -0,0 +1,26 @@
+const express = require('express')
+
+const router = express.router()
+
+
+router.get('/', (req, res) => {
+
+})
+
+router.get('/', (req, res) => {
+ 
+})
+
+router.post('/', (req, res) => {
+ 
+})
+
+router.put('/', (req, res) => {
+ 
+})
+
+router.delete('/', (req, res) => {
+ 
+})
+
+module.exports.router
---
+++ DB/Routes/user-routes.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const router = express.router()
+
+router.post('/', (req, res) => {
+
+})
+
+router.post('/', (req, res) => {
+
+})
+
+module.exports.router
