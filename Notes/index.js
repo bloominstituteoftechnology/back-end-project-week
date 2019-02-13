be255dd169ea99434e8a37ac971034c7f8de1a77
Tommmy Collison <tommy@collison.ie>
2019-02-12 20:27:44 -0800

    adds progress on post req

--- Notes/index.js
+++ Notes/index.js
@@ -1,15 +1,32 @@
-const express = require('express');
-const server = express();
 const PORT = 5566;
+const express = require('express');
+const knex = require('knex');
 
+const dbConfig = require('../knexfile');
+
+const server = express();
+const db = knex(dbConfig.development);
 server.use(express.json());
 
-server.post('/notes', (req, res) => {
+server.get('/', (req, res) => {
+    res.send(`Welcome!`)
+})
 
-});
+server.post('/notes', (req, res) => {
+    const note = req.body;
+    db('notes_table').insert(note)
+    .then(ids => {
+      res.status(201).json(ids);
+    })
+    .catch(err => {
+      res.status(500).json({err: 'Failed to insert note'});
+    });
+  });
 
 server.get('/notes/', (req, res) => {
-
+    db('notes')
+    .then(rows => res.json(rows))
+    .catch(err => {res.status(500).json({message: `Unable to find notes`})})
 });
 
 server.get('/notes/:id', (req, res) => {
