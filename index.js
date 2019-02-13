301d4183f703a4fba0e88c817497aac717e246f9
mglittleton <michaelglittleton@gmail.com>
2019-02-12 23:11:53 -0500

    Added edit functionality

--- index.js
+++ index.js
@@ -32,17 +32,37 @@ server.post("/notes/create", (req, res) => {
   const note = req.body;
   db.addNote(note)
     .then(ids => {
-      res.status(201).json(ids[0])
+      res.status(201).json(ids[0]);
     })
     .catch(err => {
       res.status(500).send(err);
     });
 });
 
-server.delete('/notes/delete/:id', (req,res) => {
+server.delete("/notes/delete/:id", (req, res) => {
+  const { id } = req.params;
+  db.deleteNote(id)
+    .then(count => {
+      res.json(count);
+    })
+    .catch(err => {
+      res.status(500).send(err);
+    });
+});
+
+server.put('/notes/edit/:id', (req, res) => {
   const {id} = req.params;
-  db.deleteNote(id).then(count => {
-    res.json(count)
+  const note = req.body;
+  db.editNote(id, note).then(count => {
+    if (count) {
+      db.getNotes(id).then(note => {
+        res.json(note)
+      }).catch(err => {
+        res.status(500).send(err)
+      })
+    } else {
+      res.status(404).send("the selected note was not updated")
+    }
   }).catch(err => {
     res.status(500).send(err)
   })
