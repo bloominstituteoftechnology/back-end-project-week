6b9c191f4e0a52bd5caffef22bc3348514ec7d82
jbrock1981 <jbrock1981@gmail.com>
2019-02-12 20:48:35 -0600

    notes displaying in postman!

--- data/Seeds/notes.js
+++ data/Seeds/notes.js
@@ -5,9 +5,9 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('notes').insert([
-        {title: test1, body: 'test1'},
-        {title: test2, body: 'test2'},
-        {title: test3, body: 'test3'}
+        {title: 'test1', body: 'test1'},
+        {title: 'test2', body: 'test2'},
+        {title: 'test3', body: 'test3'}
       ]);
     });
 };
