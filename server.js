9ce6d5dc9723942ced4bafad36fbc6f64635b4c1
shannonhealy <sahealy525@gmail.com>
2019-02-12 22:18:38 -0500

    add new note endpoint

--- server.js
+++ server.js
@@ -17,7 +17,15 @@ app.get("/api/notes", (req, res) => {
 });
 
 app.post("/api/notes", (req, res) => {
-    
+    if(req.body && req.body.title && req.body.content && typeof req.body.title === "string" && typeof req.body.content === "string") {
+        db("notes").insert(req.body).then(ids => {
+            res.status(201).json(ids[0]);
+        }).catch(error => {
+            res.status(500).json({ error: "Error inserting note", info: error });
+        });
+    } else {
+        res.status(422).json({ error: "Malformed note data" });
+    }
 });
 
 app.get("/api/notes/:id", (req, res) => {
