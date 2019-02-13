ba18ba9bfa35d21e7e04e892325540980571703c
Kendra Williams <kmonique.williams@gmail.com>
2019-02-12 22:06:33 -0500

    get /all testing completed

--- api/server.test.js
+++ api/server.test.js
@@ -3,9 +3,21 @@ const server = require("./server.js");
 const db = require("../data/dbConfig.js");
 
 describe("the route handlers", () => {
-   it("responds with 200", async () => {
-      const response = await request(server).get("/all");
+   describe("get /all", () => {
+      it("responds with 200", async () => {
+         const response = await request(server).get("/all");
 
-      expect(response.status).toBe(200);
+         expect(response.status).toBe(200);
+      });
+      it("response with json", async () => {
+         const response = await request(server).get("/all");
+
+         expect(response.type).toMatch(/json/i);
+      });
+      it("sends correct response", async () => {
+         const response = await request(server).get("/all");
+
+         expect(response.body).toEqual({api: "running"});
+      });
    });
 });
