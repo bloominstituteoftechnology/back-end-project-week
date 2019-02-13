899ecd8c288e4150a3d2ff6ad95aca4fc053d76f
TheGameFreak720 <luis.alvarez@commithub.com>
2019-02-12 21:02:49 -0500

    added delete route

--- config/routes.js
+++ config/routes.js
@@ -4,6 +4,7 @@ const notesDB = require('../database/helpers/noteDb');
 module.exports = server => {
     server.get('/', getAllNotes);
     server.post('/', addNote);
+    server.delete('/:id', deleteNote);
 }
 
 const getAllNotes = async (req, res) => {
@@ -23,5 +24,16 @@ const addNote = async (req, res) => {
         res.status(201).json(posted);
     } else {
         res.status(401).json({ message: 'The note is missing data' })
+    }
+};
+
+const deleteNote = async (req, res) => {
+    const { id } = req.params;
+    
+    try {
+        const deleted = await notesDB.remove(id);
+        res.status(200).json(deleted);
+    } catch(e) {
+        res.status.send(e);
     }
 };
