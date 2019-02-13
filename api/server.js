682713ad875eee04545cf8539e40b40e0b46b49f
Kendra Williams <kmonique.williams@gmail.com>
2019-02-12 22:30:50 -0500

    second set of test creates /notes/all

--- api/server.js
+++ api/server.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const cors = require("cors");
+const notes = require("../notes/notes.js");
 const server = express();
 
 server.use(express.json(), cors());
 
-//request for note list
-server.get("/all", async (req, res) => {
+//request to see if server running
+server.get("/", async (req, res) => {
    res.status(200).json({api: "running"});
 });
+
+//request to see all notes
+server.get("/notes/all", async (req, res) => {
+   const rows = await notes.getAll();
+
+   res.status(200).json(rows);
+});
+
 
 module.exports = server;
--- api/server.test.js
+++ api/server.test.js
@@ -3,37 +3,54 @@ const server = require("./server.js");
 const db = require("../data/dbConfig.js");
 
 describe("the route handlers", () => {
-   describe("get /all", () => {
+   describe("get /", () => {
       it("responds with 200", async () => {
-         const response = await request(server).get("/all");
+         const response = await request(server).get("/");
 
          expect(response.status).toBe(200);
       });
       it("response with json", async () => {
-         const response = await request(server).get("/all");
+         const response = await request(server).get("/");
 
          expect(response.type).toMatch(/json/i);
       });
       it("sends correct response", async () => {
-         const response = await request(server).get("/all");
+         const response = await request(server).get("/");
 
          expect(response.body).toEqual({api: "running"});
       });
    });
-   describe("get /notes", () => {
+   describe("get /notes/all", () => {
       it("responds with 200", async () => {
-         const response = await request(server).get("/notes");
+         const response = await request(server).get("/notes/all");
 
          expect(response.status).toBe(200);
       });
       it("responds with json", async () => {
-         const response = await request(server).get("/notes");
+         const response = await request(server).get("/notes/all");
 
          expect(response.type).toMatch(/json/i);
       });
       it("sends correct response", async () => {
-         const response = await request(server).get("/notes");
-         expect(response.body).toEqual([]);
+         const response = await request(server).get("/notes/all");
+         expect(response.body).toEqual([{
+               "author": "Lambda School",
+               "contents": "This is lambda!",
+               "id": 1,
+               "title": "Welcome",
+            },
+            {
+               "author": "Lambda School",
+               "contents": "We teach students to code",
+               "id": 2,
+               "title": "what We Do",
+            },
+            {
+               "author": "Lambda School",
+               "contents": "Nerds! Well, most of us anyway",
+               "id": 3,
+               "title": "Who We Are",
+            }]);
       });
    });
 });
