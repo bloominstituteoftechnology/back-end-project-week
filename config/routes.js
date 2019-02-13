f23812c5e726f3aad21d501d6f0e976e9518debf
Freekeyboy <jjhutton81@gmail.com>
2019-02-13 15:43:21 -0800

    Added getNotesById function in config/routes.js.

--- config/routes.js
+++ config/routes.js
@@ -25,6 +25,31 @@ function getNotes(req, res) {
       res.status(200).json(notes);
     })
     .catch(err => {
-      res.status(500).json({ message: "Could not get any notes." }, err);
+      res.status(500).json({ message: "No Notes for you!" }, err);
     });
+}
+/**
+* GET NOTES BY ID ENDPOINT
+*
+* Display a specific note created by the user.
+*
+* @param {Object} req - Information returned from HTTP request
+* @param {Object} res - HTTP response
+*/
+function getNotesById(req, res) {
+  const noteId = req.params.id;
+    db("notes")
+      .where("id", noteId)
+      .then(note => {
+        if (note.length) {
+          res.status(200).json(note);
+        } else {
+          res
+            .status(404)
+            .json({ message: `Could not find note with id ${noteId}` });
+        }
+      })
+      .catch(err => {
+        res.status(500).json({ message: "Could not get note.", err });
+      });
 } 
