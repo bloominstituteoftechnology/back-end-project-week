c1baf652b975225aac4376b603bc4bbced9ef91a
hraya <hraya@lssu.edu>
2019-02-12 22:18:02 -0600

    Began endpoints
    completed the get all notes endpoint

--- index.js
+++ index.js
@@ -2,12 +2,25 @@ const express = require('express');
 
 const cors = require('cors');
 
+const db = require('./database/dbConfig.js')
+
 
 const server = express();
 const PORT = 4000;
 
 server.use(express.json());
 server.use(cors());
+
+
+server.get('/api/notes', (req,res)=>{
+    db('notes')
+    .then(notes =>{
+        res.json(notes)
+    }).catch(err =>{
+        res.status(500).json({error:"Cannot get notes", err})
+    })
+})
+
 
 
 
