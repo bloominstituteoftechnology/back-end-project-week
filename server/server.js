48312ac5304cb4f6e4b1e49c57c22fecc988e4c7
Abdiel Fernandez <fernandezabdiel@gmail.com>
2019-02-12 23:55:28 -0500

    added the API endpoints

--- server/server.js
+++ server/server.js
@@ -1,8 +1,69 @@
 const express = require('express')
+const notes = require('../notes/notesModel')
 
 const server = express()
 
 server.use(express.json())
 
+server.get('/notes', async (req, res) => {
+  try {
+    const allNotes = await notes.getAllNotes()
+    res.status(200).json(allNotes)  
+  } catch (error) {
+    res.status(500).json({failure: 'unable to get all the notes'})
+  }
+})
+
+server.get('/note/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const note = await notes.getNoteByID(id)
+    res.status(200).json(note)
+  } catch (error) {
+    res.status(500).json({failure: 'unable to get the note'})
+  }
+})
+
+server.post('/note/create', async (req, res) => {
+  const {title, content} = req.body
+
+  if(title && content) {
+    try {
+      await notes.createNote(req.body)
+      res.status(201).json({success: "the note has been added"})
+    } catch (error) {
+      res.status(500).json({failure: "unable to create the server"})
+    }
+  } else {
+    res.status(422).json({failure: "please add the title or the content"})
+  }
+})
+
+server.put('/note/:id/edit', async (req, res) => {
+  const { id } = req.params
+  const { title, content } = req.body
+
+  if (title && content ) {
+    try {
+      await notes.updateNote(id, req.body)
+      res.status(201).json({success: "the note has been updated"})
+    } catch (error) {
+      res.status(500).json({failure: 'unable to update the note'})
+    }
+  } else {
+    res.status(422).json({failure: 'please make sure to add a title and content'})
+  }
+})
+
+server.delete('/note/:id/delete', async (req, res) => {
+  const { id } = req.params
+
+  try {
+    await notes.deleteNote(id)
+    res.status(200).json({success: "the note has been deleted"})
+  } catch (error) {
+    res.status(500).json({failure: 'unable to delete the note'})
+  }
+})
 
 module.exports = server
