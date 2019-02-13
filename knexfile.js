a73d7aed5d6dcab875b9a7e45f0bf9bb82113ba0
hraya <hraya@lssu.edu>
2019-02-12 21:33:42 -0600

    Seeded Data
    began to set up dbConfig file

--- knexfile.js
+++ knexfile.js
@@ -10,6 +10,9 @@ module.exports = {
     useNullAsDefault:true,
     migrations: {
       directory: './database/migrations'
+    },
+    seeds:{
+      directory:'./database/seeds'
     }
   },
 };
