1d6d08859984ba119b9ab568e7d412883ddba9cd
Michael Trew <michaeltrew@Michaels-iMac.attlocal.net>
2019-02-12 21:45:19 -0500

    posts table completed

--- data/migrations/20190211235751_posts_table.js
+++ data/migrations/20190211235751_posts_table.js
@@ -1,6 +1,16 @@
 
 exports.up = function(knex, Promise) {
- 
+  return knex.schema.createTable('posts', posts => {
+    posts.increments();
+    posts.string('title').notNullable();
+    posts.string('dateCreated').notNullable();
+    posts.string('lastModified').string();
+    posts.string('dueDate');
+    posts.string('category');
+    posts.string('owner').notNullable().references('username').inTable('users')
+    posts.boolean('completed');
+    posts.boolean('private');
+  })
 };
 
 exports.down = function(knex, Promise) {
