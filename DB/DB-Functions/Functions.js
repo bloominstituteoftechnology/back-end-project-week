8079301657cae6b8d2fadcf6d11dee55359554e8
Lorenzo Evans <lorenzo.evans94@gmail.com>
2019-02-12 21:09:28 -0500

    Adding notes table w/ basic columns for saving data.

---
+++ DB/DB-Functions/Functions.js
@@ -0,0 +1,2 @@
+const knex = require("knex")
+
--- DB/migrations/20190211224420_notes_table.js
+++ DB/migrations/20190211224420_notes_table.js
@@ -1,8 +1,14 @@
 
 exports.up = function(knex, Promise) {
- 
+ return knex.schema.createTable('notes', table => {
+  table.increments()
+  table.string('title')
+       .notNullable()
+  table.text('body', mediumtext)
+       .notNullable()
+ })
 };
 
 exports.down = function(knex, Promise) {
-  
+ return knex.schema.dropTableIfExists('notes')
 };
