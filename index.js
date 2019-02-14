df6de10edade9fb38a66fe543f1aae3422c6f608
Jason Hedrick <jasonleehedrick@gmail.com>
2019-02-13 19:50:28 -0800

    Think I found the problem. Changed process.env.port to process.env.PORT

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const server = require('./api/server');
 
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => {
     console.log(`Server is listening on port: ${PORT}.`);
