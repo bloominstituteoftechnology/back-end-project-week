754215be31b55deb6abc0eec2314eaf6438ce11b
Matthew Meitl <darksamus6@gmail.com>
2019-02-13 20:24:34 -0600

    added get by id api and post note api

--- server.js
+++ server.js
@@ -4,18 +4,33 @@ const db=[];
 server.use(express.json());
 
 server.get("/notes", (req, res) => {
-  db('notes').then(id => {
-    res.status(200).json(id); 
+  db('notes').then(notes => {
+    res.status(200).json(notes); 
   })
   .catch(err => { res.status(500).json({err: "there was an error"})
 })
   });
 
 server.get("/notes/:id", (req, res) => {
-    
+
+  const {id} = req.params
+  db('notes').where({id}).then(id => {
+    res.status(201).json(id); 
+  })
+  .catch(err => { res.status(500).json({err: "there was an error"})
+})
 });
+    
+
 
 server.post("/notes", (req, res) => {
+
+  const content = req.body
+  db('notes').insert(content).then( id => {
+    res.status(200).json(id)
+  })
+  .catch(err => { res.status(500).json({err: "there was an error"})
+})
     
 });
 
