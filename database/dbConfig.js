d16e4ef7068fb2eaca2c7d3bde315baba0eb3915
hraya <hraya@lssu.edu>
2019-02-13 21:30:56 -0600

    Completed endpoints
    all endpoints work properly, tested through postman

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -2,4 +2,25 @@ const knex = require('knex');
 
 const knexConfig = require('../knexfile.js');
 
-module.exports = knex(knexConfig.development);
+ db = knex(knexConfig.development);
+
+ module.exports = {
+    getAll: () =>{
+        return db('notes');
+    },
+
+    findById:(id)=>{
+        return db('notes').where({id}).first();
+    },
+
+    insert: (note)=>{
+        return db('notes').insert(note);
+    },
+    update: (id, note) =>{
+        return db('notes').where({ id }).update(note);
+    },
+
+    remove: (id)=>{
+        return db('notes').where({id}).del();
+    }
+ }
--- index.js
+++ index.js
@@ -12,8 +12,21 @@ server.use(express.json());
 server.use(cors());
 
 
+server.post('/api/notes', (req, res)=>{
+    const note = req.body;
+    db.insert(note)
+    .then(ids =>{
+        db.findById(ids[0])
+        .then(notes=>{
+            res.status(201).json(note)
+        })
+    }).catch(err =>{
+        res.status(500).json({error:"Problem adding note", err})
+    })
+})
+
 server.get('/api/notes', (req,res)=>{
-    db('notes')
+    db.getAll()
     .then(notes =>{
         res.json(notes)
     }).catch(err =>{
@@ -21,7 +34,56 @@ server.get('/api/notes', (req,res)=>{
     })
 })
 
+server.get('/api/notes/:id', (req, res)=>{
+    const { id } = req.params;
+    db.findById(id)
+    .then(note =>{
+        if(note){
+            res.json(note)
+        }else{
+            res.status(404).json({message:'The note with the specified id does not exist!'})
+        }
+    }).catch(err=>{
+        res.status(500).json({error:"Trouble getting the note", err})
+    })
+})
 
+server.put('/api/notes/:id', (req, res)=>{
+    const { id } = req.params;
+    const note = req.body;
+    if(note.title && note.textBody){
+        db.update(id, note)
+        .then(updated =>{
+            if(updated){
+                db.findById(id).then(notes=>{
+                    res.json(notes)
+                }).catch(err=>{
+                    res.status(500).json({message:"could not return updated note!"})
+                })
+            }else{
+                res.status(404).json({message:"The note with the specified id does not exist!"})
+            }
+        }).catch(err =>{
+            res.status(500).json({error:"The note could not be motified",err})
+        })
+    }else{
+        res.status(400).json({message:"Missing a valid title and textBody"})
+    }
+})
+
+server.delete('/api/notes/:id', (req, res)=>{
+    const { id } = req.params;
+    db.remove(id)
+    .then(removed =>{
+        if(removed){
+            res.json({message:"Note has been Deleted!"})
+        }else{
+            res.status(500).json({message:"note id does not exist!"})
+        }
+    }).catch(err =>{
+        res.status(500).json({error:"The note could not be removed!"})
+    })
+})
 
 
 server.listen(PORT, () =>{
