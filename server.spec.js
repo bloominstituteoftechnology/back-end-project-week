1e8d4038e619b78f00981afd860a0eee54964aea
cferrell2 <cferrell12182@gmail.com>
2019-02-13 23:50:34 -0500

    Create tests for PUT route handler. Create tests for DELETE route handler.

--- server.spec.js
+++ server.spec.js
@@ -6,7 +6,7 @@ const request = require('supertest');
 
 describe('server /api/notes', () =>{
 
-  describe.skip('GET /api/notes', () => {
+  describe('GET /api/notes', () => {
     it('should return status code 200', async ()=>{
       const response = await request(server).get('/api/notes');
       expect(response.status).toBe(200);
@@ -24,7 +24,7 @@ describe('server /api/notes', () =>{
     })
   })
 
-  describe.skip('GET /api/notes/:id', () =>{
+  describe('GET /api/notes/:id', () =>{
     it('should return status code 200', async ()=>{
       const id = 1;
       const response = await request(server).get(`/api/notes/${id}`);
@@ -58,6 +58,47 @@ describe('server /api/notes', () =>{
       const note = {title: 'Test 5', content: 'test note 5'};
       const response = await request(server).post(`/api/notes`).send(note);
       const expected = JSON.parse(response.text);
+      expect(response.body).toEqual(expected);
+    })
+  })
+
+  describe('PUT /api/notes/:id', () =>{
+    it('should return status code 200', async ()=>{
+      const id = 1;
+      const note = {title: 'Test 1', content: 'updated note 1'};
+      const response = await request(server).put(`/api/notes/${id}`).send(note);
+      expect(response.status).toBe(200);
+    })
+    it('should return JSON format', async ()=>{
+      const id = 2;
+      const note = {title: 'Test 2', content: 'updated note 2'};
+      const response = await request(server).put(`/api/notes/${id}`).send(note);
+      expect(response.type).toBe('application/json');
+    })
+    it('should return the updated note', async ()=>{
+      const id = 3;
+      const note = {title: 'Test 3', content: 'updated note 3'};
+      const response = await request(server).put(`/api/notes/${id}`).send(note);
+      const expected = JSON.parse(response.text);
+      expect(response.body).toEqual(expected);
+    })
+  })
+
+  describe('DELETE /api/notes/:id', () =>{
+    it('should return status code 200', async ()=>{
+      const id = 5;
+      const response = await request(server).get(`/api/notes/${id}`);
+      expect(response.status).toBe(200);
+    })
+    it('should return JSON format', async ()=>{
+      const id = 4;
+      const response = await request(server).get(`/api/notes/${id}`);
+      expect(response.type).toBe('application/json');
+    })
+    it('should return the deleted note', async ()=>{
+      const id = 3;
+      const response = await request(server).get(`/api/notes/${id}`);
+      expected = JSON.parse(response.text);
       expect(response.body).toEqual(expected);
     })
   })
