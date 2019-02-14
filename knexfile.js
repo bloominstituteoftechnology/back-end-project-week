db2b38bba74eaa362bb84c6646cd6d858e1771c7
mglittleton <michaelglittleton@gmail.com>
2019-02-14 00:48:16 -0500

    I have no earthly idea what's about to happen

--- knexfile.js
+++ knexfile.js
@@ -5,14 +5,29 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './data/notes.sqlite3'
+      filename: './notes.sqlite3'
     },
     useNullAsDefault: true,
     migrations: {
       directory: "./data/migrations"
     },
     seeds: {
-      directory: './data/seeds'
+      directory: "./data/seeds"
     }
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds/'
+    },
+    useNullAsDefault: true
   }
+
 };
+
+
