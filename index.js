5a1869f2185ecfb9696d261b89dcf3a260ba05eb
mglittleton <michaelglittleton@gmail.com>
2019-02-14 01:24:33 -0500

    Fixing cors again

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const cl = console.log;
 const express = require("express");
-const cors = require('cors')
+const cors = function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
 
 const db = require("./middleware/helpers");
 const PORT = 4700;
--- knexfile.js
+++ knexfile.js
@@ -17,7 +17,7 @@ module.exports = {
   },
 
   production: {
-    client: 'pg',
+    client: 'postgresql',
     connection: process.env.DATABASE_URL,
     migrations: {
       directory: './data/migrations'
