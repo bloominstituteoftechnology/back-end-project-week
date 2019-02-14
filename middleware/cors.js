8cab01812ddbf3e26b248bc0237462d030961e0d
mglittleton <michaelglittleton@gmail.com>
2019-02-14 01:43:18 -0500

    Cors problem still active 2

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,4 +1,5 @@
-module.exports = function() {
+
+module.exports = function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type");
