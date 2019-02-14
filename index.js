6ba18cf6edd894c8c03249356403ea800ec77f55
Phinehas <kohler.nai@gmail.com>
2019-02-13 21:52:25 -0700

    added cors middleware to interface with front-end project Notes display in front end Working on editing/deleting notes

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const server = express();
 const db = require('./data/dbHelpers');
+const cors = require('cors');
 
 server.use(express.json());
+server.use(cors());
 
 const PORT = 5050
 
