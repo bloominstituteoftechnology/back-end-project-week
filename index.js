6bbaacea0e4bd336b5d04dbf2bd3bedb1e7f899a
Dewayne87 <dewaynelindsay87@gmail.com>
2019-02-14 14:16:53 -0800

    deploy

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 require('dotenv').config();
 
 const server = require('./api/server');
+if (process.env.NODE_ENV === 'production') {
+    // Exprees will serve up production assets
+    server.use(express.static('client/build'));
 
+    // Express serve up index.html file if it doesn't recognize route
+    const path = require('path');
+    server.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 const PORT = process.env.PORT || 4000;
 server.listen(PORT, err => {
     if (err) console.log(err);
