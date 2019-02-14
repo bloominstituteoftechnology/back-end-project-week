1f724149dcde71d8114ab525438c680c8af7faca
mglittleton <michaelglittleton@gmail.com>
2019-02-13 23:41:09 -0500

    Another possible fix for cors issue

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 const cl = console.log;
 const express = require("express");
-const cors = function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
-}
+const cors = require('cors')
 
 const db = require("./middleware/helpers");
 const PORT = 4700;
