35c5d2c5533ac0845919d1d04800239ffb788e6c
Emmanium <pradoe.ep@gmail.com>
2019-02-13 20:45:33 -0700

    made endpoint to GET all notes made endpoint to POST a note

--- api/server.js
+++ api/server.js
@@ -9,25 +9,63 @@ server.use(express.json());
 
 // Display a list of notes
 server.get('/note/get/all', async (req, res) => {
+  try {
+    const rows = await notes.getAll();
+
+    res.status(200).json(rows);
+  } catch (err) {
+    res.status(500).json({ error: 'Database go boom' });
+  }
 
 })
 
 // Create a note with a title and content
 server.post('/note/create', async (req, res) => {
+  try {
+    const noteData = req.body;
+
+    if (noteData.title && noteData.textBody) {    
+      const count = await notes.insert(noteData);
+
+      res.status(201).json(count);
+
+    } else {
+      res.status(422).json({ error: 'Missing data' });
+    }
+  } catch (err) {
+    res.status(500).json({ error: 'Database go boom' });
+  }
   
 })
 
 // View an existing note
 server.get('/note/get/:id', async (req, res) => {
-  
+    try {
+
+  } catch (err) {
+    res.status(500).json({ error: 'Database go boom' });
+  }
+
 })
 
 // Edit an existing note
 server.put('/note/edit/:id', async (req, res) => {
-  
+  try {
+
+  } catch (err) {
+    res.status(500).json({ error: 'Database go boom' });
+  }
+
 })
 
 // Delete an existing note
 server.delete('/note/delete/:id', async (req, res) => {
-  
+  try {
+
+  } catch (err) {
+    res.status(500).json({ error: 'Database go boom' });
+  }
+
 })
+
+module.exports = server;
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const server = require('./api/server.js');
 
-const port = process.env.PORT || 6666;
+const port = process.env.PORT || 5000;
 server.listen(port, () => {
   console.log(`\n** Server up on PORT ${port} **\n`);
 })
