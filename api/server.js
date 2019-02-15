72b42edc13392e91482324466a5c63b19161804a
Emmanium <pradoe.ep@gmail.com>
2019-02-14 20:35:56 -0700

    added endpoint to delete note

--- api/server.js
+++ api/server.js
@@ -61,8 +61,6 @@ server.put('/note/edit/:id', async (req, res) => {
   try {
     const noteChanges = req.body;
     const { id } = req.params;
-    const note = await notes.findById(id);
-    console.log(id);
 
     if (noteChanges.title && noteChanges.textBody) {
       const array = await notes.update(id, noteChanges);
@@ -81,6 +79,14 @@ server.put('/note/edit/:id', async (req, res) => {
 // Delete an existing note
 server.delete('/note/delete/:id', async (req, res) => {
   try {
+    const { id } = req.params;
+
+    if (id) {
+      const count = await notes.remove(id);
+      res.status(200).json({ message: `${count} note successfully deleted` });
+    } else {
+      res.status(404).json({ error: `Note doesn't exist` })
+    }
 
   } catch (err) {
     res.status(500).json({ error: 'Database go boom' });
