2609027608739dd4964d4a76174fc1643e912de0
mglittleton <michaelglittleton@gmail.com>
2019-02-14 22:14:33 -0500

    Cors fixed again

--- knexfile.js
+++ knexfile.js
@@ -16,20 +16,20 @@ module.exports = {
     }
   },
 
-  // production: {
-  //   client: 'pg',
-  //   connection: process.env.DATABASE_URL,
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     directory: './data/migrations'
-  //   },
-  //   seeds: {
-  //     directory: './data/seeds/'
-  //   }
-  // }
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds/'
+    }
+  }
 
 };
 
