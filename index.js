cce8ec15a9713a6fad8e39218a1a6b56c738fcc6
hraya <hraya@lssu.edu>
2019-02-14 23:13:06 -0600

    Messing with cors

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 
-const cors = function(req, res, next) {
+const cors = (req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
