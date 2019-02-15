5df549aa6d4a61bc69640333a43755beb5e6b554
Dewayne87 <dewaynelindsay87@gmail.com>
2019-02-14 21:35:07 -0800

    testing heroku

--- data/seeds/01-notes.js
+++ data/seeds/01-notes.js
@@ -4,9 +4,9 @@ exports.seed = function (knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('notes').insert([
-        { title: 'Dj Khaled Ipsum', textBody: 'Lorem Khaled Ipsum is a major key to success. ' },
-        { title: 'Dj Khaled Ipsum 44', textBody: 'Always remember in the jungle there’s a lot of they in there, '  },
-        { title: 'Dj Khaled Ipsum 3', textBody: 'Egg whites, turkey sausage, wheat toast, water. '  },
+        { title: 'Dj Khaled Ipsum', textBody: 'Lorem Khaled Ipsum is a major key to success. Surround yourself with angels, positive energy, beautiful people, beautiful souls, clean heart, angel. To succeed you must believe. When you believe, you will succeed. Bless up. ' },
+        { title: 'Dj Khaled Ipsum 44', textBody: 'Always remember in the jungle there’s a lot of they in there, after you overcome they, you will make it to paradise. You should never complain, complaining is a weak emotion, you got life, we breathing, we blessed.'  },
+        { title: 'Dj Khaled Ipsum 3', textBody: 'Egg whites, turkey sausage, wheat toast, water. Of course they don’t want us to eat our breakfast, so we are going to enjoy our breakfast. The key to success is to keep your head above the water, never give up.'  },
       ]);
     });
 };
