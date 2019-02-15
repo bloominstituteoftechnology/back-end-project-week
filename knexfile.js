7b2538455a9c1fa7e90464c36d3bc060bf7766d6
Dewayne87 <dewaynelindsay87@gmail.com>
2019-02-14 20:42:41 -0800

    testing production

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,33 @@
-module.exports = {
+require('dotenv').config();
+const localPg ={
+  host: 'localHost',
+  database:'lambda',
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+};
 
+const dbConnection = process.env.DATABASE_URL || localPg;
+
+module.exports = {
+  // development: {
+  //   client: 'sqlite3',
+  //   connection: {
+  //     filename: './data/testNotes.db3'
+  //   },
+  //   useNullAsDefault: true,
+  //   migrations: {
+  //     directory: './data/migrations'
+  //   },
+  //   seeds: {
+  //     directory: './data/seeds'
+  //   }
+  // },
   production: {
-    client: 'sqlite3',
-    connection: {
-      filename: './data/lambdaNotes.sqlite3'
+    client: 'pg',
+    connection: dbConnection,
+    pool:{
+      min:2,
+      max:10
     },
     useNullAsDefault: true,
     migrations: {
@@ -13,17 +37,17 @@ module.exports = {
       directory: './data/seeds'
     }
   },
-  testing: {
-    client: 'sqlite3',
-    connection: {
-      filename: './data/testNotes.db3',
-    },
-    useNullAsDefault: true,
-    migrations: {
-      directory: './data/migrations',
-    },
-    seeds: {
-      directory: './data/seeds',
-    },
-  },
+  // testing: {
+  //   client: 'sqlite3',
+  //   connection: {
+  //     filename: './data/testNotes.db3',
+  //   },
+  //   useNullAsDefault: true,
+  //   migrations: {
+  //     directory: './data/migrations',
+  //   },
+  //   seeds: {
+  //     directory: './data/seeds',
+  //   },
+  // },
 };
