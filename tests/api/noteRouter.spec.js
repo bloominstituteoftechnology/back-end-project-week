0c4db34d547a7336b64320b108360aed0b8bdd47
Eric Whitcomb <eric.whitcomb@gmail.com>
2019-02-16 11:11:26 -0700

    Put and delete request unit tests done

--- tests/api/noteRouter.spec.js
+++ tests/api/noteRouter.spec.js
@@ -16,25 +16,25 @@ describe('Notes API', () => {
 
         test('responds with 201', async () => {
             const body = { title: "Title", content: "Content" };
-            const response = await request(server).post('/api/notes').send(body);
+            const response = await request(app).post('/api/notes').send(body);
             expect(response.status).toBe(201);
         });
 
         test('responds with json', async () => {
             const body = { title: "Title", content: "Content" };
-            const response = await request(server).post('/api/notes').send(body);
+            const response = await request(app).post('/api/notes').send(body);
             expect(response.type).toMatch(/json/i);
         });
 
         test('responds with id of new team', async () => {
             const body = { title: "Title", content: "Content" };
-            const response = await request(server).post('/api/notes').send(body);
+            const response = await request(app).post('/api/notes').send(body);
             expect(response.body).toEqual({id: 1, title: "Title", content: "Content" });
         });
 
         test('responds with 400', async () => {
             const body = {};
-            const response = await request(server).post('/api/notes').send(body);
+            const response = await request(app).post('/api/notes').send(body);
             expect(response.status).toBe(400);
         });
 
@@ -112,24 +112,85 @@ describe('Notes API', () => {
 
         test('responds with 404 on invalid id', async () => {
             const id = 1;
-            const response = await request(app).get(`/api/notes/${note.id}`);
+            const response = await request(app).get(`/api/notes/${id}`);
             expect(response.status).toBe(404);
         });
-
-
-
 
     });
 
     describe('PUT /api/notes/<id>', () => {
 
-        
+        beforeEach(async () => {
+            await db('notes').truncate();
+        });
+
+        afterAll(async () => {
+            await db('notes').truncate();
+        });
+
+        test('responds with 200', async () => {
+            const note = await noteModel.insert({ title: 'Ttle', content: 'Cntent' });
+            const body = { title: 'Title', content: 'Content' };
+            const response = await request(app).put(`/api/notes/${note.id}`).send(body);
+            expect(response.status).toBe(200);
+        });
+
+        test('responds with json', async () => {
+            const note = await noteModel.insert({ title: 'Ttle', content: 'Cntent' });
+            const body = { title: 'Ttle', content: 'Cntent' };
+            const response = await request(app).get(`/api/notes/${note.id}`).send(body);
+            expect(response.type).toMatch(/json/i);
+        });
+
+        test('responds with updated note object', async () => {
+            const note = await noteModel.insert({ title: 'Ttle', content: 'Cntent' });
+            const body = { title: 'Title', content: 'Content' };
+            const response = await request(app).put(`/api/notes/${note.id}`).send(body);
+            expect(response.body).toEqual({ id: 1, title: 'Title', content: 'Content' });
+        });
+
+        test('responds with 404 on invalid id', async () => {
+            const id = 1;
+            const body = { title: 'Title', content: 'Content' };
+            const response = await request(app).get(`/api/notes/${id}`).send(body);
+            expect(response.status).toBe(404);
+        });
 
     });
 
     describe('DELETE /api/notes/<id>', () => {
 
-        
+        beforeEach(async () => {
+            await db('notes').truncate();
+        });
+
+        afterAll(async () => {
+            await db('notes').truncate();
+        });
+
+        test('responds with 200', async () => {
+            const note = await noteModel.insert({ title: "Title", content: "Content"});
+            const response = await request(app).delete(`/api/notes/${id}`);
+            expect(response.status).toBe(200);
+        });
+
+        test('responds with json', async () => {
+            const note = await noteModel.insert({ title: "Title", content: "Content"});
+            const response = await request(app).delete(`/api/notes/${id}`);
+            expect(response.type).toMatch(/json/i);
+        });
+
+        test('responds with count of 1 record deleted', async () => {
+            const note = await noteModel.insert({ title: "Title", content: "Content"});
+            const response = await request(app).delete(`/api/notes/${id}`);
+            expect(response.body).toEqual({ count: 1 });
+        });
+
+        test('responds with 404 on invalid id', async () => {
+            const id = 1;
+            const response = await request(app).delete(`/api/notes/${id}`);
+            expect(response.status).toBe(404);
+        });
 
     });
 
