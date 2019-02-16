36b464d7eab4a204bcc1313cf21d267f4d92aed4
vy3191 <venkyafhs@gmail.com>
2019-02-15 23:33:47 -0500

    Added .env fiel MVP completed

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+require('dotenv').config();
+
 const server = require('./api/server.js');
-const PORT = 2300;
+const PORT = env.process.PORT || 2300;
 
 server.listen(PORT, () => {
+   console.log(env.process.PORT)
    console.log(`Server is up and running at http://localhost ${PORT}`);
 })
