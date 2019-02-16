5febded0cc5bb4a1f5605cd76ca17202933709d8
Eric Whitcomb <eric.whitcomb@gmail.com>
2019-02-16 12:53:08 -0700

    Added cors

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
+var cors = require('cors');
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use('/api', require('./api/rootRouter'));
 
 app.get('/', (req, res) => {
