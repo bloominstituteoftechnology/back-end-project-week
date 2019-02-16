48c99efdb2412875fdeaa7fd93ec531286c5b6a5
alasalle <angela.lasalle91@gmail.com>
2019-02-16 10:10:22 -0500

    added logout

--- api/routes/authRouter.js
+++ api/routes/authRouter.js
@@ -97,4 +97,14 @@ router.post("/login", (req, res) => {
   }
 });
 
+router.post("/logout", (req, res) => {
+  req.session.destroy(err => {
+    if (err) {
+      res.status(500).send("failed to logout");
+    } else {
+      res.send("logout successful");
+    }
+  });
+});
+
 module.exports = router;
