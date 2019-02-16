993dffa301d4c02ef6acee7f2213049fa0748589
iepoch <staclem@live.com>
2019-02-16 09:26:44 -0700

    should be /notes not note

--- api/server.js
+++ api/server.js
@@ -13,5 +13,5 @@ server.get('/', async (req, res) => {
 const notesRouter = require('../router/notesRouter')
 
 
-server.use('/note', notesRouter)
+server.use('/notes', notesRouter)
 module.exports = server;
