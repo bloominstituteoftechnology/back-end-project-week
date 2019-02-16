c5e1f052d23741ab3f9b1c44742a0f73d7507d4b
iepoch <staclem@live.com>
2019-02-16 09:19:16 -0700

    Trying a different approch to see the database

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,6 +1,6 @@
 const knex = require('knex');
 const config = require('../knexfile.js');
 
-const db_Env = process.env.DATABASE_URL|| 'production'
+const db_Env = process.env.DATABASE_URL|| 'development'
 
 module.exports = knex(config[db_Env]);
