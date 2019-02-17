607d4ea6a8d975bc9e59df46aa486d0fededcc3d
Obed Lorisson <Obed.lorisson@gmail.com>
2019-02-17 06:08:20 -0500

    added environment variable to make port dynamic

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,5 +1,7 @@
 const knex = require('knex');
 
-const knexConfig = require('../knexfile.js');
+const ENV = process.env.DB_ENV || 'development'
 
-module.exports = knex(knexConfig.development);
+const knexConfig = require('../knexfile.js')[ENV];
+
+module.exports = knex(knexConfig);
