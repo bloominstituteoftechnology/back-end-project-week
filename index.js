fa692c5d830521787485fe13a9d9ab4f5348fab6
Tommmy Collison <tommy@collison.ie>
2019-02-17 17:03:36 -0800

    adds minor edit to test auto-deploy in heroku

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ server.use(cors());
 server.use(express.json());
 
 server.get('https://cryptic-brook-42672.herokuapp.com/', (req, res) => {
-    res.send(`Welcome in the og repo!`)
+    res.send(`Welcome in the og repo! \o/`)
 })
 
 server.post('https://cryptic-brook-42672.herokuapp.com/notes', (req, res) => {
