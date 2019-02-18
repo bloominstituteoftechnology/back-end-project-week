e35ad7071605ebce3c48efa94fc8a8d05d273172
Eric Whitcomb <eric.whitcomb@gmail.com>
2019-02-18 15:22:06 -0700

    Updated app to show port in web browser

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ app.use(express.json());
 app.use(cors());
 app.use('/api', require('./api/rootRouter'));
 
+const PORT = process.env.PORT || '3300';
+
 app.get('/', (req, res) => {
-    res.status(200).send('API Active');
+    res.status(200).send(`API active on port: ${PORT}`);
 });
 
 module.exports = app;
