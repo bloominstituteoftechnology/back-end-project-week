5904eefc4ae750e1a77577b08a7a41ed238bef0f
Eric Whitcomb <eric.whitcomb@gmail.com>
2019-02-18 15:16:05 -0700

    Updated port to include env variable

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
+
 const app = require('./app');
 
-const PORT = '3300';
+const PORT = process.env.PORT || '3300';
 
 app.listen(PORT, () => {
     console.log(`\nListening on port ${PORT}\n`);
