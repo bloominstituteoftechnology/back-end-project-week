73bd52fd5fa40f324f14d5133cf95732bdf07a61
Abdiel Fernandez <fernandezabdiel@gmail.com>
2019-02-18 23:56:33 -0500

    started using express routes

--- server/server.js
+++ server/server.js
@@ -1,72 +1,20 @@
 const express = require('express')
-const notes = require('../notes/notesModel')
 const cors = require('cors')
 
+
+
 const server = express()
+
+
+const notesRouter = require('./routes/notesRoutes')
+const userRouter = require('./routes/userRoutes')
 
 server.use(express.json())
 server.use(cors())
 
-server.get('/notes', async (req, res) => {
-  try {
-    const allNotes = await notes.getAllNotes()
-    res.status(200).json(allNotes)  
-  } catch (error) {
-    res.status(500).json({failure: 'unable to get all the notes'})
-  }
-})
 
-server.get('/note/:id', async (req, res) => {
-  const { id } = req.params;
-  try {
-    let note = await notes.getNoteByID(id)
-    note = note[0];
-    res.status(200).json(note)
-  } catch (error) {
-    res.status(500).json({failure: 'unable to get the note'})
-  }
-})
+server.use('/:username', notesRouter)
 
-server.post('/note/create', async (req, res) => {
-  const {title, content} = req.body
-
-  if(title && content) {
-    try {
-      await notes.createNote(req.body)
-      res.status(201).json({success: "the note has been added"})
-    } catch (error) {
-      res.status(500).json({failure: "unable to create the note"})
-    }
-  } else {
-    res.status(422).json({failure: "please add the title or the content"})
-  }
-})
-
-server.put('/note/:id/edit', async (req, res) => {
-  const { id } = req.params
-  const { title, content } = req.body
-
-  if (title && content ) {
-    try {
-      await notes.updateNote(id, req.body)
-      res.status(201).json({success: "the note has been updated"})
-    } catch (error) {
-      res.status(500).json({failure: 'unable to update the note'})
-    }
-  } else {
-    res.status(422).json({failure: 'please make sure to add a title and content'})
-  }
-})
-
-server.delete('/note/:id/delete', async (req, res) => {
-  const { id } = req.params
-
-  try {
-    await notes.deleteNote(id)
-    res.status(200).json({success: "the note has been deleted"})
-  } catch (error) {
-    res.status(500).json({failure: 'unable to delete the note'})
-  }
-})
+server.use('/users', userRouter)
 
 module.exports = server
