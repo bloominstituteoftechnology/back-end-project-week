030ad5c99773cda6c7a83808005d24a400cf5d68
Willie Yturralde <willieino@hotmail.com>
2019-02-18 20:17:35 -0800

    modify action files

--- data/seeds/notes.js
+++ data/seeds/notes.js
@@ -9,7 +9,12 @@ exports.seed = function(knex, Promise) {
         {id: 2, title: 'note2', textBody: "this is note2", tags: "", _id: 1234, image: "image address", users_id: 2},
         {id: 3, title: 'note3', textBody: "this is note3", tags: "", _id: 1234, image: "image address", users_id: 1},
         {id: 4, title: 'note4', textBody: "this is note4", tags: "", _id: 1234, image: "image address", users_id: 2},
-        {id: 5, title: 'note5', textBody: "this is note5", tags: "", _id: 1234, image: "image address", users_id: 3}
+        {id: 5, title: 'note5', textBody: "this is note5", tags: "", _id: 1234, image: "image address", users_id: 3},
+        {id: 6, title: 'note6', textBody: "this is note6", tags: "", _id: 1234, image: "image address", users_id: 1},
+        {id: 7, title: 'note7', textBody: "this is note7", tags: "", _id: 1234, image: "image address", users_id: 2},
+        {id: 8, title: 'note8', textBody: "this is note8", tags: "", _id: 1234, image: "image address", users_id: 1},
+        {id: 9, title: 'note9', textBody: "this is note9", tags: "", _id: 1234, image: "image address", users_id: 2},
+        {id: 10, title: 'note10', textBody: "this is note10", tags: "", _id: 1234, image: "image address", users_id: 3}
       ]);
     });
 };
