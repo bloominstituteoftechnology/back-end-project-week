95e0f8bacd6153e2afe3c655a576652cb5cb7dd7
Emmanium <pradoe.ep@gmail.com>
2019-02-19 20:02:59 -0700

    created tests for /GET and /POST

--- api/server.test.js
+++ api/server.test.js
@@ -8,23 +8,49 @@ describe('the route handlers', () => {
   describe('GET /note/get/all', () => {
 
     afterEach(async () => {
-      await db('games').truncate();
+      await db('notes').truncate();
     });
+
+    it('responds with 200', async () => {
+      const response = await request(server).get('/note/get/all');
+
+      expect(response.status).toBe(200);
+    })
+
+    it('sends correct responds object', async () => {
+      const response = await request(server).get('/note/get/all');
+
+      expect(response.body).toEqual([]);
+    })
 
   })
 
   describe('POST /note/create', () => {
 
     afterEach(async () => {
-      await db('games').truncate();
+      await db('notes').truncate();
     });
+
+    it('responds with 201 when body is correct', async () => {
+      const body = { title: 'FakeNote', textBody: 'FakeBody' };
+      const response = await request(server).post('/note/create').send(body);
+
+      expect(response.status).toBe(201);
+    })
+
+    it('responds with 422 when body is missing data', async () => {
+      const body = {};
+      const response = await request(server).post('/note/create').send(body);
+
+      expect(response.status).toBe(422);
+    })
 
   })
 
   describe('GET /note/view/:id', () => {
 
     afterEach(async () => {
-      await db('games').truncate();
+      await db('notes').truncate();
     });
 
   })
@@ -32,7 +58,7 @@ describe('the route handlers', () => {
   describe('PUT /note/edit/:id', () => {
 
     afterEach(async () => {
-      await db('games').truncate();
+      await db('notes').truncate();
     });
 
   })
@@ -40,7 +66,7 @@ describe('the route handlers', () => {
   describe('DELETE /note/delete/:id', () => {
 
     afterEach(async () => {
-      await db('games').truncate();
+      await db('notes').truncate();
     });
 
   })
