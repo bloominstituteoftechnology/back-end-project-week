47100a34f8d64c4d2f86ef2127be10f77ec1b81c
jbierly1 <james.bierly@gmail.com>
2019-02-19 21:58:57 -0500

    made initial file structure

---
+++ knexfile.js
@@ -0,0 +1,52 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/dev.sqlite3'
+    },
+    useNullAsDefault:true,
+    seeds: {
+			directory: './data/seeds'
+    },
+    
+  migrations: {
+    directory: './data/migrations'
+  }
+  },
+
+//   staging: {
+//     client: 'postgresql',
+//     connection: {
+//       database: 'my_db',
+//       user:     'username',
+//       password: 'password'
+//     },
+//     pool: {
+//       min: 2,
+//       max: 10
+//     },
+//     migrations: {
+//       tableName: 'knex_migrations'
+//     }
+//   },
+
+//   production: {
+//     client: 'postgresql',
+//     connection: {
+//       database: 'my_db',
+//       user:     'username',
+//       password: 'password'
+//     },
+//     pool: {
+//       min: 2,
+//       max: 10
+//     },
+//     migrations: {
+//       tableName: 'knex_migrations'
+//     }
+//   }
+
+};
