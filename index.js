c1c2f07c9a07c94fcaa63205e2500b5315fb83a0
Megan Fontenot <lambda_school_loaner_28@Lambda-School-Loaner-28s-MacBook-Pro.local>
2019-02-21 00:50:03 -0600

    Add a note functionaility.

--- index.js
+++ index.js
@@ -8,26 +8,21 @@ const server = express();
 server.use(express.json());
 
 server.get("/notes", (req, res) => {
-  db.getNotes()
-    .then(notes => {
-      res.json(notes);
-    })
-    .catch(err => {
-      res.status(500).send(err);
-    });
+  db.getNotes().then(notes => { res.json(notes); })
+    .catch(err => {res.status(500).send(err);});
 });
-
 
 server.get("/notes/:id", (req, res) => {
   const { id } = req.params;
-  db.getNotes(id).then(note => {
-    res.json(note)
-  }).catch(err => {
-    res.status(500).send(err)
-  })
+  db.getNotes(id).then(note => { res.json(note);})
+    .catch(err => {res.status(500).send(err);});
 });
 
-
+server.post("/notes/create", (req, res) => {
+  const note = req.body;
+  db.addNote(note).then(ids => {res.status(201).json(ids[0])})
+    .catch(err => {res.status(500).send(err);});
+});
 
 server.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
