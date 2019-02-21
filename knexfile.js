fd626545019beef5c44d8eb3a374f83d7360db49
alasalle <angela.lasalle91@gmail.com>
2019-02-20 23:56:14 -0500

    heroku postgres complete

--- knexfile.js
+++ knexfile.js
@@ -7,46 +7,50 @@
 
 // const dbConnection = process.env.DATABASE_URL || localpostgres;
 
+require('dotenv').config('/.env');
+
 module.exports = {
-  development: {
-    client: "sqlite3",
-    connection: {
-      filename: "./data/notes.db3"
-    },
-    useNullAsDefault: true,
-    migrations: {
-      directory: "./data/migrations"
-    },
-    seeds: {
-      directory: "./data/seeds"
-    }
-  },
-  testing: {
-    client: "sqlite3",
-    connection: {
-      filename: "./data/test.db3"
-    },
-    useNullAsDefault: true,
-    migrations: {
-      directory: "./data/migrations"
-    },
-    seeds: {
-      directory: "./data/seeds"
-    }
-  },
+  // development: {
+  //   client: "sqlite3",
+  //   connection: {
+  //     filename: "./data/notes.db3"
+  //   },
+  //   useNullAsDefault: true,
+  //   migrations: {
+  //     directory: "./data/migrations"
+  //   },
+  //   seeds: {
+  //     directory: "./data/seeds"
+  //   }
+  // },
+  // testing: {
+  //   client: "sqlite3",
+  //   connection: {
+  //     filename: "./data/test.db3"
+  //   },
+  //   useNullAsDefault: true,
+  //   migrations: {
+  //     directory: "./data/migrations"
+  //   },
+  //   seeds: {
+  //     directory: "./data/seeds"
+  //   }
+  // },
 
   production: {
-    client: 'postgresql',
+    client: 'pg',
     connection: {
-      database: 'postgres://icmveonkwqjdmk:2b75738b9a60d7904542b802fb7cf0e1016aa94dc99ffcb21b838b01c8796c44@ec2-54-204-41-109.compute-1.amazonaws.com:5432/da0c07gtdf3c6o',
-      user:     'username',
+      database: process.env.DATABASE_URL,
+      user: 'username',
       password: 'password'
     },
     migrations: {
-      directory: "./data/migrations"
+      directory: "./data/migrations",
+      tableName: 'knex_migrations'
     },
+    useNullAsDefault: true,
     seeds: {
       directory: "./data/seeds"
     }
   }
-};
+}
