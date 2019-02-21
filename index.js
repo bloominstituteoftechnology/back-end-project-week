7e9deb654e54d8a9198bc731d56448509187dc53
Megan Fontenot <lambda_school_loaner_28@Lambda-School-Loaner-28s-MacBook-Pro.local>
2019-02-21 13:13:17 -0600

    edit and delete functionality implemented

--- index.js
+++ index.js
@@ -8,21 +8,29 @@ const server = express();
 server.use(express.json());
 
 server.get("/notes", (req, res) => {
-  db.getNotes().then(notes => { res.json(notes); })
+  db.getNotes().then(notes => { res.json(notes);})
     .catch(err => {res.status(500).send(err);});
 });
 
 server.get("/notes/:id", (req, res) => {
   const { id } = req.params;
-  db.getNotes(id).then(note => { res.json(note);})
+  db.getNotes(id)
+    .then(note => {res.json(note);})
     .catch(err => {res.status(500).send(err);});
 });
 
 server.post("/notes/create", (req, res) => {
   const note = req.body;
-  db.addNote(note).then(ids => {res.status(201).json(ids[0])})
+  db.addNote(note)
+    .then(ids => {res.status(201).json(ids[0])})
     .catch(err => {res.status(500).send(err);});
 });
+
+server.delete('/notes/delete/:id', (req,res) => {
+  const {id} = req.params;
+  db.deleteNote(id).then(count => {res.json(count)})
+  .catch(err => {res.status(500).send(err)})
+})
 
 server.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
--- middleware/helpers.js
+++ middleware/helpers.js
@@ -5,11 +5,21 @@ const db = knex(dbConfig.development);
 module.exports = {
   getNotes: id => {
     if (id) {
-      return db("notes").where("id", id).first();
+      return db("notes")
+        .where("id", id)
+        .first();
     } else return db("notes");
   },
 
   addNote: note => {
     return db('notes').insert(note)
+  },
+
+  deleteNote: (id) => {
+      return db('notes').where('id', id).del()
+  },
+
+  editNote: (id, note) => {
+      return db('notes').where('id', id).update(note)
   }
 };
