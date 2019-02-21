de02a07816e1f8fe634e2c91346d89f247f758c7
Emmanium <pradoe.ep@gmail.com>
2019-02-20 20:02:02 -0700

    added test for findById method added test for remove method

--- notes/notesModel.test.js
+++ notes/notesModel.test.js
@@ -12,12 +12,30 @@ describe('the notes model', () => {
 
     expect(ids.length).toBe(1);
     expect(ids[0]).toBe(1);
-    
   })
 
   it('should send correct response object when getting notes', async () => {
     const array = await notes.getAll();
     
     expect(array).toEqual([]);
+  })
+
+  it('should find by id', async () => {
+    await notes.insert({ title: 'fake', textBody: 'note' });
+    const response = await notes.findById(1);
+
+    expect(response.length).toBe(1);
+    expect(response[0]).toEqual({ "id": 1, "textBody": "note", "title": "fake" });
+  })
+
+  it('should remove a specified note', async () => {
+    const ids = await notes.insert({ title: 'fake', textBody: 'note' });
+    expect(ids.length).toBe(1);
+    expect(ids[0]).toBe(1);
+
+    await notes.remove(1);
+    expect(ids.length).toBe(1);
+    expect(ids[0]).toBe(1);
+
   })
 })
