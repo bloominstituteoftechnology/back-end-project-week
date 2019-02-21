0bcd03db8300d2fe59b4a6978a3b711320a2933b
rlmclaughlin <rlmclaughlin@yahoo.com>
2019-02-20 22:00:59 -0600

    fixed process.env.PORT for deployment

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 require('dotenv').config()
 
 const { server } = require('./api/server')
-const port = process.env.PORT || 5566
+const PORT = process.env.PORT || 5566
 
 server.listen(port, () => {
-    console.log(`\n===Server listening on port ${port}\n`)
+    console.log(`\n===Server listening on port ${PORT}\n`)
 })
