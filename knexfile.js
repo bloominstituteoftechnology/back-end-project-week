0250909f551465fdc25d2874c5e2ac4a367222b4
Obed Lorisson <Obed.lorisson@gmail.com>
2019-02-20 23:45:19 -0500

    add postgres database

--- knexfile.js
+++ knexfile.js
@@ -32,20 +32,14 @@ module.exports = {
   //   }
   // },
 
-  // production: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // }
-
+  production: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+        directory: __dirname + '/db/migrations',
+    },
+    seeds: {
+        directory: __dirname + '/db/seeds/production',
+    },
+  }
 };
