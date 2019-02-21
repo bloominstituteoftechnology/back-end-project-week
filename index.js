6df713cd3ca455f4ed0a8b6be5fa1afb1e224dce
rlmclaughlin <rlmclaughlin@yahoo.com>
2019-02-20 22:10:31 -0600

    fixed Port issue

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ require('dotenv').config()
 const { server } = require('./api/server')
 const PORT = process.env.PORT || 5566
 
-server.listen(port, () => {
+server.listen(PORT, () => {
     console.log(`\n===Server listening on port ${PORT}\n`)
 })
