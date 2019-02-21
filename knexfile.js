191ad68c22b9770f5891cf75639e172e6dd6a37c
cferrell2 <cferrell12182@gmail.com>
2019-02-20 20:25:26 -0500

    add .env files and create environment variables for postgres database connection.  Revise knexfile.js to make use of environment variables, hiding connection attributes.

--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,24 @@
 // Update with your config settings.
 
+//Allow use of .env file - environment variables
+require('dotenv').config();
+
+//local connection object
+const localPg = {
+  host: 'localhost',
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD
+}
+
+const dbConnection = process.env.DATABASE_URL || localPg;
+
 module.exports = {
 
   development: {
     client: 'pg',
-    connection: 'postgres://cferrell2:meuamor1@localhost/lambdanotes',
+    // connection: 'postgres://cferrell2:meuamor1@localhost/lambdanotes',
+    connection: `postgres://${localPg.user}:${localPg.password}@${localPg.host}/${localPg.database}`,
     useNullAsDefault: true,
     migrations: {
       directory: './data/migrations'
@@ -26,7 +40,21 @@ module.exports = {
       directory: './data/seeds'
     }
   },
-
+  
+  // production: {
+  //   client: 'pg',   
+  //   connection: dbConnection,   //an object or a string
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     directory: './data/migrations'
+  //   },
+  //   seeds: {
+  //     directory: './data/seeds'
+  //   }
+  // }
   // staging: {
   //   client: 'postgresql',
   //   connection: {
@@ -43,20 +71,5 @@ module.exports = {
   //   }
   // },
 
-  // production: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // }
 
 };
