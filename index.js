e5dba44cfc1933367c7076941e545bf1be03bd82
jbrock1981 <jbrock1981@gmail.com>
2019-02-21 19:33:04 -0600

    .

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ require('dotenv').config();
 const server = require('./server');
 const port = process.env.PORT || 3000;
 
-server.listen(port, () => {
+server.listen(port, "0.0.0.0", () => {
     console.log(`alive and listening on port ${port}`);
 })
