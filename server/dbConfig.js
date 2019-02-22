faa1b4150232eca224f5ba8019f2bec6d01be2ea
Michael Trew <michaeltrew@Michaels-iMac.attlocal.net>
2019-02-21 22:15:46 -0500

    fixed error

--- server/dbConfig.js
+++ server/dbConfig.js
@@ -4,4 +4,4 @@ const knexConfig = require('../knexfile.js');
 const dbEnv= process.env.NODE_ENV || 'development'
 
 
-module.exports = knex(knexConfig[dbEnv]);
+module.exports = knex(knexConfig.dbEnv);
