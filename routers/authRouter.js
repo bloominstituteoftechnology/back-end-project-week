8e47102461ee3276c92705ec2d6a78c526a69245
Michael Trew <michaeltrew@Michaels-iMac.attlocal.net>
2019-02-22 10:51:04 -0500

    checking

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -4,26 +4,32 @@ const db = require('../server/helpers/authHelpers');
 const bcrypt = require('bcryptjs');
 const { newToken } = require('../server/MiddleWare/middleware')
 
-db.insertUser(user)
-  .then(response => {
-    console.log('\n\n\n\n\n')
-    console.log(response)
-    console.log('\n\n\n\n\n')
-    res.send({ user })
-  })
-  //   const id = ids[0];
-  //   db.findByID(id)
-  //     .then(user => {
-  //       const token = (newToken(user));
-  //       res.status(200).json({ id: user.id, username:username,token:token});
-  //     })
-  //     .catch(err => {
-  //       console.log("error", err);
-  //       res.status(500).json({ error: 'Something went wrong' })
-  //     })
-  .catch(err => {
-    res.status(500).send(err);
-  });
+router.post('/signup', (req, res) => {
+  const user = req.body;
+  const hashedPass = bcrypt.hashSync(user.password, 12)
+  user.password = hashedPass;
+  var username = user.username
+  db.insertUser(user)
+    .then(response => {
+      console.log('\n\n\n\n\n')
+      console.log(response)
+      console.log('\n\n\n\n\n')
+      res.send({ user })
+    })
+    //   const id = ids[0];
+    //   db.findByID(id)
+    //     .then(user => {
+    //       const token = (newToken(user));
+    //       res.status(200).json({ id: user.id, username:username,token:token});
+    //     })
+    //     .catch(err => {
+    //       console.log("error", err);
+    //       res.status(500).json({ error: 'Something went wrong' })
+    //     })
+    .catch(err => {
+      res.status(500).send(err);
+    });
+
 
 router.post('/login', (req, res) => {
   const creds = req.body;
