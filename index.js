5c28f570c91f57bc56f9422202c8701884851fbe
jbrock1981 <jbrock1981@gmail.com>
2019-02-21 19:28:37 -0600

    port correction for heroku app crash

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const server = require('./server');
-const PORT = process.env.PORT || 4200;
+const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
     console.log(`alive and listening on port ${PORT}`);
