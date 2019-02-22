8cc296cd1426a8fddf9ad24868908de0e6163ce0
Michael Trew <michaeltrew@Michaels-iMac.attlocal.net>
2019-02-22 08:50:35 -0500

    fix

--- knexfile.js
+++ knexfile.js
@@ -33,7 +33,7 @@ module.exports = {
   },
 
   production: {
-    client: pg,
+    client:'pg',
     connection: 'postgres://tzqredbemqcfwd:034ce9a63bfe5c6edf4cf768e7254d220ceb72748dd0edc0e4d5f0c3e966e608@ec2-54-83-44-4.compute-1.amazonaws.com:5432/d7uugijmmsfknm',
     pool: {
       min: 2,
