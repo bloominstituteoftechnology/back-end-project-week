4793e069863d8f8c32ee751d9084f8fd33130b6b
Emmanium <pradoe.ep@gmail.com>
2019-02-23 15:46:50 -0700

    created .env file added postgres dependency added production object to knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -28,6 +28,18 @@ module.exports = {
     seeds: {
       directory: './data/migrations'
     }
-  }
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    }
+  },
 
 };
