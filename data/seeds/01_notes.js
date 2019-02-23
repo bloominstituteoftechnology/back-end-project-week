5705d8d7533b96cb3975c7ef02edc4e03d5f1291
alasalle <angela.lasalle91@gmail.com>
2019-02-23 10:37:24 -0500

    truncate to del so I can seed the postgres db

--- data/seeds/01_notes.js
+++ data/seeds/01_notes.js
@@ -1,7 +1,7 @@
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
   return knex("notes")
-    .truncate()
+    .del()
     .then(function() {
       // Inserts seed entries
       return knex("notes").insert([
