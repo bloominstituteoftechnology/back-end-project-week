831894f4a351ec639b976f9edfc20bcb4aeec054
Alex Martin <atonymartin20@yahoo.com>
2019-02-23 10:24:43 -0600

    Trying to fix database

--- knexfile.js
+++ knexfile.js
@@ -28,5 +28,21 @@ module.exports = {
     seeds: {
       directory: './data/seeds',
     },
-  },
+  }
+
+  // production: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // }
 };
