79ca6539a907ace861006061ac3d127bb70324de
vy3191 <venkyafhs@gmail.com>
2019-02-22 22:31:13 -0500

    Modified knex file for postgres db

--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,24 @@
 // Update with your config settings.
+require('dotenv').config('/.env');
+
 module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
       filename: './data/notes.db3',
     },
+    useNullAsDefault:true,
+    migrations: {
+        directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    }
+  },
+  
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
     useNullAsDefault:true,
     migrations: {
         directory: './data/migrations',
