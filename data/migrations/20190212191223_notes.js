6bb0088e432255d71c095e2ab74082befa150316
Emmanium <pradoe.ep@gmail.com>
2019-02-23 16:09:08 -0700

    removed character limit on textbody

--- data/migrations/20190212191223_notes.js
+++ data/migrations/20190212191223_notes.js
@@ -4,7 +4,7 @@ exports.up = function(knex, Promise) {
     tbl.increments();
     // small character limit for title
     tbl.string('title', 60).notNullable();
-    tbl.string('textBody', 255).notNullable();
+    tbl.string('textBody').notNullable();
   })
 };
 
