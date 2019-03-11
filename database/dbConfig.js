5d17a3bbeffdb733bb2dcf5afc0d4a71c5e102bd
Drew <drewsup123c@gmail.com>
2019-03-10 23:44:26 -0600

    following tutorial

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -1,5 +1,5 @@
-const environment = process.env.ENVIRONMENT || 'development'
+// const environment = process.env.ENVIRONMENT || 'development'
 const knex = require('knex');
-const knexConfig = require('../knexfile.js')[environment];
+const knexConfig = require('../knexfile.js');//[environment];
 module.exports = knex(knexConfig.development);
 
