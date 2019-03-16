2c7fe99a5ebe5d37886ff11ca42949920fe6169c
hraya <hraya@lssu.edu>
2019-03-15 20:32:17 -0500

    Messing with cors

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const helmet = require('helmet');
 
-const cors = (req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-}
+// const cors = (req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Credentials', true);
+//     res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+//     res.header('Access-Control-Allow-Headers', 'Content-Type');
+//     next();
+// }
+const cors = require('cors');
 
 const db = require('./database/dbConfig.js')
 
@@ -17,7 +18,7 @@ const PORT = process.env.PORT || 4000;
 
 server.use(helmet());
 server.use(express.json());
-server.use(cors);
+server.use(cors('*'));
 
 
 server.post('/api/notes', (req, res)=>{
@@ -38,7 +39,7 @@ server.get('/api/notes', (req,res)=>{
     .then(notes =>{
         res.json(notes)
     }).catch(err =>{
-        res.status(500).json({error:"Cannot get notes", err})
+     res.status(500).json({error:"Cannot get notes", err})
     })
 })
 
