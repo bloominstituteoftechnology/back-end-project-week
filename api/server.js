7d4403434c94c5c4cb575f8a49bbb3ad05bf4140
Lola <lolahef@gmail.com>
2019-03-16 23:33:44 -0400

    Added authorization, protected routes

--- api/server.js
+++ api/server.js
@@ -2,31 +2,69 @@ const notes = require('../data/notesModel.js')
 const express = require('express')
 const server = express()
 const cors = require('cors')
-// server.use(cors({ origin: 'http://localhost:8080', credentials: true }))
 server.use(cors())
 server.use(express.json())
+
+
+const admin = require("firebase-admin")
+const serviceAccount = require("../config.json")
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://lolas-notes.firebaseio.com"
+});
+
+
+
+const restricted = (req, res, next) => {
+    const token = req.headers.authorization
+    if (req.url.includes('/restricted')) {
+        if (token) {
+            admin
+                .auth()
+                .verifyIdToken(token)
+                .then((decodedToken) => {
+                    const uid = decodedToken.uid;
+                    req.uid = uid
+                    next()
+                })
+                .catch((error) => {
+                    res.status(401).json({ message: 'invalid token' }, error)
+                });
+        } else {
+            res.status(401).json({ message: 'no token' })
+        }
+    } else {
+        next()
+    }
+}
+
+server.use(restricted)
 
 server.get('/', (req, res) => {
   res.status(200).json({ api: 'alive' })
 })
 
-server.get('/notes', (req, res) => {
-    notes.get()
+server.get('/restricted/notes', (req, res) => {
+    const { uid } = req
+    notes.getByAuthor(uid)
     .then(notes => res.status(200).json(notes))
     .catch(err => res.status(500).json(err))
 })
 
-server.get('/notes/:id', (req, res) => {
+server.get('/restricted/notes/:id', (req, res) => {
+    const { uid } = req
     const { id } = req.params
-    notes.get(id)
-    .then(note => res.status(200).json(note))
+    notes.getByNote(id, uid)
     .catch(err => res.status(500).json(err))
 })
 
-server.post('/notes', (req, res) => {
+server.post('/restricted/notes', (req, res) => {
+    const { uid } = req
     const { title, content } = req.body
-    if (title && content) {
-        notes.add(req.body)
+    if (title && content && uid) {
+        const note = { title, content, uid }
+        notes.add(note)
         .then(note => res.status(201).json(note))
         .catch(err => res.status(500).json(err))
     } else {
@@ -34,11 +72,13 @@ server.post('/notes', (req, res) => {
     }
 })
 
-server.put('/notes/:id', (req, res) => {
+server.put('/restricted/notes/:id', (req, res) => {
+    const { uid } = req
     const { id } = req.params
     const { title, content } = req.body
-    if (title && content) {
-        notes.update(id, req.body)
+    if (title && content && uid) {
+        const note = { title, content, uid }
+        notes.update(id, uid, note)
         .then(result => result ? res.status(200).json(result) : res.status(500).json({ message: 'No note updated.' }))
         .catch(err => res.status(500).json(err))
     }  else {
@@ -46,9 +86,10 @@ server.put('/notes/:id', (req, res) => {
     }
 })
 
-server.delete('/notes/:id', (req, res) => {
+server.delete('/restricted/notes/:id', (req, res) => {
+    const { uid } = req
     const { id } = req.params
-    notes.remove(id)
+    notes.remove(id, uid)
     .then(result => result ? res.status(200).json(result) : res.status(500).json({ message: 'No notes deleted.' }))
     .catch(err => res.status(500).json(err))
 })
