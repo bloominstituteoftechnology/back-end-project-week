d9ae85f33916fc7cad1b9ebe611c047189775c9b
Lola <lolahef@gmail.com>
2019-03-16 23:34:16 -0400

    Updated notes model functions to accommodate users

--- data/notesModel.js
+++ data/notesModel.js
@@ -1,23 +1,25 @@
 const db = require('../data/dbConfig.js')
 
-module.exports = { get, add, update, remove }
+module.exports = { getByAuthor, getByNote, add, update, remove }
 
-async function get(id) {
-    return id ? db('notes').where({ id }).first() : db('notes')
+async function getByAuthor(uid) {
+    return db('notes').where({ uid })
+}
+
+async function getByNote(id, uid) {
+    return id ? db('notes').where({ uid, id }).first() : db('notes')
 }
 
 async function add(note) {
     return await db('notes').insert(note)
-    // return db('notes').where({ id }).first()
-    // return get(id)
 }
 
-async function update(id, changes) {
-    const count = await db('notes').where({ id }).update(changes)
-    return count > 0 ? db('notes').where({ id }).first() : null
+async function update(id, uid, changes) {
+    const count = await db('notes').where({ uid, id }).update(changes)
+    return count > 0 ? db('notes').where({ uid, id }).first() : null
 }
 
-async function remove(id) {
-    const count = await db('notes').where({ id }).del()
+async function remove(id, uid) {
+    const count = await db('notes').where({ uid, id }).del()
     return count > 0 ? db('notes') : null
 }
