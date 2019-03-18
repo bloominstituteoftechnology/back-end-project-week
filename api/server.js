3b3a237de720b758d3d7da75da75bc88583d8172
Lola <lolahef@gmail.com>
2019-03-18 16:08:34 -0400

    Updated firebase config for heroku

--- api/server.js
+++ api/server.js
@@ -7,7 +7,19 @@ server.use(express.json())
 
 
 const admin = require("firebase-admin")
-const serviceAccount = require("../config.json")
+const serviceAccount = {
+    "type": "service_account",
+    "project_id": "lolas-notes",
+    "private_key_id": process.env.private_key_id,
+    "private_key": process.env.private_key,
+    "client_email": process.env.client_email,
+    "client_id": process.env.client_id,
+    "auth_uri": process.env.auth_uri,
+    "token_uri": process.env.token_uri,
+    "auth_provider_x509_cert_url": process.env.auth_provider_x509_cert_url,
+    "client_x509_cert_url": process.env.client_x509_cert_url
+}
+
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
