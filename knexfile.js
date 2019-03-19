650277cf64ed453dc60b4ab302428f0aa25ff600
Jameson13B <jamesonbrown@neolivity.com>
2019-03-19 14:09:12 -0600

    Updateed production knex config

--- knexfile.js
+++ knexfile.js
@@ -1,45 +1,40 @@
 // Update with your config settings.
+require("dotenv").config();
 
 module.exports = {
-
   development: {
-    client: 'sqlite3',
+    client: "sqlite3",
     connection: {
-      filename: './data/database.sqlite3'
+      filename: "./data/database.sqlite3"
     },
     useNullAsDefault: true
   },
 
   staging: {
-    client: 'postgresql',
+    client: "postgresql",
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      database: "my_db",
+      user: "username",
+      password: "password"
     },
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      tableName: "knex_migrations"
     }
   },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my-db',
-      user:     'username',
-      password: 'password'
-    },
+    client: "pg",
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      tableName: "knex_migrations"
     }
   }
-
 };
