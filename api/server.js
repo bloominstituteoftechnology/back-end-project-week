b2450641994bb894bf35902b57c85a6fb2d91b10
Lola <lolahef@gmail.com>
2019-03-20 23:54:55 -0400

    Updated config to trim new line characters

--- api/server.js
+++ api/server.js
@@ -10,14 +10,14 @@ const admin = require("firebase-admin")
 const serviceAccount = {
     "type": "service_account",
     "project_id": "lolas-notes",
-    "private_key_id": process.env.private_key_id,
-    "private_key": process.env.private_key,
-    "client_email": process.env.client_email,
-    "client_id": process.env.client_id,
-    "auth_uri": process.env.auth_uri,
-    "token_uri": process.env.token_uri,
-    "auth_provider_x509_cert_url": process.env.auth_provider_x509_cert_url,
-    "client_x509_cert_url": process.env.client_x509_cert_url
+    "private_key_id": process.env.private_key_id.trim(),
+    "private_key": process.env.private_key.trim().replace(/\\n/g, '\n'),
+    "client_email": process.env.client_email.trim(),
+    "client_id": process.env.client_id.trim(),
+    "auth_uri": process.env.auth_uri.trim(),
+    "token_uri": process.env.token_uri.trim(),
+    "auth_provider_x509_cert_url": process.env.auth_provider_x509_cert_url.trim(),
+    "client_x509_cert_url": process.env.client_x509_cert_url.trim()
 }
 
 
