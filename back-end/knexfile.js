c16490df83b46073691abfcd76cafeb8fa8ca360
Fred <sohnf90@yahoo.com>
2019-03-27 20:16:27 -0700

    change client to pg

--- back-end/knexfile.js
+++ back-end/knexfile.js
@@ -23,7 +23,7 @@ module.exports = {
     }
   },
   production: {
-    client: 'postgresql',
+    client: 'pg',
     connection: process.env.DATABASE_URL,
     migrations: {
       directory: __dirname + '/db/migrations',
