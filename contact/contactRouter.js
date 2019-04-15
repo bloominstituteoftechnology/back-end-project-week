3174b73f3e544b4aa164eaa2942b1538ad847596
David Loveday <davemech@yahoo.com>
2019-04-15 12:22:36 -0700

    debugging

--- contact/contactRouter.js
+++ contact/contactRouter.js
@@ -4,13 +4,14 @@ const Contact = require("./Contact")
 
 //Borrowing from notes backend: I didn't want to create a separate backend for my portfolio site's 
 // contact form, so am using notes app's backend for that functionality.
-router.post((req, res) => {
+router.post("/contact", (req, res) => {
     const newContact = { name, email, message } = req.body;  
   Contact.create(newContact)
     .then(contact => {
       res.status(201).json(contact);
     })
     .catch(err => {
+       console.log("We have an error", err)
       res.status(500).json(err.message);
     });
 });
