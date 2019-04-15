1ffd675220952cce4b41b1593f56785d2d247462
David Loveday <davemech@yahoo.com>
2019-04-15 10:32:41 -0700

    debugging

--- contact/contact.js
+++ contact/contact.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
 
-//const ObjectID = mongoose.Schema.Types.ObjectId;
+//Borrowing from notes backend: I didn't want to create a separate backend for my portfolio site's 
+// contact form, so am using notes app's backend for that functionality.
 
 const contactSchema = new mongoose.Schema({    
+  _id: new mongoose.Schema.Types.ObjectId,
   name: {
     type: String,
     required: true
--- contact/contactRouter.js
+++ contact/contactRouter.js
@@ -1,14 +1,24 @@
-// const router = require("express").Router();
-// const Contact = require("./Contact")
-// router.post((req, res) => {
-//   const newContact = ({ name, email, message } = req.body);
-//   Contact.create(newContact)
-//     .then(contact => {
-//       res.status(201).json(contact);
-//     })
-//     .catch(err => {
-//       res.status(500).json(err.message);
-//     });
-// });
+const router = require("express").Router();
+const Contact = require("./Contact")
+const mongoose = require("mongoose");
 
-// module.exports = router;
+//Borrowing from notes backend: I didn't want to create a separate backend for my portfolio site's 
+// contact form, so am using notes app's backend for that functionality.
+router.post((req, res) => {
+    const { name, email, message } = req.body;
+    const newContact = new Contact ({
+        _id: new mongoose.Schema.Types.ObjectId(),
+        name,
+        email,
+        message
+  })
+  Contact.save()
+    .then(contact => {
+      res.status(201).json(contact);
+    })
+    .catch(err => {
+      res.status(500).json(err.message);
+    });
+});
+
+module.exports = router;
