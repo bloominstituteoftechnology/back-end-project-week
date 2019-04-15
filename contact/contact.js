27fdf6bd88cb78522c1be764cc0b5c8b555102cd
David Loveday <davemech@yahoo.com>
2019-04-15 09:55:00 -0700

    debugging

--- contact/contact.js
+++ contact/contact.js
@@ -1,25 +1,25 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-const ObjectID = mongoose.Schema.Types.ObjectId;
+// const ObjectID = mongoose.Schema.Types.ObjectId;
 
-const contactSchema = new mongoose.Schema({
-    ObjectID,
-  name: {
-    type: String,
-    required: true
-  },  
+// const contactSchema = new mongoose.Schema({
+//     ObjectID,
+//   name: {
+//     type: String,
+//     required: true
+//   },  
 
-  email: {
-    type: String,
-    required: true,
-  },
-  message : {
-    type: String,
-    required: true,
-  },
+//   email: {
+//     type: String,
+//     required: true,
+//   },
+//   message : {
+//     type: String,
+//     required: true,
+//   },
   
-});
+// });
 
 
 
-module.exports = mongoose.model("Contact", contactSchema);
+// module.exports = mongoose.model("Contact", contactSchema);
--- contact/contactRouter.js
+++ contact/contactRouter.js
@@ -1,14 +1,14 @@
-const router = require("express").Router();
-const Contact = require("./Contact")
-router.post((req, res) => {
-  const newContact = ({ name, email, message } = req.body);
-  Contact.create(newContact)
-    .then(contact => {
-      res.status(201).json(contact);
-    })
-    .catch(err => {
-      res.status(500).json(err.message);
-    });
-});
+// const router = require("express").Router();
+// const Contact = require("./Contact")
+// router.post((req, res) => {
+//   const newContact = ({ name, email, message } = req.body);
+//   Contact.create(newContact)
+//     .then(contact => {
+//       res.status(201).json(contact);
+//     })
+//     .catch(err => {
+//       res.status(500).json(err.message);
+//     });
+// });
 
-module.exports = router;
+// module.exports = router;
