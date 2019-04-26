b227a19766bd7bc64fdf6d8f52abd5cc9067e726
Sukhada Gholba <sukhadagholba@gmail.com>
2019-04-25 19:26:32 -0700

    uncommented cloudinary setup

--- Routes/noteRoutes.js
+++ Routes/noteRoutes.js
@@ -8,11 +8,11 @@ const cloudinary = require('cloudinary');
 
 
 
-/*cloudinary.config({
+cloudinary.config({
   cloud_name:process.env.cloud_name,
   api_key:process.env.api_key,
   api_secret:process.env.api_secret
-});*/
+});
 
 
 
