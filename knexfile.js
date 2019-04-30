470812770254e9de7ee37d6dbfd659c74f0a6a5e
mparedes003 <onnabugeishadev@gmail.com>
2019-04-29 19:11:22 -0700

    Add production configuration to knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -2,16 +2,32 @@
 
 module.exports = {
   development: {
-    client: 'sqlite3',
+    client: "sqlite3",
     connection: {
-      filename: './data/db.sqlite3',
+      filename: "./data/db.sqlite3"
     },
     useNullAsDefault: true,
     migrations: {
-      directory: './data/migrations',
+      directory: "./data/migrations"
     },
     seeds: {
-      directory: './data/seeds',
-    },
+      directory: "./data/seeds"
+    }
   },
-};			
+
+  production: {
+    client: "pg",
+    connection: dbConnection,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: "knex_migrations",
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    }
+  }
+};
