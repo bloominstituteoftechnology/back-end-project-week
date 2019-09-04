200c2e69e091c8f485000dc21ef640ed328b1f32
Alex Martin <atonymartin20@yahoo.com>
2019-09-04 17:41:17 -0500

    Thought this was clear

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ require('dotenv').config();
 
 const server = require('./api/server.js');
 
-const PORT = process.env.PORT || 6333;
+const PORT = process.env.PORT || 6334;
 server.listen(PORT, () => {
     console.log(`\n** server up on port ${PORT} **\n`)
 });
+
