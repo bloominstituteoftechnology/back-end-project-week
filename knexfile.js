e504abac0013be2bb9847e672fe3941747ccd7c5
Angela La Salle <angela.lasalle91@gmail.com>
2019-10-09 23:38:41 -0400

    "rid of old stuff in package.json set up testing environment"

--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,3 @@
-// const localpostgres = {
-//   host: "localhost",
-//   database: "pgNotes",
-//   user: "Mr.Stegall",
-//   password: "angela91"
-// };
-
-// const dbConnection = process.env.DATABASE_URL || localpostgres;
 
 require('dotenv').config();
 
@@ -23,19 +15,19 @@ module.exports = {
       directory: "./data/seeds"
     }
   },
-  // testing: {
-  //   client: "sqlite3",
-  //   connection: {
-  //     filename: "./data/test.db3"
-  //   },
-  //   useNullAsDefault: true,
-  //   migrations: {
-  //     directory: "./data/migrations"
-  //   },
-  //   seeds: {
-  //     directory: "./data/seeds"
-  //   }
-  // },
+  testing: {
+    client: "sqlite3",
+    connection: {
+      filename: "./data/test.db3"
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    }
+  },
 
   production: {
     client: 'pg',
