200ed7adcc8139cfda260afd4c993f2e3855a451
Angela La Salle <angela.lasalle91@gmail.com>
2019-10-15 00:22:13 -0400

    added user to notes

--- data/migrations/20190213215235_notes.js
+++ data/migrations/20190213215235_notes.js
@@ -3,6 +3,7 @@ exports.up = function(knex, Promise) {
     table.increments("id");
     table.string("title").notNullable();
     table.text("content", "mediumtext").notNullable();
+    table.text("user").notNullable()
   });
 };
 
