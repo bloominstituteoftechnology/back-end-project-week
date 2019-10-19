0cdff966747ccbbd4c764637531f80c955093282
AM-Myrick <amyrick.820@gmail.com>
2019-10-19 00:17:00 -0400

    gets rid of truncate due to prod issues

--- data/seeds/app-users.js
+++ data/seeds/app-users.js
@@ -1,7 +1,7 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('users').truncate()
+  return knex('users').del()
     .then(function () {
       // Inserts seed entries
       return knex('users').insert([
--- data/seeds/notes.js
+++ data/seeds/notes.js
@@ -1,7 +1,7 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('notes').truncate()
+  return knex('notes').del()
     .then(function () {
       // Inserts seed entries
       return knex('notes').insert([
