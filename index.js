6de8e277ea31b0561d4baec38e2c987d8c95cc81
lajawanti <lajawanti.dhake@gmail.com>
2021-01-16 11:39:12 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require('./server.js');
 
-const port = 7002;
+const port = process.env.PORT || 7002;
 
 app.listen(port, () => {
     console.log(`\n Server listening on port : ${port}`);
