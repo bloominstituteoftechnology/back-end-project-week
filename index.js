38de952fcbe04479725af338f680213e86c85bfd
lajawanti <lajawanti.dhake@gmail.com>
2021-01-15 22:13:47 -0500

    fixing heroku error

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require('./server.js');
 
-const port = process.env.PORT || 7000;
+const port = 7000;
 
 app.listen(port, () => {
     console.log(`\n Server listening on port : ${port}`);
