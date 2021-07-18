cfb6c1d2f62c4de9ed4b98677b90ac039bfbd1e4
iepoch <steaclem@gmail.com>
2021-07-18 10:26:32 -0700

    Updated SSL connections

--- knexfile.js
+++ knexfile.js
@@ -34,6 +34,7 @@ module.exports = {
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
     migrations: {
       directory: __dirname + "./data/migrations",
   
