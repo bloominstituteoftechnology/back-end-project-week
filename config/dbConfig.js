dcf41ec4e44a68229439f09d03b9981dbc824672
iepoch <steaclem@gmail.com>
2021-07-18 12:34:59 -0700

    Still having an issue

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,6 +1,6 @@
 const knex = require('knex');
 
-const env = process.env.DB_ENV || 'development'
+const env = process.env.DB_ENV || 'production'
 const config = require('../knexfile')[env];
 
 const db= knex(config);
