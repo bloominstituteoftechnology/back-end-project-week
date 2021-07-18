d681cc1a0b33d73ab57b9ccf374791bb2b836672
iepoch <steaclem@gmail.com>
2021-07-18 09:53:33 -0700

    Fixing database issues of not displaying

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,6 +1,6 @@
 const knex = require('knex');
 const config = require('../knexfile.js');
 
-const db_Env = process.env.DB_ENV || 'production'
+const db_Env = process.env.DB_ENV 
 
 module.exports = knex(config[db_Env]);
