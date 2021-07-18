fa40b94feed535fe9ec7f194552c9d517bfbb501
iepoch <steaclem@gmail.com>
2021-07-18 11:46:21 -0700

    Db Change

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,6 +1,6 @@
 const knex = require('knex');
 
-const env = process.env.DB_ENV || 'production'
+const env = process.env.DB_ENV || 'development'
 const config = require('../knexfile.js');
 
 const db= knex(config[env]);
